dd2a5f96062275face87d17aabe90b23a7ae92c0
Sean Sullivan <ssullivan61198@gmail.com>
2023-02-10 13:50:30 -0800

    feat: add defaults

--- src/llms/tests/openai.test.ts
+++ src/llms/tests/openai.test.ts
@@ -1,21 +1,8 @@
 import { test } from "@jest/globals";
 import { OpenAI } from "../openai";
 
-const baseParams = {
-  temperature: 0.5,
-  maxTokens: 10,
-  topP: 1,
-  frequencyPenalty: 1,
-  presencePenalty: 1,
-  n: 1,
-  bestOf: 1,
-};
-
 test("Test OpenAI", async () => {
-  const model = new OpenAI({
-    ...baseParams,
-  });
-
-  const res = await model.call("Print hello world.");
+  const model = new OpenAI({ maxTokens: 5 });
+  const res = await model.call("Print hello world");
   console.log({ res });
 });
