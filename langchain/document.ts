ef949cd29c6bb869817fab13a6fcd337619e6bfd
vbarda <vadim.barda@gmail.com>
2023-02-14 16:42:08 -0500

    prettier and camelcase and unittests

--- langchain/document.ts
+++ langchain/document.ts
@@ -1,21 +1,21 @@
 export class Document {
-  page_content: string;
+  pageContent: string;
 
-  lookup_str: string;
+  lookupStr: string;
 
-  lookup_index: number;
+  lookupIndex = 0;
 
   metadata: Record<string, any>;
 
   constructor(
-    page_content: string,
-    lookup_str = "",
-    lookup_index = 0,
+    pageContent: string,
+    lookupStr = "",
+    lookupIndex = 0,
     metadata: Record<string, any> = {}
   ) {}
-    
+
   get paragraphs(): string[] {
-    return this.page_content.split("\n\n");
+    return this.pageContent.split("\n\n");
   }
 
   get summary(): string {
@@ -23,20 +23,22 @@ export class Document {
   }
 
   lookup(string: string): string {
-    if (string.toLowerCase() !== this.lookup_str) {
-      this.lookup_str = string.toLowerCase();
-      this.lookup_index = 0;
+    if (string.toLowerCase() !== this.lookupStr) {
+      this.lookupStr = string.toLowerCase();
+      this.lookupIndex = 0;
     } else {
-      this.lookup_index += 1;
+      this.lookupIndex += 1;
     }
-    const lookups = this.paragraphs.filter((p) => p.toLowerCase().includes(this.lookup_str));
+    const lookups = this.paragraphs.filter((p) =>
+      p.toLowerCase().includes(this.lookupStr)
+    );
     if (lookups.length === 0) {
       return "No Results";
-    } if (this.lookup_index >= lookups.length) {
+    }
+    if (this.lookupIndex >= lookups.length) {
       return "No More Results";
-    } 
-      const result_prefix = `(Result ${this.lookup_index + 1}/${lookups.length})`;
-      return `${result_prefix} ${lookups[this.lookup_index]}`;
-    
+    }
+    const result_prefix = `(Result ${this.lookupIndex + 1}/${lookups.length})`;
+    return `${result_prefix} ${lookups[this.lookupIndex]}`;
   }
 }
