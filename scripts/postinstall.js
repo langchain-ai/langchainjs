2a9599edde0153b1007241b12c570111ca851585
moon <shawmakesmagic@gmail.com>
2023-02-14 21:54:37 -0800

    hotfix serp api

---
+++ scripts/postinstall.js
@@ -0,0 +1,19 @@
+// postinstall functions to handle any tasks, fixes or hacks that may arise as a result of dependency issues
+
+function postinstall() {
+  // This is a hack to fix a bug in the domexception package
+  // replace line 34 of node_modules/domexception/lib/utils.js
+  // original: const AsyncIteratorPrototype = Object.getPrototypeOf(Object.getPrototypeOf(async function* () {}).prototype);
+  // replaced: const AsyncIteratorPrototype = Object.getPrototypeOf(async function* () {}).prototype;
+    const fs = require('fs');
+    const path = require('path');
+    const utilsPath = path.join(__dirname, 'node_modules/domexception/lib/utils.js');
+    const utils = fs.readFileSync(utilsPath, 'utf8');
+    const fixedUtils = utils.replace(
+        "const AsyncIteratorPrototype = Object.getPrototypeOf(Object.getPrototypeOf(async function* () {}).prototype);",
+        "const AsyncIteratorPrototype = Object.getPrototypeOf(async function* () {}).prototype;"
+    );
+    // save fixedUtils
+    const fixedUtilsPath = path.join(__dirname, 'node_modules/domexception/lib/utils.js');
+    fs.writeFileSync(fixedUtilsPath, fixedUtils, 'utf8');
+}
