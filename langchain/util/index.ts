87c6aea567f62f6feae39bcdd338f797d93ccd33
Nuno Campos <nuno@boringbits.io>
2023-02-15 18:45:53 +0000

    Add Embeddings class and OpenAIEmbeddings (#7)
    * Move chunkArray to utils
    
    * Add embeddings base class and OpenAI implementation

--- langchain/util/index.ts
+++ langchain/util/index.ts
@@ -99,3 +99,12 @@ export const parseFileConfig = (file: string, supportedTypes?: string[]) => {
   const contents = fs.readFileSync(file).toString();
   return loadFileContents(contents, suffix);
 };
+
+export const chunkArray = <T>(arr: T[], chunkSize: number) =>
+  arr.reduce((chunks, elem, index) => {
+    const chunkIndex = Math.floor(index / chunkSize);
+    const chunk = chunks[chunkIndex] || [];
+    // eslint-disable-next-line no-param-reassign
+    chunks[chunkIndex] = chunk.concat([elem]);
+    return chunks;
+  }, [] as T[][]);
