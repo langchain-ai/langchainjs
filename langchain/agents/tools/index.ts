bded90843f7322b917889121e4fb54fecbd663ee
Nuno Campos <nuno@boringbits.io>
2023-02-15 17:18:35 +0000

    Rewrite Tool interface to be a class

--- langchain/agents/tools/index.ts
+++ langchain/agents/tools/index.ts
@@ -1,9 +1,3 @@
 export { SerpAPI } from "./serpapi";
 export { Calculator } from "./calculator";
-
-export interface Tool {
-  call: (arg: string) => Promise<string>;
-  name: string;
-  description: string;
-  returnDirect?: boolean;
-}
+export { Tool } from "./base";
