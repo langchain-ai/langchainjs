029bd0b2648a36a7f219f6f762ff1e528622ba16
Harrison Chase <hw.chase.17@gmail.com>
2023-02-16 07:09:29 -0800

    conversation chain (#15)

--- langchain/memory/buffer_memory.ts
+++ langchain/memory/buffer_memory.ts
@@ -1,47 +1,50 @@
 import { BaseMemory, InputValues, MemoryVariables, OutputValues } from "./base";
 
 export interface BufferMemoryInput {
-    humanPrefix: string;
-    aiPrefix: string;
-    memoryKey: string;
+  humanPrefix: string;
+  aiPrefix: string;
+  memoryKey: string;
 }
 
 const getInputValue = (inputValues: InputValues) => {
-    const keys = Object.keys(inputValues);
-    if (keys.length === 1){
-        return inputValues[keys[0]];
-    }
-    throw new Error("input values have multiple keys, memory only supported when one key currently");
+  const keys = Object.keys(inputValues);
+  if (keys.length === 1) {
+    return inputValues[keys[0]];
+  }
+  throw new Error(
+    "input values have multiple keys, memory only supported when one key currently"
+  );
 };
 
-export class BufferMemory extends BaseMemory implements BufferMemoryInput{
-    humanPrefix = "Human";
+export class BufferMemory extends BaseMemory implements BufferMemoryInput {
+  humanPrefix = "Human";
 
-    aiPrefix = "AI";
+  aiPrefix = "AI";
 
-    memoryKey = "history";
+  memoryKey = "history";
 
-    buffer = "";
+  buffer = "";
 
-    constructor(
-        fields?: Partial<BufferMemoryInput>
-      ){
-        super();
-        this.humanPrefix = fields?.humanPrefix ?? this.humanPrefix;
-        this.aiPrefix = fields?.aiPrefix ?? this.aiPrefix;
-        this.memoryKey = fields?.memoryKey ?? this.memoryKey;
-      }
-    
-    async loadMemoryVariables(_values: InputValues): Promise<MemoryVariables>{
-        const result = {[this.memoryKey]: this.buffer};
-        return result;
+  constructor(fields?: Partial<BufferMemoryInput>) {
+    super();
+    this.humanPrefix = fields?.humanPrefix ?? this.humanPrefix;
+    this.aiPrefix = fields?.aiPrefix ?? this.aiPrefix;
+    this.memoryKey = fields?.memoryKey ?? this.memoryKey;
+  }
 
-    }
+  async loadMemoryVariables(_values: InputValues): Promise<MemoryVariables> {
+    const result = { [this.memoryKey]: this.buffer };
+    return result;
+  }
 
-    saveContext(inputValues: InputValues, outputValues: OutputValues): void{
-        const human = `${this.humanPrefix  }: ${  getInputValue(inputValues)}`;
-        const ai = `${this.aiPrefix  }: ${  getInputValue(outputValues)}`;
-        const newlines = [human, ai];
-        this.buffer += `\n${  newlines.join("\n")}`;
-    }
+  async saveContext(
+    inputValues: InputValues,
+    outputValues: Promise<OutputValues>
+  ): Promise<void> {
+    const values = await outputValues;
+    const human = `${this.humanPrefix}: ${getInputValue(inputValues)}`;
+    const ai = `${this.aiPrefix}: ${getInputValue(values)}`;
+    const newlines = [human, ai];
+    this.buffer += `\n${newlines.join("\n")}`;
+  }
 }
