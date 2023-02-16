d02e1cc67b6040749c5c4c365a3360777d2f5d76
Nuno Campos <nuno@boringbits.io>
2023-02-16 17:11:47 +0000

    Set up ghpages deployment

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -16,15 +16,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://hwchase17.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/langchainjs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hwchase17', // Usually your GitHub org/user name.
   projectName: 'langchainjs', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
