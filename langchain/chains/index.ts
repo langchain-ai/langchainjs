57448c67b823f77aaeb3564389b7e2d35903dc8b
Harrison Chase <hw.chase.17@gmail.com>
2023-02-16 07:09:41 -0800

    vector db qa chain (#13)
    * vector db qa chain
    
    * cr
    
    * cr

--- langchain/chains/index.ts
+++ langchain/chains/index.ts
@@ -1,5 +1,9 @@
 export { BaseChain, ChainValues } from "./base";
 export { SerializedLLMChain, LLMChain } from "./llm_chain";
-export { SerializedStuffDocumentsChain, StuffDocumentsChain } from "./combine_docs_chain";
+export {
+  SerializedStuffDocumentsChain,
+  StuffDocumentsChain,
+} from "./combine_docs_chain";
+export { VectorDBQAChain, SerializedVectorDBQAChain } from "./vector_db_qa";
 export { loadChain } from "./load";
 export { loadQAChain } from "./question_answering/load";
