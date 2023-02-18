2daefc4eb06f30bd5a046c4b8ef395a299475d1d
Nuno Campos <nuno@boringbits.io>
2023-02-18 08:03:04 +0000

    Run format in examples package

--- examples/src/chains/llm_chain.ts
+++ examples/src/chains/llm_chain.ts
@@ -1,12 +1,12 @@
 import { OpenAI } from "langchain/llms";
 import { PromptTemplate } from "langchain/prompts";
-import {LLMChain} from "langchain/chains";
+import { LLMChain } from "langchain/chains";
 
 export const run = async () => {
-    const model = new OpenAI({temperature: 0.9});
-    const template = "What is a good name for a company that makes {product}?";
-    const prompt = new PromptTemplate({template, inputVariables: ["product"]});
-    const chain = new LLMChain({llm: model, prompt});
-    const res = await chain.call({product: "colorful socks"});
-    console.log({ res });
+  const model = new OpenAI({ temperature: 0.9 });
+  const template = "What is a good name for a company that makes {product}?";
+  const prompt = new PromptTemplate({ template, inputVariables: ["product"] });
+  const chain = new LLMChain({ llm: model, prompt });
+  const res = await chain.call({ product: "colorful socks" });
+  console.log({ res });
 };
--- examples/src/chains/question_answering.ts
+++ examples/src/chains/question_answering.ts
@@ -5,7 +5,13 @@ import { Document } from "langchain/document";
 export const run = async () => {
   const model = new OpenAI({});
   const chain = loadQAChain(model);
-  const docs = [    new Document({pageContent: 'harrison went to harvard' }),    new Document({pageContent: 'ankush went to princeton' }),   ];
-  const res = await chain.call({ input_documents: docs, question: "Where did harrison go to college" });
+  const docs = [
+    new Document({ pageContent: "harrison went to harvard" }),
+    new Document({ pageContent: "ankush went to princeton" }),
+  ];
+  const res = await chain.call({
+    input_documents: docs,
+    question: "Where did harrison go to college",
+  });
   console.log({ res });
 };
