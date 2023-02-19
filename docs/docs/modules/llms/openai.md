68a5e72364bdfd2bbea476cfac18129d582fee79
Tat Dat Duong <david@duong.cz>
2023-02-19 20:33:01 +0100

    Add PromptLayer OpenAI wrapper integration

--- docs/docs/modules/llms/openai.md
+++ docs/docs/modules/llms/openai.md
@@ -11,3 +11,21 @@ const res = await model.call(
 );
 console.log({ res });
 ```
+
+## PromptLayer OpenAI
+
+This library supports PromptLayer for logging and debugging prompts and responses. To add support for PromptLayer:
+
+1. Create a PromptLayer account here: [https://promptlayer.com](https://promptlayer.com).
+2. Create an API token and pass it either as `promptLayerApiKey` argument in the `PromptLayerOpenAI` constructor or in the `PROMPT_LAYER_API_KEY` environment variable.
+
+```typescript
+const model = new PromptLayerOpenAI({ temperature: 0.9 });
+const res = await model.call(
+  "What would be a good company name a company that makes colorful socks?"
+);
+```
+
+The request and the response will be logged in the [PromptLayer dashboard](https://promptlayer.com/home).
+
+Note: In streaming mode PromptLayer will not log the response.
--- langchain/llms/openai.ts
+++ langchain/llms/openai.ts
@@ -338,3 +338,61 @@ export class OpenAI extends BaseLLM implements OpenAIInput {
     return "openai";
   }
 }
+
+/**
+ * PromptLayer wrapper to OpenAI
+ * @augments OpenAI
+ */
+export class PromptLayerOpenAI extends OpenAI {
+  promptLayerApiKey?: string;
+
+  plTags?: string[];
+
+  constructor(
+    fields?: ConstructorParameters<typeof OpenAI>[0] & {
+      promptLayerApiKey?: string;
+      plTags?: string[];
+    }
+  ) {
+    super(fields);
+
+    this.plTags = fields?.plTags ?? [];
+    this.promptLayerApiKey =
+      fields?.promptLayerApiKey ?? process.env.PROMPTLAYER_API_KEY;
+
+    if (!this.promptLayerApiKey) {
+      throw new Error("Missing PromptLayer API key");
+    }
+  }
+
+  async completionWithRetry(request: CreateCompletionRequest) {
+    if (request.stream) {
+      return super.completionWithRetry(request);
+    }
+
+    const requestStartTime = Date.now();
+    const response = await super.completionWithRetry(request);
+    const requestEndTime = Date.now();
+
+    // https://github.com/MagnivOrg/promptlayer-js-helper
+    await fetch("https://api.promptlayer.com/track-request", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        function_name: "openai.Completion.create",
+        args: [],
+        kwargs: { engine: request.model, prompt: request.prompt },
+        tags: this.plTags ?? [],
+        request_response: response.data,
+        request_start_time: Math.floor(requestStartTime / 1000),
+        request_end_time: Math.floor(requestEndTime / 1000),
+        api_key: process.env.PROMPTLAYER_API_KEY,
+      }),
+    });
+
+    return response;
+  }
+}
