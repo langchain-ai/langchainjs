195b3fb58f612632e29e61cfa00bb65b47dac946
Harrison Chase <hw.chase.17@gmail.com>
2023-02-19 20:34:16 -0800

    cr

---
+++ examples/src/document_loaders/imsdb.ts
@@ -0,0 +1,7 @@
+import { IMSDBLoader } from "langchain/document_loaders";
+
+export const run = async () => {
+  const loader = new IMSDBLoader("https://imsdb.com/scripts/BlacKkKlansman.html");
+  const docs = await loader.load();
+  console.log({ docs });
+};
