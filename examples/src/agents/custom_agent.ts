fa39b41e5f75c6242b906cc20e80ef4251d873e5
Harrison Chase <hw.chase.17@gmail.com>
2023-02-20 17:50:53 -0800

    cr

--- examples/src/agents/custom_agent.ts
+++ examples/src/agents/custom_agent.ts
@@ -7,27 +7,28 @@ export const run = async () => {
   const model = new OpenAI({ temperature: 0 });
   const tools = [new SerpAPI(), new Calculator()];
 
-  const prefix = `Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:`
+  const prefix = `Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:`;
   const suffix = `Begin! Remember to speak as a pirate when giving your final answer. Use lots of "Args"
 
 Question: {input}
-{agent_scratchpad}`
+{agent_scratchpad}`;
 
   const createPromptArgs = {
     suffix,
     prefix,
     inputVariables: ["input", "agent_scratchpad"],
-  }
+  };
 
-  const prompt = ZeroShotAgent.createPrompt(
-    tools, createPromptArgs
-  )
+  const prompt = ZeroShotAgent.createPrompt(tools, createPromptArgs);
 
-  console.log(prompt.template)
+  console.log(prompt.template);
 
-  const llmChain = new LLMChain({llm: model, prompt})
-  const agent = new ZeroShotAgent({llmChain: llmChain, allowedTools:["search", "calculator"]})
-  const agentExecutor = AgentExecutor.fromAgentAndTools({agent, tools})
+  const llmChain = new LLMChain({ llm: model, prompt });
+  const agent = new ZeroShotAgent({
+    llmChain,
+    allowedTools: ["search", "calculator"],
+  });
+  const agentExecutor = AgentExecutor.fromAgentAndTools({ agent, tools });
   console.log("Loaded agent.");
 
   const input = `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`;
