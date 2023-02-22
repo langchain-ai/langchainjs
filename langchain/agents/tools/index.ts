fc7fbe3a25d863b2240d896e72d3acc6d33a2bc6
Sid Uppal <siddhu@gmail.com>
2023-02-21 20:57:09 -0800

    Adding Bing SERP API and Dad Joke API tools (#78)

--- langchain/agents/tools/index.ts
+++ langchain/agents/tools/index.ts
@@ -1,3 +1,5 @@
 export { SerpAPI } from "./serpapi";
 export { Calculator } from "./calculator";
+export { DadJokeAPI } from "./dadjokeapi";
+export { BingSerpAPI } from "./bingserpapi";
 export { Tool } from "./base";
