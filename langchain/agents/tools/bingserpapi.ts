41d6ec8637865bd288b28098bca3dd1cf76af8ce
Harrison Chase <hw.chase.17@gmail.com>
2023-02-21 22:38:35 -0800

    cr

--- langchain/agents/tools/bingserpapi.ts
+++ langchain/agents/tools/bingserpapi.ts
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import { Tool } from "./base";
 
 class BingSerpAPI extends Tool {
-  name = "search";
+  name = "bing-search";
 
   description =
     "a search engine. useful for when you need to answer questions about current events. input should be a search query.";
