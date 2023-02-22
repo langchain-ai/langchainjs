ca5df4847cb98062a721cdea7810fc097b5a2e9d
Harrison Chase <hw.chase.17@gmail.com>
2023-02-21 22:37:56 -0800

    cr

--- langchain/document_loaders/tests/srt.test.ts
+++
@@ -1,9 +0,0 @@
-import { test } from "@jest/globals";
-import { SRTLoader } from "../srt";
-
-test("Test subtitles file loader", async () => {
-  const loader = new SRTLoader(
-    "../examples/src/document_loaders/example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.srt"
-  );
-  await loader.load();
-});
