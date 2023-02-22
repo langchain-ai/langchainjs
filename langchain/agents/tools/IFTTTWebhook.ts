af7f88c38bc4c76f375588971443504f7ef40a4f
Harrison Chase <hw.chase.17@gmail.com>
2023-02-22 12:26:37 -0800

    cr

--- langchain/agents/tools/IFTTTWebhook.ts
+++ langchain/agents/tools/IFTTTWebhook.ts
@@ -1,15 +1,18 @@
-import fetch from 'node-fetch';
-import { Tool } from './base';
+import fetch from "node-fetch";
+import { Tool } from "./base";
 
 class IFTTTWebhook extends Tool {
-
   private url: string;
 
   name: string;
-  
+
   description: string;
 
-  constructor(url: string, name = "ifttt", description = "Send data to an IFTTT webhook URL") {
+  constructor(
+    url: string,
+    name = "ifttt",
+    description = "Send data to an IFTTT webhook URL"
+  ) {
     super();
     this.url = url;
     this.name = name;
@@ -18,7 +21,7 @@ class IFTTTWebhook extends Tool {
 
   async call(input: string): Promise<string> {
     const headers = { "Content-Type": "application/json" };
-    const body = JSON.stringify({ "this": input });
+    const body = JSON.stringify({ this: input });
 
     const response = await fetch(this.url, {
       method: "POST",
