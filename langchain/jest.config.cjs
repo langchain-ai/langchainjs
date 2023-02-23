cce065c373465a3b823dbb39559b6d5b5827090f
Nuno Campos <nuno@boringbits.io>
2023-02-23 20:52:55 +0000

    Fix jest config

--- langchain/jest.config.cjs
+++ langchain/jest.config.cjs
@@ -3,9 +3,12 @@ module.exports = {
   preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["dist/", "docs/"],
+  extensionsToTreatAsEsm: [".ts"],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js)$": "babel-jest",
+    "^.+\\.(ts|tsx)$": ["ts-jest", { useESM: true }],
   },
   transformIgnorePatterns: [],
   setupFiles: ["dotenv/config"],
