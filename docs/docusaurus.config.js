4290bc115e80d8bec703cd396d964add7b2c7cb8
Harrison Chase <hw.chase.17@gmail.com>
2023-02-23 07:48:33 -0800

    switch to env var (#115)
    * cr
    
    * cr

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   tagline: "The tagline of my site",
   favicon: "img/favicon.ico",
   customFields: {
-    mendableAnonKey: "fc146644-1dab-43f0-902a-a26f9d653bd3",
+    mendableAnonKey: process.env.MENDABLE_ANON_KEY,
   },
   // Set the production url of your site here
   url: "https://hwchase17.github.io",
