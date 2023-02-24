59890de3755e3d5934f78699c73ad571c25f9931
Ankush Gola <ankush.gola@gmail.com>
2023-02-24 12:08:22 -0800

    add json toolkit

--- langchain/agents/tools/json.ts
+++ langchain/agents/tools/json.ts
@@ -3,7 +3,7 @@ import {Tool, Toolkit} from "./base";
 function parseInput(input: string): (string | number)[] {
     const regex = /\[.*?]/g;
     const _res = input.match(regex) ?? [];
-    const res = _res.map((i) => i.slice(1, -1).replace('"', ''));
+    const res = _res.map((i) => i.slice(1, -1).replace(/['"]+/g, ''));
     return res.map((i) => (Number.isNaN(+i) ? i : +i));
 }
 
@@ -19,42 +19,34 @@ export class JsonSpec {
     }
 
     public getKeys(input: string): string {
-        try {
-            const keys = parseInput(input);
-            let res = this.obj;
-            for (const key of keys) {
-                res = res[key];
-            }
-            // check if res is a dictionary
-            if (typeof res === "object" && res !== null) {
-                return Object.keys(res).join(", ");
-            }
-            throw new Error(`Value at ${input} is not a dictionary, get the value directly instead.`);
-        } catch (error) {
-            return `${error}`;
+        const keys = parseInput(input);
+        let res = this.obj;
+        for (const key of keys) {
+            res = res[key];
         }
+        // check if res is a dictionary
+        if (res.constructor === Object) {
+            return Object.keys(res).join(", ");
+        }
+        throw new Error(`Value at ${input} is not a dictionary, get the value directly instead.`);
     }
 
     public getValue(input: string): string {
-        try {
-            const keys = parseInput(input);
-            let res = this.obj;
-            for (const key of keys) {
-                res = res[key];
-            }
-            const str = `${res}`;
-            if (typeof res === "object" && res !== null && str.length > this.max_value_length) {
-                return `Value is a large dictionary, should explore its keys directly.`;
-            }
-
-            if (str.length > this.max_value_length) {
-                // truncate the string
-                return `${str.slice(0, this.max_value_length)}...`;
-            }
-            return str;
-        } catch (error) {
-            return `${error}`;
+        const keys = parseInput(input);
+        let res = this.obj;
+        for (const key of keys) {
+            res = res[key];
         }
+        const str = res.constructor === Object ? JSON.stringify(res) : res.toString();
+        if (res.constructor === Object && str.length > this.max_value_length) {
+            return `Value is a large dictionary, should explore its keys directly.`;
+        }
+
+        if (str.length > this.max_value_length) {
+            // truncate the string
+            return `${str.slice(0, this.max_value_length)}...`;
+        }
+        return str;
     }
 }
 
@@ -67,11 +59,16 @@ export class JsonListKeysTool extends Tool {
     }
 
     async call(input: string) {
-        return this.jsonSpec.getKeys(input);
+        try {
+            return this.jsonSpec.getKeys(input);
+        } catch (error) {
+            return `${error}`;
+        }
     }
 
-    description = `Useful for getting the keys of a json object. 
-    The input to this tool should be a valid json path to a dictionary, e.g. [a, b, c]`;
+    description = `Can be used to list all keys at a given path. 
+    Before calling this you should be SURE that the path to this exists.
+    The input is a text representation of the path to the dict in Python syntax (e.g. data["key1"][0]["key2"]).`;
 }
 
 export class JsonGetValueTool extends Tool {
@@ -82,10 +79,16 @@ export class JsonGetValueTool extends Tool {
     }
 
     async call(input: string) {
-        return this.jsonSpec.getValue(input);
+        try {
+            return this.jsonSpec.getValue(input);
+        } catch (error) {
+            return `${error}`;
+        }
     }
 
-    description: string;
+    description: `Can be used to see value in string format at a given path.
+    Before calling this you should be SURE that the path to this exists.
+    The input is a text representation of the path to the dict in Python syntax (e.g. data["key1"][0]["key2"]).`;
 }
 
 export class JsonToolkit extends Toolkit {
