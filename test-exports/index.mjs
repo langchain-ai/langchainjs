f8d99493cf3fe379930dee977cf99fbace019f95
Nuno Campos <nuno@boringbits.io>
2023-02-24 14:10:49 +0000

    Add a more thorough test for packaging

--- test-exports/index.mjs
+++ test-exports/index.mjs
@@ -1,6 +1,41 @@
 import assert from "assert";
 import { OpenAI } from "langchain";
 import { loadPrompt } from "langchain/prompts";
+import { HNSWLib } from "langchain/vectorstores";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+import { InMemoryDocstore, Document } from "langchain/docstore";
 
+// Test exports
 assert(typeof OpenAI === "function");
 assert(typeof loadPrompt === "function");
+assert(typeof HNSWLib === "function");
+
+// Test dynamic imports of peer dependencies
+const { HierarchicalNSW } = await HNSWLib.imports();
+
+const vs = new HNSWLib(
+  {
+    space: "ip",
+    numDimensions: 3,
+  },
+  new OpenAIEmbeddings({ openAIApiKey: "sk-XXXX" }),
+  new InMemoryDocstore(),
+  new HierarchicalNSW("ip", 3)
+);
+
+await vs.addVectors(
+  [
+    [0, 1, 0],
+    [0, 0, 1],
+  ],
+  [
+    new Document({
+      pageContent: "a",
+    }),
+    new Document({
+      pageContent: "b",
+    }),
+  ]
+);
+
+assert((await vs.similaritySearchVectorWithScore([0, 0, 1], 1)).length === 1);
