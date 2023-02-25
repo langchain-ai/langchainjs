4f4396d1d5fc1a982b156e96be42f00ff0c84fa4
Harrison Chase <hw.chase.17@gmail.com>
2023-02-25 15:44:09 -0800

    cr

--- langchain/agents/tools/index.ts
+++ langchain/agents/tools/index.ts
@@ -5,3 +5,4 @@ export { BingSerpAPI } from "./bingserpapi";
 export { Tool } from "./base";
 export { DynamicTool } from "./dynamic";
 export { IFTTTWebhook } from "./IFTTTWebhook";
+export { ChainTool } from "./chain";
