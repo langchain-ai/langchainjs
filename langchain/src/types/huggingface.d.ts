b0b2d981f2aaa5b1c54046c3fd96550252527753
Nuno Campos <nuno@boringbits.io>
2023-02-27 17:23:42 +0000

    Fix eslint config to not exclude js/d.ts files

--- langchain/src/types/huggingface.d.ts
+++ langchain/src/types/huggingface.d.ts
@@ -3,12 +3,16 @@ declare module "huggingface" {
   // exports.types key in package.json
   declare class HuggingFace {
     private readonly apiKey;
+
     private readonly defaultOptions;
+
     constructor(apiKey: string, defaultOptions?: Options);
+
     /**
      * Tries to fill in a hole with a missing word (token to be precise). That’s the base task for BERT models.
      */
     fillMask(args: FillMaskArgs, options?: Options): Promise<FillMaskReturn>;
+
     /**
      * This task is well known to summarize longer text into shorter text. Be careful, some models have a maximum length of input. That means that the summary cannot handle full books for instance. Be careful when choosing your model.
      */
@@ -16,6 +20,7 @@ declare module "huggingface" {
       args: SummarizationArgs,
       options?: Options
     ): Promise<SummarizationReturn>;
+
     /**
      * Want to have a nice know-it-all bot that can answer any question?. Recommended model: deepset/roberta-base-squad2
      */
@@ -23,6 +28,7 @@ declare module "huggingface" {
       args: QuestionAnswerArgs,
       options?: Options
     ): Promise<QuestionAnswerReturn>;
+
     /**
      * Don’t know SQL? Don’t want to dive into a large spreadsheet? Ask questions in plain english! Recommended model: google/tapas-base-finetuned-wtq.
      */
@@ -30,6 +36,7 @@ declare module "huggingface" {
       args: TableQuestionAnswerArgs,
       options?: Options
     ): Promise<TableQuestionAnswerReturn>;
+
     /**
      * Usually used for sentiment-analysis this will output the likelihood of classes of an input. Recommended model: distilbert-base-uncased-finetuned-sst-2-english
      */
@@ -37,6 +44,7 @@ declare module "huggingface" {
       args: TextClassificationArgs,
       options?: Options
     ): Promise<TextClassificationReturn>;
+
     /**
      * Use to continue text from a prompt. This is a very generic task. Recommended model: gpt2 (it’s a simple model, but fun to play with).
      */
@@ -44,6 +52,7 @@ declare module "huggingface" {
       args: TextGenerationArgs,
       options?: Options
     ): Promise<TextGenerationReturn>;
+
     /**
      * Usually used for sentence parsing, either grammatical, or Named Entity Recognition (NER) to understand keywords contained within text. Recommended model: dbmdz/bert-large-cased-finetuned-conll03-english
      */
@@ -51,6 +60,7 @@ declare module "huggingface" {
       args: TokenClassificationArgs,
       options?: Options
     ): Promise<TokenClassificationReturn>;
+
     /**
      * This task is well known to translate text from one language to another. Recommended model: Helsinki-NLP/opus-mt-ru-en.
      */
@@ -58,6 +68,7 @@ declare module "huggingface" {
       args: TranslationArgs,
       options?: Options
     ): Promise<TranslationReturn>;
+
     /**
      * This task is super useful to try out classification with zero code, you simply pass a sentence/paragraph and the possible labels for that sentence, and you get a result. Recommended model: facebook/bart-large-mnli.
      */
@@ -65,6 +76,7 @@ declare module "huggingface" {
       args: ZeroShotClassificationArgs,
       options?: Options
     ): Promise<ZeroShotClassificationReturn>;
+
     /**
      * This task corresponds to any chatbot like structure. Models tend to have shorter max_length, so please check with caution when using a given model if you need long range dependency or not. Recommended model: microsoft/DialoGPT-large.
      *
@@ -73,6 +85,7 @@ declare module "huggingface" {
       args: ConversationalArgs,
       options?: Options
     ): Promise<ConversationalReturn>;
+
     /**
      * This task reads some text and outputs raw float values, that are usually consumed as part of a semantic database/semantic search.
      */
@@ -80,6 +93,7 @@ declare module "huggingface" {
       args: FeatureExtractionArgs,
       options?: Options
     ): Promise<FeatureExtractionReturn>;
+
     /**
      * This task reads some audio input and outputs the said words within the audio files.
      * Recommended model (english language): facebook/wav2vec2-large-960h-lv60-self
@@ -88,6 +102,7 @@ declare module "huggingface" {
       args: AutomaticSpeechRecognitionArgs,
       options?: Options
     ): Promise<AutomaticSpeechRecognitionReturn>;
+
     /**
      * This task reads some audio input and outputs the likelihood of classes.
      * Recommended model:  superb/hubert-large-superb-er
@@ -96,6 +111,7 @@ declare module "huggingface" {
       args: AudioClassificationArgs,
       options?: Options
     ): Promise<AudioClassificationReturn>;
+
     /**
      * This task reads some image input and outputs the likelihood of classes.
      * Recommended model: google/vit-base-patch16-224
@@ -104,6 +120,7 @@ declare module "huggingface" {
       args: ImageClassificationArgs,
       options?: Options
     ): Promise<ImageClassificationReturn>;
+
     /**
      * This task reads some image input and outputs the likelihood of classes & bounding boxes of detected objects.
      * Recommended model: facebook/detr-resnet-50
@@ -112,6 +129,7 @@ declare module "huggingface" {
       args: ObjectDetectionArgs,
       options?: Options
     ): Promise<ObjectDetectionReturn>;
+
     /**
      * This task reads some image input and outputs the likelihood of classes & bounding boxes of detected objects.
      * Recommended model: facebook/detr-resnet-50-panoptic
@@ -120,14 +138,16 @@ declare module "huggingface" {
       args: ImageSegmentationArgs,
       options?: Options
     ): Promise<ImageSegmentationReturn>;
+
     request(
       args: Args & {
-        data?: any;
+        data?: unknown;
       },
       options?: Options & {
         binary?: boolean;
       }
-    ): Promise<any>;
+    ): Promise<unknown>;
+
     private static toArray;
   }
 
