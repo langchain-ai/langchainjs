04f73747f5ac0385bccf8f66703b5535ce493623
Nuno Campos <nuno@boringbits.io>
2023-02-28 18:46:04 +0000

    Fix test file path assertion

--- langchain/src/document_loaders/tests/json.test.ts
+++ langchain/src/document_loaders/tests/json.test.ts
@@ -5,20 +5,16 @@ import { JSONLoader } from "../json.js";
 import { Document } from "../../document.js";
 
 test("Test JSON loader", async () => {
-  const loader = new JSONLoader(
-    path.resolve(
-      path.dirname(url.fileURLToPath(import.meta.url)),
-      "./example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.json"
-    )
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "./example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.json"
   );
+  const loader = new JSONLoader(filePath);
   const docs = await loader.load();
   expect(docs.length).toBe(32);
   expect(docs[0]).toEqual(
     new Document({
-      metadata: {
-        source:
-          "/Users/nuno/dev/langchainjs/langchain/src/document_loaders/tests/example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.json",
-      },
+      metadata: { source: filePath },
       pageContent:
         "<i>Corruption discovered at the core of the Banking Clan!</i>",
     })
--- langchain/src/document_loaders/tests/jsonl.test.ts
+++ langchain/src/document_loaders/tests/jsonl.test.ts
@@ -5,21 +5,16 @@ import { JSONLinesLoader } from "../jsonl.js";
 import { Document } from "../../document.js";
 
 test("Test JSON loader", async () => {
-  const loader = new JSONLinesLoader(
-    path.resolve(
-      path.dirname(url.fileURLToPath(import.meta.url)),
-      "./example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.jsonl"
-    ),
-    "/html"
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "./example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.jsonl"
   );
+  const loader = new JSONLinesLoader(filePath, "/html");
   const docs = await loader.load();
   expect(docs.length).toBe(32);
   expect(docs[0]).toEqual(
     new Document({
-      metadata: {
-        source:
-          "/Users/nuno/dev/langchainjs/langchain/src/document_loaders/tests/example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.jsonl",
-      },
+      metadata: { source: filePath },
       pageContent:
         "<i>Corruption discovered at the core of the Banking Clan!</i>",
     })
