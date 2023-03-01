9026c79a9c37f0460e1d4128c07aae5665ee2c61
Harrison Chase <hw.chase.17@gmail.com>
2023-03-01 07:07:28 -0800

    cr

--- langchain/src/agents/tools/vectorstore.ts
+++ langchain/src/agents/tools/vectorstore.ts
@@ -17,23 +17,22 @@ export class VectorStoreQATool extends Tool implements VectorStoreTool {
 
   description: string;
 
+  chain: VectorDBQAChain;
+
   constructor(name: string, description: string, fields: VectorStoreTool) {
     super();
     this.name = name;
     this.description = description;
     this.vectorStore = fields.vectorStore;
     this.llm = fields.llm;
+    this.chain = VectorDBQAChain.fromLLM(this.llm, this.vectorStore);
   }
 
   static getDescription(name: string, description: string): string {
-    return `Useful for when you need to answer questions about ${name}. "
-            "Whenever you need information about ${description} "
-            "you should ALWAYS use this. "
-            "Input should be a fully formed question.`;
+    return `Useful for when you need to answer questions about ${name}. Whenever you need information about ${description} you should ALWAYS use this. Input should be a fully formed question.`;
   }
 
   async call(input: string) {
-    const chain = VectorDBQAChain.fromLLM(this.llm, this.vectorStore);
-    return chain.run(input);
+    return this.chain.run(input);
   }
 }
