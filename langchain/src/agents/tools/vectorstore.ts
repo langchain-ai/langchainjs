0ff964fbdddc64e47f273c1b2aa72ed5a91f7d77
Ankush Gola <ankush.gola@gmail.com>
2023-02-28 22:28:04 -0800

    add vectorstore tool

---
+++ langchain/src/agents/tools/vectorstore.ts
@@ -0,0 +1,39 @@
+import {VectorStore} from "../../vectorstores/index.js";
+import {BaseLLM} from "../../llms/index.js";
+import {VectorDBQAChain} from "../../chains/index.js";
+import {Tool} from "./base.js";
+
+interface VectorStoreTool {
+    vectorStore: VectorStore;
+    llm: BaseLLM;
+}
+
+class VectorStoreQATool extends Tool implements VectorStoreTool {
+    vectorStore: VectorStore;
+
+    llm: BaseLLM;
+
+    name: string;
+
+    description: string;
+
+    constructor(name: string, description: string, fields: VectorStoreTool) {
+        super();
+        this.name = name;
+        this.description = description;
+        this.vectorStore = fields.vectorStore;
+        this.llm = fields.llm;
+    }
+
+    static getDescription(name: string, description: string): string {
+        return `Useful for when you need to answer questions about ${name}. "
+            "Whenever you need information about ${description} "
+            "you should ALWAYS use this. "
+            "Input should be a fully formed question.`;
+    }
+
+    async call(input: string): Promise<string> {
+        const chain = VectorDBQAChain.fromLLM( this.llm, this.vectorStore);
+        return chain.run(input);
+    }
+}
