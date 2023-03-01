64662984e58fa029514d8cdc9cefa44f62ba3417
Nuno Campos <nuno@boringbits.io>
2023-03-01 19:43:00 +0000

    Add chatgpt LLM

--- langchain/src/llms/index.ts
+++ langchain/src/llms/index.ts
@@ -1,5 +1,6 @@
 export { BaseLLM, LLM, SerializedLLM } from "./base.js";
 export { OpenAI, PromptLayerOpenAI } from "./openai.js";
+export { OpenAIChat } from "./openai-chat.js";
 export { Cohere } from "./cohere.js";
 export { HuggingFaceInference } from "./hf.js";
 export { loadLLM } from "./load.js";
---
+++ langchain/src/llms/tests/openai-chat.int.test.ts
@@ -0,0 +1,41 @@
+import { test, expect } from "@jest/globals";
+import { OpenAIChat } from "../openai-chat.js";
+
+test("Test OpenAI", async () => {
+  const model = new OpenAIChat({ modelName: "gpt-3.5-turbo" });
+  const res = await model.call("Print hello world");
+  console.log({ res });
+});
+
+test("Test OpenAI with prefix messages", async () => {
+  const model = new OpenAIChat({
+    modelName: "gpt-3.5-turbo",
+    prefixMessages: [
+      { role: "user", content: "My name is John" },
+      { role: "assistant", content: "Hi there" },
+    ],
+  });
+  const res = await model.call("What is my name");
+  console.log({ res });
+});
+
+test("Test OpenAI in streaming mode", async () => {
+  let nrNewTokens = 0;
+  let streamedCompletion = "";
+
+  const model = new OpenAIChat({
+    modelName: "gpt-3.5-turbo",
+    streaming: true,
+    callbackManager: {
+      handleNewToken(token) {
+        nrNewTokens += 1;
+        streamedCompletion += token;
+      },
+    },
+  });
+  const res = await model.call("Print hello world");
+  console.log({ res });
+
+  expect(nrNewTokens > 0).toBe(true);
+  expect(res).toBe(streamedCompletion);
+});
