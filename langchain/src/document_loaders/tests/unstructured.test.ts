983665f80bd61e19cb18d9682d7971c7c988d03f
Matt Robinson <mrobinson@unstructuredai.io>
2023-03-02 17:44:32 -0500

    temporarily disable test

--- langchain/src/document_loaders/tests/unstructured.test.ts
+++ langchain/src/document_loaders/tests/unstructured.test.ts
@@ -1,10 +1,10 @@
-import { test } from "@jest/globals";
-import { UnstructuredBaseDocumentLoader } from "../unstructured.js";
-
-test("Test Unstructured base loader", async () => {
-  const loader = new UnstructuredBaseDocumentLoader(
-    "http://127.0.0.1:8000/general/v0.0.4/general",
-    "/Users/mrobinson/repos/unstructured/example-docs/fake.docx",
-  );
-  await loader.load();
-});
+// import { test } from "@jest/globals";
+// import { UnstructuredBaseDocumentLoader } from "../unstructured.js";
+//
+// test("Test Unstructured base loader", async () => {
+//   const loader = new UnstructuredBaseDocumentLoader(
+//     "http://127.0.0.1:8000/general/v0.0.4/general",
+//     "/Users/mrobinson/repos/unstructured/example-docs/fake.docx",
+//   );
+//   await loader.load();
+// });
