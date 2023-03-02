c28948833c2e4453962120873ae4abaffda9c2be
Matt Robinson <mrobinson@unstructuredai.io>
2023-03-02 17:39:48 -0500

    finally a good test!

--- langchain/src/document_loaders/unstructured.ts
+++ langchain/src/document_loaders/unstructured.ts
@@ -1,4 +1,8 @@
-import { readFile } from "fs/promises";
+import FormData from "form-data";
+import fetch from "node-fetch";
+import { createReadStream } from "fs";
+
+// import { readFile } from "fs/promises";
 import { Document } from "../document.js";
 import { BaseDocumentLoader } from "./base.js";
 
@@ -7,23 +11,28 @@ export class UnstructuredBaseDocumentLoader extends BaseDocumentLoader {
   constructor(public webPath: string, public filePath: string) {
     super();
     this.filePath = filePath;
+
     this.webPath = webPath;
   }
 
   async _partition() {
-    const buffer = await readFile(this.filePath);
-    const blob = new Blob([buffer]);
+    // const buffer = await readFile(this.filePath);
+    // const blob = new Blob([buffer]);
 
-    const data = new FormData();
-    data.append("file", blob);
+    // const stats = statSync(self.filePath);
+    // const fileSizeInBytes = stats.size;
+    const fileStream = createReadStream(this.filePath);
+
+    const form = new FormData();
+    form.append('files', fileStream); // , { knownLength: fileSizeInBytes });
 
     const response = await fetch(this.webPath, {
       method: "POST",
-      headers: {
-        "Content-Type": "multipart/form-data",
-        "Accept": "application/json",
-      },
-      body: data,
+      // headers: {
+      //   "Content-Type": "multipart/form-data",
+      //   "Accept": "application/json",
+      // },
+      body: form,
     });
 
     const elements = await response.json();
@@ -34,8 +43,9 @@ export class UnstructuredBaseDocumentLoader extends BaseDocumentLoader {
     const elements = await this._partition();
 
     const documents = [];
+    // @ts-ignore
     for (const element of elements) {
-        const {metadata} = element;
+        const {metadata} = element.metadata;
         metadata.category = element.type;
         documents.push(new Document({ pageContent: metadata.text, metadata }));
     }
