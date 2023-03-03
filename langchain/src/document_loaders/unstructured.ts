e151ae89cd3aba9b5a0541a19bdf7d7de2a7fd05
Nuno Campos <nuno@boringbits.io>
2023-03-03 14:19:05 +0000

    Use native fetch and native FormData, add a better error message on api errors, make import of fs async because of future non-node envs

--- langchain/src/document_loaders/unstructured.ts
+++ langchain/src/document_loaders/unstructured.ts
@@ -1,12 +1,7 @@
-import FormData from "form-data";
-import fetch from "node-fetch";
-import { createReadStream } from "fs";
-
 import { Document } from "../document.js";
 import { BaseDocumentLoader } from "./base.js";
 
 export class UnstructuredBaseDocumentLoader extends BaseDocumentLoader {
-
   constructor(public webPath: string, public filePath: string) {
     super();
     this.filePath = filePath;
@@ -15,15 +10,28 @@ export class UnstructuredBaseDocumentLoader extends BaseDocumentLoader {
   }
 
   async _partition() {
-    const fileStream = createReadStream(this.filePath);
+    const { readFile } = await this.imports();
 
-    const form = new FormData();
-    form.append('files', fileStream);
+    const buffer = await readFile(this.filePath);
+
+    // I'm aware this reads the file into memory first, but we have lots of work
+    // to do on then consuming Documents in a streaming fashion anyway, so not
+    // worried about this for now.
+    const formData = new FormData();
+    formData.append("files", new Blob([buffer]));
 
     const response = await fetch(this.webPath, {
       method: "POST",
-      body: form,
+      body: formData,
     });
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to partition file ${this.filePath} with error ${
+          response.status
+        } and message ${await response.text()}`
+      );
+    }
 
     const elements = await response.json();
     return elements;
@@ -37,11 +45,18 @@ export class UnstructuredBaseDocumentLoader extends BaseDocumentLoader {
     // eslint-disable-next-line
     // @ts-ignore
     for (const element of elements) {
-        const {metadata} = element.metadata;
-        metadata.category = element.type;
-        documents.push(new Document({ pageContent: metadata.text, metadata }));
+      const { metadata } = element.metadata;
+      metadata.category = element.type;
+      documents.push(new Document({ pageContent: metadata.text, metadata }));
     }
 
     return documents;
+  }
+
+  async imports(): Promise<{
+    readFile: typeof import("fs/promises")["readFile"];
+  }> {
+    const { readFile } = await import("fs/promises");
+    return { readFile };
   }
 }
