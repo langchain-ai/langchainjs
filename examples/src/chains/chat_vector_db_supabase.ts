77c67f13cf3c9477419b38d0e77c85d9b6b6c71e
Jacob Rosenthal <jacobrosenthal@gmail.com>
2023-03-03 13:53:00 -0700

    chore: chat_vector_db_supabase example use fromDocuments

--- examples/src/chains/chat_vector_db_supabase.ts
+++ examples/src/chains/chat_vector_db_supabase.ts
@@ -2,27 +2,34 @@ import { OpenAI } from "langchain/llms";
 import { ChatVectorDBQAChain } from "langchain/chains";
 import { PGVectorStore } from "langchain/vectorstores";
 import { OpenAIEmbeddings } from "langchain/embeddings";
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
 
 export const run = async () => {
+  const client = createClient(
+    process.env.SUPABASE_URL || "",
+    process.env.SUPABASE_PRIVATE_KEY || ""
+  );
+
   /* Initialize the LLM to use to answer the question */
   const model = new OpenAI({});
+  /* Load in the file we want to do question answering over */
+  const text = fs.readFileSync("state_of_the_union.txt", "utf8");
+  /* Split the text into chunks */
+  const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+  const docs = await textSplitter.createDocuments([text]);
 
-  const client = createClient(
-    process.env.SUPABASE_URL || '',
-    process.env.SUPABASE_PRIVATE_KEY || ''
-  );
-
-  // assuming you've already inserted state_of_the_union
-  const vectorStore = await PGVectorStore.fromExistingIndex(
+  const vectorStore = await PGVectorStore.fromDocuments(
     client,
+    docs,
     new OpenAIEmbeddings(),
     "documents",
-    "match_documents",
+    "match_documents"
   );
-  
+
   const chain = ChatVectorDBQAChain.fromLLM(model, vectorStore);
-  chain.k = 0.8;
+  chain.k = 0.50;
 
   /* Ask it a question */
   const question = "What did the president say about Justice Breyer?";
