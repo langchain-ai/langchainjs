196e8bbd08be56e508a8ca559054f1853ab22034
Nuno Campos <nuno@boringbits.io>
2023-03-03 15:29:05 +0000

    Add assertions to test

--- langchain/src/document_loaders/tests/unstructured.test.ts
+++ langchain/src/document_loaders/tests/unstructured.test.ts
@@ -1,4 +1,4 @@
-import { test } from "@jest/globals";
+import { test, expect } from "@jest/globals";
 import { UnstructuredBaseDocumentLoader } from "../unstructured.js";
 
 test.skip("Test Unstructured base loader", async () => {
@@ -6,5 +6,10 @@ test.skip("Test Unstructured base loader", async () => {
     "http://127.0.0.1:8000/general/v0.0.4/general",
     "/Users/nuno/dev/langchainjs/langchain/src/document_loaders/tests/example_data/example.txt"
   );
-  await loader.load();
+  const docs = await loader.load();
+
+  expect(docs.length).toBe(3);
+  for (const doc of docs) {
+    expect(typeof doc.pageContent).toBe("string");
+  }
 });
