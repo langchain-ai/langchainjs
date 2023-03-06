d6aaa1b85df9b8329b00de487ea1784ebdc71985
Nuno Campos <nuno@boringbits.io>
2023-03-06 13:08:22 +0000

    Add llm chain example

---
+++ examples/src/chat/llm_chain.ts
@@ -0,0 +1,43 @@
+import { LLMChain, PromptTemplate } from "langchain";
+import { ChatOpenAI } from "langchain/chat_models";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from "langchain/prompts";
+
+export const run = async () => {
+  const chat = new ChatOpenAI({ temperature: 0 });
+
+  const systemPrompt = new PromptTemplate({
+    template:
+      "You are a helpful assistant that translates {input_language} to {output_language}.",
+    inputVariables: ["input_language", "output_language"],
+  });
+
+  const userPrompt = new PromptTemplate({
+    template: "{text}",
+    inputVariables: ["text"],
+  });
+
+  const chatPrompt = new ChatPromptTemplate({
+    promptMessages: [
+      new SystemMessagePromptTemplate(systemPrompt),
+      new HumanMessagePromptTemplate(userPrompt),
+    ],
+    inputVariables: ["input_language", "output_language", "text"],
+  });
+
+  const chain = new LLMChain({
+    prompt: chatPrompt,
+    llm: chat,
+  });
+
+  const response = await chain.call({
+    input_language: "English",
+    output_language: "French",
+    text: "I love programming.",
+  });
+
+  console.log(response);
+};
