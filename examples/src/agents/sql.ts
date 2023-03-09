ea5827d7e54bd7342623de51f8d59dd29c8b2c43
Ankush Gola <ankush.gola@gmail.com>
2023-03-08 19:44:58 -0800

    format

--- examples/src/agents/sql.ts
+++ examples/src/agents/sql.ts
@@ -1,7 +1,7 @@
 import { OpenAI } from "langchain";
 import { SqlDatabase } from "langchain/sql_db";
 import { createSqlAgent, SqlToolkit } from "langchain/agents";
-import {DataSource} from "typeorm";
+import { DataSource } from "typeorm";
 
 /** This example uses Chinook database, which is a sample database available for SQL Server, Oracle, MySQL, etc.
  * To set it up follow the instructions on https://database.guide/2-sample-databases-sqlite/, placing the .db file
