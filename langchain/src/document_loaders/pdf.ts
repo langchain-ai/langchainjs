226cca3d4044bba666b2309ff626be6a4a854d66
Nuno Campos <nuno@boringbits.io>
2023-03-11 13:42:09 +0000

    Add abstract BufferLoader, rewrite PdfLoader to use it, Add support for reading pdf from a Blob object

--- langchain/src/document_loaders/pdf.ts
+++ langchain/src/document_loaders/pdf.ts
@@ -1,34 +1,26 @@
-import type { readFile as ReadFileT } from "fs/promises";
 // the main entrypoint has some debug code that we don't want to import
 import pdf from "pdf-parse/lib/pdf-parse.js";
 import { Document } from "../document.js";
-import { getEnv } from "../util/env.js";
-import { BaseDocumentLoader } from "./base.js";
+import { BufferLoader } from "./buffer.js";
 
-export class PDFLoader extends BaseDocumentLoader {
-  constructor(public filePath: string) {
-    super();
-  }
-
-  public async load(): Promise<Document[]> {
-    const { readFile } = await PDFLoader.imports();
-    const buffer = await readFile(this.filePath);
-    const parsed = await pdf(buffer);
-    const metadata = { source: this.filePath };
-    return [new Document({ pageContent: parsed.text, metadata })];
-  }
-
-  static async imports(): Promise<{
-    readFile: typeof ReadFileT;
-  }> {
-    try {
-      const { readFile } = await import("fs/promises");
-      return { readFile };
-    } catch (e) {
-      console.error(e);
-      throw new Error(
-        `Failed to load fs/promises. PDFLoader available only on environment 'node'. It appears you are running environment '${getEnv()}'. See https://<link to docs> for alternatives.`
-      );
-    }
+export class PDFLoader extends BufferLoader {
+  public async parse(
+    raw: Buffer,
+    metadata: Document["metadata"]
+  ): Promise<Document[]> {
+    const parsed = await pdf(raw);
+    return [
+      new Document({
+        pageContent: parsed.text,
+        metadata: {
+          ...metadata,
+          pdf: {
+            info: parsed.info,
+            metadata: parsed.metadata,
+            numpages: parsed.numpages,
+          },
+        },
+      }),
+    ];
   }
 }
--- langchain/src/document_loaders/tests/pdf.test.ts
+++ langchain/src/document_loaders/tests/pdf.test.ts
@@ -1,14 +1,27 @@
 import { test, expect } from "@jest/globals";
 import url from "node:url";
 import path from "node:path";
+import fs from "node:fs/promises";
 import { PDFLoader } from "../pdf.js";
 
-test("Test PDF loader", async () => {
+test("Test PDF loader from file", async () => {
   const filePath = path.resolve(
     path.dirname(url.fileURLToPath(import.meta.url)),
     "./example_data/1706.03762.pdf"
   );
   const loader = new PDFLoader(filePath);
+  const docs = await loader.load();
+
+  expect(docs.length).toBe(1);
+  expect(docs[0].pageContent).toContain("Attention Is All You Need");
+});
+
+test("Test PDF loader from blob", async () => {
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "./example_data/1706.03762.pdf"
+  );
+  const loader = new PDFLoader(new Blob([await fs.readFile(filePath)]));
   const docs = await loader.load();
 
   expect(docs.length).toBe(1);
