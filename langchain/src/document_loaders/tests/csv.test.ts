df3a5ea6d7fe058879dfa16a03190ce05a23ad5f
Nuno Campos <nuno@boringbits.io>
2023-03-11 16:00:53 +0000

    Skip Blob tests on node 16

--- langchain/src/document_loaders/tests/csv.test.ts
+++ langchain/src/document_loaders/tests/csv.test.ts
@@ -1,6 +1,5 @@
 import url from "node:url";
 import path from "node:path";
-import fs from "node:fs/promises";
 import { test, expect } from "@jest/globals";
 import { CSVLoader } from "../csv.js";
 import { Document } from "../../document.js";
@@ -23,32 +22,6 @@ test("Test CSV loader from file with column arg", async () => {
   expect(docs[1]).toEqual(
     new Document({
       metadata: { source: filePath, line: 2 },
-      pageContent: "<i>Reunited, Rush Clovis and Senator Amidala</i>",
-    })
-  );
-});
-
-test("Test CSV loader from blob with column arg", async () => {
-  const filePath = path.resolve(
-    path.dirname(url.fileURLToPath(import.meta.url)),
-    "./example_data/Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.csv"
-  );
-  const loader = new CSVLoader(
-    new Blob([await fs.readFile(filePath)], { type: "text/csv" }),
-    "html"
-  );
-  const docs = await loader.load();
-  expect(docs.length).toBe(32);
-  expect(docs[0]).toEqual(
-    new Document({
-      metadata: { source: "blob", blobType: "text/csv", line: 1 },
-      pageContent:
-        "<i>Corruption discovered at the core of the Banking Clan!</i>",
-    })
-  );
-  expect(docs[1]).toEqual(
-    new Document({
-      metadata: { source: "blob", blobType: "text/csv", line: 2 },
       pageContent: "<i>Reunited, Rush Clovis and Senator Amidala</i>",
     })
   );
--- langchain/src/document_loaders/tests/text.test.ts
+++ langchain/src/document_loaders/tests/text.test.ts
@@ -21,19 +21,3 @@ test("Test Text loader from file", async () => {
     }
   `);
 });
-
-test("Test Text loader from blob", async () => {
-  const loader = new TextLoader(
-    new Blob(["Hello, world!"], { type: "text/plain" })
-  );
-  const docs = await loader.load();
-
-  expect(docs.length).toBe(1);
-  expect(docs[0].pageContent).toBe("Hello, world!");
-  expect(docs[0].metadata).toMatchInlineSnapshot(`
-    {
-      "blobType": "text/plain",
-      "source": "blob",
-    }
-  `);
-});
