ae50d76d1c9343f5d22c0b8aeea2d0db49611fbc
Nuno Campos <nuno@boringbits.io>
2023-03-12 07:44:36 +0000

    Format

--- langchain/src/callbacks/tests/langchain_tracer.int.test.ts
+++ langchain/src/callbacks/tests/langchain_tracer.int.test.ts
@@ -1,21 +1,21 @@
 import { test, expect } from "@jest/globals";
 
-import {LangChainTracer} from "../tracers/index.js";
+import { LangChainTracer } from "../tracers/index.js";
 
 test("Test LangChain tracer", async () => {
-    const tracer = new LangChainTracer();
-    await tracer.loadDefaultSession();
-    expect(tracer.alwaysVerbose).toBe(true);
+  const tracer = new LangChainTracer();
+  await tracer.loadDefaultSession();
+  expect(tracer.alwaysVerbose).toBe(true);
 
-    await tracer.handleChainStart({ name: "test" }, { foo: "bar" });
-    await tracer.handleToolStart({ name: "test" }, "test");
-    await tracer.handleLLMStart({ name: "test" }, ["test"]);
-    await tracer.handleLLMEnd({ generations: [[]] });
-    await tracer.handleToolEnd("output");
-    await tracer.handleLLMStart({ name: "test2" }, ["test"]);
-    await tracer.handleLLMEnd({ generations: [[]] });
-    await tracer.handleChainEnd({ foo: "bar" });
+  await tracer.handleChainStart({ name: "test" }, { foo: "bar" });
+  await tracer.handleToolStart({ name: "test" }, "test");
+  await tracer.handleLLMStart({ name: "test" }, ["test"]);
+  await tracer.handleLLMEnd({ generations: [[]] });
+  await tracer.handleToolEnd("output");
+  await tracer.handleLLMStart({ name: "test2" }, ["test"]);
+  await tracer.handleLLMEnd({ generations: [[]] });
+  await tracer.handleChainEnd({ foo: "bar" });
 
-    await tracer.handleLLMStart({ name: "test" }, ["test"]);
-    await tracer.handleLLMEnd({ generations: [[]] });
+  await tracer.handleLLMStart({ name: "test" }, ["test"]);
+  await tracer.handleLLMEnd({ generations: [[]] });
 });
