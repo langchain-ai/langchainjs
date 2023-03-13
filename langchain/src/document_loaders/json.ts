2dce9168ba95693622c46adc39c31cd409f43411
Maxime Thoonsen <maximet@theodo.fr>
2023-03-13 10:29:34 +0100

    json document handler can handle more complex json

--- langchain/src/document_loaders/json.ts
+++ langchain/src/document_loaders/json.ts
@@ -10,7 +10,42 @@ export class JSONLoader extends TextLoader {
   protected async parse(raw: string): Promise<string[]> {
     const json = JSON.parse(raw.trim());
     const pointer = jsonpointer.compile(this.pointer);
-    const value = pointer.get(json);
-    return Array.isArray(value) ? value : [value];
+
+    return this.extractArrayStringsFromObject(json, pointer);
+  }
+
+  private extractArrayStringsFromObject(
+    json: any,
+    pointer: jsonpointer
+  ): string[] {
+    if (!json) {
+      return [];
+    }
+
+    if (typeof json === "string") {
+      return [json];
+    }
+
+    if (Array.isArray(json)) {
+      let extractedString: string[] = [];
+      for (const element of json) {
+        extractedString = extractedString.concat(
+          this.extractArrayStringsFromObject(element, pointer)
+        );
+      }
+
+      return extractedString;
+    }
+
+    if (typeof json === "object") {
+      if (this.pointer) {
+        const targetedEntry = pointer.get(json);
+        return this.extractArrayStringsFromObject(targetedEntry, pointer);
+      }
+      const values = Object.values(json);
+      return this.extractArrayStringsFromObject(values, pointer);
+    }
+
+    return [];
   }
 }
--- langchain/src/document_loaders/tests/json.test.ts
+++ langchain/src/document_loaders/tests/json.test.ts
@@ -20,3 +20,60 @@ test("Test JSON loader from file", async () => {
     })
   );
 });
+
+test("Test JSON loader for complex json with one pointer that points nothing", async () => {
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "./example_data/complex.json"
+  );
+  const loader = new JSONLoader(filePath, "/body");
+  const docs = await loader.load();
+
+  expect(docs.length).toBe(0);
+});
+
+test("Test JSON loader for complex json with one pointer that points nothing", async () => {
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "./example_data/complex.json"
+  );
+  const loader = new JSONLoader(filePath, "/2/from");
+  const docs = await loader.load();
+
+  expect(docs.length).toBe(1);
+  expect(docs[0]).toEqual(
+    new Document({
+      metadata: { source: filePath },
+      pageContent: "LinkedIn Job2",
+    })
+  );
+});
+
+test("Test JSON loader for complex json without pointer", async () => {
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "./example_data/complex.json"
+  );
+  const loader = new JSONLoader(filePath);
+  const docs = await loader.load();
+
+  expect(docs.length).toBe(8);
+  expect(docs[0]).toEqual(
+    new Document({
+      metadata: { source: filePath },
+      pageContent: "BD 2023 SUMMER",
+    })
+  );
+  expect(docs[1]).toEqual(
+    new Document({
+      metadata: { source: filePath },
+      pageContent: "LinkedIn Job",
+    })
+  );
+  expect(docs[2]).toEqual(
+    new Document({
+      metadata: { source: filePath },
+      pageContent: "IMPORTANT",
+    })
+  );
+});
