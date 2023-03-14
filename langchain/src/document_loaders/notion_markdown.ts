7e4f6d2996bc12dd6b80f4f079263970da7b743e
Nuno Campos <nuno@boringbits.io>
2023-03-14 08:20:25 +0000

    Disable unknown files logging in notion loader

--- langchain/src/document_loaders/notion_markdown.ts
+++ langchain/src/document_loaders/notion_markdown.ts
@@ -1,10 +1,15 @@
-import { DirectoryLoader } from "./directory.js";
+import { DirectoryLoader, UnknownHandling } from "./directory.js";
 import { TextLoader } from "./text.js";
 
 export class NotionLoader extends DirectoryLoader {
   constructor(directoryPath: string) {
-    super(directoryPath, {
-      ".md": (filePath) => new TextLoader(filePath),
-    });
+    super(
+      directoryPath,
+      {
+        ".md": (filePath) => new TextLoader(filePath),
+      },
+      true,
+      UnknownHandling.Ignore
+    );
   }
 }
