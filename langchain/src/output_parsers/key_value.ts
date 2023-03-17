5bc3e33e60d561a25a81145ef141f720d6c270ff
Nuno Campos <nuno@boringbits.io>
2023-03-17 13:04:47 +0000

    Lint

--- langchain/src/output_parsers/key_value.ts
+++ langchain/src/output_parsers/key_value.ts
@@ -2,7 +2,7 @@ import { RecordOutputParser } from "./base.js";
 import { SerializedKeyValueOutputParser } from "./serde.js";
 
 /**
- * Class to parse the output of an LLM call as a comma-separated list.
+ * Class to parse the output of an LLM call as a list of key: value pairs.
  * @augments RecordOutputParser
  */
 export class KeyValueOutputParser extends RecordOutputParser {
@@ -17,6 +17,7 @@ export class KeyValueOutputParser extends RecordOutputParser {
       .filter((line) => line.length > 0)
       .map((line, i) => {
         if (i === 0 && !line.includes(":")) {
+          // Assume the key was included in the prompt
           return { key: this.keys[0], value: line.trim() };
         }
         const [key, value] = line.split(":");
