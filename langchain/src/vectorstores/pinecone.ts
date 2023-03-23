77abab39a8f8b94c4fe5e1a5c4c0e053ce8aac2a
ElektrikSpark <elektrikspark@gmail.com>
2023-03-22 21:37:15 -0500

    fixed filter to check for empty object and undefined

--- langchain/src/vectorstores/pinecone.ts
+++ langchain/src/vectorstores/pinecone.ts
@@ -76,10 +76,13 @@ export class PineconeStore extends VectorStore {
     k: number,
     filter?: object
   ): Promise<[Document, number][]> {
-    if (filter && this.filter) {
+    if (
+      (filter !== undefined && Object.keys(filter).length > 0) &&
+      (this.filter !== undefined && Object.keys(this.filter).length > 0)
+    ) {
       throw new Error("cannot provide both `filter` and `this.filter`");
     }
-    const _filter = filter ?? this.filter;
+    const _filter = (filter === undefined || Object.keys(filter).length === 0) ? this.filter : filter;
     const results = await this.pineconeIndex.query({
       queryRequest: {
         includeMetadata: true,
