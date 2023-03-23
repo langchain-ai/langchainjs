50f9cc467220fecdfc20f9fb37358279dfcab8e2
Harrison Chase <hw.chase.17@gmail.com>
2023-03-22 20:55:03 -0700

    cr

--- langchain/src/chains/index.ts
+++ langchain/src/chains/index.ts
@@ -15,8 +15,8 @@ export {
 } from "./question_answering/load.js";
 export { loadSummarizationChain } from "./summarization/load.js";
 export { SqlDatabaseChain } from "./sql_db/sql_db_chain.js";
-export { ConversationalRetrievalQAChain } from "./chat_retreival_chain.js";
-export { RetrievalQAChain } from "./retriever_qa.js";
+export { ConversationalRetrievalQAChain } from "./conversational_retreival_chain.js";
+export { RetrievalQAChain } from "./retrieval_qa.js";
 export {
   SerializedLLMChain,
   SerializedSqlDatabaseChain,
@@ -27,4 +27,3 @@ export {
   SerializedStuffDocumentsChain,
   SerializedVectorDBQAChain,
 } from "./serde.js";
-export { OutputFixingParser } from "../output_parsers/fix.js";
