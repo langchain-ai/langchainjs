181f3928b5e6ed1fb2904b0ee9caf253905d8f71
Nuno Campos <nuno@boringbits.io>
2023-04-04 17:49:29 +0100

    Fix missing type import (#608)

--- langchain/src/llms/openai.ts
+++ langchain/src/llms/openai.ts
@@ -8,7 +8,7 @@ import {
   OpenAIApi,
 } from "openai";
 import fetchAdapter from "../util/axios-fetch-adapter.js";
-import type { StreamingAxiosConfiguration } from "../util/axios-fetch-adapter.js";
+import type { StreamingAxiosConfiguration } from "../util/axios-types.js";
 import { chunkArray } from "../util/index.js";
 import { BaseLLM, BaseLLMParams } from "./base.js";
 import { calculateMaxTokens } from "../base_language/count_tokens.js";
--- langchain/src/util/axios-fetch-adapter.d.ts
+++ langchain/src/util/axios-fetch-adapter.d.ts
@@ -2,14 +2,3 @@
 import { AxiosRequestConfig, AxiosPromise } from "axios";
 
 export default function fetchAdapter(config: AxiosRequestConfig): AxiosPromise;
-
-export interface StreamingAxiosConfiguration {
-  responseType: "stream";
-
-  /**
-   * Called when a message is received. NOTE: Unlike the default browser
-   * EventSource.onmessage, this callback is called for _all_ events,
-   * even ones with a custom `event` field.
-   */
-  onmessage?: (ev: EventSourceMessage) => void;
-}
