1594cfadbcdd120fe9cc8abc2d9a2ed719420df0
Nuno Campos <nuno@boringbits.io>
2023-04-05 18:23:14 +0100

    Fix lite entrypoint definition, add 2 more lite entrypoints

---
+++ langchain/src/document_loaders/index.lite.ts
@@ -0,0 +1,16 @@
+export type { DocumentLoader } from "./base.js";
+export { BaseDocumentLoader } from "./base.js";
+export { CheerioWebBaseLoader } from "./cheerio_web_base.js";
+export { CollegeConfidentialLoader } from "./college_confidential.js";
+export { GitbookLoader } from "./gitbook.js";
+export { HNLoader } from "./hn.js";
+export { IMSDBLoader } from "./imsdb.js";
+export { SRTLoader } from "./srt.js";
+export { PDFLoader } from "./pdf.js";
+export { TextLoader } from "./text.js";
+export { JSONLoader } from "./json.js";
+export { JSONLinesLoader } from "./jsonl.js";
+export { CSVLoader } from "./csv.js";
+export { NotionLoader } from "./notion_markdown.js";
+export { GithubRepoLoader, GithubRepoLoaderParams } from "./github.js";
+export { UnstructuredLoader } from "./unstructured.js";
