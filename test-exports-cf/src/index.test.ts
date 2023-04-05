7985c8b62f5f236f71ea51f7d43441a60225ae74
Nuno Campos <nuno@boringbits.io>
2023-04-05 15:21:02 +0100

    Add test-exports-cf package to test using LangChain on Cloudflare Workers

---
+++ test-exports-cf/src/index.test.ts
@@ -0,0 +1,25 @@
+import { unstable_dev } from "wrangler";
+import type { UnstableDevWorker } from "wrangler";
+import { describe, expect, it, beforeAll, afterAll } from "vitest";
+
+describe("Worker", () => {
+  let worker: UnstableDevWorker;
+
+  beforeAll(async () => {
+    worker = await unstable_dev("src/index.ts", {
+      experimental: { disableExperimentalWarning: true },
+    });
+  });
+
+  afterAll(async () => {
+    await worker.stop();
+  });
+
+  it("should return Hello World", async () => {
+    const resp = await worker.fetch();
+    if (resp) {
+      const text = await resp.text();
+      expect(text.startsWith("Hello")).toBe(true);
+    }
+  });
+});
