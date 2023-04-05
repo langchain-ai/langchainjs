196a4eba8fda3a2380f16318d120db6865c3c36f
Nuno Campos <nuno@boringbits.io>
2023-04-05 16:50:55 +0100

    Add support for lite entrypoints

--- langchain/src/llms/index.lite.ts
+++ langchain/src/llms/index.lite.ts
@@ -1,7 +1,6 @@
 export { BaseLLM, BaseLLMParams, LLM, SerializedLLM } from "./base.js";
 export { OpenAI, PromptLayerOpenAI } from "./openai.js";
 export { OpenAIChat } from "./openai-chat.js";
-export { Cohere } from "./cohere.js";
 export { HuggingFaceInference } from "./hf.js";
 export { loadLLM } from "./load.js";
 export { Replicate } from "./replicate.js";
