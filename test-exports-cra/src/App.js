91eb1a75ecf8a65eff5665280e83641bf08199dd
Nuno Campos <nuno@boringbits.io>
2023-04-06 18:21:54 +0100

    Add test-exports-cra to test running langchain in browser with a create-react-app template

---
+++ test-exports-cra/src/App.js
@@ -0,0 +1,55 @@
+// Import a few things we'll use to test the exports
+import { LLMChain } from "langchain/chains";
+import { ChatOpenAI } from "langchain/chat_models";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+} from "langchain/prompts";
+import { OpenAI } from "langchain/llms";
+// import { TextLoader } from "langchain/document_loaders";
+
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+
+const OPENAI_API_KEY = "";
+
+function App() {
+  const runChain = async () => {
+    // Test a chain + prompt + model
+    const chain = new LLMChain({
+      llm: new ChatOpenAI({ openAIApiKey: OPENAI_API_KEY }),
+      prompt: ChatPromptTemplate.fromPromptMessages([
+        HumanMessagePromptTemplate.fromTemplate("{input}"),
+      ]),
+    });
+    const res = await chain.run("hello");
+
+    console.log("runChain", res);
+  };
+
+  useEffect(() => {
+    runChain();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
