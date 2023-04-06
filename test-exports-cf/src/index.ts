f6231c1c93f092513d346e7ac09cb38124e1fad8
Nuno Campos <nuno@boringbits.io>
2023-04-06 15:45:17 +0100

    More thorough test

--- test-exports-cf/src/index.ts
+++ test-exports-cf/src/index.ts
@@ -8,6 +8,28 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// import all entrypoints
+import * as agents from "langchain/agents";
+import * as base_language from "langchain/base_language";
+import * as tools from "langchain/tools";
+import * as chains from "langchain/chains";
+import * as embeddings from "langchain/embeddings";
+import * as llms from "langchain/llms";
+import * as prompts from "langchain/prompts";
+import * as vectorstores from "langchain/vectorstores";
+import * as text_splitter from "langchain/text_splitter";
+import * as memory from "langchain/memory";
+import * as document from "langchain/document";
+import * as docstore from "langchain/docstore";
+import * as document_loaders from "langchain/document_loaders";
+import * as chat_models from "langchain/chat_models";
+import * as schema from "langchain/schema";
+import * as sql_db from "langchain/sql_db";
+import * as callbacks from "langchain/callbacks";
+import * as output_parsers from "langchain/output_parsers";
+import * as retrievers from "langchain/retrievers";
+
+// Import a few things we'll use to test the exports
 import { LLMChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models";
 import {
@@ -16,7 +38,6 @@ import {
 } from "langchain/prompts";
 import { OpenAI } from "langchain/llms";
 import { OpenAIEmbeddings } from "langchain/embeddings";
-import { PineconeStore } from "langchain/vectorstores";
 import { TextLoader } from "langchain/document_loaders";
 
 export interface Env {
@@ -29,16 +50,6 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    console.log("Hello, world!");
-    console.log(PineconeStore);
-    console.log(OpenAI);
-    console.log(OpenAIEmbeddings);
-    console.log(TextLoader);
-    console.log(LLMChain);
-    console.log(ChatOpenAI);
-    console.log(ChatPromptTemplate);
-    console.log(HumanMessagePromptTemplate);
-
     // Intantiate a few things to test the exports
     new OpenAI({ openAIApiKey: env.OPENAI_API_KEY });
     const emb = new OpenAIEmbeddings({ openAIApiKey: env.OPENAI_API_KEY });
@@ -46,6 +57,7 @@ export default {
     // Test a document loader from a blob
     const docs = new TextLoader(new Blob(["hello"]));
 
+    // Test a chain + prompt + model
     const chain = new LLMChain({
       llm: new ChatOpenAI({ openAIApiKey: env.OPENAI_API_KEY }),
       prompt: ChatPromptTemplate.fromPromptMessages([
@@ -53,6 +65,9 @@ export default {
       ]),
     });
     const res = await chain.run("hello");
-    return new Response(`Hello, from ${request.url}. Assistant says: ${res}`);
+
+    return new Response(
+      `Hello, from Cloudflare Worker at ${request.url}. Assistant says: ${res}`
+    );
   },
 };
