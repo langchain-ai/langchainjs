5e466f55f742cdc57e356b9800e7e954f5480515
Nuno Campos <nuno@boringbits.io>
2023-04-06 17:30:35 +0100

    Add package to test exports on vercel

---
+++ test-exports-vercel/src/pages/api/hello-edge.ts
@@ -0,0 +1,63 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// import all entrypoints
+import * as agents from "langchain/agents";
+import * as base_language from "langchain/base_language";
+import * as tools from "langchain/tools";
+import * as chains from "langchain/chains";
+import * as embeddings from "langchain/embeddings";
+import * as llms from "langchain/llms";
+import * as prompts from "langchain/prompts";
+import * as vectorstores from "langchain/vectorstores";
+import * as text_splitter from "langchain/text_splitter";
+import * as memory from "langchain/memory";
+import * as document from "langchain/document";
+import * as docstore from "langchain/docstore";
+// import * as document_loaders from "langchain/document_loaders";
+import * as chat_models from "langchain/chat_models";
+import * as schema from "langchain/schema";
+import * as sql_db from "langchain/sql_db";
+import * as callbacks from "langchain/callbacks";
+import * as output_parsers from "langchain/output_parsers";
+import * as retrievers from "langchain/retrievers";
+
+// Import a few things we'll use to test the exports
+import { LLMChain } from "langchain/chains";
+import { ChatOpenAI } from "langchain/chat_models";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+} from "langchain/prompts";
+import { OpenAI } from "langchain/llms";
+import { OpenAIEmbeddings } from "langchain/embeddings";
+// import { TextLoader } from "langchain/document_loaders";
+
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req: NextRequest) {
+  // Intantiate a few things to test the exports
+  new OpenAI({ openAIApiKey: process.env.OPENAI_API_KEY });
+  const emb = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  // Test a document loader from a blob
+  // const docs = new TextLoader(new Blob(["hello"]));
+
+  // Test a chain + prompt + model
+  const chain = new LLMChain({
+    llm: new ChatOpenAI({ openAIApiKey: process.env.OPENAI_API_KEY }),
+    prompt: ChatPromptTemplate.fromPromptMessages([
+      HumanMessagePromptTemplate.fromTemplate("{input}"),
+    ]),
+  });
+  const res = await chain.run("hello");
+
+  return NextResponse.json({
+    name: `Hello, from ${req.url} I'm now an Edge Function! Assistant says: ${res}`,
+  });
+}
