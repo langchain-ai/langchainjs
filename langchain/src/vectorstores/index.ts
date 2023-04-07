fa8006b2a26e3639fbb0354202fe3f2f49629acf
Igor Shapiro <shapigor@gmail.com>
2023-04-08 02:26:02 +0300

    feat(vectorstore): implement opensearch vector store

--- langchain/src/vectorstores/index.ts
+++ langchain/src/vectorstores/index.ts
@@ -9,3 +9,4 @@ export { PineconeStore } from "./pinecone.js";
 export { VectorStore, SaveableVectorStore } from "./base.js";
 export { SupabaseVectorStore } from "./supabase.js";
 export { PrismaVectorStore } from "./prisma.js";
+export { OpenSearchVectorStore } from "./opensearch.js";
