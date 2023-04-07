27b0cae9a74e805fcd095372a1b654c40c39134a
Khairul Haaziq <101852870+khairulhaaziq@users.noreply.github.com>
2023-04-07 19:11:23 +0800

    fix: overwrite prefilled metadata if selected columns have same name

--- langchain/src/document_loaders/csv.ts
+++ langchain/src/document_loaders/csv.ts
@@ -35,31 +35,76 @@ import { TextLoader } from "./text.js";
  * // docs[0].pageContent:
  * // <i>Corruption discovered at the core of the Banking Clan!</i>
  */
-export class CSVLoader extends TextLoader {
-  constructor(filePathOrBlob: string | Blob, public column?: string) {
-    super(filePathOrBlob);
+export class CSVLoader extends BaseDocumentLoader {
+  constructor(
+    public filePathOrBlob: string | Blob,
+    public column?: string,
+    public metadataColumns?: string[]
+  ) {
+    super();
   }
 
-  protected async parse(raw: string): Promise<string[]> {
+  async load(): Promise<Document[]> {
+    const raw = await this.loadRawData();
     const { csvParse } = await CSVLoaderImports();
     const parsed = csvParse(raw.trim());
-    const { column } = this;
+    const { column, metadataColumns } = this;
 
-    if (column !== undefined) {
-      if (!parsed.columns.includes(column)) {
-        throw new Error(`Column ${column} not found in CSV file.`);
-      }
-
-      // Note TextLoader will raise an exception if the value is null.
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      return parsed.map((row) => row[column]!);
+    if (column !== undefined && !parsed.columns.includes(column)) {
+      throw new Error(`Column ${column} not found in CSV file.`);
     }
 
-    return parsed.map((row) =>
-      Object.keys(row)
-        .map((key) => `${key.trim()}: ${row[key]?.trim()}`)
-        .join("\n")
-    );
+    if (metadataColumns) {
+      for (const metadataColumn of metadataColumns) {
+        if (!parsed.columns.includes(metadataColumn)) {
+          throw new Error(`Metadata column ${metadataColumn} not found in CSV file.`);
+        }
+      }
+    }
+
+    const contents = parsed.map((row, i) => {
+      const content = column
+        ? row[column]
+        : Object.keys(row)
+            .map((key) => `${key.trim()}: ${row[key]?.trim()}`)
+            .join("\n");
+
+      if (typeof content !== "string") {
+        throw new Error(
+          `Expected string, at position ${i} got ${typeof content}`
+        );
+      }
+
+      return content;
+    });
+
+    return contents.map((content, i) => {
+      const metadata = {};
+
+      if (metadataColumns) {
+        for (const metadataColumn of metadataColumns) {
+          metadata[metadataColumn] = parsed[i][metadataColumn];
+        }
+      }
+
+      return new Document({
+        pageContent: content,
+        metadata: {
+          ...metadata,
+          source: metadataColumns?.includes("source") ? metadata["source"] : (typeof this.filePathOrBlob === "string" ? this.filePathOrBlob : "blob"),
+          line: metadataColumns?.includes("line") ? metadata["line"] : i + 1,
+        },
+      });
+    });
+  }
+
+  async loadRawData(): Promise<string> {
+    if (typeof this.filePathOrBlob === "string") {
+      const { readFile } = await TextLoader.imports();
+      return readFile(this.filePathOrBlob, "utf8");
+    } else {
+      return this.filePathOrBlob.text();
+    }
   }
 }
 
