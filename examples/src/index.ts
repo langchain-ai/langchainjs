afcae6d65ad1de39c097fce80f474476ca42a290
Jim Fisher <jameshfisher@gmail.com>
2023-04-09 12:25:37 +0200

    Provide instructions for how to run examples

--- examples/src/index.ts
+++ examples/src/index.ts
@@ -3,6 +3,11 @@ import url from "url";
 
 const [exampleName, ...args] = process.argv.slice(2);
 
+if (!exampleName) {
+  console.error("Please provide path to example to run");
+  process.exit(1);
+}
+
 // Allow people to pass all possible variations of a path to an example
 // ./src/foo.ts, ./dist/foo.js, src/foo.ts, dist/foo.js, foo.ts
 let exampleRelativePath: string;
