c9565f836331cfab038d972bf8c9715a8b59a95c
Jim Fisher <jameshfisher@gmail.com>
2023-04-10 19:19:41 +0200

    Add embedding example using Cohere
    Also nest under embeddings/ to follow package layout
    
    Also move "concurrency" to separate example

--- examples/src/embeddings/openai.ts
+++ examples/src/embeddings/openai.ts
@@ -1,12 +1,9 @@
 import { OpenAIEmbeddings } from "langchain/embeddings";
 
 export const run = async () => {
-  const model = new OpenAIEmbeddings({
-    maxConcurrency: 1,
-  });
-  console.log("yooooo");
+  const model = new OpenAIEmbeddings();
   const res = await model.embedQuery(
     "What would be a good company name a company that makes colorful socks?"
   );
-  console.log("hello", { res });
+  console.log({ res });
 };
