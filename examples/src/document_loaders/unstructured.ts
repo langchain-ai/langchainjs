0c43852925dff536d180b10029e8a4ef1e0a6b0f
Alex Liu <larch.liu@gmail.com>
2023-04-12 00:00:20 +0800

    fix: pass file name to unstructured-api (#742)
    fix #738
    
    Reference link

--- examples/src/document_loaders/unstructured.ts
+++ examples/src/document_loaders/unstructured.ts
@@ -3,7 +3,7 @@ import { UnstructuredLoader } from "langchain/document_loaders/fs/unstructured";
 export const run = async () => {
   const loader = new UnstructuredLoader(
     "http://localhost:8000/general/v0/general",
-    "langchain/src/document_loaders/tests/example_data/example.txt"
+    "src/document_loaders/example_data/notion.md"
   );
   const docs = await loader.load();
   console.log({ docs });
