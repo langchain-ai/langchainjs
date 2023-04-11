1aa9183e75ae2bbbd1efcce46b0a6b8d068bc63a
Jacob Rosenthal <jacobrosenthal@gmail.com>
2023-04-11 15:24:19 -0700

    feat: lint and export inmemoryvectorstore

--- langchain/src/vectorstores/index.ts
+++ langchain/src/vectorstores/index.ts
@@ -4,3 +4,4 @@ export { PineconeStore } from "./pinecone.js";
 export { VectorStore, SaveableVectorStore } from "./base.js";
 export { SupabaseVectorStore } from "./supabase.js";
 export { PrismaVectorStore } from "./prisma.js";
+export { MemoryVectorStore } from "./memory.js";
