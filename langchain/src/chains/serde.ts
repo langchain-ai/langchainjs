d1222e8b0b4661bb560742879f001704c36eafdd
Nuno Campos <nuno@boringbits.io>
2023-04-11 12:04:42 +0100

    Update serde interfaces after removing handling of paths, make hn test an int test

--- langchain/src/chains/serde.ts
+++ langchain/src/chains/serde.ts
@@ -5,45 +5,37 @@ import type { SerializedSqlDatabase } from "../util/sql_utils.js";
 export type SerializedLLMChain = {
   _type: "llm_chain";
   llm?: SerializedLLM;
-  llm_path?: string;
   prompt?: SerializedBasePromptTemplate;
-  prompt_path?: string;
 };
 
 export type SerializedSqlDatabaseChain = {
-  sql_database: SerializedSqlDatabase;
   _type: "sql_database_chain";
+  sql_database: SerializedSqlDatabase;
   llm: SerializedLLM;
-  sql_database_chain_path?: string;
 };
 
 export type SerializedVectorDBQAChain = {
   _type: "vector_db_qa";
   k: number;
   combine_documents_chain: SerializedBaseChain;
-  combine_documents_chain_path?: string;
 };
 
 export type SerializedStuffDocumentsChain = {
   _type: "stuff_documents_chain";
   llm_chain?: SerializedLLMChain;
-  llm_chain_path?: string;
 };
 
 export type SerializedChatVectorDBQAChain = {
   _type: "chat-vector-db";
   k: number;
   combine_documents_chain: SerializedBaseChain;
-  combine_documents_chain_path?: string;
   question_generator: SerializedLLMChain;
 };
 
 export type SerializedMapReduceDocumentsChain = {
   _type: "map_reduce_documents_chain";
   llm_chain?: SerializedLLMChain;
-  llm_chain_path?: string;
   combine_document_chain?: SerializedBaseChain;
-  combine_document_chain_path?: string;
 };
 
 export type SerializedRefineDocumentsChain = {
@@ -55,7 +47,6 @@ export type SerializedRefineDocumentsChain = {
 export type SerializedAnalyzeDocumentChain = {
   _type: "analyze_document_chain";
   combine_document_chain?: SerializedBaseChain;
-  combine_document_chain_path?: string;
 };
 
 export type SerializedBaseChain =
