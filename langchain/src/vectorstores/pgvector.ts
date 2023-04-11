2f566135321047220318b10c1d4e263a6eed3b5e
wlaurance <w.laurance@gmail.com>
2023-04-10 17:07:58 -0700

    Add pgvector

---
+++ langchain/src/vectorstores/pgvector.ts
@@ -0,0 +1,91 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as pg from "pg";
+import {toSql, registerType} from "pgvector/pg";
+import { VectorStore } from "./base.js";
+import { Embeddings } from "../embeddings/base.js";
+import { Document } from "../document.js";
+
+
+type Client = pg.Client;
+
+interface SearchEmbeddingsParams {
+  query_embedding: number[];
+  match_count: number; // int
+}
+
+export interface SearchEmbeddingsResponse {
+  id: number;
+  content: string;
+  metadata: object;
+  similarity: number;
+}
+
+export interface PGVectorLibArgs {
+  client: Client;
+  tableName?: string;
+  queryName?: string;
+}
+
+export class PGVectorStore extends VectorStore {
+  client: Client;
+
+  tableName: string;
+
+  queryName: string;
+
+  constructor(embeddings: Embeddings, args: PGVectorLibArgs) {
+    super(embeddings, args);
+
+    this.client = args.client;
+    this.tableName = args.tableName || "documents";
+    this.queryName = args.queryName || "match_documents";
+  }
+
+  async addVectors(vectors: number[][], documents: Document[]): Promise<void> {
+    const rows = vectors.map((embedding, idx) => ({
+      content: documents[idx].pageContent,
+      embedding,
+      metadata: documents[idx].metadata,
+    }));
+    await registerType(this.client);
+    await Promise.all(rows.map(r => this.client.query(
+      `insert into documents (content, metadata, embedding) VALUES ($1, $2, $3)`,
+      [r.content, r.metadata, toSql(r.embedding)]
+    )));
+  }
+
+  async addDocuments(documents: Document[]): Promise<void> {
+    const texts = documents.map(({ pageContent }) => pageContent);
+    return this.addVectors(
+      await this.embeddings.embedDocuments(texts),
+      documents
+    );
+  }
+
+  async similaritySearchVectorWithScore(query: number[], k: number, _filter?: object | undefined): Promise<[Document, number][]> {
+    const matchDocumentsParams: SearchEmbeddingsParams = {
+      query_embedding: query,
+      match_count: k,
+    };
+
+    const {rows} = await this.client.query(
+      'select * from match_documents($1, $2)', [toSql(matchDocumentsParams.query_embedding), matchDocumentsParams.match_count],
+    );
+    const result: [Document, number][] = rows.map((row: SearchEmbeddingsResponse) => [
+      new Document({
+        metadata: row.metadata,
+        pageContent: row.content,
+      }),
+      row.similarity,
+    ]);
+
+    return result;
+  }
+
+  static async fromDocuments(docs: Document[], embeddings: Embeddings, dbConfig: PGVectorLibArgs): Promise<PGVectorStore> {
+    const instance = new this(embeddings, dbConfig);
+    await instance.addDocuments(docs);
+    return instance;
+  }
+
+}
---
+++ langchain/src/vectorstores/tests/pgvector.int.test.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { test, expect } from "@jest/globals";
+
+import * as pg from 'pg';
+
+import { OpenAIEmbeddings } from "../../embeddings/index.js";
+import { Document } from "../../document.js";
+import { PGVectorStore } from "../pgvector.js";
+
+const {Client} = pg;
+
+test("PGVectorStore with external ids", async () => {
+  const client = new Client();
+  await client.connect();
+  const embeddings = new OpenAIEmbeddings();
+
+  const store = new PGVectorStore(embeddings, { client });
+
+  expect(store).toBeDefined();
+
+  await store.addDocuments([
+    { pageContent: "hello", metadata: { a: 1 } },
+    { pageContent: "hi", metadata: { a: 1 } },
+    { pageContent: "bye", metadata: { a: 1 } },
+    { pageContent: "what's this", metadata: { a: 1 } },
+  ]);
+
+  const results = await store.similaritySearch("hello", 1);
+
+  expect(results).toHaveLength(1);
+
+  expect(results).toEqual([
+    new Document({ metadata: { a: 1 }, pageContent: "hello" }),
+  ]);
+});
