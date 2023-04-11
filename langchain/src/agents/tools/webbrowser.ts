e8592f4c4cc7b91c4c42dab5d2ea279189b96622
Jacob Rosenthal <jacobrosenthal@gmail.com>
2023-04-10 20:41:01 -0700

    fix: webbrowser dont use br header tag, axios doesnt seem to support

--- langchain/src/agents/tools/webbrowser.ts
+++ langchain/src/agents/tools/webbrowser.ts
@@ -45,7 +45,7 @@ export const getText = (html: string, baseUrl: string): string => {
 const DEFAULT_HEADERS = {
   Accept:
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-  "Accept-Encoding": "gzip, deflate, br",
+  "Accept-Encoding": "gzip, deflate",
   "Accept-Language": "en-US,en;q=0.5",
   "Alt-Used": "LEAVE-THIS-KEY-SET-BY-TOOL",
   Connection: "keep-alive",
