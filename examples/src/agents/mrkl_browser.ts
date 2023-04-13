a3984c10c255a02fcff16d5daa3a1f542d789740
Jacob Rosenthal <jacobrosenthal@gmail.com>
2023-04-12 22:51:25 -0700

    fix: webbrowser example serpapi add gl and hl

--- examples/src/agents/mrkl_browser.ts
+++ examples/src/agents/mrkl_browser.ts
@@ -7,6 +7,8 @@ export const run = async () => {
   const tools = [
     new SerpAPI(process.env.SERPAPI_API_KEY, {
       location: "Austin,Texas,United States",
+      hl: "en",
+      gl: "us",
     }),
     new Calculator(),
     new WebBrowser(model),
