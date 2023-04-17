bf6fb14d6b1f12dde2da8aa819ab659169becaae
choonkeat <choonkeat@gmail.com>
2023-04-17 22:02:06 +0800

    fix format

--- examples/src/models/chat/chat_streaming_stdout.ts
+++ examples/src/models/chat/chat_streaming_stdout.ts
@@ -14,23 +14,23 @@ const chat = new ChatOpenAI({
 await chat.call([
   new HumanChatMessage("Write me a song about sparkling water."),
 ]);
-  /*
-  Verse 1:
-  Bubbles rise, crisp and clear
-  Refreshing taste that brings us cheer
-  Sparkling water, so light and pure
-  Quenches our thirst, it's always secure
+/*
+Verse 1:
+Bubbles rise, crisp and clear
+Refreshing taste that brings us cheer
+Sparkling water, so light and pure
+Quenches our thirst, it's always secure
 
-  Chorus:
-  Sparkling water, oh how we love
-  Its fizzy bubbles and grace above
-  It's the perfect drink, anytime, anyplace
-  Refreshing as it gives us a taste
+Chorus:
+Sparkling water, oh how we love
+Its fizzy bubbles and grace above
+It's the perfect drink, anytime, anyplace
+Refreshing as it gives us a taste
 
-  Verse 2:
-  From morning brunch to evening feast
-  It's the perfect drink for a treat
-  A sip of it brings a smile so bright
-  Our thirst is quenched in just one sip so light
-  ...
-  */
+Verse 2:
+From morning brunch to evening feast
+It's the perfect drink for a treat
+A sip of it brings a smile so bright
+Our thirst is quenched in just one sip so light
+...
+*/
--- examples/src/models/llm/llm_streaming_stdout.ts
+++ examples/src/models/llm/llm_streaming_stdout.ts
@@ -13,23 +13,23 @@ const chat = new OpenAI({
 });
 
 await chat.call("Write me a song about sparkling water.");
-  /*
-  Verse 1
-  Crystal clear and made with care
-  Sparkling water on my lips, so refreshing in the air
-  Fizzy bubbles, light and sweet
-  My favorite beverage I can’t help but repeat
+/*
+Verse 1
+Crystal clear and made with care
+Sparkling water on my lips, so refreshing in the air
+Fizzy bubbles, light and sweet
+My favorite beverage I can’t help but repeat
 
-  Chorus
-  A toast to sparkling water, I’m feeling so alive
-  Let’s take a sip, and let’s take a drive
-  A toast to sparkling water, it’s the best I’ve had in my life
-  It’s the best way to start off the night
+Chorus
+A toast to sparkling water, I’m feeling so alive
+Let’s take a sip, and let’s take a drive
+A toast to sparkling water, it’s the best I’ve had in my life
+It’s the best way to start off the night
 
-  Verse 2
-  It’s the perfect drink to quench my thirst
-  It’s the best way to stay hydrated, it’s the first
-  A few ice cubes, a splash of lime
-  It will make any day feel sublime
-  ...
- */
+Verse 2
+It’s the perfect drink to quench my thirst
+It’s the best way to stay hydrated, it’s the first
+A few ice cubes, a splash of lime
+It will make any day feel sublime
+...
+*/
