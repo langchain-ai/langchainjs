7516f2be3f762b731ec57632989f48bd73c38717
NJordan72 <NJordan72@users.noreply.github.com>
2023-04-19 10:14:01 +0000

    feat: human tool

---
+++ langchain/src/tools/human.ts
@@ -0,0 +1,31 @@
+import { Tool } from "./base.js";
+
+/**
+*    Represents a tool that allows an agent to ask a human for guidance when they are stuck 
+*    or unsure of what to do next.
+*    @extends Tool
+*/
+export class HumanTools extends Tool {
+    /**
+    * The name of the tool.
+    * @type {string}
+    */
+    name = "Human";
+
+    /**
+    * A description for the agent to use
+    * @type {string}
+    */
+    description = `You can ask a human for guidance when you think you
+        got stuck or you are not sure what to do next.
+        The input should be a question for the human.`;
+
+    /**
+     * Calls the tool with the provided input and returns a promise that resolves with a response from the human.
+     * @param {string} input - The input to provide to the human.
+     * @returns {Promise<string>} A promise that resolves with a response from the human.
+    */
+    _call(input: string): Promise<string> {
+        return Promise.resolve(`${input}`);
+    }
+}
