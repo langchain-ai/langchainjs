6be16cd945f272e1a2aa2cfd5dd987f0dfc87677
katsumata <12413150+winor30@users.noreply.github.com>
2023-04-20 22:27:44 +0900

    refactor: Refactor time-weighted retriever constructor and add JSDoc comments
    - Simplified the constructor arguments using optional chaining and default values.
    - Improved code clarity by adding JSDoc comments for private functions.
    - Renamed variable for better understanding of its purpose.
    - Removed unnecessary keyword in a method.

--- langchain/src/retrievers/time_weighted.ts
+++ langchain/src/retrievers/time_weighted.ts
@@ -16,32 +16,49 @@ export const LAST_ACCESSED_AT_KEY = "last_accessed_at";
 export const BUFFER_IDX = "buffer_idx";
 
 /**
- * TimeWeightedVectorStoreRetriever is a class that retrieves documents based on their time-weighted relevance.
+ * TimeWeightedVectorStoreRetriever retrieves documents based on their time-weighted relevance.
  * ref: https://github.com/hwchase17/langchain/blob/master/langchain/retrievers/time_weighted_retriever.py
  */
 export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
-  // The vectorstore to store documents and determine salience.
+  /**
+   * The vectorstore to store documents and determine salience.
+   */
   private vectorStore: VectorStore;
 
-  // The number of top K most relevant documents to consider when searching.
+  /**
+   * The number of top K most relevant documents to consider when searching.
+   */
   private searchKwargs: number;
 
-  // The memory_stream of documents to search through.
+  /**
+   * The memory_stream of documents to search through.
+   */
   private memoryStream: Document[];
 
-  // The exponential decay factor used as (1.0-decay_rate)**(hrs_passed).
+  /**
+   * The exponential decay factor used as (1.0-decay_rate)**(hrs_passed).
+   */
   private decayRate: number;
 
-  // The maximum number of documents to retrieve in a given call.
+  /**
+   * The maximum number of documents to retrieve in a given call.
+   */
   private k: number;
 
-  // Other keys in the metadata to factor into the score, e.g. 'importance'.
+  /**
+   * Other keys in the metadata to factor into the score, e.g. 'importance'.
+   */
   private otherScoreKeys: string[];
 
-  // The salience to assign memories not retrieved from the vector store.
+  /**
+   * The salience to assign memories not retrieved from the vector store.
+   */
   private defaultSalience: number | null;
 
-  // Constructor to initialize the required fields
+  /**
+   * Constructor to initialize the required fields
+   * @param fields - The fields required for initializing the TimeWeightedVectorStoreRetriever
+   */
   constructor(fields: TimeWeightedVectorStoreRetrieverFields) {
     super();
     this.vectorStore = fields.vectorStore;
@@ -55,7 +72,7 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
 
   /**
    * Get relevant documents based on time-weighted relevance
-   * @param query The query to search for
+   * @param query - The query to search for
    * @returns The relevant documents
    */
   async getRelevantDocuments(query: string): Promise<Document[]> {
@@ -74,7 +91,7 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
    * This is because it is necessary to process the document
    * in prepareDocuments.
    *
-   * @param docs The documents to add to vector store in the retriever
+   * @param docs - The documents to add to vector store in the retriever
    */
   async addDocuments(docs: Document[]): Promise<void> {
     const now = Math.floor(Date.now() / 1000);
@@ -84,7 +101,10 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
     await this.vectorStore.addDocuments(savedDocs);
   }
 
-  // Get memory documents and their scores
+  /**
+   * Get memory documents and their scores
+   * @returns An object containing memory documents and their scores
+   */
   private getMemoryDocsAndScores(): Record<
     number,
     { doc: Document; score: number }
@@ -103,7 +123,11 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
     return memoryDocsAndScores;
   }
 
-  // Get salient documents and their scores based on the query
+  /**
+   * Get salient documents and their scores based on the query
+   * @param query - The query to search for
+   * @returns An object containing salient documents and their scores
+   */
   private async getSalientDocuments(
     query: string
   ): Promise<Record<number, { doc: Document; score: number }>> {
@@ -121,7 +145,12 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
     return results;
   }
 
-  // Compute the final result set of documents based on the combined scores
+  /**
+   * Compute the final result set of documents based on the combined scores
+   * @param docsAndScores - An object containing documents and their scores
+   * @param now - The current timestamp
+   * @returns The final set of documents
+   */
   private computeResults(
     docsAndScores: Record<number, { doc: Document; score: number }>,
     now: number
@@ -142,7 +171,12 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
     return results;
   }
 
-  // Prepare documents with necessary metadata before saving
+  /**
+   * Prepare documents with necessary metadata before saving
+   * @param docs - The documents to prepare
+   * @param now - The current timestamp
+   * @returns The prepared documents
+   */
   private prepareDocuments(docs: Document[], now: number): Document[] {
     return docs.map((doc, i) => ({
       ...doc,
@@ -154,7 +188,14 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
     }));
   }
 
-  // Calculate the combined score based on vector relevance and other factors
+  /**
+   * Calculate the combined score based on vector relevance and other factors
+   * @param doc - The document to calculate the score for
+   * @param vectorRelevance - The relevance score
+   * from the vector store
+@param nowMsec - The current timestamp in milliseconds
+@returns The combined score for the document
+*/
   private getCombinedScore(
     doc: Document,
     vectorRelevance: number | null,
@@ -174,7 +215,13 @@ export class TimeWeightedVectorStoreRetriever extends BaseRetriever {
     return score;
   }
 
-  // Calculate the hours passed between two time points
+  /**
+
+  Calculate the hours passed between two time points
+  @param time - The current time in seconds
+  @param refTime - The reference time in seconds
+  @returns The number of hours passed between the two time points
+  */
   private getHoursPassed(time: number, refTime: number): number {
     return (time - refTime) / 3600;
   }
