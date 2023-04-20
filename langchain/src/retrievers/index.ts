07bbf262cf939e98352ff9fa45853e6b5fb2a18b
katsumata <12413150+winor30@users.noreply.github.com>
2023-04-20 09:49:46 +0900

    refactor: Reorganize retrievers exports in langchain codebase
    - Reorganize exports in the `index.ts` file in the `retrievers` directory
    - Move `MetalRetriever` and `RemoteLangChainRetriever` exports to the top
    - Add `TimeWeightedVectorStoreRetriever` export at the bottom for improved organization

--- langchain/src/retrievers/index.ts
+++ langchain/src/retrievers/index.ts
@@ -3,11 +3,12 @@
   "[WARN] Importing from 'langchain/retrievers' is deprecated. Import from eg. 'langchain/retrievers/remote' instead. See https://js.langchain.com/docs/getting-started/install#updating-from-0052 for upgrade instructions."
 );
 
+export { MetalRetriever } from "./metal.js";
 export { RemoteRetriever } from "./remote/base.js";
 export { ChatGPTPluginRetriever } from "./remote/chatgpt-plugin.js";
+export { RemoteLangChainRetriever } from "./remote/remote-retriever.js";
 export {
   SupabaseHybridSearch,
-  SupabaseHybridSearchParams,
+  SupabaseHybridSearchParams
 } from "./supabase.js";
-export { RemoteLangChainRetriever } from "./remote/remote-retriever.js";
-export { MetalRetriever } from "./metal.js";
+export { TimeWeightedVectorStoreRetriever } from "./time_weighted.js";
