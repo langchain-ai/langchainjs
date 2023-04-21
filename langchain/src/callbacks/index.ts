fec1ff3508e6adaf3bd4aae187069b439ab1a553
Nuno Campos <nuno@boringbits.io>
2023-04-21 19:14:21 +0100

    Add more exports

--- langchain/src/callbacks/index.ts
+++ langchain/src/callbacks/index.ts
@@ -14,5 +14,12 @@ export {
 
 export { getTracingCallbackHandler } from "./handlers/initialize.js";
 
-export { CallbackManager } from "./manager.js";
+export {
+  CallbackManager,
+  CallbackManagerForChainRun,
+  CallbackManagerForLLMRun,
+  CallbackManagerForToolRun,
+  CallbackManagerOptions,
+  Callbacks,
+} from "./manager.js";
 export { ConsoleCallbackHandler } from "./handlers/console.js";
