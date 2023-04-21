35ff829a30634b2b957542fafedeb20957fd5441
Nuno Campos <nuno@boringbits.io>
2023-04-21 14:11:34 +0100

    Update a few tools for new callbacks interface

--- langchain/src/tools/chain.ts
+++ langchain/src/tools/chain.ts
@@ -1,6 +1,15 @@
-import { CallbackManager } from "callbacks/base.js";
-import { Tool } from "./base.js";
+import { BaseCallbackHandler } from "../callbacks/base.js";
+import {
+  CallbackManager,
+  CallbackManagerForToolRun,
+} from "../callbacks/manager.js";
+import { DynamicToolInput } from "./dynamic.js";
 import { BaseChain } from "../chains/base.js";
+import { Tool } from "./base.js";
+
+export interface ChainToolInput extends Omit<DynamicToolInput, "func"> {
+  chain: BaseChain;
+}
 
 export class ChainTool extends Tool {
   name: string;
@@ -17,17 +26,22 @@ export class ChainTool extends Tool {
     chain: BaseChain;
     returnDirect?: boolean;
     verbose?: boolean;
-    callbackManager?: CallbackManager;
+    callbacks?: CallbackManager | BaseCallbackHandler[];
   }) {
-    super(fields.verbose, fields.callbackManager);
+    super(fields.verbose, fields.callbacks);
     this.name = fields.name;
     this.description = fields.description;
     this.chain = fields.chain;
     this.returnDirect = fields.returnDirect ?? this.returnDirect;
+    this.verbose = fields.verbose ?? this.verbose;
+    this.callbacks = fields.callbacks ?? this.callbacks;
   }
 
   /** @ignore */
-  async _call(input: string): Promise<string> {
-    return this.chain.run(input);
+  async _call(
+    input: string,
+    runManager?: CallbackManagerForToolRun
+  ): Promise<string> {
+    return this.chain.run(input, runManager?.getChild());
   }
 }
