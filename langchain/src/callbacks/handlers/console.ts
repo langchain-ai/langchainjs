fc9b94b24b9cd7537c79483f0cff5712a251846b
Nuno Campos <nuno@boringbits.io>
2023-04-21 19:05:03 +0100

    Implement llm methods in console handler

--- langchain/src/callbacks/handlers/console.ts
+++ langchain/src/callbacks/handlers/console.ts
@@ -1,30 +1,52 @@
-import { AgentAction, AgentFinish, ChainValues } from "../../schema/index.js";
+import {
+  AgentAction,
+  AgentFinish,
+  ChainValues,
+  LLMResult,
+} from "../../schema/index.js";
 import { BaseCallbackHandler } from "../base.js";
 
 export class ConsoleCallbackHandler extends BaseCallbackHandler {
   name = "console_callback_handler";
 
-  async handleChainStart(chain: { name: string }) {
+  handleLLMStart(llm: { name: string }, prompts: string[], runId: string) {
+    console.log(
+      `Starting LLM ${runId} with name ${llm.name} with prompts: ${prompts.join(
+        ", "
+      )}`
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  handleLLMError(err: any, runId: string) {
+    console.log(`LLM ${runId} errored: ${err}`);
+  }
+
+  handleLLMEnd(output: LLMResult, runId: string) {
+    console.log(`LLM ${runId} finished: ${output}`);
+  }
+
+  handleChainStart(chain: { name: string }) {
     console.log(`Entering new ${chain.name} chain...`);
   }
 
-  async handleChainEnd(_output: ChainValues) {
+  handleChainEnd(_output: ChainValues) {
     console.log("Finished chain.");
   }
 
-  async handleAgentAction(action: AgentAction) {
+  handleAgentAction(action: AgentAction) {
     console.log(action.log);
   }
 
-  async handleToolEnd(output: string) {
+  handleToolEnd(output: string) {
     console.log(output);
   }
 
-  async handleText(text: string) {
+  handleText(text: string) {
     console.log(text);
   }
 
-  async handleAgentEnd(action: AgentFinish) {
+  handleAgentEnd(action: AgentFinish) {
     console.log(action.log);
   }
 }
