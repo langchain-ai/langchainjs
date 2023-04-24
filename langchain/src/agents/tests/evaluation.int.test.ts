fc2dfe1dddd61153d4abb2c89131339d48d22c46
Nuno Campos <nuno@boringbits.io>
2023-04-24 10:57:25 +0100

    Finish test harness for agent scenarios, Add some more simple test cases

--- langchain/src/agents/tests/evaluation.int.test.ts
+++ langchain/src/agents/tests/evaluation.int.test.ts
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable tree-shaking/no-side-effects-in-initialization */
 import { describe } from "@jest/globals";
 import { ChatOpenAI } from "../../chat_models/openai.js";
 import { initializeAgentExecutorWithOptions } from "../initialize.js";
-import { OpenAI } from "../../index.js";
+import { OpenAI } from "../../llms/openai.js";
 import { Tool } from "../../tools/base.js";
 import { SerpAPI } from "../../tools/serpapi.js";
 import { Calculator } from "../../tools/calculator.js";
 import { RequestsGetTool, RequestsPostTool } from "../../tools/requests.js";
-// import { AIPluginTool } from "../../tools/aiplugin.js";
+import { AIPluginTool } from "../../tools/aiplugin.js";
 
 const agents = [
   (tools) =>
@@ -32,17 +31,17 @@ const agents = [
 ) => ReturnType<typeof initializeAgentExecutorWithOptions>)[];
 
 const scenarios = [
-  {
+  async () => ({
     tools: [
       new RequestsGetTool(),
       new RequestsPostTool(),
-      // await AIPluginTool.fromPluginUrl(
-      //   "https://www.klarna.com/.well-known/ai-plugin.json"
-      // ),
+      await AIPluginTool.fromPluginUrl(
+        "https://www.klarna.com/.well-known/ai-plugin.json"
+      ),
     ],
     input: "what t shirts are available in klarna?",
-  },
-  {
+  }),
+  async () => ({
     tools: [
       new SerpAPI(undefined, {
         location: "Austin,Texas,United States",
@@ -52,16 +51,41 @@ const scenarios = [
       new Calculator(),
     ],
     input: `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`,
-  },
-] as { tools: Tool[]; input: string }[];
+  }),
+  async () => ({
+    tools: [
+      new SerpAPI(undefined, {
+        location: "Austin,Texas,United States",
+        hl: "en",
+        gl: "us",
+      }),
+      new Calculator(),
+    ],
+    input: `how is your day going?`,
+  }),
+  async () => ({
+    tools: [
+      new SerpAPI(undefined, {
+        location: "Austin,Texas,United States",
+        hl: "en",
+        gl: "us",
+      }),
+      new Calculator(),
+    ],
+    input: `whats is 9 to the 2nd power?`,
+  }),
+] as (() => Promise<{ tools: Tool[]; input: string }>)[];
 
-describe.each(agents)(
-  `Run agent with %p`,
-  async (initializeAgentExecutorWithTools) => {
-    test.each(scenarios)(`Run agent with %p`, async ({ tools, input }) => {
-      const agent = await initializeAgentExecutorWithTools(tools);
-      const result = await agent.call({ input });
-      console.log({ result });
+describe.each(agents)(`Run agent %#`, (initializeAgentExecutorWithTools) => {
+  test.concurrent.each(scenarios)(`With scenario %#`, async (scenario) => {
+    const agentIndex = agents.indexOf(initializeAgentExecutorWithTools);
+    const scenarioIndex = scenarios.indexOf(scenario);
+    const { tools, input } = await scenario();
+    const agent = await initializeAgentExecutorWithTools(tools);
+    const result = await agent.call({ input });
+    console.log(`Agent #${agentIndex}`, `Scenario #${scenarioIndex}`, {
+      result,
     });
-  }
-);
+    expect(typeof result.output).toBe("string");
+  });
+});
