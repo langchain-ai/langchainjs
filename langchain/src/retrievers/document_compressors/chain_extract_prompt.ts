e21e3af539f7f1ec8501ff92bebb8eafd3e12f53
Antonio Ulloa <joseanu@gmail.com>
2023-04-24 13:18:09 -0600

    Adding contextual compression with an LLMChainExtractor

---
+++ langchain/src/retrievers/document_compressors/chain_extract_prompt.ts
@@ -0,0 +1,12 @@
+export const prompt_template = (
+  no_output_str: string
+) => `Given the following question and context, extract any part of the context *AS IS* that is relevant to answer the question. If none of the context is relevant return ${no_output_str}.
+
+Remember, *DO NOT* edit the extracted parts of the context.
+
+> Question: {question}
+> Context:
+>>>
+{context}
+>>>
+Extracted relevant parts:`;
