9e811b3fd2795927af74270b6cd5fb3dbd499286
Matt Robinson <mrobinson@unstructuredai.io>
2023-04-25 12:52:39 -0400

    use constructor from the directory loader

--- langchain/src/document_loaders/fs/unstructured.ts
+++ langchain/src/document_loaders/fs/unstructured.ts
@@ -1,9 +1,9 @@
 import type { basename as BasenameT } from "node:path";
 import type { readFile as ReaFileT } from "node:fs/promises";
+import { DirectoryLoader, UnknownHandling } from "directory.js";
 import { getEnv } from "../../util/env.js";
 import { Document } from "../../document.js";
 import { BaseDocumentLoader } from "../base.js";
-import { DirectoryLoader } from "directory.js";
 
 interface Element {
   type: string;
@@ -94,64 +94,17 @@ export class UnstructuredLoader extends BaseDocumentLoader {
 }
 
 
-export class UnstructuredLoader extends DirectoryLoader {
-  constructor(public webPath: string, public filePath: string) {
-    super();
-    this.filePath = filePath;
+export class UnstructuredDirectoryLoader extends DirectoryLoader {
+  constructor(
+    public webPath: string,
+    public directoryPath: string,
+    public recursive: boolean = true,
+    public unknown: UnknownHandling = UnknownHandling.Warn,
+  ) {
 
-    this.webPath = webPath;
-  }
-
-  async _partition() {
-    const { readFile, basename } = await this.imports();
-
-    const buffer = await readFile(this.filePath);
-    const fileName = basename(this.filePath);
-
-    // I'm aware this reads the file into memory first, but we have lots of work
-    // to do on then consuming Documents in a streaming fashion anyway, so not
-    // worried about this for now.
-    const formData = new FormData();
-    formData.append("files", new Blob([buffer]), fileName);
-
-    const response = await fetch(this.webPath, {
-      method: "POST",
-      body: formData,
-    });
-
-    if (!response.ok) {
-      throw new Error(
-        `Failed to partition file ${this.filePath} with error ${
-          response.status
-        } and message ${await response.text()}`
-      );
+    const loaders = {
+      ".txt": (p) => new UnstructuredLoader(webPath, p),
     }
-
-    const elements = await response.json();
-    if (!Array.isArray(elements)) {
-      throw new Error(
-        `Expected partitioning request to return an array, but got ${elements}`
-      );
-    }
-    return elements.filter((el) => typeof el.text === "string") as Element[];
+    super(directoryPath, loaders, recursive, unkown);
   }
-
-  async load(): Promise<Document[]> {
-    const elements = await this._partition();
-
-    const documents: Document[] = [];
-    for (const element of elements) {
-      const { metadata, text } = element;
-      documents.push(
-        new Document({
-          pageContent: text,
-          metadata: {
-            ...metadata,
-            category: element.type,
-          },
-        })
-      );
-    }
-
-    return documents;
-  }
+}
