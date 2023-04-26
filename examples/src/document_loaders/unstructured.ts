a4bcbf6a8e29c740a115b36c971bd6af600226ea
Matt Robinson <mrobinson@unstructuredai.io>
2023-04-26 17:49:43 -0400

    add API key to instructions and examples

--- examples/src/document_loaders/unstructured.ts
+++ examples/src/document_loaders/unstructured.ts
@@ -2,6 +2,7 @@ import { UnstructuredLoader } from "langchain/document_loaders/fs/unstructured";
 
 const loader = new UnstructuredLoader(
   "https://api.unstructured.io/general/v0/general",
-  "src/document_loaders/example_data/notion.md"
+  "src/document_loaders/example_data/notion.md",
+  "MY_API_KEY"
 );
 const docs = await loader.load();
--- examples/src/document_loaders/unstructured_directory.ts
+++ examples/src/document_loaders/unstructured_directory.ts
@@ -2,6 +2,7 @@ import { UnstructuredDirectoryLoader } from "langchain/document_loaders/fs/unstr
 
 const loader = new UnstructuredDirectoryLoader(
   "https://api.unstructured.io/general/v0/general",
-  "langchain/src/document_loaders/tests/example_data"
+  "langchain/src/document_loaders/tests/example_data",
+  "MY_API_KEY"
 );
 const docs = await loader.load();
