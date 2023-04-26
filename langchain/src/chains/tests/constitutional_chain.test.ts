e15f47fffaf7374e2b4b1401eea73e7eed6fb0f0
Priya X. Pramesi <ppramesi@gmail.com>
2023-04-26 14:16:35 +0700

    fixed constitutional chain tests

--- langchain/src/chains/tests/constitutional_chain.test.ts
+++ langchain/src/chains/tests/constitutional_chain.test.ts
@@ -1,12 +1,34 @@
-import { test } from "@jest/globals";
+import { test, expect } from "@jest/globals";
 import { ConstitutionalChain } from "../constitutional_ai/constitutional_chain.js";
 import { ConstitutionalPrinciple } from "../constitutional_ai/constitutional_principle.js";
 import { LLMChain } from "../llm_chain.js";
 import { PromptTemplate } from "../../prompts/index.js";
-import { OpenAI } from "../../llms/openai.js";
+import { BaseLLM } from "../../llms/base.js";
+import { LLMResult } from "../../schema/index.js";
+
+class FakeLLM extends BaseLLM {
+  nrMapCalls = 0;
+
+  nrReduceCalls = 0;
+
+  _llmType(): string {
+    return "fake";
+  }
+
+  async _generate(prompts: string[], _?: string[]): Promise<LLMResult> {
+    return {
+      generations: prompts.map((prompt) => [
+          {
+            text: prompt,
+            score: 0,
+          },
+        ]),
+    };
+  }
+}
 
 test("Test ConstitutionalChain", async () => {
-  const llm = new OpenAI();
+  const llm = new FakeLLM({});
   const qaPrompt = new PromptTemplate({
     template: "Q: {question} A:",
     inputVariables: ["question"],
@@ -17,18 +39,22 @@ test("Test ConstitutionalChain", async () => {
     prompt: qaPrompt,
   });
 
+  const critiqueWord = "Tell me if this answer is good.";
+  const revisionWord = "Give a better answer.";
+
   const constitutionalChain = ConstitutionalChain.fromLLM(llm, {
     chain: qaChain,
     constitutionalPrinciples: [
       new ConstitutionalPrinciple({
-        critiqueRequest: "Tell me if this answer is good.",
-        revisionRequest: "Give a better answer.",
+        critiqueRequest: critiqueWord,
+        revisionRequest: revisionWord,
       }),
     ],
   });
 
-  const res = await constitutionalChain.call({
+  const { output } = await constitutionalChain.call({
     question: "What is the meaning of life?",
   });
-  console.log({ res });
+  expect(output).toContain(critiqueWord);
+  expect(output).toContain(revisionWord);
 });
