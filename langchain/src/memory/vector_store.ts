ad5dfbfdde0a53014c5e740495ffa858b061233c
Nuno Campos <nuno@boringbits.io>
2023-05-01 09:58:12 +0100

    Revise implementation to match py, add test

---
+++ langchain/src/memory/vector_store.ts
@@ -0,0 +1,63 @@
+import { Document } from "../document.js";
+import { VectorStoreRetriever } from "../vectorstores/base.js";
+import {
+  BaseMemory,
+  getInputValue,
+  InputValues,
+  MemoryVariables,
+  OutputValues,
+} from "./base.js";
+
+interface VectorStoreMemoryParams {
+  vectorStoreRetriever: VectorStoreRetriever;
+  inputKey?: string;
+  outputKey?: string;
+  memoryKey?: string;
+  returnDocs?: boolean;
+}
+
+export class VectorStoreRetrieverMemory extends BaseMemory {
+  vectorStoreRetriever: VectorStoreRetriever;
+
+  inputKey?: string;
+
+  memoryKey: string;
+
+  returnDocs: boolean;
+
+  constructor(fields: VectorStoreMemoryParams) {
+    super();
+    this.vectorStoreRetriever = fields.vectorStoreRetriever;
+    this.inputKey = fields.inputKey;
+    this.memoryKey = fields.memoryKey ?? "memory";
+    this.returnDocs = fields.returnDocs ?? false;
+  }
+
+  get memoryKeys(): string[] {
+    return [this.memoryKey];
+  }
+
+  async loadMemoryVariables(values: InputValues): Promise<MemoryVariables> {
+    const query = getInputValue(values, this.inputKey);
+    const results = await this.vectorStoreRetriever.getRelevantDocuments(query);
+    return {
+      [this.memoryKey]: this.returnDocs
+        ? results
+        : results.map((r) => r.pageContent).join("\n"),
+    };
+  }
+
+  async saveContext(
+    inputValues: InputValues,
+    outputValues: OutputValues
+  ): Promise<void> {
+    const text = Object.entries(inputValues)
+      .filter(([k]) => k !== this.memoryKey)
+      .concat(Object.entries(outputValues))
+      .map(([k, v]) => `${k}: ${v}`)
+      .join("\n");
+    await this.vectorStoreRetriever.addDocuments([
+      new Document({ pageContent: text }),
+    ]);
+  }
+}
