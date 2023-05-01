497be517ced6835aaa8e00e4e43aedc4f5b6090f
Nuno Campos <nuno@boringbits.io>
2023-05-01 14:23:40 +0100

    Lint

--- langchain/src/agents/tests/chat_agent.int.test.ts
+++ langchain/src/agents/tests/chat_agent.int.test.ts
@@ -1,11 +1,9 @@
 /* eslint-disable no-process-env */
 import { expect, test } from "@jest/globals";
 import { ChatOpenAI } from "../../chat_models/openai.js";
-import { OpenAIEmbeddings } from "../../embeddings/openai.js";
 import { SerpAPI } from "../../tools/serpapi.js";
 import { Calculator } from "../../tools/calculator.js";
 import { initializeAgentExecutorWithOptions } from "../initialize.js";
-import { WebBrowser } from "../../tools/webbrowser.js";
 import { HumanChatMessage } from "../../schema/index.js";
 import { RequestsGetTool, RequestsPostTool } from "../../tools/requests.js";
 import { AIPluginTool } from "../../tools/aiplugin.js";
@@ -84,33 +82,4 @@ test("Run agent with incorrect api key should throw error", async () => {
   await expect(() => executor.call({ input })).rejects.toThrowError(
     "Request failed with status code 401"
   );
-});
-
-test.skip("Run tool web-browser", async () => {
-  const model = new ChatOpenAI({ temperature: 0 });
-  const tools = [
-    new SerpAPI(process.env.SERPAPI_API_KEY, {
-      location: "Austin,Texas,United States",
-      hl: "en",
-      gl: "us",
-    }),
-    new Calculator(),
-    new WebBrowser({ model, embeddings: new OpenAIEmbeddings() }),
-  ];
-
-  const executor = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: "chat-zero-shot-react-description",
-    returnIntermediateSteps: true,
-  });
-
-  const input = `What is the word of the day on merriam webster`;
-  console.log(`Executing with input "${input}"...`);
-
-  const result = await executor.call({ input });
-
-  console.log(`Got output ${result.output}`);
-
-  expect(result.intermediateSteps.length).toEqual(1);
-  expect(result.intermediateSteps[0].action.tool).toEqual("web-browser");
-  expect(result.output).not.toEqual("");
 });
