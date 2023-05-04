8783a6aff4921b793fcfdf4e317f3efb2ea2cc40
Matt Robinson <mrobinson@unstructuredai.io>
2023-05-04 15:59:53 -0400

    fix examples

--- examples/src/document_loaders/unstructured_directory.ts
+++ examples/src/document_loaders/unstructured_directory.ts
@@ -6,7 +6,6 @@ const options = {
 
 const loader = new UnstructuredDirectoryLoader(
   "langchain/src/document_loaders/tests/example_data",
-  "https://api.unstructured.io/general/v0/general",
   options
 );
 const docs = await loader.load();
