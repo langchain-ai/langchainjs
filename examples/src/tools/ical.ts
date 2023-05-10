dfb9f3cc6ae20bb4c8d4a0fc6f03260b3a1d3e4e
rpidanny <abhishekmaharjan1993@gmail.com>
2023-05-10 04:54:25 +0200

    fix: formatting

--- examples/src/tools/ical.ts
+++ examples/src/tools/ical.ts
@@ -1,17 +1,17 @@
 import { ICalTool } from "langchain/tools/ical";
 import { OpenAIChat } from "langchain/llms/openai";
-import { NodeFileStore } from 'langchain/stores/file/node';
+import { NodeFileStore } from "langchain/stores/file/node";
 
 export async function run() {
-  const outputDir = './output/events';
+  const outputDir = "./output/events";
 
   const iCalTool = new ICalTool({
     store: new NodeFileStore(outputDir),
-    llm:  new OpenAIChat({ temperature: 0 }),
+    llm: new OpenAIChat({ temperature: 0 }),
   });
 
   const result = await iCalTool.call(
-    'Create a 1 week running plan starting on May 10, 2023 to prepare for 10k run.'
+    "Create a 1 week running plan starting on May 10, 2023 to prepare for 10k run."
   );
 
   console.log(result);
