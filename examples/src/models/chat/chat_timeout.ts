bf6337f88ea54a77ca8ac7a0e02d9dd3eebf271b
Nuno Campos <nuno@boringbits.io>
2023-05-11 09:53:22 +0100

    Update docs for chat models

--- examples/src/models/chat/chat_timeout.ts
+++ examples/src/models/chat/chat_timeout.ts
@@ -2,15 +2,16 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage } from "langchain/schema";
 
 export const run = async () => {
-  const chat = new ChatOpenAI(
-    { temperature: 1, timeout: 1000 } // 1s timeout
-  );
+  const chat = new ChatOpenAI({ temperature: 1 });
 
-  const response = await chat.call([
-    new HumanChatMessage(
-      "What is a good name for a company that makes colorful socks?"
-    ),
-  ]);
+  const response = await chat.call(
+    [
+      new HumanChatMessage(
+        "What is a good name for a company that makes colorful socks?"
+      ),
+    ],
+    { timeout: 1000 } // 1s timeout
+  );
   console.log(response);
   // AIChatMessage { text: '\n\nRainbow Sox Co.' }
 };
