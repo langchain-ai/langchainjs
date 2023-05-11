679f73fe4908864e0527b64cd3029d0c3d3b511f
Jeremy <jeremy@fabrikapp.fr>
2023-05-11 23:01:24 +0200

    (chore) rename vars & lint

--- examples/src/document_loaders/pdf.ts
+++ examples/src/document_loaders/pdf.ts
@@ -1,11 +1,9 @@
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
 export const run = async () => {
-  const loader = new PDFLoader(
-    "src/document_loaders/example_data/bitcoin.pdf"
-  );
+  const loader = new PDFLoader("src/document_loaders/example_data/bitcoin.pdf");
 
-  const document = await loader.load();
+  const docs = await loader.load();
 
-  console.log({ document });
+  console.log({ docs });
 };
