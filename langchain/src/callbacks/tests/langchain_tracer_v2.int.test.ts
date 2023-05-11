b77108973949d400ba67c5f468b6b6caca01d34d
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-05-10 22:42:56 -0700

    Add Chat Message Serialization

--- langchain/src/callbacks/tests/langchain_tracer_v2.int.test.ts
+++ langchain/src/callbacks/tests/langchain_tracer_v2.int.test.ts
@@ -3,6 +3,7 @@ import * as uuid from "uuid";
 import { test } from "@jest/globals";
 
 import { LangChainTracerV2 } from "../handlers/tracers.js";
+import { HumanChatMessage } from "../../schema/index.js";
 
 test("Test LangChain V2 tracer", async () => {
   const tracer = new LangChainTracerV2();
@@ -10,10 +11,17 @@ test("Test LangChain V2 tracer", async () => {
   const chainRunId = uuid.v4();
   const toolRunId = uuid.v4();
   const llmRunId = uuid.v4();
+  const chatRunId = uuid.v4();
   await tracer.handleChainStart({ name: "test" }, { foo: "bar" }, chainRunId);
   await tracer.handleToolStart({ name: "test" }, "test", toolRunId, chainRunId);
   await tracer.handleLLMStart({ name: "test" }, ["test"], llmRunId, toolRunId);
   await tracer.handleLLMEnd({ generations: [[]] }, llmRunId);
+  await tracer.handleChatModelStart(
+    { name: "testChatModel" },
+    [[new HumanChatMessage("I'm a human.")]],
+    chatRunId
+  );
+  await tracer.handleLLMEnd({ generations: [[]] }, chatRunId);
   await tracer.handleToolEnd("output", toolRunId);
   const llmRunId2 = uuid.v4();
   await tracer.handleLLMStart(
