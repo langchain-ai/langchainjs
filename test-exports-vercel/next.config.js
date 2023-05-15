a4f942ae34d65947517a45183700cf887afdacd2
Tat Dat Duong <david@duong.cz>
2023-05-15 23:27:19 +0200

    Remove WASM configuration changes

--- test-exports-vercel/next.config.js
+++ test-exports-vercel/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
