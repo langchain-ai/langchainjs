6c98a40aab0ca0d27bdbeb81359b0862f6e37134
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-05-15 09:33:15 -0700

    Add Environment Info to Run Logs

--- langchain/src/util/env.ts
+++ langchain/src/util/env.ts
@@ -5,6 +5,11 @@ import {
   isJsDom,
   isDeno,
 } from "browser-or-node";
+import { platform } from "os";
+import { fileURLToPath } from "url";
+import { dirname, join as joinPath } from "path";
+import { version as nodeVersion } from "process";
+import fs from "fs";
 
 export const getEnv = () => {
   let env: string;
@@ -24,3 +29,54 @@ export const getEnv = () => {
 
   return env;
 };
+
+async function getVersion(): Promise<string> {
+  if (isNode || isDeno) {
+    const fileName = fileURLToPath(import.meta.url);
+    const dirName = dirname(fileName);
+    const rawPackageJson = await fs.promises.readFile(
+      joinPath(dirName, "../../package.json"),
+      "utf-8"
+    );
+    const packageJson = JSON.parse(rawPackageJson);
+    return packageJson.version;
+  } else {
+    // TODO: Implement for other environments
+    return "unknown";
+  }
+}
+
+export type RuntimeEnvironment = {
+  library: string;
+  libraryVersion: string;
+  platform: string;
+  runtime: string;
+  runtimeVersion: string;
+};
+
+let runtimeEnvironment: RuntimeEnvironment | undefined;
+
+export async function getRuntimeEnvironment(): Promise<RuntimeEnvironment> {
+  if (runtimeEnvironment === undefined) {
+    const langchainVersion = await getVersion();
+    const env = getEnv();
+    let runtimeVersion: string;
+
+    switch (env) {
+      case "node":
+        runtimeVersion = nodeVersion;
+        break;
+      default:
+        runtimeVersion = "unknown";
+    }
+
+    runtimeEnvironment = {
+      library: "langchain-js",
+      libraryVersion: langchainVersion,
+      platform: platform(),
+      runtime: env,
+      runtimeVersion,
+    };
+  }
+  return runtimeEnvironment;
+}
