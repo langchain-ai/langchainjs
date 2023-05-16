63e26fab4a9735b65ee1e8b9ea9d10fce60ab739
Priya X. Pramesi <ppramesi@gmail.com>
2023-05-16 14:49:32 +0700

    removed unused lint disable on self_query/base.ts

--- langchain/src/retrievers/self_query/base.ts
+++ langchain/src/retrievers/self_query/base.ts
@@ -48,7 +48,6 @@ export class SelfQueryRetriever
 
   async getRelevantDocuments(
     query: string
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ): Promise<Document<Record<string, unknown>>[]> {
     const { [this.llmChain.outputKey]: output } = await this.llmChain.call({
       [this.llmChain.inputKeys[0]]: query,
