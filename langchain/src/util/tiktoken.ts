7fbb3b1532e1bfe7542173fa553b70366c7fc0c5
Tat Dat Duong <david@duong.cz>
2023-05-17 14:32:48 +0200

    Upgrade to 1.0.6 for CRA fixes, mark js-tiktoken/lite as an external dependency

--- langchain/src/util/tiktoken.ts
+++ langchain/src/util/tiktoken.ts
@@ -9,15 +9,20 @@ import { AsyncCaller } from "./async_caller.js";
 
 const cache: Record<string, Promise<TiktokenBPE>> = {};
 
-const caller = new AsyncCaller({});
+const caller = /* #__PURE__ */ new AsyncCaller({});
 
 export async function getEncoding(
   encoding: TiktokenEncoding,
-  extendedSpecialTokens?: Record<string, number>
+  options?: {
+    signal?: AbortSignal;
+    extendedSpecialTokens?: Record<string, number>;
+  }
 ) {
   if (!(encoding in cache)) {
     cache[encoding] = caller
-      .fetch(`https://tiktoken.pages.dev/js/${encoding}.json`)
+      .fetch(`https://tiktoken.pages.dev/js/${encoding}.json`, {
+        signal: options?.signal,
+      })
       .then((res) => res.json())
       .catch((e) => {
         delete cache[encoding];
@@ -25,12 +30,15 @@ export async function getEncoding(
       });
   }
 
-  return new Tiktoken(await cache[encoding], extendedSpecialTokens);
+  return new Tiktoken(await cache[encoding], options?.extendedSpecialTokens);
 }
 
 export async function encodingForModel(
   model: TiktokenModel,
-  extendedSpecialTokens?: Record<string, number>
+  options?: {
+    signal?: AbortSignal;
+    extendedSpecialTokens?: Record<string, number>;
+  }
 ) {
-  return getEncoding(getEncodingNameForModel(model), extendedSpecialTokens);
+  return getEncoding(getEncodingNameForModel(model), options);
 }
