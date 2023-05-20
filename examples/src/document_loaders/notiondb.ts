95718062447377712b3630fc990708713ad7a016
Orgil <mu.munkhorgil@gmail.com>
2023-05-20 14:42:31 +0800

    little refactor

--- examples/src/document_loaders/notiondb.ts
+++ examples/src/document_loaders/notiondb.ts
@@ -1,10 +1,9 @@
 import { NotionDBLoader } from "langchain/document_loaders/web/notiondb";
 
-export const run = async () => {
-  const loader = new NotionDBLoader({
-    integrationToken: "NOTION_TOKEN",
-    databaseId: "databaseId",
-  });
-  const docs = await loader.load();
-  console.log({ docs });
-};
+const loader = new NotionDBLoader({
+  integrationToken: "NOTION_TOKEN",
+  databaseId: "databaseId",
+});
+const docs = await loader.load();
+
+console.log({ docs });
