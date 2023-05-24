828e91e78a55a8107297c5b4d92f481c2092959b
Jacob Lee <jacoblee93@gmail.com>
2023-05-24 12:21:26 -0700

    Make Google custom search tool read from environment variables if possible, change param names

--- langchain/src/tools/google_custom_search.ts
+++ langchain/src/tools/google_custom_search.ts
@@ -1,8 +1,8 @@
 import { Tool } from "./base.js";
 
-export interface GoogleSearchParams {
-  apiKey: string;
-  searchEngineId: string;
+export interface GoogleCustomSearchParams {
+  apiKey?: string;
+  googleCSEId?: string;
 }
 
 export class GoogleCustomSearch extends Tool {
@@ -10,21 +10,44 @@ export class GoogleCustomSearch extends Tool {
 
   protected apiKey: string;
 
-  protected searchEngineId: string;
+  protected googleCSEId: string;
 
   description =
-    "a custom search engine. useful for when you need to answer questions about current events. input should be a search query.";
+    "a custom search engine. useful for when you need to answer questions about current events. input should be a search query. outputs a JSON array of results.";
 
-  constructor({ apiKey, searchEngineId }: GoogleSearchParams) {
+  constructor(
+    fields: GoogleCustomSearchParams = {
+      apiKey:
+        typeof process !== "undefined"
+          ? // eslint-disable-next-line no-process-env
+            process.env.GOOGLE_API_KEY
+          : undefined,
+      googleCSEId:
+        typeof process !== "undefined"
+          ? // eslint-disable-next-line no-process-env
+            process.env.GOOGLE_CSE_ID
+          : undefined,
+    }
+  ) {
     super();
-    this.apiKey = apiKey;
-    this.searchEngineId = searchEngineId;
+    if (!fields.apiKey) {
+      throw new Error(
+        `Google API key not set. You can set it as "GOOGLE_API_KEY" in your environment variables.`
+      );
+    }
+    if (!fields.googleCSEId) {
+      throw new Error(
+        `Google custom search engine id not set. You can set it as "GOOGLE_CSE_ID" in your environment variables.`
+      );
+    }
+    this.apiKey = fields.apiKey;
+    this.googleCSEId = fields.googleCSEId;
   }
 
   async _call(input: string) {
     const res = await fetch(
       `https://www.googleapis.com/customsearch/v1?key=${this.apiKey}&cx=${
-        this.searchEngineId
+        this.googleCSEId
       }&q=${encodeURIComponent(input)}`
     );
 
--- langchain/src/tools/tests/google_custom_search.int.test.ts
+++ langchain/src/tools/tests/google_custom_search.int.test.ts
@@ -1,14 +1,8 @@
-/* eslint-disable no-process-env */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
 import { test } from "@jest/globals";
 import { GoogleCustomSearch } from "../google_custom_search.js";
 
 test("GoogleCustomSearchTool", async () => {
-  const tool = new GoogleCustomSearch({
-    apiKey: process.env.GOOGLE_CUSTOM_SEARCH_API_KEY!,
-    searchEngineId: process.env.GOOGLE_CUSTOM_SEARCH_ENGINE_ID!,
-  });
+  const tool = new GoogleCustomSearch();
 
   const result = await tool.call("What is Langchain?");
 
