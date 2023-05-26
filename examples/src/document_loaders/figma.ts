b4a1a9dcfd595b09046b4cb34bb7dc8b52b74dba
Orgil <mu.munkhorgil@gmail.com>
2023-05-26 12:56:16 +0800

    update figma example and int test

--- examples/src/document_loaders/figma.ts
+++ examples/src/document_loaders/figma.ts
@@ -1,7 +1,7 @@
 import { FigmaFileLoader } from "langchain/document_loaders/web/figma";
 
 const loader = new FigmaFileLoader({
-  accessToken: "FIGMA_ACCESS_TOKEN",
+  accessToken: "FIGMA_ACCESS_TOKEN", // or load it from process.env.FIGMA_ACCESS_TOKEN
   ids: "ids",
   key: "key",
 });
