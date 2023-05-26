793950f2338a9be298307e8c5791e89c0f1b93ad
Priya X. Pramesi <ppramesi@gmail.com>
2023-05-26 17:00:21 +0700

    added support for supabase filter that can use more comparators in the form of functional filter

--- langchain/src/vectorstores/tests/supabase.int.test.ts
+++ langchain/src/vectorstores/tests/supabase.int.test.ts
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 
 import { OpenAIEmbeddings } from "../../embeddings/openai.js";
 import { Document } from "../../document.js";
-import { SupabaseVectorStore } from "../supabase.js";
+import { SupabaseVectorStore, SupabaseFilterRPCCall } from "../supabase.js";
 
 test.skip("SupabaseVectorStore with external ids", async () => {
   const client = createClient(
@@ -69,6 +69,74 @@ test.skip("Search a SupabaseVectorStore using a metadata filter", async () => {
     new Document({
       metadata: { created_at: createdAt + 2 },
       pageContent: "hello 2",
+    }),
+  ]);
+});
+
+test.skip("Search a SupabaseVectorStore with a functional metadata filter", async () => {
+  const client = createClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_PRIVATE_KEY!
+  );
+
+  const embeddings = new OpenAIEmbeddings();
+
+  const store = new SupabaseVectorStore(embeddings, {
+    client,
+    tableName: "documents",
+  });
+
+  expect(store).toBeDefined();
+
+  const docs = [
+    {
+      pageContent:
+        "This is a long text, but it actually means something because vector database does not understand Lorem Ipsum. So I would need to expand upon the notion of quantum fluff, a theorectical concept where subatomic particles coalesce to form transient multidimensional spaces. Yet, this abstraction holds no real-world application or comprehensible meaning, reflecting a cosmic puzzle.",
+      metadata: { b: 1, c: 10, stuff: "right" },
+    },
+    {
+      pageContent:
+        "This is a long text, but it actually means something because vector database does not understand Lorem Ipsum. So I would need to proceed by discussing the echo of virtual tweets in the binary corridors of the digital universe. Each tweet, like a pixelated canary, hums in an unseen frequency, a fascinatingly perplexing phenomenon that, while conjuring vivid imagery, lacks any concrete implication or real-world relevance, portraying a paradox of multidimensional spaces in the age of cyber folklore.",
+      metadata: { b: 2, c: 9, stuff: "right" },
+    },
+    { pageContent: "hello", metadata: { b: 1, c: 9, stuff: "right" } },
+    { pageContent: "hello", metadata: { b: 1, c: 9, stuff: "wrong" } },
+    { pageContent: "hi", metadata: { b: 2, c: 8, stuff: "right" } },
+    { pageContent: "bye", metadata: { b: 3, c: 7, stuff: "right" } },
+    { pageContent: "what's this", metadata: { b: 4, c: 6, stuff: "right" } },
+  ];
+
+  await store.addDocuments(docs);
+
+  const funcFilterA: SupabaseFilterRPCCall = (rpc) =>
+    rpc
+      .filter("metadata->b::int", "lt", 3)
+      .filter("metadata->c::int", "gt", 7)
+      .textSearch("content", `'multidimensional' & 'spaces'`, {
+        config: "english",
+      });
+
+  const resultA = await store.similaritySearch("quantum", 4, funcFilterA);
+
+  const gibberish = resultA.map((doc) => doc.pageContent);
+  expect(gibberish).toEqual([docs[0].pageContent, docs[1].pageContent]);
+
+  const funcFilterB: SupabaseFilterRPCCall = (rpc) =>
+    rpc
+      .filter("metadata->b::int", "lt", 3)
+      .filter("metadata->c::int", "gt", 7)
+      .filter("metadata->>stuff", "eq", "right");
+
+  const resultB = await store.similaritySearch("hello", 2, funcFilterB);
+
+  expect(resultB).toEqual([
+    new Document({
+      pageContent: "hello",
+      metadata: { b: 1, c: 9, stuff: "right" },
+    }),
+    new Document({
+      pageContent: "hi",
+      metadata: { b: 2, c: 8, stuff: "right" },
     }),
   ]);
 });
