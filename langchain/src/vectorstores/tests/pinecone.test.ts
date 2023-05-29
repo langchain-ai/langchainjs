d66e67f64643a3cbee504b2ac9ee55b6d46370e6
Yuji Nakayama <nkymyj@gmail.com>
2023-05-29 14:23:18 +0900

    Add test for Pinecone metadata normalization

--- langchain/src/vectorstores/tests/pinecone.test.ts
+++ langchain/src/vectorstores/tests/pinecone.test.ts
@@ -21,10 +21,7 @@ test("PineconeStore with external ids", async () => {
     [
       {
         pageContent: "hello",
-        metadata: {
-          a: 1,
-          b: { nested: [1, { a: 4 }] },
-        },
+        metadata: { a: 1 },
       },
     ],
     ["id1"]
@@ -38,7 +35,7 @@ test("PineconeStore with external ids", async () => {
       vectors: [
         {
           id: "id1",
-          metadata: { a: 1, "b.nested.0": 1, "b.nested.1.a": 4, text: "hello" },
+          metadata: { a: 1, text: "hello" },
           values: [0.1, 0.2, 0.3, 0.4],
         },
       ],
@@ -70,4 +67,61 @@ test("PineconeStore with generated ids", async () => {
   const results = await store.similaritySearch("hello", 1);
 
   expect(results).toHaveLength(0);
+});
+
+test("PineconeStore flattens metadata values and removes null values when adding documents", async () => {
+  const client = { upsert: jest.fn() };
+  const embeddings = new FakeEmbeddings();
+  const store = new PineconeStore(embeddings, { pineconeIndex: client as any });
+
+  const document = {
+    pageContent: "hello",
+    metadata: {
+      string: "some_string",
+      number: 3.14,
+      boolean: true,
+      string_array: ["string_0", "string_1"],
+      null: null,
+      number_array: [0, 1],
+      object: {
+        string_in_nested_object: "some_string",
+        string_array_in_nested_object: ["string_0", "string_1"]
+      },
+    }
+  }
+
+  await store.addDocuments([document], ["some_id"]);
+
+  expect(client.upsert).toHaveBeenCalledTimes(1);
+
+  expect(client.upsert).toHaveBeenLastCalledWith({
+    upsertRequest: {
+      namespace: undefined,
+      vectors: [
+        {
+          id: "some_id",
+          metadata: {
+            string: "some_string",
+            boolean: true,
+            number: 3.14,
+            "string_array.0": "string_0",
+            "string_array.1": "string_1",
+            "number_array.0": 0,
+            "number_array.1": 1,
+            "object.string_in_nested_object": "some_string",
+            "object.string_array_in_nested_object.0": "string_0",
+            "object.string_array_in_nested_object.1": "string_1",
+            // pageContent
+            "text": "hello"
+          },
+          values: [
+            0.1,
+            0.2,
+            0.3,
+            0.4
+          ]
+        }
+      ]
+    }
+  });
 });
