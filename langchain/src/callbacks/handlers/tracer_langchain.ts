7c1b6512356de4075dd5dfa34c5c72687d0075c2
Ankush Gola <ankush.gola@gmail.com>
2023-06-01 16:37:41 -0700

    change tracer to use post/patch

--- langchain/src/callbacks/handlers/tracer_langchain.ts
+++ langchain/src/callbacks/handlers/tracer_langchain.ts
@@ -4,10 +4,20 @@ import {
   getRuntimeEnvironment,
 } from "../../util/env.js";
 import { BaseTracer, Run, BaseRun } from "./tracer.js";
+import { RunOutputs } from "../../schema/index.js";
 
 export interface RunCreate extends BaseRun {
+  parent_run_id?: string; // uuid
   child_runs: this[];
   session_name?: string;
+}
+
+export interface RunUpdate {
+  end_time: number;
+  error?: string;
+  outputs?: RunOutputs;
+  parent_run_id?: string; // uuid
+  reference_example_id?: string; // uuid
 }
 
 export interface LangChainTracerFields {
@@ -70,22 +80,24 @@ export class LangChainTracer
       start_time: run.start_time,
       end_time: run.end_time,
       run_type: run.run_type,
-      reference_example_id: example_id,
+      // example_id is only set for the root run
+      reference_example_id: run.parent_run_id ? undefined : example_id,
       extra: runExtra,
+      parent_run_id: run.parent_run_id,
       execution_order: run.execution_order,
       serialized: run.serialized,
       error: run.error,
       inputs: run.inputs,
       outputs: run.outputs ?? {},
       session_name: this.sessionName,
-      child_runs: await Promise.all(
-        run.child_runs.map((child_run) => this._convertToCreate(child_run))
-      ),
+      child_runs: [],
     };
     return persistedRun;
   }
 
-  protected async persistRun(run: Run): Promise<void> {
+  protected async persistRun(_run: Run): Promise<void> {}
+
+  protected async _persistRunSingle(run: Run): Promise<void> {
     const persistedRun: RunCreate = await this._convertToCreate(
       run,
       this.exampleId
@@ -105,5 +117,66 @@ export class LangChainTracer
         `Failed to persist run: ${response.status} ${response.statusText} ${body}`
       );
     }
+  }
+
+  protected async _updateRunSingle(run: Run): Promise<void> {
+    const runUpdate: RunUpdate = {
+      end_time: run.end_time,
+      error: run.error,
+      outputs: run.outputs,
+      parent_run_id: run.parent_run_id,
+      reference_example_id: run.reference_example_id,
+    };
+    const endpoint = `${this.endpoint}/runs/${run.id}`;
+    const response = await this.caller.call(fetch, endpoint, {
+      method: "PATCH",
+      headers: this.headers,
+      body: JSON.stringify(runUpdate),
+      signal: AbortSignal.timeout(this.timeout),
+    });
+    // consume the response body to release the connection
+    // https://undici.nodejs.org/#/?id=garbage-collection
+    const body = await response.text();
+    if (!response.ok) {
+      throw new Error(
+        `Failed to update run: ${response.status} ${response.statusText} ${body}`
+      );
+    }
+  }
+
+  async onLLMStart(run: Run): Promise<void> {
+    await this._persistRunSingle(run);
+  }
+
+  async onLLMEnd(run: Run): Promise<void> {
+    await this._updateRunSingle(run);
+  }
+
+  async onLLMError(run: Run): Promise<void> {
+    await this._updateRunSingle(run);
+  }
+
+  async onChainStart(run: Run): Promise<void> {
+    await this._persistRunSingle(run);
+  }
+
+  async onChainEnd(run: Run): Promise<void> {
+    await this._updateRunSingle(run);
+  }
+
+  async onChainError(run: Run): Promise<void> {
+    await this._updateRunSingle(run);
+  }
+
+  async onToolStart(run: Run): Promise<void> {
+    await this._persistRunSingle(run);
+  }
+
+  async onToolEnd(run: Run): Promise<void> {
+    await this._updateRunSingle(run);
+  }
+
+  async onToolError(run: Run): Promise<void> {
+    await this._updateRunSingle(run);
   }
 }
