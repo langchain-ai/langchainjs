e148d23615f2da3b89ad0e83cd0308da6f4ba6e5
Yuku Takahashi <taka84u9@gmail.com>
2023-06-02 15:46:52 +0900

    Use official Notion SDK types for type safety in NotionDBLoader
    - Introduced the official SDK from NotionHQ as dev and peer dependencies
    - Replaced custom NotionPage interface with types from the official Notion SDK
    - Updated the request/response handling code to utilize the official SDK types
    - Adjusted error handling to account for different response structures defined by official SDK types
    - Managed response pagination based on the official SDK's data structure

--- langchain/src/document_loaders/web/notiondb.ts
+++ langchain/src/document_loaders/web/notiondb.ts
@@ -1,3 +1,10 @@
+import type {
+  GetPageResponse,
+  ListBlockChildrenResponse,
+  QueryDatabaseResponse,
+  RichTextItemResponse,
+} from "@notionhq/client/build/src/api-endpoints.js";
+
 import { BaseDocumentLoader } from "../base.js";
 import { Document } from "../../document.js";
 import { getEnvironmentVariable } from "../../util/env.js";
@@ -9,24 +16,6 @@ export interface NotionDBLoaderParams {
   notionIntegrationToken?: string;
   notionApiVersion?: string;
   pageSizeLimit?: number;
-}
-
-interface NotionPage {
-  id: string;
-  object: "page";
-  created_time: string;
-  last_edited_time: string;
-  archived: boolean;
-  parent: {
-    type: "database_id" | "page_id";
-    database_id?: string;
-    page_id?: string;
-  };
-  properties: {
-    // @see https://developers.notion.com/reference/retrieve-a-page
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [propertyName: string]: any;
-  };
 }
 
 export class NotionDBLoader
@@ -82,7 +71,7 @@ export class NotionDBLoader
     const url = `${NOTION_BASE_URL}/databases/${this.databaseId}/query`;
 
     const pageIds: string[] = [];
-    const query: { page_size: number; start_cursor?: number } = {
+    const query: { page_size: number; start_cursor?: string } = {
       page_size: this.pageSizeLimit,
     };
     let hasMore = true;
@@ -94,65 +83,67 @@ export class NotionDBLoader
         headers: this.headers,
       });
 
-      const data = await response.json();
-
       if (!response.ok) {
         throw new Error(
           `Failed to load data from Notion. Please check your integration token and database id.`
         );
       }
 
-      const { results, has_more, next_cursor } = data;
+      const { results, next_cursor, has_more } =
+        (await response.json()) as QueryDatabaseResponse;
 
-      pageIds.push(...(results?.map((page: NotionPage) => page.id) ?? []));
+      pageIds.push(...results.map((page) => page.id));
       hasMore = has_more;
-      query.start_cursor = next_cursor;
+      query.start_cursor = next_cursor ?? undefined;
     }
 
     return pageIds;
   }
 
   private async loadPage(pageId: string): Promise<Document> {
-    const url = `${NOTION_BASE_URL}/pages/${pageId}`;
-    const response = await fetch(url, { method: "GET", headers: this.headers });
-    const data = await response.json();
+    // Call a `Retrieve page` API
+    // See https://developers.notion.com/reference/retrieve-a-page
+    const response = await fetch(`${NOTION_BASE_URL}/pages/${pageId}`, {
+      method: "GET",
+      headers: this.headers,
+    });
 
     if (!response.ok) {
       throw new Error(
-        `Unable to fetch page: ${response.status} ${JSON.stringify(data)}`
+        `Unable to fetch page: ${response.status} ${await response.text()}`
+      );
+    }
+
+    const data = (await response.json()) as GetPageResponse;
+
+    // The official Notion SDK declares the response type of `Retrieve page` API
+    // as `GetPageResponse`, which is a union of `PageObjectResponse` and
+    // `PartialPageObjectResponse`. However, it is not explained in the
+    // documentation when the partial response is returned.
+    if (!("properties" in data)) {
+      throw new Error(
+        `PartialPageObjectResponse is returned: ${response.status}`
       );
     }
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const metadata: Record<string, any> = {};
-    const { properties } = data;
 
-    for (const key of Object.keys(properties)) {
-      const item = properties[key];
-      const itemType = item.type;
+    for (const [key, item] of Object.entries(data.properties)) {
       let value;
 
-      switch (itemType) {
+      switch (item.type) {
         case "rich_text":
-          value =
-            item?.rich_text && item?.rich_text.length > 0
-              ? item?.rich_text[0].plain_text
-              : null;
+          value = item.rich_text[0]?.plain_text ?? null;
           break;
         case "title":
-          value =
-            item?.title && item?.title.length > 0
-              ? item?.title[0].plain_text
-              : null;
+          value = item.title[0]?.plain_text ?? null;
           break;
         case "multi_select":
-          if (item?.multi_select && item?.multi_select.length > 0) {
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            value = item?.multi_select.map((el: any) => el.name);
-          }
+          value = item.multi_select.map((el) => el.name);
           break;
         case "url":
-          value = item?.url ? item.url : null;
+          value = item.url;
           break;
         default:
           break;
@@ -173,9 +164,11 @@ export class NotionDBLoader
 
   private async loadBlocks(blockId: string, numberOfTabs = 0): Promise<string> {
     const resultLinesArr = [];
-    let currentBlockId = blockId;
+    let currentBlockId: string | null = blockId;
 
     while (currentBlockId) {
+      // Call a `Retrieve block children` API
+      // See https://developers.notion.com/reference/get-block-children
       const response = await fetch(
         `${NOTION_BASE_URL}/blocks/${currentBlockId}/children`,
         {
@@ -183,25 +176,35 @@ export class NotionDBLoader
           headers: this.headers,
         }
       );
-      const data = await response.json();
 
       if (!response.ok) {
         throw new Error(
-          `Unable to fetch block: ${response.status} ${JSON.stringify(data)}`
+          `Unable to fetch block: ${response.status} ${await response.text()}`
         );
       }
 
-      for (const result of data.results) {
-        const resultObj = result[result.type];
+      const data = (await response.json()) as ListBlockChildrenResponse;
 
-        if (!resultObj.rich_text) {
+      for (const result of data.results) {
+        // `result` is PartialBlockObjectResponse or BlockObjectResponse as SDK
+        // declares. However, it is also not explained in the documentation when
+        // the partial response is returned.
+        if (!("type" in result)) {
           continue;
         }
 
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const resultObj = (result as any)[result.type];
+
+        if (resultObj?.rich_text === undefined) {
+          continue;
+        }
+        const richTexts = resultObj.rich_text as RichTextItemResponse[];
+
         const curResultTextArr = [];
 
-        for (const richText of resultObj.rich_text) {
-          if (richText.text) {
+        for (const richText of richTexts) {
+          if ("text" in richText) {
             curResultTextArr.push(
               "\t".repeat(numberOfTabs) + richText.text.content
             );
