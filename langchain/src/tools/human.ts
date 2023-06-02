5e8c92a4c8fcd507a2a9e3001d51fd10ab2e035a
Andrew Lei <andrewlei@gmail.com>
2023-06-02 01:33:24 -0700

    Add human as a tool

--- langchain/src/tools/human.ts
+++ langchain/src/tools/human.ts
@@ -21,7 +21,7 @@ export class HumanTool extends DynamicTool {
     super({
       verbose: input.verbose,
       callbacks: input.callbacks,
-      name: "human",
+      name: "Human",
       description:
         "You can ask a human for guidance when you think you got stuck or you are not sure what to do next. The input should be a question for the human.",
       func: input.func ?? (async (input: string) => input),
