20a8e66a727c1bdc3eb0488ad022f829c577d8f8
Andrew Lei <andrewlei@gmail.com>
2023-06-02 01:30:23 -0700

    Add human as a tool

--- langchain/src/tools/tests/human.int.test.ts
+++ langchain/src/tools/tests/human.int.test.ts
@@ -1,35 +1,37 @@
-import { test } from "@jest/globals";
+import { describe, test } from "@jest/globals";
 import { HumanTool } from "../human.js";
 import { ChatOpenAI } from "../../chat_models/openai.js";
 import { initializeAgentExecutorWithOptions } from "../../agents/initialize.js";
 
-test("HumanTool", async () => {
-  const tool = new HumanTool();
+describe("Human as a tool test suite", () => {
+  test("HumanTool call", async () => {
+    const tool = new HumanTool();
 
-  const result = await tool.call("I need help attributing a quote");
+    const result = await tool.call("I need help attributing a quote");
 
-  console.log({ result });
+    console.log({ result });
 
-  expect(result).toEqual("I need help attributing a quote");
-});
-
-test("Run in an agent", async () => {
-  const model = new ChatOpenAI({ temperature: 0 });
-  const tools = [new HumanTool()];
-
-  const executor = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: "chat-zero-shot-react-description",
-    verbose: true,
+    expect(result).toEqual("I need help attributing a quote");
   });
 
-  const input = `I need help attributing a quote`;
-  console.log(`Executing with input "${input}"...`);
+  test("Run in an agent", async () => {
+    const model = new ChatOpenAI({ temperature: 0 });
+    const tools = [new HumanTool()];
 
-  const result = await executor.call({ input });
+    const executor = await initializeAgentExecutorWithOptions(tools, model, {
+      agentType: "chat-zero-shot-react-description",
+      verbose: true,
+    });
 
-  console.log(`Got output ${result.output}`);
+    const input = `I need help attributing a quote`;
+    console.log(`Executing with input "${input}"...`);
 
-  expect(result.output).toContain("what is the quote");
-  expect(result.output).toContain("attribute");
-  expect(result.output).toContain("?");
+    const result = await executor.call({ input });
+
+    console.log(`Got output ${result.output}`);
+
+    expect(result.output).toContain("what is the quote");
+    expect(result.output).toContain("attribute");
+    expect(result.output).toContain("?");
+  });
 });
