c0f4527c98cac47d49c6794d8c604fad175a41fe
jacoblee93 <jacoblee93@gmail.com>
2023-06-03 08:36:18 -0700

    Adds pipeline prompt

---
+++ langchain/src/prompts/pipeline.ts
@@ -0,0 +1,117 @@
+import { InputValues, PartialValues } from "../schema/index.js";
+import { BasePromptTemplate, BasePromptTemplateInput } from "./base.js";
+import { ChatPromptTemplate } from "./chat.js";
+import { SerializedBasePromptTemplate } from "./serde.js";
+
+export type PipelinePromptParams<T extends BasePromptTemplate> = {
+  name: string;
+  prompt: T;
+};
+
+export type PipelinePromptTemplateInput<T extends BasePromptTemplate> = Omit<
+  BasePromptTemplateInput,
+  "inputVariables"
+> & {
+  pipelinePrompts: PipelinePromptParams<T>[];
+  finalPrompt: T;
+};
+
+export class PipelinePromptTemplate<
+  T extends BasePromptTemplate
+> extends BasePromptTemplate {
+  pipelinePrompts: PipelinePromptParams<T>[];
+
+  finalPrompt: T;
+
+  constructor(input: PipelinePromptTemplateInput<T>) {
+    super({ ...input, inputVariables: [] });
+    this.pipelinePrompts = input.pipelinePrompts;
+    this.finalPrompt = input.finalPrompt as T;
+    this.inputVariables = this.computeInputValues();
+  }
+
+  protected computeInputValues() {
+    const intermediateValues = this.pipelinePrompts.map(
+      (pipelinePrompt) => pipelinePrompt.name
+    );
+    const inputValues = this.pipelinePrompts
+      .map((pipelinePrompt) =>
+        pipelinePrompt.prompt.inputVariables.filter(
+          (inputValue) => !intermediateValues.includes(inputValue)
+        )
+      )
+      .flat();
+    return [...new Set(inputValues)];
+  }
+
+  protected static extractRequiredInputValues(
+    allValues: InputValues,
+    requiredValueNames: string[]
+  ) {
+    return requiredValueNames.reduce((requiredValues, valueName) => {
+      // eslint-disable-next-line no-param-reassign
+      requiredValues[valueName] = allValues[valueName];
+      return requiredValues;
+    }, {} as InputValues);
+  }
+
+  protected async formatPipelinePrompts(
+    values: InputValues
+  ): Promise<InputValues> {
+    const allValues = await this.mergePartialAndUserVariables(values);
+    for (const { name: pipelinePromptName, prompt: pipelinePrompt } of this
+      .pipelinePrompts) {
+      const pipelinePromptInputValues =
+        PipelinePromptTemplate.extractRequiredInputValues(
+          allValues,
+          pipelinePrompt.inputVariables
+        );
+      // eslint-disable-next-line no-instanceof/no-instanceof
+      if (pipelinePrompt instanceof ChatPromptTemplate) {
+        allValues[pipelinePromptName] = await pipelinePrompt.formatMessages(
+          pipelinePromptInputValues
+        );
+      } else {
+        allValues[pipelinePromptName] = await pipelinePrompt.format(
+          pipelinePromptInputValues
+        );
+      }
+    }
+    return PipelinePromptTemplate.extractRequiredInputValues(
+      allValues,
+      this.finalPrompt.inputVariables
+    );
+  }
+
+  async formatPromptValue(
+    values: InputValues
+  ): Promise<T["PromptValueReturnType"]> {
+    return this.finalPrompt.formatPromptValue(
+      await this.formatPipelinePrompts(values)
+    );
+  }
+
+  async format(values: InputValues): Promise<string> {
+    return this.finalPrompt.format(await this.formatPipelinePrompts(values));
+  }
+
+  async partial(values: PartialValues): Promise<PipelinePromptTemplate<T>> {
+    const promptDict = { ...this };
+    promptDict.inputVariables = this.inputVariables.filter(
+      (iv) => !(iv in values)
+    );
+    promptDict.partialVariables = {
+      ...(this.partialVariables ?? {}),
+      ...values,
+    };
+    return new PipelinePromptTemplate<T>(promptDict);
+  }
+
+  serialize(): SerializedBasePromptTemplate {
+    throw new Error("Not implemented.");
+  }
+
+  _getPromptType(): string {
+    return "pipeline";
+  }
+}
