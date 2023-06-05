a78e1720adc57416548ba6a705a0b6117cfd1972
Priya X. Pramesi <ppramesi@gmail.com>
2023-06-05 14:55:19 +0700

    added supabase self query

--- langchain/src/retrievers/self_query/index.ts
+++ langchain/src/retrievers/self_query/index.ts
@@ -7,9 +7,10 @@ import { StructuredQuery } from "../../chains/query_constructor/ir.js";
 import { Document } from "../../document.js";
 import { BaseRetriever } from "../../schema/index.js";
 import { VectorStore } from "../../vectorstores/base.js";
+import { SupabaseTranslator } from "./supabase_translator.js";
 import { BaseTranslator, BasicTranslator } from "./translator.js";
 
-export { BaseTranslator, BasicTranslator };
+export { BaseTranslator, BasicTranslator, SupabaseTranslator };
 
 export type SelfQueryRetrieverArgs = {
   vectorStore: VectorStore;
