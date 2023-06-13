d414094a410052e83b479a0d7a9b55ef589804bc
aamir <aamir@zir-ai.com>
2023-06-13 21:38:43 +0500

    Added hash based id generation.

--- langchain/src/vectorstores/tests/vectara.int.test.ts
+++ langchain/src/vectorstores/tests/vectara.int.test.ts
@@ -3,33 +3,42 @@ import { test, expect } from "@jest/globals";
 import { VectaraStore } from "../vectara.js";
 import { OpenAIEmbeddings } from "../../embeddings/openai.js";
 import { Document } from "../../document.js";
-import { v4 } from "uuid";
+
+const hashCode = (s: string) => {
+  return s.split("").reduce((a, b) => {
+    a = ((a << 5) - a + b.charCodeAt(0)) | 0;
+    return a;
+  }, 0);
+};
 
 test.skip("Vectara Add Documents", async () => {
   const store = new VectaraStore(new OpenAIEmbeddings(), {
     customer_id: Number(process.env.VECTARA_CUSTOMER_ID) || 0,
     corpus_id: Number(process.env.VECTARA_CORPUS_ID) || 0,
     api_key: process.env.VECTARA_API_KEY || ""
-  }
-  );
+  });
+
+  // Some text from Lord of the Rings
+  const firstText = "It all depends on what you want. You can trust us to stick to you through thick and thin to the bitter end. And you can trust us to keep any secret of yours – closer than you keep it yourself. But you cannot trust us to let you face trouble alone, and go off without a word. We are your friends, Frodo. Anyway: there it is. We know most of what Gandalf has told you. We know a good deal about the Ring. We are horribly afraid – but we are coming with you; or following you like hounds.";
+  const secondText = "Sam lay back, and stared with open mouth, and for a moment, between bewilderment and great joy, he could not answer. At last he gasped: “Gandalf! I thought you were dead! But then I thought I was dead myself. Is everything sad going to come untrue? What’s happened to the world?";
 
   const documents = [
     new Document({
-      pageContent: "This is a sample text from langchain unit test",
+      pageContent: firstText,
       metadata: {
-        document_id: v4(), // Generate a random document id
-        title: "First Title",
-        author: "Judd Trump",
+        document_id: hashCode(firstText), // Generate a hashcode for document id based on the text
+        title: "Lord of the Rings",
+        author: "Tokien",
         genre: "fiction"
       },
     }),
     new Document({
-      pageContent: "This is some more text from langchain unit test",
+      pageContent: secondText,
       metadata: {
-        document_id: v4(), // Generate a random document id
-        title: "Second Title",
-        author: "Ronnie O'Sullivan",
-        genre: "snooker"
+        document_id: hashCode(secondText), // Generate a hashcode for document id based on the text
+        title: "Lord of the Rings",
+        author: "Tolkien",
+        genre: "fiction"
       },
     })
   ];
@@ -39,12 +48,12 @@ test.skip("Vectara Add Documents", async () => {
   expect(code).toEqual(200);
   expect(detail).toEqual(`Successfully added ${documents.length} documents to Vectara`);
 
-  const resultsWithScore = await store.similaritySearchWithScore("test", 1);
+  const resultsWithScore = await store.similaritySearchWithScore("What did Sam do?", 1);
   expect(resultsWithScore.length).toBeGreaterThan(0);
   expect(resultsWithScore[0][0].pageContent.length).toBeGreaterThan(0);
   expect(resultsWithScore[0][0].metadata.length).toBeGreaterThan(0);
 
-  const results = await store.similaritySearch("test", 1);
+  const results = await store.similaritySearch("Was Gandalf dead?", 1);
   expect(results.length).toBeGreaterThan(0);
   expect(results[0].pageContent.length).toBeGreaterThan(0);
   expect(results[0].metadata.length).toBeGreaterThan(0);
