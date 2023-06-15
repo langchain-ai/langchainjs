5964f97919a2acc51c234ca4f570dae6482657cf
vowelparrot <130414180+vowelparrot@users.noreply.github.com>
2023-06-15 12:49:26 -0700

    add funny /*pure*/ comments

--- langchain/src/evaluation/qa/prompt.ts
+++ langchain/src/evaluation/qa/prompt.ts
@@ -15,7 +15,7 @@ QUESTION: {query}
 STUDENT ANSWER: {result}
 TRUE ANSWER: {answer}
 GRADE:`;
-export const QA_PROMPT = new PromptTemplate({
+export const QA_PROMPT = /* #__PURE__ */ new PromptTemplate({
   inputVariables: ["query", "result", "answer"],
   template: QA_TEMPLATE,
 });
@@ -32,7 +32,7 @@ const SQL_TEMPLATE = `You are comparing a submitted answer to an expert answer o
 [END DATA]
 Compare the content and correctness of the submitted SQL with the expert answer. Ignore any differences in whitespace, style, or output column names. The submitted answer may either be correct or incorrect. Determine which case applies. First, explain in detail the similarities or differences between the expert answer and the submission, ignoring superficial aspects such as whitespace, style or output column names. Do not state the final answer in your initial explanation. Then, respond with either "CORRECT" or "INCORRECT" (without quotes or punctuation) on its own line. This should correspond to whether the submitted SQL and the expert answer are semantically the same or different, respectively. Then, repeat your final answer on a new line.`;
 
-export const SQL_PROMPT = new PromptTemplate({
+export const SQL_PROMPT = /* #__PURE__ */ new PromptTemplate({
   inputVariables: ["query", "answer", "result"],
   template: SQL_TEMPLATE,
 });
--- langchain/src/evaluation/run_evaluators/criteria_prompt.ts
+++ langchain/src/evaluation/run_evaluators/criteria_prompt.ts
@@ -13,7 +13,7 @@ const CRITERIA_TEMPLATE = `You are assessing a submitted answer on a given task 
 [END DATA]
 Does the submission meet the Criteria? First, write out in a step by step manner your reasoning about the criterion to be sure that your conclusion is correct. Avoid simply stating the correct answers at the outset. Then print only the single character "Y" or "N" (without quotes or punctuation) on its own line corresponding to the correct answer. At the end, repeat just the letter again by itself on a new line.`;
 
-export const CRITERIA_PROMPT = new PromptTemplate({
+export const CRITERIA_PROMPT = /* #__PURE__ */ new PromptTemplate({
   inputVariables: ["input", "output", "criteria"],
   template: CRITERIA_TEMPLATE,
 });
