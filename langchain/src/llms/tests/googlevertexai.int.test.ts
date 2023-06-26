722d8e5ccc772fa4876e37257641f88b3500d097
Allen Firstenberg <prisoner@addventure.com>
2023-06-26 14:20:13 -0400

    Refactor how the code- models are implemented for Google Vertex AI (#1730)

--- langchain/src/llms/tests/googlevertexai.int.test.ts
+++ langchain/src/llms/tests/googlevertexai.int.test.ts
@@ -1,25 +1,25 @@
 import { expect, test } from "@jest/globals";
-import { GoogleVertexAI, GoogleVertexAICode } from "../googlevertexai.js";
+import { GoogleVertexAI } from "../googlevertexai.js";
 
-test.skip("Test Google Vertex", async () => {
+test("Test Google Vertex", async () => {
   const model = new GoogleVertexAI({ maxOutputTokens: 50 });
   const res = await model.call("1 + 1 = ");
   console.log({ res });
 });
 
-test.skip("Test Google Vertex generation", async () => {
+test("Test Google Vertex generation", async () => {
   const model = new GoogleVertexAI({ maxOutputTokens: 50 });
   const res = await model.generate(["1 + 1 = "]);
   console.log(JSON.stringify(res, null, 2));
 });
 
-test.skip("Test Google Vertex generation", async () => {
+test("Test Google Vertex generation", async () => {
   const model = new GoogleVertexAI({ maxOutputTokens: 50 });
   const res = await model.generate(["Print hello world."]);
   console.log(JSON.stringify(res, null, 2));
 });
 
-test.skip("Test Google Vertex generation", async () => {
+test("Test Google Vertex generation", async () => {
   const model = new GoogleVertexAI({ maxOutputTokens: 50 });
   const res = await model.generate([
     `Translate "I love programming" into Korean.`,
@@ -27,16 +27,18 @@ test.skip("Test Google Vertex generation", async () => {
   console.log(JSON.stringify(res, null, 2));
 });
 
-test.skip("Test Google Vertex Codey gecko model", async () => {
-  const model = new GoogleVertexAICode();
+test("Test Google Vertex Codey gecko model", async () => {
+  const model = new GoogleVertexAI({ model: "code-gecko" });
   expect(model.model).toEqual("code-gecko");
+  expect(model.temperature).toEqual(0.2);
+  expect(model.maxOutputTokens).toEqual(256);
 
   const res = await model.call("for( let co = 0");
   console.log(res);
 });
 
-test.skip("Test Google Vertex Codey bison model", async () => {
-  const model = new GoogleVertexAICode({
+test("Test Google Vertex Codey bison model", async () => {
+  const model = new GoogleVertexAI({
     model: "code-bison",
     maxOutputTokens: 2048,
   });
