06e65ecec9682beb00480b09bda6e2124cd1fdca
Clemens Peters <13015002+clemenspeters@users.noreply.github.com>
2023-06-28 10:38:23 +0200

    🏷️ Add 'azureOpenAiBasePath' to 'AzureOpenAIInput'.

--- langchain/src/types/openai-types.ts
+++ langchain/src/types/openai-types.ts
@@ -130,4 +130,11 @@ export declare interface AzureOpenAIInput {
    * this will be used in the endpoint URL: https://{InstanceName}.openai.azure.com/openai/deployments/my-openai-deployment/
    */
   azureOpenAIApiCompletionsDeploymentName?: string;
+
+  /**
+   * Custom endpoint for Azure OpenAI API. This is useful in case you have a deployment in Europe.
+   * e.g. "https://westeurope.api.cognitive.microsoft.com/"
+   * this will be used in the endpoint URL: https://westeurope.api.cognitive.microsoft.com/openai/deployments/{DeploymentName}/
+   */
+  azureOpenAiBasePath?: string;
 }
