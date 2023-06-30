9ade2af4a6e21f8b7759838023e5a506190ad634
jacoblee93 <jacoblee93@gmail.com>
2023-06-30 01:19:35 -0700

    Remove AWS Sfn tool from index file

--- langchain/src/tools/index.ts
+++ langchain/src/tools/index.ts
@@ -38,8 +38,3 @@ export {
 export { AIPluginTool } from "./aiplugin.js";
 export { ReadFileTool, WriteFileTool } from "./fs.js";
 export { BraveSearch, BraveSearchParams } from "./brave_search.js";
-export {
-  StartExecutionAWSSfnTool,
-  DescribeExecutionAWSSfnTool,
-  SendTaskSuccessAWSSfnTool,
-} from "./aws_sfn.js";
