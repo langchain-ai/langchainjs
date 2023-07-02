4c565af393b4e94d48700334f271e6c6de1eedc0
Clemens Peters <13015002+clemenspeters@users.noreply.github.com>
2023-07-02 18:08:15 +0200

    📝 Update documentation on azureOpenAIBasePath.

--- langchain/src/util/azure.ts
+++ langchain/src/util/azure.ts
@@ -19,7 +19,7 @@ export interface OpenAIEndpointConfig {
  * @property {string} config.basePath - The base path URL.
  *
  * The function operates as follows:
- * - If both `azureOpenAIBasePath` and `azureOpenAIApiDeploymentName` are provided, it returns an URL combining these two parameters.
+ * - If both `azureOpenAIBasePath` and `azureOpenAIApiDeploymentName` (plus `azureOpenAIApiKey`) are provided, it returns an URL combining these two parameters (`${azureOpenAIBasePath}/${azureOpenAIApiDeploymentName}`).
  * - If `azureOpenAIApiKey` is provided, it checks for `azureOpenAIApiInstanceName` and `azureOpenAIApiDeploymentName` and throws an error if any of these is missing. If both are provided, it generates an URL incorporating these parameters.
  * - If none of the above conditions are met and `basePath` is not provided, it throws an error.
  * - The function returns the generated URL as a string.
