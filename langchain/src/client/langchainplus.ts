5510b44d91cf6e0532d3755c01372dcd2455878e
Nuno Campos <nuno@boringbits.io>
2023-07-03 18:31:24 +0100

    Add max concurrency to runOnDataset function (#1851)

--- langchain/src/client/langchainplus.ts
+++ langchain/src/client/langchainplus.ts
@@ -1,3 +1,4 @@
+import PQueueMod from "p-queue";
 import { Example, LangChainPlusClient } from "langchainplus-sdk";
 
 import { LangChainTracer } from "../callbacks/handlers/tracer_langchain.js";
@@ -78,128 +79,118 @@ async function getModelOrFactoryType(
 const runLLM = async (
   example: Example,
   tracer: LangChainTracer,
-  llm: BaseLLM,
-  { numRepetitions = 1 }: { numRepetitions?: number }
-): Promise<(LLMResult | string)[]> => {
-  const results: (LLMResult | string)[] = await Promise.all(
-    Array.from({ length: numRepetitions }).map(async () => {
-      try {
-        const prompt = example.inputs.prompt as string;
-        return await llm.generate([prompt], undefined, [tracer]);
-      } catch (e) {
-        console.error(e);
-        return stringifyError(e);
-      }
-    })
-  );
-  return results;
+  llm: BaseLLM
+): Promise<LLMResult | string> => {
+  try {
+    const prompt = example.inputs.prompt as string;
+    return await llm.generate([prompt], undefined, [tracer]);
+  } catch (e) {
+    console.error(e);
+    return stringifyError(e);
+  }
 };
 
 const runChain = async (
   example: Example,
   tracer: LangChainTracer,
-  chainFactory: () => Promise<BaseChain>,
-  {
-    numRepetitions = 1,
-  }: {
-    numRepetitions?: number;
+  chainFactory: () => Promise<BaseChain>
+): Promise<ChainValues | string> => {
+  try {
+    const chain = await chainFactory();
+    return await chain.call(example.inputs, [tracer]);
+  } catch (e) {
+    console.error(e);
+    return stringifyError(e);
   }
-): Promise<(ChainValues | string)[]> => {
-  const results: (ChainValues | string)[] = await Promise.all(
-    Array.from({ length: numRepetitions }).map(async () => {
-      try {
-        const chain = await chainFactory();
-        return await chain.call(example.inputs, [tracer]);
-      } catch (e) {
-        console.error(e);
-        return stringifyError(e);
-      }
-    })
-  );
-  return results;
 };
 
 const runChatModel = async (
   example: Example,
   tracer: LangChainTracer,
-  chatModel: BaseChatModel,
-  {
-    numRepetitions = 1,
-  }: {
-    numRepetitions?: number;
+  chatModel: BaseChatModel
+): Promise<LLMResult | string> => {
+  try {
+    const messages = example.inputs.messages as StoredMessage[];
+    return await chatModel.generate(
+      [mapStoredMessagesToChatMessages(messages)],
+      undefined,
+      [tracer]
+    );
+  } catch (e) {
+    console.error(e);
+    return stringifyError(e);
   }
-): Promise<(LLMResult | string)[]> => {
-  const results: (LLMResult | string)[] = await Promise.all(
-    Array.from({ length: numRepetitions }).map(async () => {
-      try {
-        const messages = example.inputs.messages as StoredMessage[];
-        return await chatModel.generate(
-          [mapStoredMessagesToChatMessages(messages)],
-          undefined,
-          [tracer]
-        );
-      } catch (e) {
-        console.error(e);
-        return stringifyError(e);
-      }
-    })
-  );
-  return results;
 };
 
 export const runOnDataset = async (
   datasetName: string,
   llmOrChainFactory: BaseLanguageModel | (() => Promise<BaseChain>),
   {
+    maxConcurrency = 8,
     numRepetitions = 1,
     projectName,
     client,
   }: {
+    maxConcurrency?: number;
     numRepetitions?: number;
     projectName?: string;
     client?: LangChainPlusClient;
   } = {}
 ): Promise<DatasetRunResults> => {
+  const PQueue = "default" in PQueueMod ? PQueueMod.default : PQueueMod;
+  const queue = new PQueue({ concurrency: maxConcurrency });
   const client_ = client ?? new LangChainPlusClient({});
   const examples = await client_.listExamples({ datasetName });
   let projectName_: string;
   if (projectName === undefined) {
     const currentTime = new Date().toISOString();
-    projectName_ = `${datasetName}-${llmOrChainFactory.constructor.name}-${currentTime}`;
+    projectName_ = `${datasetName}-${
+      typeof llmOrChainFactory === "function"
+        ? ""
+        : llmOrChainFactory.constructor.name
+    }-${currentTime}`;
   } else {
     projectName_ = projectName;
   }
   await client_.createProject({ projectName: projectName_, mode: "eval" });
-  const results: DatasetRunResults = {};
+  const results: DatasetRunResults = examples.reduce(
+    (acc, example) => ({ ...acc, [example.id]: [] }),
+    {}
+  );
   const modelOrFactoryType = await getModelOrFactoryType(llmOrChainFactory);
   await Promise.all(
-    examples.map(async (example) => {
-      const tracer = new LangChainTracer({
-        exampleId: example.id,
-        projectName: projectName_,
-      });
-      if (modelOrFactoryType === "llm") {
-        const llm = llmOrChainFactory as BaseLLM;
-        const llmResult = await runLLM(example, tracer, llm, {
-          numRepetitions,
+    Array.from({ length: numRepetitions })
+      .flatMap(() => examples)
+      .map(async (example) => {
+        const tracer = new LangChainTracer({
+          exampleId: example.id,
+          projectName: projectName_,
         });
-        results[example.id] = llmResult;
-      } else if (modelOrFactoryType === "chainFactory") {
-        const chainFactory = llmOrChainFactory as () => Promise<BaseChain>;
-        const chainResult = await runChain(example, tracer, chainFactory, {
-          numRepetitions,
-        });
-        results[example.id] = chainResult;
-      } else if (modelOrFactoryType === "chatModel") {
-        const chatModel = llmOrChainFactory as BaseChatModel;
-        const chatModelResult = await runChatModel(example, tracer, chatModel, {
-          numRepetitions,
-        });
-        results[example.id] = chatModelResult;
-      } else {
-        throw new Error(` llm or chain type: ${llmOrChainFactory}`);
-      }
-    })
+        if (modelOrFactoryType === "llm") {
+          const llm = llmOrChainFactory as BaseLLM;
+          const llmResult = await queue.add(
+            () => runLLM(example, tracer, llm),
+            { throwOnTimeout: true }
+          );
+          results[example.id].push(llmResult);
+        } else if (modelOrFactoryType === "chainFactory") {
+          const chainFactory = llmOrChainFactory as () => Promise<BaseChain>;
+          const chainResult = await queue.add(
+            () => runChain(example, tracer, chainFactory),
+            { throwOnTimeout: true }
+          );
+          results[example.id].push(chainResult);
+        } else if (modelOrFactoryType === "chatModel") {
+          const chatModel = llmOrChainFactory as BaseChatModel;
+          const chatModelResult = await queue.add(
+            () => runChatModel(example, tracer, chatModel),
+            { throwOnTimeout: true }
+          );
+          results[example.id].push(chatModelResult);
+        } else {
+          throw new Error(` llm or chain type: ${llmOrChainFactory}`);
+        }
+      })
   );
   return results;
 };
