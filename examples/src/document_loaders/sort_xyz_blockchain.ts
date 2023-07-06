b4f42ac83eb73b522246ded401390f799f74b577
Jason Zucchetto <zuketo@gmail.com>
2023-07-06 14:02:29 -0700

    Rename BlockchainPagesLoader to SortXYZBlockchainLoader

---
+++ examples/src/document_loaders/sort_xyz_blockchain.ts
@@ -0,0 +1,84 @@
+import { SortXYZBlockchainLoader } from 'langchain/document_loaders/web/sort_xyz_blockchain';
+import { OpenAI } from 'langchain/llms/openai';
+
+/**
+ * See https://docs.sort.xyz/docs/api-keys to get your free Sort API key.
+ * See https://docs.sort.xyz for more information on the available queries.
+ * See https://docs.sort.xyz/reference for more information about Sort's REST API.
+ */
+
+/**
+ * Run the example.
+ */
+export const run = async () => {
+  // Initialize the OpenAI model. Use OPENAI_API_KEY from .env in /examples
+  const model = new OpenAI({ temperature: 0.9 });
+
+  const apiKey = 'dd46a0ae-5a1a-4c6f-8328-46618c4a73d4';
+  const contractAddress = '0x887F3909C14DAbd9e9510128cA6cBb448E932d7f'.toLowerCase();
+
+  /*
+  Load NFT metadata from the Ethereum blockchain. Hint: to load by a specific ID, see SQL query example below.
+  */
+
+  const nftMetadataLoader = new SortXYZBlockchainLoader({
+    apiKey,
+    query: {
+      type: 'NFTMetadata',
+      blockchain: 'ethereum',
+      contractAddress
+    }
+  });
+
+  const nftMetadataDocs = await nftMetadataLoader.load();
+
+  const nft_prompt = "Describe the character with the attributes from the following json document in a 4 sentence story. ";
+  const nft_res = await model.call(
+    nft_prompt +
+    JSON.stringify(nftMetadataDocs[0], null, 2)
+  );
+  console.log(`user > ${nft_prompt}`);
+  console.log(`chatgpt > ${nft_res}`);
+
+  /*
+    Load the latest transactions for a contract address from the Ethereum blockchain.
+  */
+  const latestTransactionsLoader = new SortXYZBlockchainLoader({
+    apiKey,
+    query: {
+      type: "latestTransactions",
+      blockchain: "ethereum",
+      contractAddress
+    }
+  });
+
+  const latestTransactionsDocs = await latestTransactionsLoader.load();
+
+  const latest_prompt = "Describe the following json documents in only 4 sentences per document. Include as much detail as possible. ";
+  const latest_res = await model.call(
+    latest_prompt +
+    JSON.stringify(latestTransactionsDocs[0], null, 2)
+  );
+  console.log(`\n\nuser > ${nft_prompt}`);
+  console.log(`chatgpt > ${latest_res}`);
+
+
+  /*
+    Load metadata for a specific NFT by using raw SQL and the NFT index. See https://docs.sort.xyz for forumulating SQL.
+  */
+
+  const sqlQueryLoader = new SortXYZBlockchainLoader({
+    apiKey,
+    query: `SELECT * FROM ethereum.nft_metadata WHERE contract_address = '${contractAddress}' AND token_id = 1 LIMIT 1`
+  });
+
+  const sqlDocs = await sqlQueryLoader.load();
+
+  const sql_prompt = 'Describe the character with the attributes from the following json document in an ad for a new coffee shop. ';
+  const sql_res = await model.call(
+    sql_prompt +
+    JSON.stringify(sqlDocs[0], null, 2)
+  );
+  console.log(`\n\nuser > ${sql_prompt}`);
+  console.log(`chatgpt > ${sql_res}`);
+};
---
+++ langchain/src/document_loaders/web/sort_xyz_blockchain.ts
@@ -0,0 +1,138 @@
+import { Document } from "../../document.js";
+import { BaseDocumentLoader } from "../base.js";
+
+/**
+ * See https://docs.sort.xyz/docs/api-keys to get your free Sort API key.
+ * See https://docs.sort.xyz for more information on the available queries.
+ * See https://docs.sort.xyz/reference for more information about Sort's REST API.
+ */
+
+export interface Query {
+  type: 'NFTMetadata' | 'latestTransactions';
+  contractAddress: string;
+  blockchain: 'ethereum' | 'polygon' | 'goerli';
+  limit?: number;
+}
+
+export interface SortXYZBlockchainLoaderParams {
+  apiKey: string;
+  query: Query | string;
+}
+
+export interface SortXYZBlockchainAPIResponse {
+  code: number;
+  data: {
+    durationMs: number;
+    id: string;
+    query: string;
+    records: any[];
+    recordCount: number;
+  }
+}
+
+export class SortXYZBlockchainLoader extends BaseDocumentLoader {
+  private readonly ITEMS_PER_PAGE = 100;
+
+  public readonly contractAddress: string;
+
+  public readonly blockchain: string;
+
+  public readonly apiKey: string;
+
+  public readonly queryType: string;
+
+  public readonly sql: string;
+
+  public readonly limit: number;
+
+  constructor({
+    apiKey,
+    query
+  }: SortXYZBlockchainLoaderParams) {
+    super();
+
+    if (!apiKey) {
+      throw new Error(
+        `apiKey is required! Head over to https://docs.sort.xyz/docs/api-keys to get your free Sort API key.`
+      );
+    }
+
+    this.apiKey = apiKey;
+
+    if (typeof query === 'string') {
+      this.sql = query;
+    } else {
+      this.contractAddress = query.contractAddress.toLowerCase();
+      this.blockchain = query.blockchain;
+      this.queryType = query.type;
+      this.limit = query.limit || 100;
+    }
+  }
+
+  public async load(): Promise<Document[]> {
+    if (this.limit > 1000) {
+      throw new Error(
+        `Limit is set too high. Please set limit to 1000 or lower.`
+      );
+    }
+
+    const docs: Document[] = [];
+    let queryOffset = 0;
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      let query = '';
+
+      if (this.sql) {
+        query = this.sql;
+      } else if (this.queryType === 'NFTMetadata') {
+        query = `SELECT * FROM ${this.blockchain}.nft_metadata WHERE contract_address = '${this.contractAddress}' ORDER BY token_id DESC LIMIT ${this.ITEMS_PER_PAGE} OFFSET ${queryOffset}`;
+      } else if (this.queryType === 'latestTransactions') {
+        query = `SELECT * FROM ${this.blockchain}.transaction t, ethereum.block b WHERE t.to_address = '${this.contractAddress}' AND b.id=t.block_id ORDER BY b.timestamp DESC LIMIT ${this.ITEMS_PER_PAGE} OFFSET ${queryOffset}`;
+      }
+
+      try {
+        const response = await fetch('https://api.sort.xyz/v1/queries/run', {
+          method: 'POST',
+          headers: {
+            'x-api-key': this.apiKey as string,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ query }),
+        })
+
+        const full_response = await response.json();
+
+        // Reached the end, no more records
+        if (full_response && full_response.data && full_response.data.records.length === 0) {
+          break;
+        }
+
+        const data = full_response?.data || [];
+
+        for (let i = 0; i < data.records.length; i += 1) {
+          const doc = new Document({
+            pageContent: JSON.stringify(data.records[i], null, 2),
+            metadata: {
+              row: i
+            },
+          });
+
+          docs.push(doc);
+        }
+
+        queryOffset += this.ITEMS_PER_PAGE;
+
+        if (queryOffset >= this.limit || this.sql) {
+          break;
+        }
+
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+
+    return docs;
+  }
+}
