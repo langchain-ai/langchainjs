d1e959edef03244b364c77181a33fba810dd110f
Merbin J Anselm <merbin.j.anselm@sap.com>
2023-07-09 15:48:13 +0530

    updated examples

--- examples/src/llms/googlepalm.ts
+++ examples/src/llms/googlepalm.ts
@@ -1,11 +1,13 @@
 import { GooglePalm } from "langchain/llms/googlepalm";
 
-// make sure Google PALM API key (from MakerSuite etc.) is set as GOOGLEPALM_API_KEY env
-const model = new GooglePalm({
-  // other params
-  temperature: 1,
-});
-const res = await model.call(
-  "What would be a good company name for a company that makes colorful socks?"
-);
-console.log({ res });
+export const run = async () => {
+  // make sure Google PALM API key (from MakerSuite etc.) is set as GOOGLEPALM_API_KEY env
+  const model = new GooglePalm({
+    // other params
+    temperature: 1,
+  });
+  const res = await model.call(
+    "What would be a good company name for a company that makes colorful socks?"
+  );
+  console.log({ res });
+};
--- examples/src/models/embeddings/googlepalm.ts
+++ examples/src/models/embeddings/googlepalm.ts
@@ -1,7 +1,13 @@
 import { GooglePalmEmbeddings } from "langchain/embeddings/googlepalm";
 
-const model = new GooglePalmEmbeddings();
-const res = await model.embedQuery(
-  "What would be a good company name for a company that makes colorful socks?"
-);
-console.log({ res });
+export const run = async () => {
+  const model = new GooglePalmEmbeddings();
+  /* Embed queries */
+  const res = await model.embedQuery(
+    "What would be a good company name for a company that makes colorful socks?"
+  );
+  console.log({ res });
+  /* Embed documents */
+  const documentRes = await model.embedDocuments(["Hello world", "Bye bye"]);
+  console.log({ documentRes });
+};
