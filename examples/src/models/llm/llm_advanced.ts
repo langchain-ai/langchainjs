7024619870e9326827a5301cf1cd276aa3d34ce5
jacoblee93 <jacoblee93@gmail.com>
2023-07-10 13:04:20 -0700

    Example tweak

--- examples/src/models/llm/llm_advanced.ts
+++ examples/src/models/llm/llm_advanced.ts
@@ -3,7 +3,7 @@ import { OpenAI } from "langchain/llms/openai";
 export const run = async () => {
   const model = new OpenAI({
     // customize openai model that's used, `text-davinci-003` is the default
-    modelName: "gpt-3.5-turbo",
+    modelName: "text-ada-001",
 
     // `max_tokens` supports a magic -1 param where the max token length for the specified modelName
     //  is calculated and included in the request to OpenAI as the `max_tokens` param
