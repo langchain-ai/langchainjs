2b14202bede777dd5fde2ada3131f09241cb46e5
Erick Friis <erick@langchain.dev>
2023-07-13 12:34:28 -0700

    example tracing endpoint to smith

--- examples/src/client/tracing_datasets.ts
+++ examples/src/client/tracing_datasets.ts
@@ -57,7 +57,7 @@ for (const input of inputs) {
 
 // So that you don't have to create the dataset manually, we will create it for you
 const client = new Client({
-  // apiUrl: "https://api.langchain.plus", // Default: LANGCHAIN_ENDPOINT environment variable of "http://localhost:1984"
+  // apiUrl: "https://api.smith.langchain.com", // Default: LANGCHAIN_ENDPOINT environment variable of "http://localhost:1984"
   // apiKey: "<Your API Key>", // Default: LANGCHAIN_API_KEY environment variable
 });
 const csvContent = `
