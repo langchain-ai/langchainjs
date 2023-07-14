700b4c6e52b22bdb6c59cc16e79f4ea8240487a5
Youngwook Kim <youngwook.kim@gmail.com>
2023-07-15 08:07:04 +0900

    feat: Add example for using SerpAPI Loader in LangChain
    This commit adds a new example file that demonstrates how to use the `SerpAPILoader` in LangChain to load web search results from SerpAPI. The example includes code that initializes the necessary components, defines a question and query, uses the loader to load the search results, stores the loaded documents in memory using `MemoryVectorStore`, and uses `RetrievalQAChain` to retrieve the most relevant documents and answer the question.

---
+++ examples/src/document_loaders/serpapi.ts
@@ -0,0 +1,27 @@
+import { OpenAI } from "langchain/llms/openai";
+import { RetrievalQAChain } from "langchain/chains";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { SerpAPILoader } from "langchain/document_loaders/web/SerpAPILoader";
+
+// Initialize the necessary components
+const llm = new OpenAI("Your OpenAI API key");
+const embeddings = new OpenAIEmbeddings(llm);
+const apiKey = "Your SerpAPI API key";
+
+// Define your question and query
+const question = "Your question here";
+const query = "Your query here";
+
+// Use SerpAPILoader to load web search results
+const loader = new SerpAPILoader(apiKey, query);
+const docs = await loader.load();
+
+// Use MemoryVectorStore to store the loaded documents in memory
+const vectorStore = await MemoryVectorStore.fromDocuments(docs, embeddings);
+
+// Use RetrievalQAChain to retrieve documents and answer the question
+const chain = RetrievalQAChain.fromLLM(llm, vectorStore.asRetriever());
+const answer = await chain.call({ query: question });
+
+console.log(answer.text);
