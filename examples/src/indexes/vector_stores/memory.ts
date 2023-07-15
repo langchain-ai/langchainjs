04b81aaa3080cfa87029f57b71a9ab9dfc44696e
jacoblee93 <jacoblee93@gmail.com>
2023-07-15 10:18:38 -0700

    More progress

--- examples/src/indexes/vector_stores/memory.ts
+++ examples/src/indexes/vector_stores/memory.ts
@@ -1,13 +1,20 @@
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
-export const run = async () => {
-  const vectorStore = await MemoryVectorStore.fromTexts(
-    ["Hello world", "Bye bye", "hello nice world"],
-    [{ id: 2 }, { id: 1 }, { id: 3 }],
-    new OpenAIEmbeddings()
-  );
+const vectorStore = await MemoryVectorStore.fromTexts(
+  ["Hello world", "Bye bye", "hello nice world"],
+  [{ id: 2 }, { id: 1 }, { id: 3 }],
+  new OpenAIEmbeddings()
+);
 
-  const resultOne = await vectorStore.similaritySearch("hello world", 1);
-  console.log(resultOne);
-};
+const resultOne = await vectorStore.similaritySearch("hello world", 1);
+console.log(resultOne);
+
+/*
+  [
+    Document {
+      pageContent: "Hello world",
+      metadata: { id: 2 }
+    }
+  ]
+*/
