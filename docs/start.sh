1d98a948da5766e61ca1869dcc6f97543a7acbe6
jacoblee93 <jacoblee93@gmail.com>
2023-07-18 16:46:19 -0700

    Fix docs build

--- docs/start.sh
+++ docs/start.sh
@@ -8,10 +8,5 @@ set -o xtrace
 SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
 cd "${SCRIPT_DIR}"
 
-rm -rf _dist/
-mkdir -p _dist/docs_skeleton
-cp -r {docs_skeleton,snippets} _dist
-cp -r extras/ _dist/docs_skeleton/docs
 cd _dist/docs_skeleton
-yarn install
-yarn build
+yarn start
