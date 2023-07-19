1c951b87a5ce07143a82c32adb9939263c652fab
archie-swif <artem.ryabokon@gmail.com>
2023-07-19 16:05:11 -0500

    Added Maximal Marginal Relevance function to math utils

--- langchain/src/util/math_utils.ts
+++ langchain/src/util/math_utils.ts
@@ -11,29 +11,124 @@ import { Tensor1D } from "@tensorflow/tfjs-core";
  *
  * @returns {number[][] | [[]]} A matrix where each row represents the cosine similarity values between the corresponding rows of X and Y.
  */
-export function cosineSimilarity(
-  X: number[][],
-  Y: number[][]
-): Promise<number[][]> {
+export function cosineSimilarity(X: number[][], Y: number[][]): number[][] {
   const xTensor = tf.tensor(X);
   const yTensor = tf.tensor(Y);
 
   if (xTensor.size === 0 || yTensor.size === 0) {
-    return Promise.resolve([[]]);
+    xTensor.dispose();
+    yTensor.dispose();
+    return [[]];
   }
 
   if (xTensor.shape[1] !== yTensor.shape[1]) {
+    xTensor.dispose();
+    yTensor.dispose();
     throw new Error(
       `Number of columns in X and Y must be the same. X has shape ${xTensor.shape} and Y has shape ${yTensor.shape}.`
     );
   }
+  const similarityTensor = tf.tidy(() => {
+    const xNorm = tf.norm(xTensor, undefined, 1) as Tensor1D;
+    const yNorm = tf.norm(yTensor, undefined, 1) as Tensor1D;
+    return tf.divNoNan(
+      tf.dot(xTensor, tf.transpose(yTensor)),
+      tf.outerProduct(xNorm, yNorm)
+    );
+  });
 
-  const xNorm = tf.norm(xTensor, undefined, 1) as Tensor1D;
-  const yNorm = tf.norm(yTensor, undefined, 1) as Tensor1D;
-  const cosineSimilarity = tf.divNoNan(
-    tf.dot(xTensor, tf.transpose(yTensor)),
-    tf.outerProduct(xNorm, yNorm)
-  );
+  const cosineSimilarity = similarityTensor.arraySync() as number[][];
+  xTensor.dispose();
+  yTensor.dispose();
+  similarityTensor.dispose();
 
-  return cosineSimilarity.array() as Promise<number[][]>;
+  return cosineSimilarity;
+}
+
+/**
+ * This function implements the Maximal Marginal Relevance algorithm
+ * to select a set of embeddings that maximizes the diversity and relevance to a query embedding.
+ *
+ * @param {number[]} queryEmbedding - The query embedding.
+ * @param {number[][]} embeddingList - The list of embeddings to select from.
+ * @param {number} [lambda=0.5] - The trade-off parameter between relevance and diversity.
+ * @param {number} [k=4] - The maximum number of embeddings to select.
+ *
+ * @returns {number[]} The indexes of the selected embeddings in the embeddingList.
+ */
+export function maximalMarginalRelevance(
+  queryEmbedding: number[],
+  embeddingList: number[][],
+  lambda = 0.5,
+  k = 4
+): number[] {
+  if (Math.min(k, embeddingList.length) <= 0) {
+    return [];
+  }
+
+  const queryEmbeddingExpanded = (
+    Array.isArray(queryEmbedding[0]) ? queryEmbedding : [queryEmbedding]
+  ) as number[][];
+
+  const similarityToQuery = cosineSimilarity(
+    queryEmbeddingExpanded,
+    embeddingList
+  )[0];
+  const mostSimilarEmbeddingIndex = argMax(similarityToQuery);
+
+  const selectedEmbeddings = [embeddingList[mostSimilarEmbeddingIndex]];
+  const selectedEmbeddingsIndexes = [mostSimilarEmbeddingIndex];
+
+  while (selectedEmbeddingsIndexes.length < Math.min(k, embeddingList.length)) {
+    let bestScore = -Infinity;
+    let bestIndex = -1;
+
+    const similarityToSelected = cosineSimilarity(
+      embeddingList,
+      selectedEmbeddings
+    );
+
+    similarityToQuery.forEach((queryScore, queryScoreIndex) => {
+      if (queryScoreIndex in selectedEmbeddingsIndexes) {
+        return;
+      }
+      const maxSimilarityToSelected = Math.max(
+        ...similarityToSelected[queryScoreIndex]
+      );
+      const score =
+        lambda * queryScore - (1 - lambda) * maxSimilarityToSelected;
+
+      if (score > bestScore) {
+        bestScore = score;
+        bestIndex = queryScoreIndex;
+      }
+    });
+    selectedEmbeddings.push(embeddingList[bestIndex]);
+    selectedEmbeddingsIndexes.push(bestIndex);
+  }
+
+  return selectedEmbeddingsIndexes;
+}
+
+/**
+ * Finds the index of the maximum value in the given array.
+ * @param {number[]} array - The input array.
+ *
+ * @returns {number} The index of the maximum value in the array. If the array is empty, returns -1.
+ */
+function argMax(array: number[]): number {
+  if (array.length === 0) {
+    return -1;
+  }
+
+  let maxValue = array[0];
+  let maxIndex = 0;
+
+  for (let i = 1; i < array.length; i += 1) {
+    if (array[i] > maxValue) {
+      maxIndex = i;
+      maxValue = array[i];
+    }
+  }
+  return maxIndex;
 }
--- langchain/src/util/tests/math_utils.test.ts
+++ langchain/src/util/tests/math_utils.test.ts
@@ -1,19 +1,23 @@
 import { test, expect } from "@jest/globals";
 import * as tf from "@tensorflow/tfjs-core";
 import "@tensorflow/tfjs-backend-cpu";
-import { cosineSimilarity } from "../math_utils.js";
+import { cosineSimilarity, maximalMarginalRelevance } from "../math_utils.js";
+
+// tensor memory cleanup
+beforeEach(() => tf.engine().startScope(expect.getState().currentTestName));
+afterEach(() => tf.engine().endScope(expect.getState().currentTestName));
 
 test("Test cosine similarity zero", async () => {
   const X = tf.randomUniform([3, 3]).arraySync() as number[][];
   const Y = tf.zeros([3, 3]).arraySync() as number[][];
   const expected = tf.zeros([3, 3]).arraySync() as number[][];
-  const actual = await cosineSimilarity(X, Y);
+  const actual = cosineSimilarity(X, Y);
   expect(actual).toEqual(expected);
 });
 
 test("Test cosine similarity identity", async () => {
   const X = tf.randomUniform([4, 4]).arraySync() as number[][];
-  const actual = await cosineSimilarity(X, X);
+  const actual = cosineSimilarity(X, X);
 
   // Diagonal is expected to be [1, 1, 1, 1]
   for (let i = 0; i < 4; i += 1) {
@@ -47,15 +51,15 @@ test("Test cosine similarity", async () => {
     ])
     .arraySync() as number[][];
 
-  const actual = await cosineSimilarity(X, Y);
+  const actual = cosineSimilarity(X, Y);
   expect(actual).toEqual(expected);
 });
 
 test("Test cosine similarity empty", async () => {
   const X = [[]];
   const Y = tf.randomUniform([3, 3]).arraySync() as number[][];
-  expect(await cosineSimilarity(X, X)).toEqual([[]]);
-  expect(await cosineSimilarity(X, Y)).toEqual([[]]);
+  expect(cosineSimilarity(X, X)).toEqual([[]]);
+  expect(cosineSimilarity(X, Y)).toEqual([[]]);
 });
 
 test("Test cosine similarity wrong shape", async () => {
@@ -64,8 +68,97 @@ test("Test cosine similarity wrong shape", async () => {
   expect(() => cosineSimilarity(X, Y)).toThrowError();
 });
 
-test("Test cosine similarity correct shape", async () => {
+test("Test cosine similarity different shape", async () => {
   const X = tf.randomUniform([2, 2]).arraySync() as number[][];
   const Y = tf.randomUniform([4, 2]).arraySync() as number[][];
   expect(() => cosineSimilarity(X, Y)).not.toThrowError();
+});
+
+test("Test cosine similarity memory leaks", async () => {
+  const X = tf.randomUniform([3, 3]).arraySync() as number[][];
+  const Y = tf.randomUniform([3, 3]).arraySync() as number[][];
+
+  const expected = tf.memory().numTensors;
+  cosineSimilarity(X, Y);
+  const actual = tf.memory().numTensors;
+  expect(actual).toEqual(expected);
+});
+
+test("Test maximal marginal relevance lambda zero", async () => {
+  const queryEmbedding = tf.randomUniform([5]).arraySync() as number[];
+  const zeros = tf.zeros([5]).arraySync() as number[];
+  const embeddingList = [queryEmbedding, queryEmbedding, zeros];
+
+  const expected = [0, 2];
+  const actual = maximalMarginalRelevance(queryEmbedding, embeddingList, 0, 2);
+
+  expect(actual).toEqual(expected);
+});
+
+test("Test maximal marginal relevance lambda one", async () => {
+  const queryEmbedding = tf.randomUniform([5]).arraySync() as number[];
+  const zeros = tf.zeros([5]).arraySync() as number[];
+  const embeddingList = [queryEmbedding, queryEmbedding, zeros];
+
+  const expected = [0, 1];
+  const actual = maximalMarginalRelevance(queryEmbedding, embeddingList, 1, 2);
+
+  expect(actual).toEqual(expected);
+});
+
+test("Test maximal marginal relevance", async () => {
+  // Vectors that are 30, 45 and 75 degrees from query vector (cosine similarity of
+  // 0.87, 0.71, 0.26) and the latter two are 15 and 60 degree from the first
+  // (cosine similarity 0.97 and 0.71). So for 3rd vector be chosen, must be case that
+  // 0.71lambda - 0.97(1 - lambda) < 0.26lambda - 0.71(1-lambda) -> lambda ~< .26 / .71
+
+  const queryEmbedding = [1, 0];
+  const embeddingList = [
+    [3 ** 0.5, 1],
+    [1, 1],
+    [1, 2 + 3 ** 0.5],
+  ];
+
+  let expected = [0, 2];
+  let actual = maximalMarginalRelevance(
+    queryEmbedding,
+    embeddingList,
+    25 / 71,
+    2
+  );
+  expect(actual).toEqual(expected);
+
+  expected = [0, 1];
+  actual = maximalMarginalRelevance(queryEmbedding, embeddingList, 27 / 71, 2);
+  expect(actual).toEqual(expected);
+});
+
+test("Test maximal marginal relevance query dim", async () => {
+  const randomTensor = tf.randomUniform([5]);
+
+  const queryEmbedding = randomTensor.arraySync() as number[];
+  const queryEmbedding2D = tf
+    .reshape(randomTensor, [1, 5])
+    .arraySync() as number[];
+  const embeddingList = tf.randomUniform([4, 5]).arraySync() as number[][];
+
+  const first = maximalMarginalRelevance(queryEmbedding, embeddingList, 1, 2);
+  const second = maximalMarginalRelevance(
+    queryEmbedding2D,
+    embeddingList,
+    1,
+    2
+  );
+
+  expect(first).toEqual(second);
+});
+
+test("Test maximal marginal relevance memory leaks", async () => {
+  const queryEmbedding = tf.randomUniform([5]).arraySync() as number[];
+  const embeddingList = tf.randomUniform([4, 5]).arraySync() as number[][];
+
+  const expected = tf.memory().numTensors;
+  maximalMarginalRelevance(queryEmbedding, embeddingList, 25 / 71, 2);
+  const actual = tf.memory().numTensors;
+  expect(actual).toEqual(expected);
 });
