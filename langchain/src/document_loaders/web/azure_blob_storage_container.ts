93856aef0af54f9937815eb37ca5b5ab372fe922
Dmitry Tyumentsev <tyumentsevde@gmail.com>
2023-07-19 10:05:59 +0000

    fmt

--- langchain/src/document_loaders/web/azure_blob_storage_container.ts
+++ langchain/src/document_loaders/web/azure_blob_storage_container.ts
@@ -4,25 +4,33 @@ import { BaseDocumentLoader } from "../base.js";
 import { Document } from "../../document.js";
 
 export class AzureBlobStorageContainerLoader extends BaseDocumentLoader {
-    private readonly connStr: string;
+  private readonly connStr: string;
 
-    private readonly container: string;
-  
-    constructor(connStr: string, container: string) {
-      super();
-      this.connStr = connStr;
-      this.container = container;
-    }
+  private readonly container: string;
 
-    public async load() {
-      const blobServiceClient = BlobServiceClient.fromConnectionString(this.connStr);
-      
-      const containerClient = blobServiceClient.getContainerClient(this.container);
+  constructor(connStr: string, container: string) {
+    super();
+    this.connStr = connStr;
+    this.container = container;
+  }
 
-      const docs: Document[] = []
-      for await (const blob of containerClient.listBlobsFlat()) {
-        const loader = new AzureBlobStorageFileLoader(this.connStr, this.container, blob.name)
-        docs.concat(await loader.load())
+  public async load() {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(
+      this.connStr
+    );
+
+    const containerClient = blobServiceClient.getContainerClient(
+      this.container
+    );
+
+    const docs: Document[] = [];
+    for await (const blob of containerClient.listBlobsFlat()) {
+      const loader = new AzureBlobStorageFileLoader(
+        this.connStr,
+        this.container,
+        blob.name
+      );
+      docs.concat(await loader.load());
     }
 
     return docs;
