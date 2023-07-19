7b3995f34a34d247dc8df362065c9036cf535729
jacoblee93 <jacoblee93@gmail.com>
2023-07-18 19:08:11 -0700

    Fix build script

--- docs/build.sh
+++ docs/build.sh
@@ -11,7 +11,7 @@ cd "${SCRIPT_DIR}"
 rm -rf _dist/
 mkdir -p _dist/docs_skeleton
 cp -r {docs_skeleton,snippets} _dist
-cp -r extras/ _dist/docs_skeleton/docs
+cp -r extras/* _dist/docs_skeleton/docs
 cd _dist/docs_skeleton
 yarn install
 yarn build
