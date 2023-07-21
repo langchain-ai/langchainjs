3a5ca456cdca2c53df9bc939cf354baf84009cb4
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-07-21 14:39:55 +0800

    feat: 1. add docs for CombineMemory && ConversationSummaryBufferMemory

--- examples/src/memory/combine.ts
+++ examples/src/memory/combine.ts
@@ -4,28 +4,28 @@ import {
   CombinedMemory,
   ConversationSummaryMemory,
 } from "langchain/memory";
-import { ConversationChain, LLMChain } from "langchain/chains";
+import { ConversationChain } from "langchain/chains";
 import { PromptTemplate } from "langchain/prompts";
-import { ChatOpenAI } from "langchain/chat_models";
 
-// buffer memory
-const conv_memory = new BufferMemory({
-  memoryKey: "chat_history_lines",
-  inputKey: "input",
-});
+export const run = async () => {
+  // buffer memory
+  const conv_memory = new BufferMemory({
+    memoryKey: "chat_history_lines",
+    inputKey: "input",
+  });
 
-// summary memory
-const summary_memory = new ConversationSummaryMemory({
-  llm: new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 }),
-  inputKey: "input",
-});
+  // summary memory
+  const summary_memory = new ConversationSummaryMemory({
+    llm: new OpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 }),
+    inputKey: "input",
+  });
 
-//
-const memory = new CombinedMemory({
-  memories: [conv_memory, summary_memory],
-});
+  //
+  const memory = new CombinedMemory({
+    memories: [conv_memory, summary_memory],
+  });
 
-const _DEFAULT_TEMPLATE = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+  const _DEFAULT_TEMPLATE = `The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
 
 Summary of conversation:
 {history}
@@ -34,16 +34,16 @@ Current conversation:
 Human: {input}
 AI:`;
 
-const PROMPT = new PromptTemplate({
-  inputVariables: ["input", "history", "chat_history_lines"],
-  template: _DEFAULT_TEMPLATE,
-});
+  const PROMPT = new PromptTemplate({
+    inputVariables: ["input", "history", "chat_history_lines"],
+    template: _DEFAULT_TEMPLATE,
+  });
+  const model = new OpenAI({ temperature: 0.9, verbose: true });
+  const chain = new ConversationChain({ llm: model, memory, prompt: PROMPT });
 
-const model = new OpenAI({ temperature: 0.9, verbose: true });
-const chain = new ConversationChain({ llm: model, memory, prompt: PROMPT });
+  const res1 = await chain.call({ input: "Hi! I'm Jim." });
+  console.log({ res1 });
 
-const res1 = await chain.call({ input: "Hi! I'm Jim." });
-console.log({ res1 });
-
-const res2 = await chain.call({ input: "Can you tell me a joke?" });
-console.log({ res2 });
+  const res2 = await chain.call({ input: "Can you tell me a joke?" });
+  console.log({ res2 });
+};
--- examples/src/memory/summary_buffer.ts
+++ examples/src/memory/summary_buffer.ts
@@ -1,42 +1,58 @@
 import { OpenAI } from "langchain/llms/openai";
 import { ConversationSummaryBufferMemory } from "langchain/memory";
-import { ConversationChain, LLMChain } from "langchain/chains";
-import { ChatOpenAI } from "langchain/chat_models";
+import { ConversationChain } from "langchain/chains";
 
-// summary buffer memory
-const memory = new ConversationSummaryBufferMemory({
-  llm: new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 }),
-  max_token_limit: 10,
-});
+export const run = async () => {
+  // summary buffer memory
+  let memory = new ConversationSummaryBufferMemory({
+    llm: new OpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 }),
+    max_token_limit: 10,
+  });
 
-// await memory.saveContext({input: "hi"}, {output: "whats up"});
-// await memory.saveContext({input: "not much you"}, {output: "not much"});
-// const history = await memory.loadMemoryVariables({});
-// console.log(JSON.stringify(history));
+  await memory.saveContext({ input: "hi" }, { output: "whats up" });
+  await memory.saveContext({ input: "not much you" }, { output: "not much" });
+  const history = await memory.loadMemoryVariables({});
+  console.log({ history });
 
-// const messages = await memory.chatHistory.getMessages();
-// const previous_summary = "";
-// const predictSummary = await memory.predictNewSummary(messages, previous_summary);
-// console.log(JSON.stringify(predictSummary));
+  // We can also get the history as a list of messages (this is useful if you are using this with a chat model).
+  memory = new ConversationSummaryBufferMemory({
+    llm: new OpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 }),
+    max_token_limit: 10,
+    returnMessages: true,
+  });
+  await memory.saveContext({ input: "hi" }, { output: "whats up" });
+  await memory.saveContext({ input: "not much you" }, { output: "not much" });
 
-const model = new ChatOpenAI({ temperature: 0.9, verbose: true });
-// const model = new OpenAI({temperature: 0.9, verbose: true});
-const chain = new ConversationChain({ llm: model, memory });
+  // We can also utilize the predict_new_summary method directly.
+  const messages = await memory.chatHistory.getMessages();
+  const previous_summary = "";
+  const predictSummary = await memory.predictNewSummary(
+    messages,
+    previous_summary
+  );
+  console.log(JSON.stringify(predictSummary));
 
-const res1 = await chain.predict({ input: "Hi, what's up?" });
-console.log({ res1 });
+  // Using in a chain
+  // Let's walk through an example, again setting verbose=True so we can see the prompt.
 
-const res2 = await chain.predict({
-  input: "Just working on writing some documentation!",
-});
-console.log({ res2 });
+  const model = new OpenAI({ temperature: 0.9, verbose: true });
+  const chain = new ConversationChain({ llm: model, memory });
 
-const res3 = await chain.predict({
-  input: "For LangChain! Have you heard of it?",
-});
-console.log({ res3 });
+  const res1 = await chain.predict({ input: "Hi, what's up?" });
+  console.log({ res1 });
 
-const res4 = await chain.predict({
-  input: "Haha nope, although a lot of people confuse it for that",
-});
-console.log({ res4 });
+  const res2 = await chain.predict({
+    input: "Just working on writing some documentation!",
+  });
+  console.log({ res2 });
+
+  const res3 = await chain.predict({
+    input: "For LangChain! Have you heard of it?",
+  });
+  console.log({ res3 });
+
+  const res4 = await chain.predict({
+    input: "Haha nope, although a lot of people confuse it for that",
+  });
+  console.log({ res4 });
+};
