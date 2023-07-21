88812b3f1f087999d0f178a2bc58304e809fa171
Seif <seifaziz10@gmail.com>
2023-07-20 21:56:46 -0700

    Add more comprehensive tests for retriever

--- langchain/src/retrievers/tests/vectara.int.test.ts
+++ langchain/src/retrievers/tests/vectara.int.test.ts
@@ -1,56 +1,148 @@
 /* eslint-disable no-process-env */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { test, expect } from "@jest/globals";
-
+import { Document } from "../../document.js";
 import { VectaraRetriever } from "../vectara.js";
 import { VectaraStore } from "../../vectorstores/vectara.js";
 
 /**
  * Steps to run this test:
  * 1. Create a Vectara account at https://vectara.com/
- * 2. Create a corpus
- * 3. Upload a specific document to the corpus (e.g. the US constitution)
- * 4. Set the appropriate environment variables (VECTARA_CUSTOMER_ID, VECTARA_CORPUS_ID, VECTARA_API_KEY)
- * 5. Run the test
+ * 2. Create a corpus with the following filter attributes: can_fly (level: document, data type: boolean), category (level: document, data type: integer)
+ * 3. Set the appropriate environment variables in your .env file (VECTARA_CUSTOMER_ID, VECTARA_CORPUS_ID, VECTARA_API_KEY)
+ * 4. Run the test
  */
 
-test("VectaraRetriever (with filter)", async () => {
+const VECTARA_CUSTOMER_ID = Number(process.env.VECTARA_CUSTOMER_ID) || 0;
+const VECTARA_CORPUS_ID = Number(process.env.VECTARA_CORPUS_ID) || 0;
+const VECTARA_API_KEY = process.env.VECTARA_API_KEY || "";
+
+const getdoc = (): Document[] => {
+  // Summary of 4 different animals
+  const lion = `The lion, often referred to as the "king of the jungle, " is one of the largest and most powerful 
+    members of the cat family, known for its majestic mane and powerful roar. These carnivorous mammals reside primarily 
+    in Sub-Saharan Africa and parts of Asia, where they roam grasslands and open plains in prides consisting of related 
+    females, their offspring, and a coalition of males. Lions are known for their cooperative hunting strategy and deep 
+    social bonds, marking them as unique among the world's wild cats.`;
+
+  const flyingSquirrel = `The flying squirrel is a fascinating mammal known for its ability to glide through the air. 
+    These nocturnal creatures are found in a variety of habitats, including forests and suburban areas across parts of 
+    North America, Europe, and Asia. They possess a unique patagium, a membrane that extends from wrist to ankle, 
+    facilitating their gliding abilities. Despite their name, flying squirrels don't technically fly but instead extend 
+    their arms and legs to glide from tree to tree, using their tail for steering and balance.`;
+
+  const penguin = `Penguins are flightless seabirds known for their distinct tuxedo-like appearance and waddling walk. 
+    They are primarily found in the Southern Hemisphere, particularly in Antarctica, but species can also be found in 
+    South Africa, South America, New Zealand, and the Galapagos Islands. Penguins are expert swimmers, using their 
+    flippers for propulsion and their feet as rudders, and they can dive deep into the ocean to catch their diet of fish, 
+    squid, and krill.`;
+
+  const turtle = `Turtles are cold-blooded reptiles identifiable by their bony or cartilaginous shell developed from their 
+    ribs. They are one of the oldest and most primitive groups of reptiles and have been on Earth for more than 200 million 
+    years. Found in various habitats from deserts to marine environments around the globe, turtles are known for their 
+    slow movement on land, longevity, and often complex mating and nesting behaviors.`;
+
+  const documents = [
+    new Document({
+      pageContent: lion,
+      metadata: {
+        document_id: "1",
+        title: "Lion",
+        category: "Mammal",
+        can_fly: false,
+      },
+    }),
+    new Document({
+      pageContent: flyingSquirrel,
+      metadata: {
+        document_id: "2",
+        title: "Flying Squirrel",
+        category: "Mammal",
+        can_fly: true,
+      },
+    }),
+    new Document({
+      pageContent: penguin,
+      metadata: {
+        document_id: "3",
+        title: "Penguin",
+        category: "Bird",
+        can_fly: false,
+      },
+    }),
+    new Document({
+      pageContent: turtle,
+      metadata: {
+        document_id: "4",
+        title: "Turtle",
+        category: "Reptile",
+        can_fly: false,
+      },
+    }),
+  ];
+  return documents;
+};
+
+beforeAll(async () => {
+  // Upload the documents to Vectara
   const store = new VectaraStore({
-    customerId: process.env.VECTARA_CUSTOMER_ID
-      ? parseInt(process.env.VECTARA_CUSTOMER_ID, 10)
-      : 0,
-    corpusId: process.env.VECTARA_CORPUS_ID
-      ? parseInt(process.env.VECTARA_CORPUS_ID, 10)
-      : 0,
-    apiKey: process.env.VECTARA_API_KEY || "",
+    customerId: VECTARA_CUSTOMER_ID,
+    corpusId: VECTARA_CORPUS_ID,
+    apiKey: VECTARA_API_KEY,
   });
-  const retriever = new VectaraRetriever(store);
 
-  // If you've uploaded the US constituion, this will return the relevant section
-  const docs = await retriever.getRelevantDocuments("Article 1 Section 7");
-
-  expect(docs.length).toBeGreaterThan(0);
-  // This checks that the document contains the relevant section
-  expect(docs[0].pageContent).toContain(
-    "All Bills for raising Revenue shall originate in the House of Representatives"
-  );
-
-  console.log(docs);
+  await store.addDocuments(getdoc());
 });
 
-test("VectaraRetriever (without filter)", async () => {
+test("Vectara Retriever (with filters)", async () => {
   const store = new VectaraStore({
-    customerId: process.env.VECTARA_CUSTOMER_ID
-      ? parseInt(process.env.VECTARA_CUSTOMER_ID, 10)
-      : 0,
-    corpusId: process.env.VECTARA_CORPUS_ID
-      ? parseInt(process.env.VECTARA_CORPUS_ID, 10)
-      : 0,
-    apiKey: process.env.VECTARA_API_KEY || "",
+    customerId: VECTARA_CUSTOMER_ID,
+    corpusId: VECTARA_CORPUS_ID,
+    apiKey: VECTARA_API_KEY,
   });
+
+  const retriever = new VectaraRetriever(store);
+  const mammals = await retriever.getRelevantDocuments("animal", 5, {
+    filter: "doc.category = 'Mammal'",
+  });
+  // expect all mammals results to contain either Lion or Flying Squirrel
+  expect(
+    mammals[0].pageContent.includes("Lion") ||
+      mammals[0].pageContent.includes("FLying Squirrel")
+  ).toBeTruthy();
+
+  const flyingAnimals = await retriever.getRelevantDocuments("animal", 5, {
+    filter: "doc.can_fly = true",
+  });
+
+  const flyingMammals = await retriever.getRelevantDocuments("animal", 5, {
+    filter: "doc.category = 'Mammal' AND doc.can_fly = true",
+  });
+  // expect all flyingMammals results to be same as flyingAnimals results
+  expect(flyingMammals).toEqual(flyingAnimals);
+
+  const reptileLion = await retriever.getRelevantDocuments("lion", 1, {
+    filter: "doc.category = 'Reptile' AND doc.can_fly = false",
+  });
+  const reptile = await retriever.getRelevantDocuments("lion", 1, {
+    filter: "doc.category = 'Reptile' AND doc.can_fly = false",
+  });
+  // expect all reptileLion results to be same as reptile results
+  expect(reptileLion).toEqual(reptile);
+});
+
+test("VectaraRetriever (without filters)", async () => {
+  const store = new VectaraStore({
+    customerId: VECTARA_CUSTOMER_ID,
+    corpusId: VECTARA_CORPUS_ID,
+    apiKey: VECTARA_API_KEY,
+  });
+
   const retriever = new VectaraRetriever(store);
 
-  const docs = await retriever.getRelevantDocuments("hello");
+  const doc = await retriever.getRelevantDocuments("cold-blooded animal");
 
-  expect(docs.length).toBeGreaterThan(0);
+  console.log(doc);
+  expect(doc.length).toBeGreaterThan(0);
+  expect(doc[0].pageContent).toContain("Turtles are cold-blooded reptiles");
 });
