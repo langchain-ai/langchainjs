9c168e1992c82d895fac4d217e9f0d8981770a63
Dmitry Tyumentsev <tyumentsevde@gmail.com>
2023-07-25 18:02:19 +0300

    add node: prefix

--- langchain/src/document_loaders/web/azure_blob_storage_file.ts
+++ langchain/src/document_loaders/web/azure_blob_storage_file.ts
@@ -1,6 +1,6 @@
-import * as fs from "fs";
-import * as path from "path";
-import * as os from "os";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as os from "node:os";
 import { BlobServiceClient } from "@azure/storage-blob";
 import { BaseDocumentLoader } from "../base.js";
 import {
