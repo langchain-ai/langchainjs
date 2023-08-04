66f7a99a8fc40f607dd47f051a4917b880704418
archie-swif <artem.ryabokon@gmail.com>
2023-08-03 19:31:31 -0500

    Refactored MongoDBAtlasVectorSearch to use abstract similaritySearchVectorWithScore for MMR
    Co-authored-by: Simone DM <simone.dellamarca@mongodb.com>

--- langchain/src/vectorstores/tests/mongodb_atlas.int.test.ts
+++ langchain/src/vectorstores/tests/mongodb_atlas.int.test.ts
@@ -1,12 +1,13 @@
 /* eslint-disable no-process-env */
 /* eslint-disable no-promise-executor-return */
 
-import { test, expect } from "@jest/globals";
-import { MongoClient } from "mongodb";
+import {test, expect} from "@jest/globals";
+import {MongoClient} from "mongodb";
 import { CohereEmbeddings } from "../../embeddings/cohere.js";
-import { MongoDBAtlasVectorSearch } from "../mongodb_atlas.js";
+import {MongoDBAtlasVectorSearch} from "../mongodb_atlas.js";
 
-import { Document } from "../../document.js";
+import {Document} from "../../document.js";
+import {OpenAIEmbeddings} from "../../embeddings/openai.js";
 
 /**
  * The following json can be used to create an index in atlas for Cohere embeddings.
@@ -26,70 +27,112 @@ import { Document } from "../../document.js";
 */
 
 function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-test.skip("MongoDBAtlasVectorSearch with external ids", async () => {
-  expect(process.env.MONGODB_ATLAS_URI).toBeDefined();
+test("MongoDBAtlasVectorSearch with external ids", async () => {
+    expect(process.env.MONGODB_ATLAS_URI).toBeDefined();
+    expect(process.env.COHERE_API_KEY).toBeDefined();
 
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  const client = new MongoClient(process.env.MONGODB_ATLAS_URI!);
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const client = new MongoClient(process.env.MONGODB_ATLAS_URI!);
 
-  try {
-    const namespace = "langchain.test";
-    const [dbName, collectionName] = namespace.split(".");
-    const collection = client.db(dbName).collection(collectionName);
+    try {
+        const namespace = "langchain.test";
+        const [dbName, collectionName] = namespace.split(".");
+        const collection = client.db(dbName).collection(collectionName);
 
     const vectorStore = new MongoDBAtlasVectorSearch(new CohereEmbeddings(), {
-      collection,
-    });
+            collection,
+        });
 
-    expect(vectorStore).toBeDefined();
+        expect(vectorStore).toBeDefined();
 
-    // check if the database is empty
-    await collection.deleteMany({});
+        // check if the database is empty
+        await collection.deleteMany({});
 
-    await vectorStore.addDocuments([
-      { pageContent: "Dogs are tough.", metadata: { a: 1 } },
-      { pageContent: "Cats have fluff.", metadata: { b: 1 } },
-      { pageContent: "What is a sandwich?", metadata: { c: 1 } },
-      { pageContent: "That fence is purple.", metadata: { d: 1, e: 2 } },
-    ]);
+        await vectorStore.addDocuments([
+            {pageContent: "Dogs are tough.", metadata: {a: 1}},
+            {pageContent: "Cats have fluff.", metadata: {b: 1}},
+            {pageContent: "What is a sandwich?", metadata: {c: 1}},
+            {pageContent: "That fence is purple.", metadata: {d: 1, e: 2}},
+        ]);
 
-    // we sleep 2 seconds to make sure the index in atlas has replicated the new documents
-    await sleep(2000);
-    const results: Document[] = await vectorStore.similaritySearch(
-      "Sandwich",
-      1
-    );
+        // we sleep 2 seconds to make sure the index in atlas has replicated the new documents
+        await sleep(2000);
+        const results: Document[] = await vectorStore.similaritySearch(
+            "Sandwich",
+            1
+        );
 
-    expect(results.length).toEqual(1);
-    expect(results).toMatchObject([
-      { pageContent: "What is a sandwich?", metadata: { c: 1 } },
-    ]);
+        expect(results.length).toEqual(1);
+        expect(results).toMatchObject([
+            {pageContent: "What is a sandwich?", metadata: {c: 1}},
+        ]);
 
-    // we can pre filter the search
-    const preFilter = {
-      range: { lte: 1, path: "e" },
-    };
+        // we can pre filter the search
+        const preFilter = {
+            range: {lte: 1, path: "e"},
+        };
 
-    const filteredResults = await vectorStore.similaritySearch(
-      "That fence is purple",
-      1,
-      preFilter
-    );
+        const filteredResults = await vectorStore.similaritySearch(
+            "That fence is purple",
+            1,
+            preFilter
+        );
 
-    expect(filteredResults).toEqual([]);
+        expect(filteredResults).toEqual([]);
 
-    const retriever = vectorStore.asRetriever({
-      filter: {
-        preFilter,
-      },
-    });
+        const retriever = vectorStore.asRetriever({
+            filter: {
+                preFilter,
+            },
+        });
 
-    const docs = await retriever.getRelevantDocuments("That fence is purple");
-    console.log(docs);
-  } finally {
-    await client.close();
-  }
+        const docs = await retriever.getRelevantDocuments("That fence is purple");
+        expect(docs).toEqual([]);
+    } finally {
+        await client.close();
+    }
+});
+
+test("MongoDBAtlasVectorSearch with Maximal Marginal Relevance", async () => {
+    expect(process.env.MONGODB_ATLAS_URI).toBeDefined();
+    expect(process.env.OPENAI_API_KEY || process.env.AZURE_OPENAI_API_KEY).toBeDefined();
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const client = new MongoClient(process.env.MONGODB_ATLAS_URI!);
+    try {
+        const namespace = "langchain.test";
+        const [dbName, collectionName] = namespace.split(".");
+        const collection = client.db(dbName).collection(collectionName);
+
+        await collection.deleteMany({});
+
+        const texts = ["foo", "foo", "fou", "foy"];
+        const vectorStore = await MongoDBAtlasVectorSearch.fromTexts(
+            texts,
+            {},
+            new OpenAIEmbeddings(),
+            {collection}
+        );
+
+        // we sleep 2 seconds to make sure the index in atlas has replicated the new documents
+        await sleep(2000);
+
+        const output = await vectorStore.maxMarginalRelevanceSearch(
+            "foo",
+            10,
+            20,
+            0.1,
+        );
+
+        expect(output).toHaveLength(texts.length);
+
+        const actual = output.map((doc) => doc.pageContent);
+        const expected = ["foo", "fou", "foy", "foo"];
+        expect(actual).toEqual(expected);
+    } finally {
+        await client.close();
+    }
 });
