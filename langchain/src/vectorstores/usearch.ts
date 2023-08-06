ba9ad40e7064b4809849a8f1a2cc39d4043bbef3
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2023-08-06 22:50:51 +0400

    Add: USearch Vector Store
    Co-authored-by: Davit Vardanyan <78792753+davvard@users.noreply.github.com>

---
+++ langchain/src/vectorstores/usearch.ts
@@ -0,0 +1,202 @@
+import type { Index } from "usearch";
+import * as uuid from "uuid";
+import { Embeddings } from "../embeddings/base.js";
+import { SaveableVectorStore } from "./base.js";
+import { Document } from "../document.js";
+import { SynchronousInMemoryDocstore } from "../stores/doc/in_memory.js";
+
+
+export interface USearchArgs {
+  docstore?: SynchronousInMemoryDocstore;
+  index?: Index;
+  mapping?: Record<number, string>;
+}
+
+
+export class USearch extends SaveableVectorStore {
+  _index?: Index;
+
+
+  _mapping: Record<number, string>;
+
+
+  docstore: SynchronousInMemoryDocstore;
+
+
+  args: USearchArgs;
+
+
+  _vectorstoreType(): string {
+    return "usearch";
+  }
+
+
+  constructor(embeddings: Embeddings, args: USearchArgs) {
+    super(embeddings, args);
+    this.args = args;
+    this._index = args.index;
+    this._mapping = args.mapping ?? {};
+    this.embeddings = embeddings;
+    this.docstore = args?.docstore ?? new SynchronousInMemoryDocstore();
+  }
+
+
+  async addDocuments(documents: Document[]) {
+    const texts = documents.map(({ pageContent }) => pageContent);
+    return this.addVectors(
+      await this.embeddings.embedDocuments(texts),
+      documents
+    );
+  }
+
+
+  public get index(): Index {
+    if (!this._index) {
+      throw new Error(
+        "Vector store not initialised yet. Try calling `fromTexts` or `fromDocuments` first."
+      );
+    }
+    return this._index;
+  }
+
+
+  private set index(index: Index) {
+    this._index = index;
+  }
+
+
+  async addVectors(vectors: number[][], documents: Document[]) {
+    if (vectors.length === 0) {
+      return [];
+    }
+    if (vectors.length !== documents.length) {
+      throw new Error(`Vectors and documents must have the same length`);
+    }
+    const dv = vectors[0].length;
+    if (!this._index) {
+      const { Index } = await USearch.importUSearch();
+      this._index = new Index({ metric: 'l2sq', connectivity: 16, dimensions: dv });
+    }
+    const d = this.index.dimensions();
+    if (dv !== d) {
+      throw new Error(
+        `Vectors must have the same length as the number of dimensions (${d})`
+      );
+    }
+
+
+    const docstoreSize = this.index.size();
+    const documentIds = [];
+    for (let i = 0; i < vectors.length; i += 1) {
+      const documentId = uuid.v4();
+      documentIds.push(documentId);
+      const id = docstoreSize + i;
+      this.index.add(BigInt(id), new Float32Array(vectors[i]));
+      this._mapping[id] = documentId;
+      this.docstore.add({ [documentId]: documents[i] });
+    }
+    return documentIds;
+  }
+
+
+  async similaritySearchVectorWithScore(query: number[], k: number) {
+    const d = this.index.dimensions();
+    if (query.length !== d) {
+      throw new Error(
+        `Query vector must have the same length as the number of dimensions (${d})`
+      );
+    }
+    if (k > this.index.size()) {
+      const total = this.index.size();
+      console.warn(
+        `k (${k}) is greater than the number of elements in the index (${total}), setting k to ${total}`
+      );
+      // eslint-disable-next-line no-param-reassign
+      k = total;
+    }
+    const result = this.index.search(new Float32Array(query), k);
+
+
+    let return_list: [Document, number][] = [];
+    for (let i = 0; i < result.count; i++) {
+      const uuid = this._mapping[Number(result.keys[i])];
+      return_list.push([this.docstore.search(uuid), result.distances[i]]);
+    }
+
+
+    return return_list;
+  }
+
+
+  async save(directory: string) {
+    const fs = await import("node:fs/promises");
+    const path = await import("node:path");
+    await fs.mkdir(directory, { recursive: true });
+    await Promise.all([
+      this.index.save(path.join(directory, "usearch.index")),
+      await fs.writeFile(
+        path.join(directory, "docstore.json"),
+        JSON.stringify([
+          Array.from(this.docstore._docs.entries()),
+          this._mapping,
+        ])
+      ),
+    ]);
+  }
+
+
+  static async fromTexts(
+    texts: string[],
+    metadatas: object[] | object,
+    embeddings: Embeddings,
+    dbConfig?: {
+      docstore?: SynchronousInMemoryDocstore;
+    }
+  ): Promise<USearch> {
+    const docs: Document[] = [];
+    for (let i = 0; i < texts.length; i += 1) {
+      const metadata = Array.isArray(metadatas) ? metadatas[i] : metadatas;
+      const newDoc = new Document({
+        pageContent: texts[i],
+        metadata,
+      });
+      docs.push(newDoc);
+    }
+    return this.fromDocuments(docs, embeddings, dbConfig);
+  }
+
+
+  static async fromDocuments(
+    docs: Document[],
+    embeddings: Embeddings,
+    dbConfig?: {
+      docstore?: SynchronousInMemoryDocstore;
+    }
+  ): Promise<USearch> {
+    const args: USearchArgs = {
+      docstore: dbConfig?.docstore,
+    };
+    const instance = new this(embeddings, args);
+    await instance.addDocuments(docs);
+    return instance;
+  }
+
+
+  static async importUSearch(): Promise<{ Index: typeof Index }> {
+    try {
+      const {
+        default: { Index },
+      } = await import("usearch");
+
+
+      return { Index };
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      throw new Error(
+        `Could not import usearch. Please install usearch as a dependency with, e.g. \`npm install usearch\`.\n\nError: ${err?.message}`
+      );
+    }
+  }
+
+
+}
