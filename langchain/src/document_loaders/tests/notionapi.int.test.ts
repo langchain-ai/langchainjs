d5170ba195d26f33c54fea14674ed5e21c151329
skarard <skarard@gmail.com>
2023-08-07 12:50:01 +0100

    fixed some dependancies and removed human verification of output from notionapi test results

--- langchain/src/document_loaders/tests/notionapi.int.test.ts
+++ langchain/src/document_loaders/tests/notionapi.int.test.ts
@@ -2,8 +2,6 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { test } from "@jest/globals";
 import { NotionAPILoader } from "../web/notionapi.js";
-import * as fs from "node:fs";
-import YAML from "yaml";
 
 test("Test Notion API Loader Page", async () => {
   const loader = new NotionAPILoader({
@@ -40,14 +38,4 @@ test("Test Notion API Loader Database", async () => {
   const titles = docs.map((doc) => doc.metadata.properties.title);
   console.log("Titles:", titles);
   console.log(`Loaded ${docs.length} pages from the database`);
-
-  for (const doc of docs) {
-    const fileName = doc.metadata.properties.title;
-    const contents = `---\n${YAML.stringify(doc.metadata)}---
-${doc.pageContent}`;
-    fs.writeFileSync(
-      `C:\\Users\\richa\\Desktop\\results\\${fileName}.md`,
-      contents
-    );
-  }
 });
