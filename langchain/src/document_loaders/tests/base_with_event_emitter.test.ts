dacfa3ca4d93a549a6d0738579706a4d70c37ce0
skarard <skarard@gmail.com>
2023-08-08 20:03:50 +0100

    Revert "improving the speed of the test"
    This reverts commit 4518a3bc0bc6feb23bb3999bd25b5c6ae863f157.

--- langchain/src/document_loaders/tests/base_with_event_emitter.test.ts
+++ langchain/src/document_loaders/tests/base_with_event_emitter.test.ts
@@ -19,9 +19,9 @@ class ExampleTextLoaderWithEvents extends BaseDocumentLoaderWithEventEmitter {
 
   private async parseText(text: string | string[]) {
     // Simulate async delay
-    // await new Promise((resolve) => {
-    //   setTimeout(resolve, 250);
-    // });
+    await new Promise((resolve) => {
+      setTimeout(resolve, 250);
+    });
 
     if (typeof text === "string") {
       const doc = new Document({
@@ -62,6 +62,42 @@ test("Example text loader with events", async () => {
       "There's so much to learn.",
     ],
     "I need to do some grocery shopping.",
+    "I went for a run in the park.",
+    "Listening to the sound of waves is calming.",
+    "I love taking photographs to capture memories.",
+    "eating pizza.",
+    "Football is my favorite sport.",
+    "The cat chased the mouse.",
+    "I enjoy reading mystery novels.",
+    "Coffee keeps me awake.",
+    "My car broke down yesterday.",
+    ["The sun is shining.", "It's a beautiful day."],
+    "I like listening to music.",
+    ["I prefer summer over winter.", "I enjoy swimming and sunbathing."],
+    "I took a long walk in the park.",
+    "Watching movies is a favorite pastime.",
+    "The dog wagged its tail happily.",
+    "I am going on vacation tomorrow.",
+    "Learning new things is exciting.",
+    "The rain gave life to the flowers.",
+    "I couldn't stop laughing at the comedian's jokes.",
+    "Going to the beach is always fun.",
+    ["Hiking in the mountains is breathtaking.", "The view is spectacular."],
+    ["Cooking is a great way to relax.", "I enjoy trying new recipes."],
+    "I woke up early this morning.",
+    "Reading a book before bed helps me relax.",
+    "I bought a new laptop today.",
+    "I love spending time with my family.",
+    "The cake tasted delicious.",
+    "I enjoy playing video games.",
+    [
+      "Visiting museums is a enriching experience.",
+      "There's so much to learn.",
+    ],
+    "I need to do some grocery shopping.",
+    "I went for a run in the park.",
+    "Listening to the sound of waves is calming.",
+    "I love taking photographs to capture memories.",
   ];
 
   const loader = new ExampleTextLoaderWithEvents(sampleData);
@@ -77,5 +113,5 @@ test("Example text loader with events", async () => {
 
   const docs = await loader.load();
 
-  expect(docs.length).toBe(6);
+  expect(docs.length).toBe(44);
 });
