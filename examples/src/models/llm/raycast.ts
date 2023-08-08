39470ef5a181add92a30579e5d32ffdd8d06269b
Alireza <contact@alireza.cc>
2023-08-08 22:20:06 +0200

    docs: tried and found the best model to integrate raycast ai and langchain agents

--- examples/src/models/llm/raycast.ts
+++ examples/src/models/llm/raycast.ts
@@ -6,7 +6,8 @@ import { initializeAgentExecutorWithOptions } from "langchain/agents";
 
 const model = new RaycastAI({
   rateLimitPerMinute: 10, // It is 10 by default so you can omit this line
-  model: "text-davinci-003",
+  model: "gpt-3.5-turbo",
+  creativity: 0, // `creativity` is a term used by Raycast which is equivalent to `temperature` in some other LLMs
 });
 
 const tools: Tool[] = [
@@ -16,7 +17,7 @@ const tools: Tool[] = [
 export default async function main() {
   // Initialize the agent executor with RaycastAI model
   const executor = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: "zero-shot-react-description",
+    agentType: "chat-conversational-react-description",
   });
 
   const input = `Describe my today's schedule as Gabriel Garcia Marquez would describe it`;
