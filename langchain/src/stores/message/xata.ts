84b901012facf13bba2004def8a8c90a3d54a863
Tudor Golubenco <tudor@xata.io>
2023-08-08 20:06:12 +0100

    Adding Xata as a chat messages memory store

---
+++ langchain/src/stores/message/xata.ts
@@ -0,0 +1,106 @@
+import { BaseClient, BaseClientOptions } from "@xata.io/client";
+import {
+  BaseMessage,
+  BaseListChatMessageHistory,
+  StoredMessage,
+  StoredMessageData,
+} from "../../schema/index.js";
+import {
+  mapChatMessagesToStoredMessages,
+  mapStoredMessagesToChatMessages,
+} from "./utils.js";
+
+export type XataChatMessageHistoryInput<XataClient> = {
+  sessionId: string;
+  config?: BaseClientOptions;
+  client?: XataClient;
+  table?: string;
+};
+
+interface storedMessagesDTO {
+  id: string;
+  sessionId: string;
+  type: string;
+  content: string;
+  role?: string;
+  name?: string;
+  additionalKwargs: string;
+}
+
+export class XataChatMessageHistory<
+  XataClient extends BaseClient
+> extends BaseListChatMessageHistory {
+  lc_namespace = ["langchain", "stores", "message", "xata"];
+
+  public client: XataClient;
+
+  private sessionId: string;
+
+  private table: string;
+
+  constructor(fields: XataChatMessageHistoryInput<XataClient>) {
+    super(fields);
+
+    const { sessionId, config, client, table } = fields;
+    this.sessionId = sessionId;
+    this.table = table || "memory";
+    if (client) {
+      this.client = client;
+    } else if (config) {
+      this.client = new BaseClient(config) as XataClient;
+    } else {
+      throw new Error(
+        "Either a client or a config must be provided to XataChatMessageHistoryInput"
+      );
+    }
+  }
+
+  async getMessages(): Promise<BaseMessage[]> {
+    const records = await this.client.db[this.table]
+      .filter({ sessionId: this.sessionId })
+      .getAll();
+
+    const rawStoredMessages = records as unknown as storedMessagesDTO[];
+    const orderedMessages: StoredMessage[] = rawStoredMessages.map(
+      (message: storedMessagesDTO) => {
+        const data = {
+          content: message.content,
+          additional_kwargs: JSON.parse(message.additionalKwargs),
+        } as StoredMessageData;
+        if (message.role) {
+          data.role = message.role;
+        }
+        if (message.name) {
+          data.name = message.name;
+        }
+
+        return {
+          type: message.type,
+          data,
+        };
+      }
+    );
+    return mapStoredMessagesToChatMessages(orderedMessages);
+  }
+
+  async addMessage(message: BaseMessage): Promise<void> {
+    const messageToAdd = mapChatMessagesToStoredMessages([message]);
+    await this.client.db[this.table].create({
+      sessionId: this.sessionId,
+      type: messageToAdd[0].type,
+      content: messageToAdd[0].data.content,
+      role: messageToAdd[0].data.role,
+      name: messageToAdd[0].data.name,
+      additionalKwargs: JSON.stringify(messageToAdd[0].data.additional_kwargs),
+    });
+  }
+
+  async clear(): Promise<void> {
+    const records = await this.client.db[this.table]
+      .select(["id"])
+      .filter({ sessionId: this.sessionId })
+      .getAll();
+    const ids = records.map((m) => m.id);
+    await this.client.db[this.table].delete(ids);
+  }
+}
---
+++ langchain/src/stores/tests/xata.int.test.ts
@@ -0,0 +1,95 @@
+/* eslint-disable no-process-env */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BaseClient } from "@xata.io/client";
+import { XataChatMessageHistory } from "../message/xata.js";
+import { AIMessage, HumanMessage } from "../../schema/index.js";
+import { BufferMemory } from "../../memory/buffer_memory.js";
+import { ChatOpenAI } from "../../chat_models/openai.js";
+import { ConversationChain } from "../../chains/conversation.js";
+
+describe("XataChatMessageHistory", () => {
+  if (!process.env.XATA_API_KEY) {
+    throw new Error("XATA_API_KEY not set");
+  }
+
+  if (!process.env.XATA_DB_URL) {
+    throw new Error("XATA_DB_URL not set");
+  }
+  const xata = new BaseClient({
+    databaseURL: process.env.XATA_DB_URL,
+    apiKey: process.env.XATA_API_KEY,
+    branch: process.env.XATA_BRANCH || "main",
+  });
+
+  afterAll(async () => {
+    const records = await xata.db.memory.select(["id"]).getAll();
+    await xata.db.memory.delete(records.map((m) => m.id));
+  });
+
+  test("Test Xata history store", async () => {
+    const chatHistory = new XataChatMessageHistory({
+      sessionId: new Date().toISOString(),
+      client: xata,
+    });
+
+    const blankResult = await chatHistory.getMessages();
+    expect(blankResult).toStrictEqual([]);
+
+    await chatHistory.addUserMessage("Who is the best vocalist?");
+    await chatHistory.addAIChatMessage("Ozzy Osbourne");
+
+    const expectedMessages = [
+      new HumanMessage("Who is the best vocalist?"),
+      new AIMessage("Ozzy Osbourne"),
+    ];
+
+    const resultWithHistory = await chatHistory.getMessages();
+    expect(resultWithHistory).toEqual(expectedMessages);
+  });
+
+  test("Test Xata memory with Buffer Memory", async () => {
+    const memory = new BufferMemory({
+      returnMessages: true,
+      chatHistory: new XataChatMessageHistory({
+        sessionId: new Date().toISOString(),
+        client: xata,
+      }),
+    });
+
+    await memory.saveContext(
+      { input: "Who is the best vocalist?" },
+      { response: "Ozzy Osbourne" }
+    );
+
+    const expectedHistory = [
+      new HumanMessage("Who is the best vocalist?"),
+      new AIMessage("Ozzy Osbourne"),
+    ];
+
+    const result2 = await memory.loadMemoryVariables({});
+    expect(result2).toStrictEqual({ history: expectedHistory });
+  });
+
+  test("Test Xata memory with LLM Chain", async () => {
+    const memory = new BufferMemory({
+      chatHistory: new XataChatMessageHistory({
+        sessionId: new Date().toISOString(),
+        client: xata,
+      }),
+    });
+
+    const model = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+    const chain = new ConversationChain({ llm: model, memory });
+
+    const res1 = await chain.call({ input: "Hi! I'm Jim." });
+    console.log({ res1 });
+
+    const res2 = await chain.call({
+      input: "What did I just say my name was?",
+    });
+    console.log({ res2 });
+  });
+});
