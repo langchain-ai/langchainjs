01f521b239e8c919a12011438c2fa89d500c82c1
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-08-09 21:53:56 -0700

    complete tests

--- langchain/src/vectorstores/tests/zep.test.ts
+++ langchain/src/vectorstores/tests/zep.test.ts
@@ -1,107 +1,236 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import {expect, jest, test} from "@jest/globals";
+import { expect, jest, test } from "@jest/globals";
 
-import {DocumentCollection, NotFoundError, ZepClient} from "@getzep/zep-js";
-import {IZepConfig, ZepVectorStore} from "../zep.js";
-import {Embeddings} from "../../embeddings/base.js";
-import {FakeEmbeddings} from "../../embeddings/fake.js";
+import {
+  Document as ZepDocument,
+  DocumentCollection,
+  IDocument,
+  NotFoundError,
+  ZepClient,
+} from "@getzep/zep-js";
+import { Document } from "../../document.js";
+import { IZepConfig, ZepVectorStore } from "../zep.js";
+import { Embeddings } from "../../embeddings/base.js";
+import { FakeEmbeddings } from "../../embeddings/fake.js";
 
-
-jest.mock('@getzep/zep-js');
+jest.mock("@getzep/zep-js");
 
 const mockDocuments = [
-    {
-        pageContent: "foo bar baz",
-        metadata: {bar: "baz"},
-    },
-    {
-        pageContent: "foo qux baz",
-        metadata: {qux: "bar"},
-    },
-    {
-        pageContent: "foo bar baz",
-        metadata: {foo: "bar"},
-    }
-]
+  {
+    pageContent: "foo bar baz",
+    metadata: { bar: "baz" },
+  },
+  {
+    pageContent: "foo qux baz",
+    metadata: { qux: "bar" },
+  },
+  {
+    pageContent: "foo bar baz",
+    metadata: { foo: "bar" },
+  },
+];
+
+const mockZepDocuments: IDocument[] = mockDocuments.map((doc, index) => ({
+  uuid: `uuid${index}`,
+  content: doc.pageContent,
+  metadata: doc.metadata,
+  score: Math.random(),
+}));
 
 const mockCollection = {
-    addDocuments: jest.fn<DocumentCollection["addDocuments"]>().mockResolvedValue(["uuid1", "uuid2", "uuid3"]),
-    search: jest.fn<DocumentCollection["search"]>().mockResolvedValue(mockDocuments as any),
-    deleteDocument: jest.fn<DocumentCollection["deleteDocument"]>().mockResolvedValue(undefined as any),
+  addDocuments: jest
+    .fn<DocumentCollection["addDocuments"]>()
+    .mockResolvedValue(["uuid1", "uuid2", "uuid3"]),
+  search: jest
+    .fn<DocumentCollection["search"]>()
+    .mockResolvedValue(mockZepDocuments as any),
+  deleteDocument: jest
+    .fn<DocumentCollection["deleteDocument"]>()
+    .mockResolvedValue(undefined as any),
 } as any;
 
 const mockClient = {
-    document: {
-        getCollection: jest.fn<any>().mockResolvedValue(mockCollection),
-        addCollection: jest.fn<any>().mockResolvedValue(mockCollection)
-    }
+  document: {
+    getCollection: jest.fn<any>().mockResolvedValue(mockCollection),
+    addCollection: jest.fn<any>().mockResolvedValue(mockCollection),
+  },
 } as any;
 
+describe("ZepVectorStore", () => {
+  let zepConfig: IZepConfig;
+  let embeddings: Embeddings;
 
-describe('ZepVectorStore', () => {
-    let zepConfig: IZepConfig;
-    let embeddings: Embeddings;
+  beforeEach(() => {
+    zepConfig = {
+      apiUrl: "http://api.zep.com",
+      apiKey: "123456",
+      collectionName: "testCollection",
+      description: "Test Description",
+      metadata: {},
+      embeddingDimensions: 100,
+      isAutoEmbedded: true,
+    };
+    embeddings = new FakeEmbeddings();
 
-    beforeEach(() => {
-        zepConfig = {
-            apiUrl: 'http://api.zep.com',
-            apiKey: '123456',
-            collectionName: 'testCollection',
-            description: 'Test Description',
-            metadata: {},
-            embeddingDimensions: 100,
-            isAutoEmbedded: true
-        };
-        embeddings = new FakeEmbeddings();
+    jest
+      .spyOn(ZepClient, "init")
+      .mockImplementation(() => Promise.resolve(mockClient));
+  });
 
-        jest.spyOn(ZepClient, 'init').mockImplementation(() => Promise.resolve(mockClient));
+  test("should instantiate class successfully when a Collection exists", async () => {
+    new ZepVectorStore(embeddings, zepConfig);
+
+    // Wait for any promises in constructor to resolve
+    await new Promise(setImmediate);
+
+    expect(ZepClient.init).toHaveBeenCalledWith(
+      zepConfig.apiUrl,
+      zepConfig.apiKey
+    );
+    expect(mockClient.document.getCollection).toHaveBeenCalledWith(
+      zepConfig.collectionName
+    );
+  });
+
+  test("should instantiate class successfully when a Collection does not exist", async () => {
+    mockClient.document.getCollection.mockRejectedValueOnce(
+      new NotFoundError("Collection not found")
+    );
+
+    new ZepVectorStore(embeddings, zepConfig);
+
+    // Wait for any promises in constructor to resolve
+    await new Promise(setImmediate);
+
+    expect(ZepClient.init).toHaveBeenCalledWith(
+      zepConfig.apiUrl,
+      zepConfig.apiKey
+    );
+    expect(mockClient.document.getCollection).toHaveBeenCalledWith(
+      zepConfig.collectionName
+    );
+    expect(mockClient.document.addCollection).toHaveBeenCalledWith({
+      name: zepConfig.collectionName,
+      description: zepConfig.description,
+      metadata: zepConfig.metadata,
+      embeddingDimensions: zepConfig.embeddingDimensions,
+      isAutoEmbedded: zepConfig.isAutoEmbedded,
     });
+  });
 
-    test('should instantiate class successfully when a Collection exists', async () => {
-        new ZepVectorStore(embeddings, zepConfig);
+  test("should add documents successfully", async () => {
+    const zepVectorStore = new ZepVectorStore(embeddings, zepConfig);
 
-        // Wait for any promises in constructor to resolve
-        await new Promise(setImmediate);
+    (zepVectorStore as any).collection = mockCollection;
 
-        expect(ZepClient.init).toHaveBeenCalledWith(zepConfig.apiUrl, zepConfig.apiKey);
-        expect(mockClient.document.getCollection).toHaveBeenCalledWith(zepConfig.collectionName);
+    const result = await zepVectorStore.addDocuments(mockDocuments);
+
+    expect(mockCollection.addDocuments).toHaveBeenCalledWith(
+      expect.arrayContaining([
+        expect.objectContaining({
+          content: "foo bar baz",
+          metadata: { bar: "baz" },
+        }),
+        expect.objectContaining({
+          content: "foo qux baz",
+          metadata: { qux: "bar" },
+        }),
+        expect.objectContaining({
+          content: "foo bar baz",
+          metadata: { foo: "bar" },
+        }),
+      ])
+    );
+
+    expect(result).toEqual(["uuid1", "uuid2", "uuid3"]);
+  });
+
+  test("should delete documents successfully", async () => {
+    const zepVectorStore = new ZepVectorStore(embeddings, zepConfig);
+    // Inject mockCollection into zepVectorStore
+    (zepVectorStore as any).collection = mockCollection;
+
+    const uuidsToDelete = ["uuid1", "uuid2", "uuid3"];
+
+    await zepVectorStore.delete({ uuids: uuidsToDelete });
+
+    uuidsToDelete.forEach((uuid) => {
+      expect(mockCollection.deleteDocument).toHaveBeenCalledWith(uuid);
     });
+  });
 
-    test('should instantiate class successfully when a Collection does not exist', async () => {
-        mockClient.document.getCollection.mockRejectedValueOnce(new NotFoundError("Collection not found"));
+  test("should create ZepVectorStore from texts successfully", async () => {
+    const texts = ["text1", "text2", "text3"];
+    const metadatas = [{ foo: "bar" }, { baz: "qux" }, { quux: "corge" }];
 
-        new ZepVectorStore(embeddings, zepConfig);
-
-        // Wait for any promises in constructor to resolve
-        await new Promise(setImmediate);
-
-        expect(ZepClient.init).toHaveBeenCalledWith(zepConfig.apiUrl, zepConfig.apiKey);
-        expect(mockClient.document.getCollection).toHaveBeenCalledWith(zepConfig.collectionName);
-        expect(mockClient.document.addCollection).toHaveBeenCalledWith({
-            name: zepConfig.collectionName,
-            description: zepConfig.description,
-            metadata: zepConfig.metadata,
-            embeddingDimensions: zepConfig.embeddingDimensions,
-            isAutoEmbedded: zepConfig.isAutoEmbedded
-        });
-    });
-
-    test('should add documents successfully', async () => {
-        const zepVectorStore = new ZepVectorStore(embeddings, zepConfig);
-
+    // Mock ZepVectorStore.fromDocuments to inject mockCollection
+    const originalFromDocuments = ZepVectorStore.fromDocuments;
+    ZepVectorStore.fromDocuments = jest.fn(
+      async (docs, embeddings, zepConfig) => {
+        const zepVectorStore = await originalFromDocuments.call(
+          ZepVectorStore,
+          docs as Document[],
+          embeddings as Embeddings,
+          zepConfig as IZepConfig
+        );
         (zepVectorStore as any).collection = mockCollection;
+        return zepVectorStore;
+      }
+    );
 
-        const result = await zepVectorStore.addDocuments(mockDocuments);
+    const zepVectorStore = await ZepVectorStore.fromTexts(
+      texts,
+      metadatas,
+      embeddings,
+      zepConfig
+    );
 
-        expect(mockCollection.addDocuments).toHaveBeenCalledWith(expect.arrayContaining([
-            expect.objectContaining({content: 'foo bar baz', metadata: {bar: 'baz'}}),
-            expect.objectContaining({content: 'foo qux baz', metadata: {qux: 'bar'}}),
-            expect.objectContaining({content: 'foo bar baz', metadata: {foo: 'bar'}}),
-        ]));
+    expect(zepVectorStore).toBeInstanceOf(ZepVectorStore);
 
-        expect(result).toEqual(["uuid1", "uuid2", "uuid3"]);
-    });
+    expect(mockCollection.addDocuments).toHaveBeenCalledWith(
+      expect.arrayContaining([
+        expect.any(ZepDocument),
+        expect.any(ZepDocument),
+        expect.any(ZepDocument),
+      ])
+    );
 
+    // Restore the original ZepVectorStore.fromDocuments
+    ZepVectorStore.fromDocuments = originalFromDocuments;
+  });
 
+  test("should perform similarity search with score successfully", async () => {
+    const zepVectorStore = new ZepVectorStore(embeddings, zepConfig);
+    // Inject mockCollection into zepVectorStore
+    (zepVectorStore as any).collection = mockCollection;
+
+    const query = [0.1, 0.2, 0.3, 0.4, 0.5];
+    const k = 3;
+    const filter = { foo: "bar" };
+
+    const result = await zepVectorStore.similaritySearchVectorWithScore(
+      query,
+      k,
+      filter
+    );
+
+    expect(mockCollection.search).toHaveBeenCalledWith(
+      expect.objectContaining({
+        embedding: new Float32Array(query),
+        metadata: filter,
+      }),
+      k
+    );
+
+    const docsAndScores = mockZepDocuments.map((doc) => [
+      new Document({
+        pageContent: doc.content,
+        metadata: doc.metadata,
+      }),
+      doc.score,
+    ]);
+
+    expect(result).toEqual(docsAndScores);
+  });
 });
--- langchain/src/vectorstores/zep.ts
+++ langchain/src/vectorstores/zep.ts
@@ -1,168 +1,188 @@
-import {Document as ZepDocument, DocumentCollection, NotFoundError, ZepClient} from "@getzep/zep-js";
+import {
+  Document as ZepDocument,
+  DocumentCollection,
+  NotFoundError,
+  ZepClient,
+} from "@getzep/zep-js";
 
-import {VectorStore} from "./base.js";
-import {Embeddings} from "../embeddings/base.js";
-import {Document} from "../document.js";
-
+import { VectorStore } from "./base.js";
+import { Embeddings } from "../embeddings/base.js";
+import { Document } from "../document.js";
 
 export interface IZepArgs {
-    collection: DocumentCollection;
+  collection: DocumentCollection;
 }
 
 export interface IZepConfig {
-    apiUrl: string;
-    apiKey: string;
-    collectionName: string;
-    description?: string;
-    metadata?: Record<string, any>;
-    embeddingDimensions?: number;
-    isAutoEmbedded?: boolean;
+  apiUrl: string;
+  apiKey: string;
+  collectionName: string;
+  description?: string;
+  metadata?: Record<string, any>;
+  embeddingDimensions?: number;
+  isAutoEmbedded?: boolean;
 }
 
 export interface IZepDeleteParams {
-    uuids: string[];
+  uuids: string[];
 }
 
 export class ZepVectorStore extends VectorStore {
-    private client: ZepClient;
+  private client: ZepClient;
 
-    private collection: DocumentCollection;
+  private initPromise: Promise<void>;
 
-    constructor(embeddings: Embeddings, args: IZepConfig) {
-        super(embeddings, args);
+  private collection: DocumentCollection;
 
-        this.initCollection(args).catch(err => {
-            console.error('Error retrieving collection:', err);
-        });
-        this.embeddings = embeddings;
+  constructor(embeddings: Embeddings, args: IZepConfig) {
+    super(embeddings, args);
+
+    this.initPromise = this.initCollection(args).catch((err) => {
+      console.error("Error retrieving collection:", err);
+    });
+    this.embeddings = embeddings;
+  }
+
+  private async initCollection(args: IZepConfig) {
+    this.client = await ZepClient.init(args.apiUrl, args.apiKey);
+    try {
+      this.collection = await this.client.document.getCollection(
+        args.collectionName
+      );
+    } catch (err) {
+      // eslint-disable-next-line no-instanceof/no-instanceof
+      if (err instanceof NotFoundError) {
+        await this.createCollection(args);
+      } else {
+        throw err;
+      }
     }
+  }
 
-    private async initCollection(args: IZepConfig) {
-        this.client = await ZepClient.init(args.apiUrl, args.apiKey);
-        try {
-            this.collection = await this.client.document.getCollection(args.collectionName);
-        } catch (err) {
-            // eslint-disable-next-line no-instanceof/no-instanceof
-            if (err instanceof NotFoundError) {
-                await this.createCollection(args);
-            } else {
-                throw err;
-            }
-        }
-    }
-
-    private async createCollection(args: IZepConfig) {
-        if (!args.embeddingDimensions || !args.isAutoEmbedded) {
-            throw new Error(`Collection ${args.collectionName} not found. 
+  private async createCollection(args: IZepConfig) {
+    if (!args.embeddingDimensions || !args.isAutoEmbedded) {
+      throw new Error(`Collection ${args.collectionName} not found. 
  You can create a new Collection by providing embeddingDimensions and isAutoEmbedded.`);
-        }
-        this.collection = await this.client.document.addCollection({
-            name: args.collectionName,
-            description: args.description,
-            metadata: args.metadata,
-            embeddingDimensions: args.embeddingDimensions,
-            isAutoEmbedded: args.isAutoEmbedded
-        });
+    }
+    this.collection = await this.client.document.addCollection({
+      name: args.collectionName,
+      description: args.description,
+      metadata: args.metadata,
+      embeddingDimensions: args.embeddingDimensions,
+      isAutoEmbedded: args.isAutoEmbedded,
+    });
+  }
+
+  async addVectors(
+    vectors: number[][],
+    documents: Document[]
+  ): Promise<string[]> {
+    if (vectors.length === 0) {
+      return [];
+    }
+    if (vectors.length !== documents.length) {
+      throw new Error(`Vectors and documents must have the same length`);
     }
 
-    async addVectors(vectors: number[][], documents: Document[]): Promise<string[]> {
-        if (vectors.length === 0) {
-            return [];
-        }
-        if (vectors.length !== documents.length) {
-            throw new Error(`Vectors and documents must have the same length`);
-        }
-
-        const docs: Array<ZepDocument> = [];
-        for (let i = 0; i < documents.length; i += 1) {
-            const doc = new ZepDocument(
-                {
-                    content: documents[i].pageContent,
-                    metadata: documents[i].metadata,
-                    embedding: new Float32Array(vectors[i])
-                });
-            docs.push(doc);
-        }
-        return await this.collection.addDocuments(docs);
+    const docs: Array<ZepDocument> = [];
+    for (let i = 0; i < documents.length; i += 1) {
+      const doc = new ZepDocument({
+        content: documents[i].pageContent,
+        metadata: documents[i].metadata,
+        embedding: new Float32Array(vectors[i]),
+      });
+      docs.push(doc);
     }
+    // Wait for collection to be initialized
+    await this.initPromise;
+    return await this.collection.addDocuments(docs);
+  }
 
-    async addDocuments(documents: Document[]): Promise<string[]> {
-        const texts = documents.map(({pageContent}) => pageContent);
-        const vectors = await this.embeddings.embedDocuments(texts);
-        return this.addVectors(vectors, documents);
+  async addDocuments(documents: Document[]): Promise<string[]> {
+    const texts = documents.map(({ pageContent }) => pageContent);
+    const vectors = await this.embeddings.embedDocuments(texts);
+    return this.addVectors(vectors, documents);
+  }
+
+  _vectorstoreType(): string {
+    return "zep";
+  }
+
+  async delete(params: IZepDeleteParams): Promise<void> {
+    // Wait for collection to be initialized
+    await this.initPromise;
+    for (const uuid of params.uuids) {
+      await this.collection.deleteDocument(uuid);
     }
+  }
 
-    _vectorstoreType(): string {
-        return "zep";
+  private isRecord(value: unknown): value is Record<string, unknown> {
+    return (
+      typeof value === "object" &&
+      value !== null &&
+      !Array.isArray(value) &&
+      // eslint-disable-next-line no-instanceof/no-instanceof
+      !(value instanceof Function)
+    );
+  }
+
+  async similaritySearchVectorWithScore(
+    query: number[],
+    k: number,
+    filter?: Record<string, unknown> | string | object
+  ): Promise<[Document, number][]> {
+    if (!this.isRecord(filter)) {
+      throw new Error(`Filter must be a record, got ${filter}`);
     }
+    await this.initPromise;
+    const results = await this.collection.search(
+      {
+        embedding: new Float32Array(query),
+        metadata: filter,
+      },
+      k
+    );
 
-    async delete(params: IZepDeleteParams): Promise<void> {
-        for (const uuid of params.uuids) {
-            await this.collection.deleteDocument(uuid);
-        }
+    const docsAndScore: [Document, number][] = [];
+    results.forEach((d) => {
+      docsAndScore.push([
+        new Document({
+          pageContent: d.content,
+          metadata: d.metadata,
+        }),
+        d.score ? d.score : 0,
+      ]);
+    });
+    return docsAndScore;
+  }
+
+  static async fromTexts(
+    texts: string[],
+    metadatas: object[] | object,
+    embeddings: Embeddings,
+    zepConfig: IZepConfig
+  ): Promise<ZepVectorStore> {
+    const docs: Document[] = [];
+    for (let i = 0; i < texts.length; i += 1) {
+      const metadata = Array.isArray(metadatas) ? metadatas[i] : metadatas;
+      const newDoc = new Document({
+        pageContent: texts[i],
+        metadata,
+      });
+      docs.push(newDoc);
     }
+    return ZepVectorStore.fromDocuments(docs, embeddings, zepConfig);
+  }
 
-    private isRecord(value: unknown): value is Record<string, unknown> {
-        return typeof value === 'object'
-            && value !== null
-            && !Array.isArray(value)
-            // eslint-disable-next-line no-instanceof/no-instanceof
-            && !(value instanceof Function);
-    }
-
-
-    async similaritySearchVectorWithScore(
-        query: number[],
-        k: number,
-        filter?: Record<string, unknown> | string | object
-    ): Promise<[Document, number][]> {
-        if (!this.isRecord(filter)) {
-            throw new Error(`Filter must be a record, got ${filter}`);
-        }
-        const results = await this.collection.search({
-            embedding: new Float32Array(query),
-            metadata: filter
-        }, k);
-
-        const docsAndScore: [Document, number][] = [];
-        results.forEach((d) => {
-            docsAndScore.push([
-                new Document({
-                    pageContent: d.content,
-                    metadata: d.metadata,
-                }),
-                d.score ? d.score : 0
-            ]);
-        });
-        return docsAndScore;
-    }
-
-
-    static async fromTexts(
-        texts: string[],
-        metadatas: object[] | object,
-        embeddings: Embeddings,
-        zepConfig: IZepConfig
-    ): Promise<ZepVectorStore> {
-        const docs: Document[] = [];
-        for (let i = 0; i < texts.length; i += 1) {
-            const metadata = Array.isArray(metadatas) ? metadatas[i] : metadatas;
-            const newDoc = new Document({
-                pageContent: texts[i],
-                metadata,
-            });
-            docs.push(newDoc);
-        }
-        return ZepVectorStore.fromDocuments(docs, embeddings, zepConfig);
-    }
-
-    static async fromDocuments(
-        docs: Document[],
-        embeddings: Embeddings,
-        zepConfig: IZepConfig
-    ): Promise<ZepVectorStore> {
-        const instance = new this(embeddings, zepConfig);
-        await instance.addDocuments(docs);
-        return instance;
-    }
+  static async fromDocuments(
+    docs: Document[],
+    embeddings: Embeddings,
+    zepConfig: IZepConfig
+  ): Promise<ZepVectorStore> {
+    const instance = new this(embeddings, zepConfig);
+    // Wait for collection to be initialized
+    await instance.initPromise;
+    await instance.addDocuments(docs);
+    return instance;
+  }
 }
