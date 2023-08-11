b1fd6c1a669353656c790577a329ab92d2de35ac
Vitaly Neyman <vitaly@prompt.security>
2023-08-11 18:15:49 +0300

    Fix Bedrock mdx documentation to show embedded example

--- examples/src/models/llm/bedrock.ts
+++ examples/src/models/llm/bedrock.ts
@@ -1,9 +1,9 @@
 import { Bedrock } from "langchain/llms/bedrock";
 
 async function test() {
-  const model = new Bedrock({model: "ai21", regionName: "us-west-2"});
+  const model = new Bedrock({ model: "ai21", regionName: "us-west-2" });
   const res = await model.call("Tell me a joke");
   console.log(res);
 }
 
-test()
+test();
