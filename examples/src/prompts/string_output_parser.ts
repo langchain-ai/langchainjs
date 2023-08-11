f04950580f3f49cf821db8da05e00f267720d69b
Jacob Lee <jacoblee93@gmail.com>
2023-08-11 15:44:13 -0700

    Adds output parser docs (#2247)

---
+++ examples/src/prompts/string_output_parser.ts
@@ -0,0 +1,12 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { StringOutputParser } from "langchain/schema/output_parser";
+
+const parser = new StringOutputParser();
+
+const model = new ChatOpenAI({ temperature: 0 });
+
+const stream = await model.pipe(parser).stream("Hello there!");
+
+for await (const chunk of stream) {
+  console.log(chunk);
+}
