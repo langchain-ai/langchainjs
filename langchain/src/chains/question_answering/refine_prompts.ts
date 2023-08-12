29d6ff3e404c04681e3275218187fbf30b718f65
l4b4r4b4b4 <lucas.cansino@mail.de>
2023-08-13 01:09:28 +0200

    fixed minor gramma mistake

--- langchain/src/chains/question_answering/refine_prompts.ts
+++ langchain/src/chains/question_answering/refine_prompts.ts
@@ -53,7 +53,7 @@ export const DEFAULT_TEXT_QA_PROMPT_TMPL = `Context information is below.
 ---------------------
 {context}
 ---------------------
-Given the context information and not prior knowledge, answer the question: {question}`;
+Given the context information and no prior knowledge, answer the question: {question}`;
 export const DEFAULT_TEXT_QA_PROMPT = /*#__PURE__*/ new PromptTemplate({
   inputVariables: ["context", "question"],
   template: DEFAULT_TEXT_QA_PROMPT_TMPL,
@@ -63,7 +63,7 @@ const chat_qa_prompt_template = `Context information is below.
 ---------------------
 {context}
 ---------------------
-Given the context information and not prior knowledge, answer any questions`;
+Given the context information and no prior knowledge, answer any questions`;
 const chat_messages = [
   /*#__PURE__*/ SystemMessagePromptTemplate.fromTemplate(
     chat_qa_prompt_template
