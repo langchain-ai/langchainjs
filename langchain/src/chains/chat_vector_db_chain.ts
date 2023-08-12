43c7bb19e715822632764f79d467c141fa056a34
l4b4r4b4b4 <lucas.cansino@mail.de>
2023-08-13 01:37:19 +0200

    Prompt engineering: chat_history -> qa_template & internationalized answer

--- langchain/src/chains/chat_vector_db_chain.ts
+++ langchain/src/chains/chat_vector_db_chain.ts
@@ -11,14 +11,17 @@ import { CallbackManagerForChainRun } from "../callbacks/manager.js";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type LoadValues = Record<string, any>;
 
-const question_generator_template = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+const question_generator_template = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question using the language addressed in by the user.
 
 Chat History:
 {chat_history}
 Follow Up Input: {question}
 Standalone question:`;
 
-const qa_template = `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+const qa_template = `Chat History:
+{chat_history}
+
+Use the chat history above and the following pieces of additional context knowledge to answer the question at the end in the language addressed in by the user. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
 {context}
 
@@ -104,7 +107,7 @@ export class ChatVectorDBQAChain
         newQuestion = result[keys[0]];
       } else {
         throw new Error(
-          "Return from llm chain has multiple values, only single values supported."
+          `Return from llm chain has multiple values, only single values supported. \n Keys: ${JSON.stringify(keys,null,2)}`
         );
       }
     }
