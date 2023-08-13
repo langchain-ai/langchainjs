53b9677c680a4088b8f436b07b90003304339528
Priya X. Pramesi <ppramesi@gmail.com>
2023-08-13 15:50:59 +0700

    oops formatting

--- langchain/src/retrievers/self_query/weaviate.ts
+++ langchain/src/retrievers/self_query/weaviate.ts
@@ -152,7 +152,7 @@ export class WeaviateTranslator extends BaseTranslator {
         valueNumber: parseFloat(comparison.value as string),
       };
     }
-    
+
     throw new Error("Value type is not supported");
   }
 
