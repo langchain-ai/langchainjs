4620f03b7bd751dbc2baa1abbf6f0113679a15d0
l4b4r4b4b4 <lucas.cansino@mail.de>
2023-08-13 23:09:59 +0200

    Local embeddings function with Transformer.js API

---
+++ langchain/src/embeddings/transformer.ts
@@ -0,0 +1,80 @@
+import { TransformersEmbeddingFunction } from "chromadb";
+import { chunkArray } from "../util/chunk.js";
+import { Embeddings, EmbeddingsParams } from "./base.js";
+
+export interface TransformerEmbeddingsParams extends EmbeddingsParams {
+  /** Model name to use */
+  modelName: string;
+
+  /**
+   * Timeout to use when making requests to OpenAI.
+   */
+  timeout?: number;
+
+  /**
+   * The maximum number of documents to embed in a single request. This is
+   * limited by the OpenAI API to a maximum of 2048.
+   */
+  batchSize?: number;
+
+  /**
+   * Whether to strip new lines from the input text. This is recommended by
+   * OpenAI, but may not be suitable for all use cases.
+   */
+  stripNewLines?: boolean;
+}
+
+export class TransformerEmbeddings
+  extends Embeddings
+  implements TransformerEmbeddingsParams
+{
+  modelName = "Xenova/all-MiniLM-L6-v2";
+
+  embedder = new TransformersEmbeddingFunction({
+    model: this.modelName,
+  });
+
+  batchSize = 512;
+
+  stripNewLines = true;
+
+  timeout?: number;
+
+  constructor(fields?: Partial<TransformerEmbeddingsParams>) {
+    super(fields ?? {});
+
+    this.modelName = fields?.modelName ?? this.modelName;
+    this.embedder = new TransformersEmbeddingFunction({
+      model: this.modelName,
+    });
+    this.stripNewLines = fields?.stripNewLines ?? this.stripNewLines;
+    this.timeout = fields?.timeout;
+  }
+
+  async embedDocuments(texts: string[]): Promise<number[][]> {
+    const subPrompts = chunkArray(
+      this.stripNewLines ? texts.map((t) => t.replace(/\n/g, " ")) : texts,
+      this.batchSize
+    );
+
+    const embeddings: number[][] = [];
+
+    for (let i = 0; i < subPrompts.length; i += 1) {
+      const input = subPrompts[i];
+      const data = await this.embedder.generate(input);
+
+      for (let j = 0; j < input.length; j += 1) {
+        embeddings.push(data[j]);
+      }
+    }
+
+    return embeddings;
+  }
+
+  async embedQuery(text: string): Promise<number[]> {
+    const data = await this.embedder.generate([
+      this.stripNewLines ? text.replace(/\n/g, " ") : text,
+    ]);
+    return data[0];
+  }
+}
