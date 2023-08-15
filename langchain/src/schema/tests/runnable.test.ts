44a798ce9856c3be67f9b62373ab6435960dcaf2
jacoblee93 <jacoblee93@gmail.com>
2023-08-15 13:51:01 -0700

    Update tests

--- langchain/src/schema/tests/runnable.test.ts
+++ langchain/src/schema/tests/runnable.test.ts
@@ -306,7 +306,7 @@ test("Router runnables", async () => {
   expect(result2).toEqual(["I am a math genius!", "I am an English genius!"]);
 });
 
-test.only("RunnableWithFallbacks", async () => {
+test("RunnableWithFallbacks", async () => {
   const llm = new FakeLLM({
     thrownErrorString: "Bad error!",
   });
@@ -318,5 +318,24 @@ test.only("RunnableWithFallbacks", async () => {
     fallbacks: [new FakeLLM({})],
   });
   const result2 = await llmWithFallbacks.invoke("What up");
-  console.log(result2);
+  expect(result2).toEqual("What up");
+});
+
+test("RunnableWithFallbacks batch", async () => {
+  const llm = new FakeLLM({
+    thrownErrorString: "Bad error!",
+  });
+  await expect(async () => {
+    const result1 = await llm.batch(["What up"]);
+    console.log(result1);
+  }).rejects.toThrow();
+  const llmWithFallbacks = llm.withFallbacks({
+    fallbacks: [new FakeLLM({})],
+  });
+  const result2 = await llmWithFallbacks.batch([
+    "What up 1",
+    "What up 2",
+    "What up 3",
+  ]);
+  expect(result2).toEqual(["What up 1", "What up 2", "What up 3"]);
 });
