b3a455d0511aa814346473edc38cf260d73529d6
jacoblee93 <jacoblee93@gmail.com>
2023-08-17 14:45:54 -0700

    Update example

--- examples/src/indexes/vector_stores/hnswlib_delete.ts
+++ examples/src/indexes/vector_stores/hnswlib_delete.ts
@@ -1,7 +1,10 @@
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 
-export const run = async () => {
-  // Delete the vector store.
-  const directory = "your/directory/here";
-  await HNSWLib.delete(directory);
-};
+// Save the vector store to a directory
+const directory = "your/directory/here";
+
+// Load the vector store from the same directory
+const loadedVectorStore = await HNSWLib.load(directory, new OpenAIEmbeddings());
+
+await loadedVectorStore.delete({ directory });
