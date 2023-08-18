ed54e8099403f7896cb0743d8746c27a8cea1b70
Cory Cooper <cory.cooper@postman.com>
2023-08-17 17:41:46 -0700

    OpenAIEmbeddings and CohereEmbeddings: Parallelize embedDocuments (#2294)
    * Parallelize OpenAI embedDocuments
    
    * Set max concurrency to 2 to minimize breaking changes, rename, tests, implement for Cohere
    
    * Fix defaults for Cohere embedings
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- langchain/src/embeddings/tests/openai.int.test.ts
+++ langchain/src/embeddings/tests/openai.int.test.ts
@@ -14,3 +14,22 @@ test("Test OpenAIEmbeddings.embedDocuments", async () => {
   expect(typeof res[0][0]).toBe("number");
   expect(typeof res[1][0]).toBe("number");
 });
+
+test("Test OpenAIEmbeddings concurrency", async () => {
+  const embeddings = new OpenAIEmbeddings({
+    batchSize: 1,
+    maxConcurrency: 2,
+  });
+  const res = await embeddings.embedDocuments([
+    "Hello world",
+    "Bye bye",
+    "Hello world",
+    "Bye bye",
+    "Hello world",
+    "Bye bye",
+  ]);
+  expect(res).toHaveLength(6);
+  expect(res.find((embedding) => typeof embedding[0] !== "number")).toBe(
+    undefined
+  );
+});
