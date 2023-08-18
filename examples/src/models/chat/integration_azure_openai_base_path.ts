2755636714e6dc709fa2d9b501f60433ef63f708
Jacob Lee <jacoblee93@gmail.com>
2023-08-17 17:52:25 -0700

    Add examples to Azure OpenAI docs (#2318)
    * Mark peer deps as optional
    
    * Add examples to Azure OpenAI docs

--- examples/src/models/chat/integration_azure_openai_base_path.ts
+++ examples/src/models/chat/integration_azure_openai_base_path.ts
@@ -4,6 +4,7 @@ const model = new ChatOpenAI({
   temperature: 0.9,
   azureOpenAIApiKey: "SOME_SECRET_VALUE", // In Node.js defaults to process.env.AZURE_OPENAI_API_KEY
   azureOpenAIApiVersion: "YOUR-API-VERSION", // In Node.js defaults to process.env.AZURE_OPENAI_API_VERSION
-  azureOpenAIApiInstanceName: "{MY_INSTANCE_NAME}", // In Node.js defaults to process.env.AZURE_OPENAI_API_INSTANCE_NAME
   azureOpenAIApiDeploymentName: "{DEPLOYMENT_NAME}", // In Node.js defaults to process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME
+  azureOpenAIBasePath:
+    "https://westeurope.api.microsoft.com/openai/deployments", // In Node.js defaults to process.env.AZURE_OPENAI_BASE_PATH
 });
