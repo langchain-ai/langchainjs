a3aee79d5fe692afb90cd0317827600d728ae27c
Kaelin Hooper <ka3lin@gmail.com>
2023-08-19 21:40:57 -0600

    Use block comments for ease of copy and pasting

--- examples/src/indexes/vector_stores/vectara.ts
+++ examples/src/indexes/vector_stores/vectara.ts
@@ -36,28 +36,30 @@ const resultsWithScore = await store.similaritySearchWithScore(
 
 // Print the results.
 console.log(JSON.stringify(resultsWithScore, null, 2));
-// [
-//   [
-//     {
-//       "pageContent": "In the room the women come and go talking of Michelangelo",
-//       "metadata": [
-//         {
-//           "name": "lang",
-//           "value": "eng"
-//         },
-//         {
-//           "name": "offset",
-//           "value": "0"
-//         },
-//         {
-//           "name": "len",
-//           "value": "57"
-//         }
-//       ]
-//     },
-//     0.38169062
-//   ]
-// ]
+/*
+[
+  [
+    {
+      "pageContent": "In the room the women come and go talking of Michelangelo",
+      "metadata": [
+        {
+          "name": "lang",
+          "value": "eng"
+        },
+        {
+          "name": "offset",
+          "value": "0"
+        },
+        {
+          "name": "len",
+          "value": "57"
+        }
+      ]
+    },
+    0.38169062
+  ]
+]
+*/
 
 const resultsWithSummary = await store.similaritySearchWithSummary(
   "What were the women talking about?",
@@ -75,36 +77,38 @@ const resultsWithSummary = await store.similaritySearchWithSummary(
 );
 
 console.log(JSON.stringify(resultsWithSummary, null, 2));
-// {
-//  "documents": [
-//   [
-//     {
-//       "pageContent": "In the room the women come and go talking of Michelangelo",
-//       "metadata": [
-//         {
-//           "name": "lang",
-//           "value": "eng"
-//         },
-//         {
-//           "name": "offset",
-//           "value": "0"
-//         },
-//         {
-//           "name": "len",
-//           "value": "57"
-//         }
-//       ]
-//     },
-//     0.38169062
-//   ]
-// ],
-// "summary": [
-//   {
-//      "text": "Nella stanza le donne vanno e vengono parlando di Michelangelo. Inoltre, sembra che stiano valutando se mangiare una pesca.",
-//      "lang": "ita",
-//      "prompt": "",
-//      "status": [],
-//      "futureId": 2
-//   }
-//  ]
-// }
+/*
+{
+ "documents": [
+  [
+    {
+      "pageContent": "In the room the women come and go talking of Michelangelo",
+      "metadata": [
+        {
+          "name": "lang",
+          "value": "eng"
+        },
+        {
+          "name": "offset",
+          "value": "0"
+        },
+        {
+          "name": "len",
+          "value": "57"
+        }
+      ]
+    },
+    0.38169062
+  ]
+],
+"summary": [
+  {
+     "text": "Nella stanza le donne vanno e vengono parlando di Michelangelo. Inoltre, sembra che stiano valutando se mangiare una pesca.",
+     "lang": "ita",
+     "prompt": "",
+     "status": [],
+     "futureId": 2
+  }
+ ]
+}
+*/
