5ccfe2e1a657772213eb9a4a4d27cb1331398e1e
Benyuel <ben.pleasanton@gmail.com>
2023-08-23 07:18:06 -0400

    remove extraneous example previously added

--- examples/src/llms/bedrock-chat.js
+++
@@ -1,8 +0,0 @@
-import { BedrockChat } from "langchain/llms/bedrock-chat";
-
-async function test() {
-  const model = new BedrockChat({model: "bedrock-model-name", region: "aws-region", streaming: true});
-  const res = await model.call("Question: What would be a good company name a company that makes colorful socks?\nAnswer:");
-  console.log(res);
-}
-test();
