5ce00538085225abfe80a6f975c6424f647004d0
jacoblee93 <jacoblee93@gmail.com>
2023-08-24 12:31:10 -0700

    Formatting

--- langchain/src/hub.ts
+++ langchain/src/hub.ts
@@ -2,12 +2,19 @@ import { Client, ClientConfiguration, HubPushOptions } from "langchainhub";
 import { Serializable } from "./load/serializable.js";
 import { load } from "./load/index.js";
 
-export async function push(repoFullName: string, serializable: Serializable, options?: HubPushOptions & ClientConfiguration) {
+export async function push(
+  repoFullName: string,
+  serializable: Serializable,
+  options?: HubPushOptions & ClientConfiguration
+) {
   const client = new Client(options);
   return client.push(repoFullName, JSON.stringify(serializable), options);
 }
 
-export async function pull<T extends Serializable>(ownerRepoCommit: string, options?: ClientConfiguration) {
+export async function pull<T extends Serializable>(
+  ownerRepoCommit: string,
+  options?: ClientConfiguration
+) {
   const client = new Client(options);
   const result = await client.pull(ownerRepoCommit);
   return load<T>(result);
--- langchain/src/tests/hub.int.test.ts
+++ langchain/src/tests/hub.int.test.ts
@@ -2,9 +2,15 @@ import * as hub from "../hub.js";
 import { PromptTemplate } from "../prompts/prompt.js";
 
 test("Test LangChain Hub client pushing a new repo", async () => {
-  const prompt = PromptTemplate.fromTemplate(`You are a parrot. The current date is ${new Date().toISOString()}\n{input}`)
-  const repoName = `${process.env.LANGCHAIN_HUB_USERNAME}/langchainjs-${new Date().getTime()}`;
+  const prompt = PromptTemplate.fromTemplate(
+    `You are a parrot. The current date is ${new Date().toISOString()}\n{input}`
+  );
+  const repoName = `${
+    process.env.LANGCHAIN_HUB_USERNAME
+  }/langchainjs-${new Date().getTime()}`;
   await hub.push(repoName, prompt);
   const pulledPrompt = await hub.pull<PromptTemplate>(repoName);
-  expect(prompt.format({ input: "testing" })).toEqual(pulledPrompt.format({ input: "testing" }));
+  expect(prompt.format({ input: "testing" })).toEqual(
+    pulledPrompt.format({ input: "testing" })
+  );
 });
