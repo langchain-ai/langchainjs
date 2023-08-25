16fe63878a1e9b91b5a086f74ad7fb1d60bcdf1e
nigel-daniels <nigel.daniels@me.com>
2023-08-25 09:59:53 -0700

    Local tests.

--- langchain/src/llms/llama_cpp.ts
+++ langchain/src/llms/llama_cpp.ts
@@ -1,4 +1,6 @@
+import { getEnvironmentVariable } from "../util/env.js";
 import { LLM, BaseLLMParams } from "./base.js";
+
 import type { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
 /**
  * Documentation regarding the parameters can be found as part of the llama-cpp-node documentation.
@@ -37,30 +39,53 @@ export class LlamaCpp extends LLM {
 	useMmap = true;
 	vocabOnly = false;
 
+    model: LlamaModel;
+    context: LlamaContext;
+
 	static lc_name() {
       return "Llama2-CPP";
     }
 
 	constructor(fields: LlamaCppInputs) {
 		super(fields);
-		if (fields.modelPath) {
-			this.batchSize = fields?.batchSize ?? this.batchSize;
-			this.contextSize = fields?.contextSize ?? this.contextSize;
-			//this.embedding = fields.embedding;
-			this.f16Kv = fields?.f16Kv ?? this.f16Kv;
-			//this.gpuLayers = fields.gpuLayers;
-			this.logitsAll = fields?.logitsAll ?? this.logitsAll;
-			//this.lowVram = fields.lowVram;
-			this.modelPath = fields.modelPath;
-			this.seed = fields?.seed ?? this.seed;
-			this.useMlock = fields?.useMlock ?? this.useMlock;
-			this.useMmap = fields?.useMmap ?? this.useMmap;
-			this.vocabOnly = fields?.vocabOnly ?? this.vocabOnly;
-		} else {
-			throw new Error(
-	          "A path to the Llama2 model is required."
-	        );
-		}
+        LlamaCpp
+            .constructorImports()
+            .then(({ LlamaModel, LlamaContext }) => {
+                const llamaPath = fields?.modelPath ?? getEnvironmentVariable("LLAMA_PATH");
+
+                if (llamaPath) {
+        			this.batchSize = fields?.batchSize ?? this.batchSize;
+        			this.contextSize = fields?.contextSize ?? this.contextSize;
+        			//this.embedding = fields.embedding;
+        			this.f16Kv = fields?.f16Kv ?? this.f16Kv;
+        			//this.gpuLayers = fields.gpuLayers;
+        			this.logitsAll = fields?.logitsAll ?? this.logitsAll;
+        			//this.lowVram = fields.lowVram;
+        			this.modelPath = llamaPath;
+        			this.seed = fields?.seed ?? this.seed;
+        			this.useMlock = fields?.useMlock ?? this.useMlock;
+        			this.useMmap = fields?.useMmap ?? this.useMmap;
+        			this.vocabOnly = fields?.vocabOnly ?? this.vocabOnly;
+
+                    this.model = new LlamaModel({
+            			batchSize: this.batchSize,
+            			contextSize: this.contextSize,
+            			f16Kv: this.f16Kv,
+            			logitsAll: this.logitsAll,
+            			modelPath: this.modelPath,
+            			seed: this.seed,
+            			useMlock: this.useMlock,
+            			useMmap: this.useMmap,
+            			vocabOnly: this.vocabOnly,
+            		});
+                    this.context = new LlamaContext({model: this.model});
+
+        		} else {
+        			throw new Error(
+        	          "A path to the Llama2 model is required."
+        	        );
+        		}
+            });
 	}
 
 	_llmType() {
@@ -72,21 +97,10 @@ export class LlamaCpp extends LLM {
       prompt: string,
       options: this["ParsedCallOptions"]
     ): Promise<string> {
-		const { LlamaModel, LlamaContext, LlamaChatSession }  = await LlamaCpp.imports();
+		const { LlamaChatSession }  = await LlamaCpp.imports();
 
-		const model = new LlamaModel({
-			batchSize: this.batchSize,
-			contextSize: this.contextSize,
-			f16Kv: this.f16Kv,
-			logitsAll: this.logitsAll,
-			modelPath: this.modelPath,
-			seed: this.seed,
-			useMlock: this.useMlock,
-			useMmap: this.useMmap,
-			vocabOnly: this.vocabOnly
-		});
-		const context = new LlamaContext({model});
-		const session = new LlamaChatSession({context})
+		const session = new LlamaChatSession({context: this.context});
+
 		try {
 			const compleation = await session.prompt(prompt, undefined, {signal: options.signal});
 			return compleation;
@@ -97,23 +111,33 @@ export class LlamaCpp extends LLM {
 		}
 	}
 
+    /** @ignore */
+	static async constructorImports(): Promise<{
+		LlamaModel: typeof LlamaModel;
+		LlamaContext: typeof LlamaContext;
+	}> {
+		try {
+			const { LlamaModel, LlamaContext } = await import("node-llama-cpp");
+			return { LlamaModel, LlamaContext };
+		} catch (e) {
+			throw new Error(
+				"Please install node-llama-cpp as a dependency with, e.g. `npm install node-llama-cpp`\n" + e
+			);
+		}
+	}
 
 	/** @ignore */
 	static async imports(): Promise<{
-		LlamaModel: typeof LlamaModel;
-		LlamaContext: typeof LlamaContext;
 		LlamaChatSession: typeof LlamaChatSession;
 	}> {
 		try {
-			const {
-				LlamaModel,
-				LlamaContext,
-				LlamaChatSession
-	        } = await import("node-llama-cpp");
-			return { LlamaModel, LlamaContext, LlamaChatSession };
+            console.log("Get import");
+			const { LlamaChatSession } = await import("node-llama-cpp");
+            console.log("Got import");
+			return { LlamaChatSession };
 		} catch (e) {
 			throw new Error(
-				"Please install cohere-ai as a dependency with, e.g. `npm install node-llama-cpp`"
+				"Please install node-llama-cpp as a dependency with, e.g. `npm install node-llama-cpp`\n" + e
 			);
 		}
 	}
