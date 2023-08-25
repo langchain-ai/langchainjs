392855c7dc31dad6ac6823e06b65a7d968f6e28f
Richard Casemore <skarard@gmail.com>
2023-08-25 04:31:01 +0100

    Ported Recursive URL Loader: From Python to TypeScript with Concurrency Support (#2379)
    * Recursive loader based on the python implementation with concurrency
    
    * Added inheritence from  BaseDocumentLoader and implements DocumentLoader
    
    * Fixed linting
    
    * Update entry point
    
    * export options
    
    * Move to just document_loaders/recursive_url, clean up code
    
    * Adds install instructions
    
    * More docs updates
    
    * Fix typo
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- langchain/src/load/import_constants.ts
+++ langchain/src/load/import_constants.ts
@@ -67,6 +67,7 @@ export const optionalImportEntrypoints = [
   "langchain/document_loaders/web/github",
   "langchain/document_loaders/web/notiondb",
   "langchain/document_loaders/web/notionapi",
+  "langchain/document_loaders/web/recursive_url",
   "langchain/document_loaders/web/s3",
   "langchain/document_loaders/web/sonix_audio",
   "langchain/document_loaders/web/confluence",
