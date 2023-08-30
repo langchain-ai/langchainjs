522145b88a59acedf30f1cf5dc38559d28d55a1f
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-08-30 16:52:21 +0800

    feat: unit test for minimax

--- langchain/src/chat_models/tests/minimaxai.int.test.ts
+++ langchain/src/chat_models/tests/minimaxai.int.test.ts
@@ -1,17 +1,28 @@
+import { test } from "@jest/globals";
 import { ChatMinimax } from "../minimax.js";
-import { HumanMessage } from "../../schema/index.js";
+import { HumanMessage, SystemMessage } from "../../schema/index.js";
 
-test("Test ChatOpenAI", async () => {
-  const chat = new ChatMinimax({
-    modelName: "abab5.5-chat",
-    botSetting: [
-      {
-        bot_name: "MM Assistant",
-        content: "MM Assistant is an AI Assistant developed by minimax.",
-      },
-    ],
-  });
+// test("Test ChatOpenAI", async () => {
+//   const chat = new ChatMinimax({
+//     modelName: "abab5.5-chat",
+//     botSetting: [
+//       {
+//         bot_name: "MM Assistant",
+//         content: "MM Assistant is an AI Assistant developed by minimax.",
+//       },
+//     ],
+//   });
+//   const message = new HumanMessage("Hello!");
+//   const res = await chat.call([message]);
+//   console.log({ res });
+// });
+
+
+test("Test ChatOpenAI with SystemChatMessage", async () => {
+  const chat = new ChatMinimax();
+  const system_message = new SystemMessage("You are to chat with a user.");
   const message = new HumanMessage("Hello!");
-  const res = await chat.call([message]);
+  const res = await chat.call([system_message, message]);
   console.log({ res });
 });
+
