04a480a9e08975dd98bfb75c9362e3b1973ee7e1
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-08-31 15:19:54 +0800

    feat: anthropic functions

--- langchain/src/chat_models/anthropicfunctions.ts
+++ langchain/src/chat_models/anthropicfunctions.ts
@@ -2,7 +2,8 @@ import {
   ChatCompletionFunctions,
   CreateChatCompletionRequestFunctionCall,
 } from "openai";
-import { BaseMessage, ChatResult } from "../schema/index.js";
+import { XMLBuilder, XMLParser } from "fast-xml-parser";
+import { AIMessage, BaseMessage, ChatResult } from "../schema/index.js";
 import { CallbackManagerForLLMRun } from "../callbacks/manager.js";
 import { AnthropicInput, ChatAnthropic } from "./anthropic.js";
 import { StructuredTool } from "../tools/index.js";
@@ -10,22 +11,27 @@ import { BaseLanguageModelCallOptions } from "../base_language/index.js";
 import { BaseChatModel, BaseChatModelParams } from "./base.js";
 import { SystemMessagePromptTemplate } from "../prompts/index.js";
 
-const prompt = `In addition to responding, you can use tools. \
+const prompt = `In addition to responding, you can use tools. 
 You have access to the following tools.
 
 {tools}
 
-In order to use a tool, you can use <tool></tool> to specify the name, \
-and the <tool_input></tool_input> tags to specify the parameters. \
-Each parameter should be passed in as <$param_name>$value</$param_name>, \
-Where $param_name is the name of the specific parameter, and $value \
+In order to use a tool, you can use <tool></tool> to specify the name, 
+and the <tool_input></tool_input> tags to specify the parameters. 
+Each parameter should be passed in as <$param_name>$value</$param_name>, 
+Where $param_name is the name of the specific parameter, and $value 
 is the value for that parameter.
 
-For example, if you have a tool called 'search' that accepts a single \
-parameter 'query' that could run a google search, in order to search \
-for the weather in SF you would respond:
+If a tool is chosen: 
+  You will get back a response in the XML format only about the tool. 
+  For example, if you have a tool called 'search' that accepts a single 
+  parameter 'query' that could run a google search, in order to search 
+  for the weather in SF you would respond:
 
-<tool>search</tool><tool_input><query>weather in SF</query></tool_input>
+  <tool>search</tool><tool_input><query>weather in SF</query></tool_input>
+  
+else:
+  You will get back a response in the normal format without xml form. 
 `;
 
 export interface ChatOpenAICallOptions extends BaseLanguageModelCallOptions {
@@ -41,12 +47,22 @@ export interface ChatOpenAICallOptions extends BaseLanguageModelCallOptions {
 export class ChatAnthropicFunctions extends BaseChatModel<ChatOpenAICallOptions> {
   model: ChatAnthropic;
 
-  forced: boolean;
-
-  function_call: CreateChatCompletionRequestFunctionCall;
+  functionEnabled: boolean;
 
   static lc_name() {
     return "ChatAnthropicFunctions";
+  }
+
+  get lc_secrets(): { [key: string]: string } | undefined {
+    return {
+      anthropicApiKey: "ANTHROPIC_API_KEY",
+    };
+  }
+
+  get lc_aliases(): Record<string, string> {
+    return {
+      modelName: "model",
+    };
   }
 
   constructor(fields?: Partial<AnthropicInput> & BaseChatModelParams) {
@@ -61,27 +77,44 @@ export class ChatAnthropicFunctions extends BaseChatModel<ChatOpenAICallOptions>
     runManager?: CallbackManagerForLLMRun
   ): Promise<ChatResult> {
     const newOptions = { ...options };
-    if (options.functions) {
-      const systemMessage = await SystemMessagePromptTemplate.fromTemplate(
-        prompt
-      ).format({ tools: JSON.stringify(options.functions) });
-      // put to the front of the messages
-      messages.unshift(systemMessage);
-      if (!options.stop) {
+    if (newOptions.functions && newOptions.functions.length > 0) {
+      this.functionEnabled = true;
+
+      if (!newOptions.stop) {
         newOptions.stop = ["</tool_input>"];
       } else {
         newOptions.stop?.push("</tool_input>");
       }
-      if (options.function_call) {
-        this.forced = true;
+
+      if (newOptions.function_call) {
         // if options.function_call is string , this.function_call = options.function_call
         // else if options.function_call is object , this.function_call = options.function_call.name
-        if (typeof options.function_call === "string") {
-          this.function_call = options.function_call;
-        } else if (typeof options.function_call === "object") {
-          this.function_call = options.function_call.name;
+        let function_call: CreateChatCompletionRequestFunctionCall = "auto";
+        if (typeof newOptions.function_call === "string") {
+          function_call = newOptions.function_call;
+        } else if (typeof newOptions.function_call === "object") {
+          function_call = newOptions.function_call.name;
+        }
+
+
+        if (function_call === "none") {
+          newOptions.functions = [];
+        } else if (function_call !== "auto") {
+          // newOptions.functions中name!=function_call的移除
+          newOptions.functions = newOptions.functions.filter(
+            (item) => item.name === function_call
+          );
         }
       }
+
+      const builder = new XMLBuilder();
+      const functionsXml = builder.build(newOptions.functions);
+
+      const systemMessage = await SystemMessagePromptTemplate.fromTemplate(
+        prompt
+      ).format({ tools: functionsXml });
+      // put to the front of the messages
+      messages.unshift(systemMessage);
     } else {
       if (options.function_call) {
         throw new Error(
@@ -90,19 +123,41 @@ export class ChatAnthropicFunctions extends BaseChatModel<ChatOpenAICallOptions>
       }
     }
 
-    const generations = await this.model._generate(
+    const chatResult = await this.model._generate(
       messages,
       newOptions,
       runManager
     );
 
-    console.log("generations", JSON.stringify(generations));
+    const { generations: resultGenerations } = chatResult;
+    const { message } = resultGenerations[0];
+    const result = `here is the result: ${message.content.trim()}`;
+    const trimmedResult = result.trim();
 
-    const ll = generations.generations;
-    const { text } = ll[0];
-    console.log("text", text);
+    let generations = resultGenerations;
 
-    return generations;
+    //  Determine whether the trimmed Result contains <tool>, if yes, parse it into xml and turn it into AIMessage.
+    if (this.functionEnabled && trimmedResult.includes("<tool>")) {
+      const xmlParser = new XMLParser();
+      const jObj = xmlParser.parse(`${trimmedResult}</tool_input>`);
+      const aiMessage = new AIMessage("", {
+        function_call: {
+          name: jObj.tool,
+          arguments: JSON.stringify(jObj.tool_input ?? {}),
+        },
+      });
+      // clear generations and push new item
+      generations = [];
+      generations.push({
+        text: "",
+        message: aiMessage,
+      });
+    }
+
+    return {
+      generations,
+      llmOutput: chatResult.llmOutput,
+    };
   }
 
   _llmType() {
--- langchain/src/chat_models/tests/chatanthropicfunctions.int.test.ts
+++ langchain/src/chat_models/tests/chatanthropicfunctions.int.test.ts
@@ -1,32 +1,119 @@
 /* eslint-disable no-process-env */
 
-import { test } from "@jest/globals";
+import { expect, test } from "@jest/globals";
 import { ChatAnthropicFunctions } from "../anthropicfunctions.js";
-import { HumanMessage } from "../../schema/index.js";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} from "../../prompts/index.js";
 
-test("Test ChatAnthropic", async () => {
-  const weatherFunction = {
-    name: "get_weather",
-    description: " Get weather information.",
-    parameters: {
-      type: "object",
-      properties: {
-        location: {
-          type: "string",
-          description: " The location to get the weather",
+test("Test ChatAnthropicFunctions call", async () => {
+  const functions = [
+    {
+      name: "get_current_weather",
+      description: "Get the current weather in a given location",
+      parameters: {
+        type: "object",
+        properties: {
+          location: {
+            type: "string",
+            description: "The city and state, e.g. San Francisco, CA",
+          },
+          unit: {
+            type: "string",
+            enum: ["celsius", "fahrenheit"],
+          },
+        },
+        required: ["location"],
+      },
+    },
+    {
+      name: "accounting_tool",
+      description:
+        "Accounting tool which can record a transaction (time , location , amount).",
+      parameters: {
+        type: "object",
+        properties: {
+          content: {
+            type: "string",
+            description: "Where was the consumption made?",
+          },
+          time: {
+            type: "string",
+            description:
+              "time when the transaction happened, format: 2023-02-12 10:10:12",
+          },
+          amount: {
+            type: "number",
+            description: "amount of the transaction",
+          },
         },
       },
-      required: ["location"],
     },
-  };
+    {
+      name: "todo_tool",
+      description:
+        "Todo tool which can add todo item(location , time , content)",
+      parameters: {
+        type: "object",
+        properties: {
+          location: {
+            type: "string",
+            description: "location where the todo happened",
+          },
+          time: {
+            type: "string",
+            description: "time when the todo happened",
+          },
+          content: {
+            type: "string",
+            description: "content of the todo task",
+          },
+        },
+      },
+    },
+    {
+      name: "google_search",
+      description:
+        "a search engine which have access to real-time information. only useful for when you need to answer questions about current events. input should be a search query.",
+      parameters: {
+        type: "object",
+        properties: {
+          input: {
+            type: "string",
+          },
+        },
+        additionalProperties: false,
+        $schema: "http://json-schema.org/draft-07/schema#",
+      },
+    },
+  ];
 
   const chat = new ChatAnthropicFunctions({
-    // modelName: "claude-2",
-    modelName: "claude-instant-v1",
+    modelName: "claude-2",
   }).bind({
-    functions: [weatherFunction],
+    functions,
   });
-  const message = new HumanMessage("What is the weather in Beijing?");
-  const res = await chat.invoke([message]);
-  console.log({ res });
+
+  const systemPrompt = PromptTemplate.fromTemplate(
+    "You are a helpful AI assistant. current date : 2023-08-31"
+  );
+
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    new SystemMessagePromptTemplate(systemPrompt),
+    HumanMessagePromptTemplate.fromTemplate("{text}"),
+  ]);
+  const res = await chat.invoke(
+    await chatPrompt.formatMessages({
+      // text: "Hello "
+      // text: "How many tesla model 3 sale in 2022",
+      // text: " The personnel appointment and removal meeting will be held at 4:00 pm tomorrow.",
+      text: " It cost me $10 for lunch.",
+    })
+  );
+  console.log({res});
+  expect(res.additional_kwargs.function_call?.name).toBe("accounting_tool");
+
 });
