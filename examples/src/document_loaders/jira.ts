04b8c77a51e23f78af4ebccec0638d30375b5c14
xteam-ivoneijr <ivoneijr@gmail.com>
2023-08-31 23:13:48 -0300

    add jira document loader documentation

---
+++ examples/src/document_loaders/jira.ts
@@ -0,0 +1,20 @@
+import { JiraProjectLoader } from "langchain/document_loaders/web/jira";
+
+const username = process.env.CONFLUENCE_USERNAME;
+const accessToken = process.env.CONFLUENCE_ACCESS_TOKEN;
+
+if (username && accessToken) {
+  const loader = new JiraProjectLoader({
+    baseUrl: "https://example.atlassian.net/wiki",
+    projectKey: "PI",
+    username,
+    accessToken,
+  });
+
+  const documents = await loader.load();
+  console.log(documents);
+} else {
+  console.log(
+    "You must provide a username and access token to run this example."
+  );
+}
