1ff10f08f9017674457c40c86dc06f771eb21dc7
Predrag Gruevski <obi1kenobi82@gmail.com>
2023-09-06 20:45:09 +0000

    Switch to `wrangler deploy` because `wrangler publish` is deprecated.

--- test-exports-cf/src/index.ts
+++ test-exports-cf/src/index.ts
@@ -3,7 +3,7 @@
  *
  * - Run `wrangler dev src/index.ts` in your terminal to start a development server
  * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ * - Run `wrangler deploy src/index.ts --name my-worker` to publish your worker
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
@@ -37,16 +37,16 @@ export default {
     ctx: ExecutionContext
   ): Promise<Response> {
 
-    const constructorParameters
-      = env.AZURE_OPENAI_API_KEY ? {
-        azureOpenAIApiKey: env.AZURE_OPENAI_API_KEY,
-        azureOpenAIApiInstanceName: env.AZURE_OPENAI_API_INSTANCE_NAME,
-        azureOpenAIApiDeploymentName: env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
-        azureOpenAIApiVersion: env.AZURE_OPENAI_API_VERSION,
-      } 
+    const constructorParameters = env.AZURE_OPENAI_API_KEY
+      ? {
+          azureOpenAIApiKey: env.AZURE_OPENAI_API_KEY,
+          azureOpenAIApiInstanceName: env.AZURE_OPENAI_API_INSTANCE_NAME,
+          azureOpenAIApiDeploymentName: env.AZURE_OPENAI_API_DEPLOYMENT_NAME,
+          azureOpenAIApiVersion: env.AZURE_OPENAI_API_VERSION,
+        }
       : {
-        openAIApiKey: env.OPENAI_API_KEY,
-      }
+          openAIApiKey: env.OPENAI_API_KEY,
+        };
 
     // Intantiate a few things to test the exports
     new OpenAI(constructorParameters);
