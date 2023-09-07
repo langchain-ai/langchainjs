7440cc33f5b26da5ad3898e7b5713802d62390b3
jacoblee93 <jacoblee93@gmail.com>
2023-09-07 15:15:22 -0700

    Removes deprecated entrypoints

--- langchain/src/embeddings/index.ts
+++
@@ -1,8 +0,0 @@
-/* #__PURE__ */ console.error(
-  "[WARN] Importing from 'langchain/embeddings' is deprecated. Import from eg. 'langchain/embeddings/openai' instead. See https://js.langchain.com/docs/getting-started/install#updating-from-0052 for upgrade instructions."
-);
-
-export { OpenAIEmbeddings } from "./openai.js";
-export { CohereEmbeddings } from "./cohere.js";
-export { Embeddings } from "./base.js";
-export { FakeEmbeddings } from "./fake.js";
