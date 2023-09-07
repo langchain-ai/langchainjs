e575b80718fbe0906491069acdbf15c888365b2f
guangjin2.xian <guangjin2.xian@zhaopin.com.cn>
2023-09-07 13:50:50 +0800

    feat: reset mrkl example

--- examples/src/agents/mrkl.ts
+++ examples/src/agents/mrkl.ts
@@ -1,25 +1,23 @@
-import {initializeAgentExecutorWithOptions} from "langchain/agents";
-import {OpenAI} from "langchain/llms/openai";
-import {SerpAPI} from "langchain/tools";
-import {Calculator} from "langchain/tools/calculator";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
+import { OpenAI } from "langchain/llms/openai";
+import { SerpAPI } from "langchain/tools";
+import { Calculator } from "langchain/tools/calculator";
 
-const model = new OpenAI({temperature: 0}, {baseURL: process.env.BASE_URL});
+const model = new OpenAI({ temperature: 0 });
 const tools = [
-    new SerpAPI(process.env.SERPAPI_API_KEY, {
-        location: "Austin,Texas,United States",
-        hl: "en",
-        gl: "us",
-    }),
-    new Calculator(),
+  new SerpAPI(process.env.SERPAPI_API_KEY, {
+    location: "Austin,Texas,United States",
+    hl: "en",
+    gl: "us",
+  }),
+  new Calculator(),
 ];
 
 const executor = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: "zero-shot-react-description",
-    returnIntermediateSteps: true,
+  agentType: "zero-shot-react-description",
+  verbose: true,
 });
 
 const input = `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`;
 
-const result = await executor.call({input});
-
-console.log(`Got output `,JSON.stringify(result));
+const result = await executor.call({ input });
