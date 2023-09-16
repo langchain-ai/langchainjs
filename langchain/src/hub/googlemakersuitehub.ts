e73f50fcf2772a71ce8f83163fdb9052a81db4f9
afirstenberg <github@addventure.com>
2023-09-16 18:27:29 -0400

    Initial implementation of prompt types

--- langchain/src/hub/googlemakersuitehub.ts
+++ langchain/src/hub/googlemakersuitehub.ts
@@ -1,37 +1,157 @@
-import { GooglePaLMTextInput } from "../llms/googlepalm.js";
-import { GooglePaLMChatInput } from "../chat_models/googlepalm.js";
+import { protos } from "@google-ai/generativelanguage";
+
+import { GooglePaLM } from "../llms/googlepalm.js";
+import { ChatGooglePaLM } from "../chat_models/googlepalm.js";
 import { PromptTemplate } from "../prompts/index.js";
+import { BaseChatModel } from "../chat_models/base.js";
+import { LLM } from "../llms/base.js";
+import {Runnable} from "../schema/runnable/index.js";
 
 export interface MakerSuiteHubConfig {
   cacheTimeout: number,
 }
+
+type MakerSuitePromptType = "text" | "data" | "chat";
 
 export interface MakerSuitePromptVariable {
   variableId: string,
   displayName: string,
 }
 
-export interface MakerSuitePromptData {
+export interface MakerSuiteRunSettings {
+  temperature?: number,
+  model: string,
+  candidateCount?: number,
+  topP?: number,
+  topK?: number,
+  maxOutputTokens: number,
+  safetySettings?: protos.google.ai.generativelanguage.v1beta2.ISafetySetting[],
+}
+
+export interface MakerSuiteTextPromptData {
   textPrompt: {
     value?: string,
     variables?: MakerSuitePromptVariable[],
   },
-  runSettings?: GooglePaLMTextInput | GooglePaLMChatInput,
-  // There may be other values we're not concerned about
+  runSettings?: MakerSuiteRunSettings,
+  testExamples?: unknown,
 }
 
+export interface MakerSuiteDataPromptColumn {
+  columnId: string,
+  displayName: string,
+  isInput?: boolean,
+}
+
+export interface MakerSuiteDataPromptRow {
+  rowId: string,
+  columnBindings: Record<string, string>,
+}
+
+export interface MakerSuiteDataPromptData {
+  dataPrompt: {
+    preamble: string,
+    columns: MakerSuiteDataPromptColumn[],
+    rows: MakerSuiteDataPromptRow[],
+    rowsUsed: string[],
+  },
+  runSettings?: MakerSuiteRunSettings,
+  testExamples?: unknown,
+}
+
+export interface MakerSuiteChatExchange {
+  request?: string,
+  response?: string,
+  source: string,
+  id: string,
+}
+
+export interface MakerSuiteChatPromptData {
+  multiturnPrompt: {
+    preamble: string,
+    primingExchanges: MakerSuiteChatExchange[],
+    sessions: {
+      sessionExchanges: MakerSuiteChatExchange[]
+    }[]
+  },
+  runSettings?: MakerSuiteRunSettings,
+}
+
+export type MakerSuitePromptData =
+  MakerSuiteTextPromptData |
+  MakerSuiteDataPromptData |
+  MakerSuiteChatPromptData;
+
 export class MakerSuitePrompt {
+
+  promptType: MakerSuitePromptType;
 
   promptData: MakerSuitePromptData;
 
   constructor(promptData: MakerSuitePromptData) {
     this.promptData = promptData;
+    this._determinePromptType();
+  }
+
+  _determinePromptType() {
+    if (Object.hasOwn(this.promptData, "textPrompt")) {
+      this.promptType = "text";
+
+    } else if (Object.hasOwn(this.promptData, "dataPrompt")) {
+      this.promptType = "data";
+
+    } else if (Object.hasOwn(this.promptData, "multiturnPrompt")) {
+      this.promptType = "chat";
+
+    } else {
+      const error = new Error("Unable to identify prompt type.");
+      (error as any).promptData = this.promptData;
+      throw error;
+    }
+  }
+
+  _promptValueText(): string {
+    return (this.promptData as MakerSuiteTextPromptData)?.textPrompt?.value ?? "";
+  }
+
+  _promptValueData(): string {
+    return "FIXME";
+  }
+
+  _promptValueChat(): string {
+    return "FIXME";
+  }
+
+  _promptValue(): string {
+    switch (this.promptType) {
+      case "text": return this._promptValueText();
+      case "data": return this._promptValueData();
+      case "chat": return this._promptValueChat();
+      default: throw new Error(`Invalid promptType: ${this.promptType}`);
+    }
   }
 
   toTemplate(): PromptTemplate {
-    const value = this.promptData?.textPrompt?.value ?? "";
+    const value = this._promptValue();
     const promptString = value.replaceAll(/{{.*:(.+):.*}}/g, "{$1}");
     return PromptTemplate.fromTemplate(promptString);
+  }
+
+  toModel(): LLM | BaseChatModel {
+    const modelName = this.promptData?.runSettings?.model || "models/text-bison-001";
+    const modelSettings = {
+      modelName,
+      ...this.promptData?.runSettings
+    }
+    if (this.promptType === "chat") {
+      return new ChatGooglePaLM(modelSettings);
+    } else {
+      return new GooglePaLM(modelSettings);
+    }
+  }
+
+  toChain() {
+    return this.toTemplate().pipe(this.toModel() as Runnable);
   }
 
 }
--- langchain/src/hub/tests/googlemakersuitehub.test.ts
+++ langchain/src/hub/tests/googlemakersuitehub.test.ts
@@ -1,16 +1,49 @@
+import fs from "fs";
+import { fileURLToPath } from "node:url";
+import * as path from "path";
+
 import { describe, test } from "@jest/globals";
-import {MakerSuitePrompt} from "../googlemakersuitehub.js";
+import { MakerSuitePrompt } from "../googlemakersuitehub.js";
 
 describe("Google Maker Suite Hub", () => {
 
-  test("Prompt template", () => {
-    const prompt = new MakerSuitePrompt({
-      textPrompt: {
-        value: "What would be a good name for a company that makes {{30E275F8-0B60-4E71-843D-9865F4D4EFD4:product:}}?"
-      }
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const chatFile = JSON.parse(fs.readFileSync(`${__dirname}/googlemakersuite-files/chatPrompt.json`, "utf8"));
+  const dataFile = JSON.parse(fs.readFileSync(`${__dirname}/googlemakersuite-files/dataPrompt.json`, "utf8"));
+  const textFile = JSON.parse(fs.readFileSync(`${__dirname}/googlemakersuite-files/textPrompt.json`, "utf8"));
+
+  describe("Prompt", () => {
+
+    test("text type", () => {
+      const prompt = new MakerSuitePrompt(textFile);
+      expect(prompt.promptType).toEqual("text");
     });
-    const template = prompt.toTemplate();
-    expect(template.template).toEqual("What would be a good name for a company that makes {product}?")
+
+    test("text template", () => {
+      const prompt = new MakerSuitePrompt(textFile);
+      const template = prompt.toTemplate();
+      expect(template.template).toEqual("What would be a good name for a company that makes {product}?")
+    });
+
+    test("text model", () => {
+      const prompt = new MakerSuitePrompt(textFile);
+      const model = prompt.toModel();
+      // console.log(model.lc_namespace);
+      expect(model.lc_namespace).toEqual(["langchain", "llms", "googlepalm"]);
+    })
+
+    test("data type", () => {
+      const prompt = new MakerSuitePrompt(dataFile);
+      expect(prompt.promptType).toEqual("data")
+    });
+
+    test("chat type", () => {
+      const prompt = new MakerSuitePrompt(chatFile);
+      expect(prompt.promptType).toEqual("chat");
+    })
+
   });
 
 })
