0b57164bcd17882543624636369e2a6171739eb5
Brace Sproul <braceasproul@gmail.com>
2023-09-17 17:37:12 -0700

    feat: finished pgvector integration

--- langchain/src/vectorstores/pgvector.ts
+++ langchain/src/vectorstores/pgvector.ts
@@ -1,5 +1,5 @@
 import { Metadata } from "@opensearch-project/opensearch/api/types.js";
-import { Pool, PoolConfig } from "pg";
+import pg, { Pool, PoolConfig } from "pg";
 import { VectorStore } from "./base.js";
 import { Embeddings } from "../embeddings/base.js";
 import { Document } from "../document.js";
@@ -12,19 +12,13 @@ import { getEnvironmentVariable } from "../util/env.js";
  */
 export interface PGVectorStoreArgs {
   postgresConnectionOptions: PoolConfig;
-  tableName?: string;
+  tableName: string;
+  idColumnName: string;
+  vectorColumnName: string;
+  contentColumnName: string;
+  metadataColumnName?: string;
   filter?: Metadata;
   verbose?: boolean;
-}
-
-/**
- * Class that extends the `Document` base class and adds an `embedding`
- * property. It represents a document in the vector store.
- */
-export class PGVectorStoreDocument extends Document {
-  embedding: string;
-
-  id?: string;
 }
 
 const defaultDocumentTableName = "documents";
@@ -40,6 +34,14 @@ export class PGVectorStore extends VectorStore {
 
   tableName: string;
 
+  idColumnName: string;
+
+  vectorColumnName: string;
+
+  contentColumnName: string;
+
+  metadataColumnName: string;
+
   filter?: Metadata;
 
   _verbose?: boolean;
@@ -47,37 +49,45 @@ export class PGVectorStore extends VectorStore {
   pool: Pool;
 
   _vectorstoreType(): string {
-    return "typeorm";
+    return "pgvector";
   }
 
-  private constructor(embeddings: Embeddings, fields: PGVectorStoreArgs) {
-    super(embeddings, fields);
-    this.tableName = fields.tableName || defaultDocumentTableName;
-    this.filter = fields.filter;
+  private constructor(embeddings: Embeddings, config: PGVectorStoreArgs) {
+    super(embeddings, config);
+    this.tableName = config.tableName || defaultDocumentTableName;
+    this.filter = config.filter;
 
-    const pool = new Pool(fields.postgresConnectionOptions);
+    this.vectorColumnName = config.vectorColumnName;
+    this.contentColumnName = config.contentColumnName;
+    this.idColumnName = config.idColumnName;
+
+    this.metadataColumnName = config.metadataColumnName ?? "metadata";
+
+    const pool = new pg.Pool(config.postgresConnectionOptions);
     this.pool = pool;
 
     this._verbose =
       getEnvironmentVariable("LANGCHAIN_VERBOSE") === "true" ??
-      fields.verbose ??
-      false;
+      !!config.verbose;
   }
 
   /**
    * Static method to create a new `PGVectorStore` instance from a
-   * connection. It calls `connect` on `pool` and returns
-   * a new instance of `PGVectorStore`.
+   * connection. It creates a table if one does not exist, and calls
+   * `connect` to return a new instance of `PGVectorStore`.
    * @param embeddings Embeddings instance.
    * @param fields `PGVectorStoreArgs` instance.
    * @returns A new instance of `PGVectorStore`.
    */
-  static async fromDataSource(
+  static async initialize(
     embeddings: Embeddings,
-    fields: PGVectorStoreArgs
+    config: PGVectorStoreArgs
   ): Promise<PGVectorStore> {
-    const postgresqlVectorStore = new PGVectorStore(embeddings, fields);
+    const postgresqlVectorStore = new PGVectorStore(embeddings, config);
+
     await postgresqlVectorStore.pool.connect();
+    await postgresqlVectorStore.ensureTableInDatabase();
+
     return postgresqlVectorStore;
   }
 
@@ -90,9 +100,7 @@ export class PGVectorStore extends VectorStore {
    */
   async addDocuments(documents: Document[]): Promise<void> {
     const texts = documents.map(({ pageContent }) => pageContent);
-    // This will create the table if it does not exist. We can call it every time as it doesn't
-    // do anything if the table already exists, and it is not expensive in terms of performance
-    await this.ensureTableInDatabase();
+
     return this.addVectors(
       await this.embeddings.embedDocuments(texts),
       documents
@@ -100,37 +108,71 @@ export class PGVectorStore extends VectorStore {
   }
 
   /**
+   * Generates the SQL placeholders for a specific row at the provided index.
+   *
+   * @param index - The index of the row for which placeholders need to be generated.
+   * @returns The SQL placeholders for the row values.
+   */
+  generatePlaceholderForRowAt(index: number): string {
+    const base = index * 3;
+    return `($${base + 1}, $${base + 2}, $${base + 3})`;
+  }
+
+  /**
+   * Constructs the SQL query for inserting rows into the specified table.
+   *
+   * @param rows - The rows of data to be inserted, consisting of values and records.
+   * @param chunkIndex - The starting index for generating query placeholders based on chunk positioning.
+   * @returns The complete SQL INSERT INTO query string.
+   */
+  buildInsertQuery(
+    rows: (string | Record<string, any>)[][],
+    chunkIndex: number
+  ) {
+    const valuesPlaceholders = rows
+      .map((_, j) => this.generatePlaceholderForRowAt(chunkIndex + j))
+      .join(", ");
+
+    const text = `
+      INSERT INTO ${this.tableName}(
+        ${this.contentColumnName},
+        ${this.vectorColumnName},
+        ${this.metadataColumnName}
+      )
+      VALUES ${valuesPlaceholders}
+    `;
+    return text;
+  }
+
+  /**
    * Method to add vectors to the vector store. It converts the vectors into
    * rows and inserts them into the database.
-   * @param vectors Array of vectors.
-   * @param documents Array of `Document` instances.
+   * @param vectors - Array of vectors.
+   * @param documents - Array of `Document` instances.
    * @returns Promise that resolves when the vectors have been added.
    */
   async addVectors(vectors: number[][], documents: Document[]): Promise<void> {
     const rows = vectors.map((embedding, idx) => {
       const embeddingString = `[${embedding.join(",")}]`;
-      const documentRow = {
-        pageContent: documents[idx].pageContent,
-        embedding: embeddingString,
-        metadata: documents[idx].metadata,
-      };
-
-      return documentRow;
+      return [
+        documents[idx].pageContent,
+        embeddingString,
+        documents[idx].metadata,
+      ];
     });
 
     const chunkSize = 500;
-    const insertQuery = `
-      INSERT INTO your_table_name(column1, column2, ...)
-      VALUES $1::text[], $2::text[], ...
-    `;
+
     for (let i = 0; i < rows.length; i += chunkSize) {
       const chunk = rows.slice(i, i + chunkSize);
-    
+      const insertQuery = this.buildInsertQuery(chunk, i);
+      const flatValues = chunk.flat();
+
       try {
-        await this.pool.query(insertQuery, chunk);
+        await this.pool.query(insertQuery, flatValues);
       } catch (e) {
         console.error(e);
-        throw new Error(`Error inserting: ${chunk[0].pageContent}`);
+        throw new Error(`Error inserting: ${chunk[1]}`);
       }
     }
   }
@@ -142,38 +184,37 @@ export class PGVectorStore extends VectorStore {
    * @param query Query vector.
    * @param k Number of most similar documents to return.
    * @param filter Optional filter to apply to the search.
-   * @returns Promise that resolves with an array of tuples, each containing a `PGVectorStoreDocument` and its similarity score.
+   * @returns Promise that resolves with an array of tuples, each containing a `Document` and its similarity score.
    */
   async similaritySearchVectorWithScore(
     query: number[],
     k: number,
     filter?: this["FilterType"]
-  ): Promise<[PGVectorStoreDocument, number][]> {
+  ): Promise<[Document, number][]> {
     const embeddingString = `[${query.join(",")}]`;
     const _filter = filter ?? "{}";
 
     const queryString = `
-      SELECT *, embedding <=> $1 as "_distance"
+      SELECT *, ${this.vectorColumnName} <=> $1 as "_distance"
       FROM ${this.tableName}
-      WHERE metadata @> $2
+      WHERE ${this.metadataColumnName} @> $2
       ORDER BY "_distance" ASC
       LIMIT $3;`;
 
-    const documents = (await this.pool.query<PGVectorStoreDocument & { _distance: number }>(queryString, [
-      embeddingString,
-      _filter,
-      k,
-    ])).rows;
+    const documents = (
+      await this.pool.query(queryString, [embeddingString, _filter, k])
+    ).rows;
 
-    const results = [] as [PGVectorStoreDocument, number][];
+    const results = [] as [Document, number][];
     for (const doc of documents) {
-      if (doc._distance != null && doc.pageContent != null) {
-        const document = new Document(doc) as PGVectorStoreDocument;
-        document.id = doc.id;
+      if (doc._distance != null && doc[this.contentColumnName] != null) {
+        const document = new Document({
+          pageContent: doc[this.contentColumnName],
+          metadata: doc[this.metadataColumnName],
+        });
         results.push([document, doc._distance]);
       }
     }
-
     return results;
   }
 
@@ -184,16 +225,14 @@ export class PGVectorStore extends VectorStore {
    */
   async ensureTableInDatabase(): Promise<void> {
     await this.pool.query("CREATE EXTENSION IF NOT EXISTS vector;");
-    await this.pool.query(
-      'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
-    );
+    await this.pool.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
 
     await this.pool.query(`
       CREATE TABLE IF NOT EXISTS ${this.tableName} (
-        "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-        "pageContent" text,
-        metadata jsonb,
-        embedding vector
+        "${this.idColumnName}" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+        "${this.contentColumnName}" text,
+        "${this.metadataColumnName}" jsonb,
+        "${this.vectorColumnName}" vector
       );
     `);
   }
@@ -240,24 +279,17 @@ export class PGVectorStore extends VectorStore {
     embeddings: Embeddings,
     dbConfig: PGVectorStoreArgs
   ): Promise<PGVectorStore> {
-    const instance = await PGVectorStore.fromDataSource(embeddings, dbConfig);
+    const instance = await PGVectorStore.initialize(embeddings, dbConfig);
     await instance.addDocuments(docs);
 
     return instance;
   }
 
   /**
-   * Static method to create a new `PGVectorStore` instance from an
-   * existing index.
-   * @param embeddings Embeddings instance.
-   * @param dbConfig `PGVectorStoreArgs` instance.
-   * @returns Promise that resolves with a new instance of `PGVectorStore`.
+   * Closes all the clients in the pool and terminates the pool.
+   * @returns Promise that resolves when all clients are closed and the pool is terminated.
    */
-  static async fromExistingIndex(
-    embeddings: Embeddings,
-    dbConfig: PGVectorStoreArgs
-  ): Promise<PGVectorStore> {
-    const instance = await PGVectorStore.fromDataSource(embeddings, dbConfig);
-    return instance;
+  async end(): Promise<void> {
+    return this.pool.end();
   }
 }
