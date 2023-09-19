40fb701f507236a08538f9c9b2820731625769ff
oliver@rivaltech.com <oliver@rivaltech.com>
2023-09-19 08:14:55 -0700

    Added tests for getModelContextSize

--- langchain/src/base_language/tests/count_tokens.test.ts
+++ langchain/src/base_language/tests/count_tokens.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { calculateMaxTokens } from "../count_tokens.js";
+import { calculateMaxTokens, getModelContextSize } from "../count_tokens.js";
 
 test("properly calculates correct max tokens", async () => {
   expect(
@@ -22,4 +22,12 @@ test("properly calculates correct max tokens", async () => {
   expect(await calculateMaxTokens({ prompt: "", modelName: "gpt-4-32k" })).toBe(
     32768
   );
+});
+
+test("properly gets model context size", async () => {
+  expect(await getModelContextSize("gpt-3.5-turbo-16k")).toBe(16384);
+  expect(await getModelContextSize("gpt-3.5-turbo-16k-0613")).toBe(16384);
+  expect(await getModelContextSize("gpt-3.5-turbo")).toBe(4096);
+  expect(await getModelContextSize("gpt-4")).toBe(8192);
+  expect(await getModelContextSize("gpt-4-32k")).toBe(32768);
 });
