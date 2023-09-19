3b451ad4d449a46e222552becdf4413bc771eda1
jacoblee93 <jacoblee93@gmail.com>
2023-09-18 20:30:58 -0700

    Rename files

--- langchain/src/experimental/chat_models/tests/bittensor_chat_model.int.test.ts
+++ langchain/src/experimental/chat_models/tests/bittensor_chat_model.int.test.ts
@@ -2,7 +2,7 @@ import { test } from "@jest/globals";
 import { NIBittensorChatModel } from "../bittensor.js";
 import { HumanMessage } from "../../../schema/index.js";
 
-test("Test", async () => {
+test.skip("Test", async () => {
   const chat = new NIBittensorChatModel();
   const message = new HumanMessage("What is bittensor?");
   const res = await chat.call([message]);
