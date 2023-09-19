dadc9eaeb17e1d6f74910253fbbcea359bd9d499
Tom Rudick <tmrudick@gmail.com>
2023-09-19 15:33:43 -0400

    Add test to verify that we are not getting back the system prompt

--- langchain/src/llms/tests/llama_cpp.int.test.ts
+++ langchain/src/llms/tests/llama_cpp.int.test.ts
@@ -16,3 +16,16 @@ test.skip("Test Llama_CPP", async () => {
   const res = await model.call("Where do Pandas live?");
   console.log(res);
 }, 100000);
+
+test.skip("Test Llama_CPP", async () => {
+  const model = new LlamaCpp({ modelPath: llamaPath });
+
+  // Attempt to make several queries and make sure that the system prompt
+  // is not returned as part of any follow-on query.
+  for (let i = 0; i < 5; i += 1) {
+    const res = await model.call("Where do Pandas live?");
+    expect(res).not.toContain(
+      "You are a helpful, respectful and honest assistant."
+    );
+  }
+}, 100000);
