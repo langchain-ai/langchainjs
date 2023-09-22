fc65a58218b66122b3486d64e77d0e39bcc9867f
Brace Sproul <braceasproul@gmail.com>
2023-09-22 12:14:49 -0700

    lint

--- langchain/src/chat_models/base.ts
+++ langchain/src/chat_models/base.ts
@@ -340,7 +340,9 @@ export abstract class BaseChatModel<
     const { cache } = this;
     const params = this.serialize();
     params.stop = callOptions.stop ?? params.stop;
-    const llmStringKey = `${Object.entries(params).sort()}${Object.entries(callOptions).sort()}`;
+    const llmStringKey = `${Object.entries(params).sort()} ${Object.entries(
+      callOptions
+    ).sort()}`;
 
     const missingPromptIndices: number[] = [];
     const generations = await Promise.all(
