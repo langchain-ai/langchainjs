c9041793efca04841dc028e947ab559086ad57ab
Anej Gorkič <anej.gorkic@gmail.com>
2023-09-28 23:45:41 +0200

    Remove default prompt

--- langchain/src/chains/graph_qa/prompts.ts
+++ langchain/src/chains/graph_qa/prompts.ts
@@ -1,16 +1,5 @@
 import { PromptTemplate } from "../../prompts/prompt.js";
 
-const _DEFAULT_GRAPH_QA_TEMPLATE = `Use the following knowledge triplets to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-
-{context}
-
-Question: {question}
-Helpful Answer:`;
-export const GRAPH_QA_PROMPT = new PromptTemplate({
-  template: _DEFAULT_GRAPH_QA_TEMPLATE,
-  inputVariables: ["context", "question"],
-});
-
 const CYPHER_GENERATION_TEMPLATE = `Task:Generate Cypher statement to query a graph database.
 Instructions:
 Use only the provided relationship types and properties in the schema.
