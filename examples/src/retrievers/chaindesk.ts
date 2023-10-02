4106a2efb3475f4e385b47a5e5475cee055ecd81
Georges Petrov <georgesm.petrov@gmail.com>
2023-10-02 12:04:16 +0200

    fix: Add back Chaindesk retriever

---
+++ examples/src/retrievers/chaindesk.ts
@@ -0,0 +1,13 @@
+import { ChaindeskRetriever } from "langchain/retrievers/chaindesk";
+
+export const run = async () => {
+  const retriever = new ChaindeskRetriever({
+    datastoreId: "DATASTORE_ID",
+    apiKey: "CHAINDESK_API_KEY", // optional: needed for private datastores
+    topK: 8, // optional: default value is 3
+  });
+
+  const docs = await retriever.getRelevantDocuments("hello");
+
+  console.log(docs);
+};
