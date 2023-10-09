b4dee853625e3ffb97023732f24d4ce08120f1bc
Oskar Hane <oh@oskarhane.com>
2023-10-09 11:43:26 +0200

    Convert Neo4j Integers to strings in the resultsets
    So the LLMs can interpret them correctly.
    Added to test.

--- langchain/src/graphs/tests/neo4j_graph.int.test.ts
+++ langchain/src/graphs/tests/neo4j_graph.int.test.ts
@@ -3,7 +3,7 @@
 import { test } from "@jest/globals";
 import { Neo4jGraph } from "../neo4j_graph.js";
 
-test.skip("Test that Neo4j database is correctly instantiated and connected", async () => {
+test("Test that Neo4j database is correctly instantiated and connected", async () => {
   const url = process.env.NEO4J_URI as string;
   const username = process.env.NEO4J_USERNAME as string;
   const password = process.env.NEO4J_PASSWORD as string;
@@ -14,9 +14,9 @@ test.skip("Test that Neo4j database is correctly instantiated and connected", as
 
   const graph = await Neo4jGraph.initialize({ url, username, password });
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return graph.query('RETURN "test" AS output').then((output: any) => {
-    const expectedOutput = [{ output: "test" }];
-    expect(output).toEqual(expectedOutput);
-  });
+  // Integers are casted to strings in the output
+  const expectedOutput = [{ output: { str: "test", int: "1" } }];
+  const res = await graph.query('RETURN {str: "test", int: 1} AS output');
+  await graph.close();
+  expect(res).toEqual(expectedOutput);
 });
