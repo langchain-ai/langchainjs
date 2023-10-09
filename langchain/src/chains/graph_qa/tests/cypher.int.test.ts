2b55f5e518c56f685aa59cfcae5c818976ee8596
jacoblee93 <jacoblee93@gmail.com>
2023-10-09 11:49:07 -0700

    Fix test

--- langchain/src/chains/graph_qa/tests/cypher.int.test.ts
+++ langchain/src/chains/graph_qa/tests/cypher.int.test.ts
@@ -111,6 +111,7 @@ describe("testCypherGeneratingRun", () => {
     expect(output.result.includes(expectedOutput)).toBeTruthy();
 
     const { query } = output[INTERMEDIATE_STEPS_KEY][0];
+    console.log(query);
     // const expectedQuery =
     //   "\n\nMATCH (a:Actor)-[:ACTED_IN]->" +
     //   "(m:Movie) WHERE m.title = 'Pulp Fiction' RETURN a.name";
