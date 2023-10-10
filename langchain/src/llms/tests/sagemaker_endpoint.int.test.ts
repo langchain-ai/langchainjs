72e2db8a8f15931ee89f13c9eb8d6a5e83e36afc
Ivonei Junior <ivoneijr@productbot.ai>
2023-10-10 16:34:40 -0300

    feat: Add support for streaming in the SageMaker LLM (#2734)
    * init SageMakerEndpointWithStream implementation
    
    * add entrypoint and run lint and format
    
    * add docs
    
    * fix example usage
    
    * create a unified LLM for aws sagemaker with streaming option flag
    
    * unify SageMakerEndpoint and SageMakerEndpointWithStream into SageMaker LLM
    
    * Revert rename
    
    * Throw errors on SageMakerEndpoint streaming errors
    
    * Fix docs
    
    * Add comments back in
    
    * Pass thrown errors
    
    * add missing transformOutput content handler call for streaming calls and fix test
    
    * Update docs
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- langchain/src/llms/tests/sagemaker_endpoint.int.test.ts
+++ langchain/src/llms/tests/sagemaker_endpoint.int.test.ts
@@ -1,51 +1,133 @@
 /* eslint-disable no-process-env */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { test, expect } from "@jest/globals";
+import { expect, test } from "@jest/globals";
 import {
-  SageMakerLLMContentHandler,
   SageMakerEndpoint,
+  SageMakerLLMContentHandler,
 } from "../sagemaker_endpoint.js";
 
-class HuggingFaceTextGenerationGPT2ContentHandler
-  implements SageMakerLLMContentHandler
-{
-  contentType = "application/json";
+// yarn test:single /{path_to}/langchain/src/llms/tests/sagemaker.int.test.ts
+describe.skip("Test SageMaker LLM", () => {
+  test("without streaming", async () => {
+    interface ResponseJsonInterface {
+      generation: {
+        content: string;
+      };
+    }
 
-  accepts = "application/json";
+    class LLama213BHandler implements SageMakerLLMContentHandler {
+      contentType = "application/json";
 
-  async transformInput(prompt: string, modelKwargs: Record<string, unknown>) {
-    const inputString = JSON.stringify({
-      text_inputs: prompt,
-      ...modelKwargs,
-    });
-    return Buffer.from(inputString);
-  }
+      accepts = "application/json";
 
-  async transformOutput(output: Uint8Array) {
-    const responseJson = JSON.parse(Buffer.from(output).toString("utf-8"));
-    return responseJson.generated_texts[0];
-  }
-}
+      async transformInput(
+        prompt: string,
+        modelKwargs: Record<string, unknown>
+      ): Promise<Uint8Array> {
+        const payload = {
+          inputs: [[{ role: "user", content: prompt }]],
+          parameters: modelKwargs,
+        };
 
-// Requires a pre-configured sagemaker endpoint
-test.skip("Test SageMakerEndpoint", async () => {
-  const contentHandler = new HuggingFaceTextGenerationGPT2ContentHandler();
+        const input_str = JSON.stringify(payload);
 
-  const model = new SageMakerEndpoint({
-    endpointName:
-      "jumpstart-example-huggingface-textgener-2023-05-16-22-35-45-660",
-    modelKwargs: { temperature: 1e-10 },
-    contentHandler,
-    clientOptions: {
-      region: "us-east-2",
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        return new TextEncoder().encode(input_str);
+      }
+
+      async transformOutput(output: Uint8Array): Promise<string> {
+        const response_json = JSON.parse(
+          new TextDecoder("utf-8").decode(output)
+        ) as ResponseJsonInterface[];
+        const content = response_json[0]?.generation.content ?? "";
+        return content;
+      }
+    }
+
+    const contentHandler = new LLama213BHandler();
+    const model = new SageMakerEndpoint({
+      endpointName: "aws-productbot-ai-dev-llama-2-13b-chat",
+      streaming: false,
+      modelKwargs: {
+        temperature: 0.5,
+        max_new_tokens: 700,
+        top_p: 0.9,
       },
-    },
+      endpointKwargs: {
+        CustomAttributes: "accept_eula=true",
+      },
+      contentHandler,
+      clientOptions: {
+        region: "us-east-1",
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        },
+      },
+    });
+
+    const response = await model.call(
+      "hello, my name is John Doe, tell me a fun story about llamas."
+    );
+
+    expect(response.length).toBeGreaterThan(0);
   });
 
-  const res = await model.call("Hello, my name is ");
+  test("with streaming", async () => {
+    class LLama213BHandler implements SageMakerLLMContentHandler {
+      contentType = "application/json";
 
-  expect(typeof res).toBe("string");
+      accepts = "application/json";
+
+      async transformInput(
+        prompt: string,
+        modelKwargs: Record<string, unknown>
+      ): Promise<Uint8Array> {
+        const payload = {
+          inputs: [[{ role: "user", content: prompt }]],
+          parameters: modelKwargs,
+        };
+
+        const input_str = JSON.stringify(payload);
+
+        return new TextEncoder().encode(input_str);
+      }
+
+      async transformOutput(output: Uint8Array): Promise<string> {
+        return new TextDecoder("utf-8").decode(output);
+      }
+    }
+
+    const contentHandler = new LLama213BHandler();
+    const model = new SageMakerEndpoint({
+      endpointName: "aws-productbot-ai-dev-llama-2-13b-chat",
+      streaming: true, // specify streaming
+      modelKwargs: {
+        temperature: 0.5,
+        max_new_tokens: 700,
+        top_p: 0.9,
+      },
+      endpointKwargs: {
+        CustomAttributes: "accept_eula=true",
+      },
+      contentHandler,
+      clientOptions: {
+        region: "us-east-1",
+        credentials: {
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        },
+      },
+    });
+
+    const response = await model.call(
+      "hello, my name is John Doe, tell me a fun story about llamas in 3 paragraphs"
+    );
+
+    const chunks = [];
+    for await (const chunk of response) {
+      chunks.push(chunk);
+    }
+
+    expect(response.length).toBeGreaterThan(0);
+  });
 });
