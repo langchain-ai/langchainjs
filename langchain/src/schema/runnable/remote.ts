3d2f349f869a9eb29ac070a311e301cf09a48cd6
Erick Friis <erick@langchain.dev>
2023-10-10 12:25:34 -0700

    initial

--- langchain/src/schema/runnable/remote.ts
+++ langchain/src/schema/runnable/remote.ts
@@ -2,10 +2,132 @@ import { Runnable, RunnableBatchOptions } from "./base.js";
 import { RunnableConfig } from "./config.js";
 import { IterableReadableStream } from "../../util/stream.js";
 import { CallbackManagerForChainRun } from "../../callbacks/manager.js";
+import {
+  getBytes,
+  getLines,
+  getMessages,
+} from "../../util/event-source-parse.js";
+import { Document } from "../../document.js";
+import {
+  AIMessage,
+  AIMessageChunk,
+  ChatMessage,
+  ChatMessageChunk,
+  FunctionMessage,
+  FunctionMessageChunk,
+  HumanMessage,
+  HumanMessageChunk,
+  SystemMessage,
+  SystemMessageChunk,
+} from "../index.js";
+import { StringPromptValue } from "../../prompts/base.js";
+import { ChatPromptValue } from "../../prompts/chat.js";
 
 type RemoteRunnableOptions = {
   timeout?: number;
 };
+
+function isSuperset(set: Set<string>, subset: Set<string>) {
+  for (const elem of subset) {
+    if (!set.has(elem)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function revive(obj: any): any {
+  if (obj === null) return null;
+  if (Array.isArray(obj)) return obj.map(revive);
+  if (typeof obj === "object") {
+    const keysArr = Object.keys(obj);
+    const keys = new Set(keysArr);
+    if (isSuperset(keys, new Set(["page_content", "metadata"])))
+      return new Document({
+        pageContent: obj.page_content,
+        metadata: obj.metadata,
+      });
+    //content, type
+    if (isSuperset(keys, new Set(["content", "type", "is_chunk"]))) {
+      if (!obj.is_chunk) {
+        if (obj.type === "human") {
+          return new HumanMessage({
+            content: obj.content,
+          });
+        }
+        if (obj.type === "system") {
+          return new SystemMessage({
+            content: obj.content,
+          });
+        }
+        if (obj.type === "chat") {
+          return new ChatMessage({
+            content: obj.content,
+            role: obj.role,
+          });
+        }
+        if (obj.type === "function") {
+          return new FunctionMessage({
+            content: obj.content,
+            name: obj.name,
+          });
+        }
+        if (obj.type === "ai") {
+          return new AIMessage({
+            content: obj.content,
+          });
+        }
+      } else {
+        if (obj.type === "human") {
+          return new HumanMessageChunk({
+            content: obj.content,
+          });
+        }
+        if (obj.type === "system") {
+          return new SystemMessageChunk({
+            content: obj.content,
+          });
+        }
+        if (obj.type === "chat") {
+          return new ChatMessageChunk({
+            content: obj.content,
+            role: obj.role,
+          });
+        }
+        if (obj.type === "function") {
+          return new FunctionMessageChunk({
+            content: obj.content,
+            name: obj.name,
+          });
+        }
+        if (obj.type === "ai") {
+          return new AIMessageChunk({
+            content: obj.content,
+          });
+        }
+      }
+    }
+    if (isSuperset(keys, new Set(["text"]))) {
+      return new StringPromptValue(obj.text);
+    }
+    if (isSuperset(keys, new Set(["messages"]))) {
+      return new ChatPromptValue({
+        messages: obj.messages.map((msg: any) => revive(msg)),
+      });
+    }
+    const innerRevive: (key: string) => [string, any] = (key: string) => [
+      key,
+      revive(obj[key]),
+    ];
+    const rtn = Object.fromEntries(keysArr.map(innerRevive));
+    return rtn;
+  }
+  return obj;
+}
+function deserialize<RunOutput>(str: string): RunOutput {
+  const obj = JSON.parse(str);
+  return revive(obj);
+}
 
 function withoutCallbacks(
   options?: RunnableConfig
@@ -29,10 +151,13 @@ export class RemoteRunnable<RunInput, RunOutput> extends Runnable<
     this.options = options;
   }
 
-  private post<Body>(path: string, body: Body) {
-    return fetch(`${this.url}${path}`, {
+  private async post<Body>(path: string, body: Body) {
+    return await fetch(`${this.url}${path}`, {
       method: "POST",
       body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+      },
       signal: AbortSignal.timeout(this.options?.timeout ?? 5000),
     });
   }
@@ -47,7 +172,7 @@ export class RemoteRunnable<RunInput, RunOutput> extends Runnable<
       config: withoutCallbacks(options),
       kwargs: {},
     });
-    return response.body as RunOutput;
+    return revive((await response.json()).output) as RunOutput;
   }
 
   async _batch(
@@ -76,7 +201,7 @@ export class RemoteRunnable<RunInput, RunOutput> extends Runnable<
 
     if (!body.output) throw new Error("Invalid response from remote runnable");
 
-    return JSON.parse(body.output);
+    return revive(body.output);
   }
 
   async batch(
@@ -105,7 +230,12 @@ export class RemoteRunnable<RunInput, RunOutput> extends Runnable<
     if (batchOptions?.returnExceptions) {
       throw Error("returnExceptions is not supported for remote clients");
     }
-    return this._batchWithConfig(this._batch, inputs, options, batchOptions);
+    return this._batchWithConfig(
+      this._batch.bind(this),
+      inputs,
+      options,
+      batchOptions
+    );
   }
 
   async stream(
@@ -132,16 +262,23 @@ export class RemoteRunnable<RunInput, RunOutput> extends Runnable<
         "Could not begin LangServe stream. Please check the given URL and try again."
       );
     }
-    const decoder = new TextDecoder();
-    const { readable, writable } = new TransformStream<Uint8Array, RunOutput>({
-      transform: (chunk, controller) => {
-        const decoded = decoder.decode(chunk);
-        const parsed = JSON.parse(decoded);
-        controller.enqueue(JSON.parse(parsed.data));
+    const body = response.body;
+    const stream = new ReadableStream({
+      start(controller) {
+        const enqueueLine = getMessages((msg) => {
+          if (msg.data) controller.enqueue(deserialize(msg.data));
+        });
+        const onLine = (
+          line: Uint8Array,
+          fieldLength: number,
+          flush?: boolean
+        ) => {
+          enqueueLine(line, fieldLength, flush);
+          if (flush) controller.close();
+        };
+        getBytes(body, getLines(onLine));
       },
     });
-    response.body.pipeTo(writable);
-    const stream = IterableReadableStream.fromReadableStream(readable);
-    return stream;
+    return IterableReadableStream.fromReadableStream(stream);
   }
 }
---
+++ langchain/src/schema/tests/runnable_remote.test.ts
@@ -0,0 +1,86 @@
+/* eslint-disable no-promise-executor-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { jest, test } from "@jest/globals";
+
+import { RemoteRunnable } from "../runnable/remote.js";
+
+const BASE_URL = "http://my-langserve-endpoint";
+
+describe("RemoteRunnable", () => {
+  beforeAll(() => {
+    // mock langserve service
+    const returnDataByEndpoint: Record<string, BodyInit> = {
+      "/invoke": JSON.stringify({ output: ["a", "b", "c"] }),
+      "/batch": JSON.stringify({
+        output: [
+          ["a", "b", "c"],
+          ["d", "e", "f"],
+        ],
+      }),
+      "/stream": new ReadableStream<Uint8Array>({
+        start(controller) {
+          const dataChunks = [
+            "event: data",
+            `data: ["a", "b", "c", "d"]\n`,
+            "event: end",
+          ];
+          // Push chunks of data to the stream
+          for (const chunk of dataChunks) {
+            controller.enqueue(Buffer.from(chunk + "\n"));
+          }
+          // Close the stream
+          controller.close();
+        },
+      }),
+    };
+
+    const oldFetch = global.fetch;
+
+    global.fetch = jest
+      .fn()
+      .mockImplementation(async (url: any, init?: any) => {
+        if (!url.startsWith(BASE_URL)) return await oldFetch(url, init);
+        const { pathname } = new URL(url);
+        const resp: Response = new Response(returnDataByEndpoint[pathname]);
+        return resp;
+      }) as any;
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+
+  test("Invoke local langserve", async () => {
+    // mock fetch, expect /invoke
+    const remote = new RemoteRunnable(BASE_URL);
+    const result = await remote.invoke({ text: "string" });
+    expect(fetch).toHaveBeenCalledWith(
+      `${BASE_URL}/invoke`,
+      expect.objectContaining({
+        body: '{"input":{"text":"string"},"config":{},"kwargs":{}}',
+      })
+    );
+    expect(result).toEqual(["a", "b", "c"]);
+  });
+
+  test("Batch local langserve", async () => {
+    const returnData = [
+      ["a", "b", "c"],
+      ["d", "e", "f"],
+    ];
+    const remote = new RemoteRunnable(BASE_URL);
+    const result = await remote.batch([{ text: "1" }, { text: "2" }]);
+    expect(result).toEqual(returnData);
+  });
+
+  test("Stream local langserve", async () => {
+    const remote = new RemoteRunnable(BASE_URL);
+    const stream = await remote.stream({ text: "What are the 5 best apples?" });
+    let chunkCount = 0;
+    for await (const chunk of stream) {
+      expect(chunk).toEqual(["a", "b", "c", "d"]);
+      chunkCount += 1;
+    }
+    expect(chunkCount).toBe(1);
+  });
+});
