c7e11aa22ec20bf29fb843b601ac5a3f3afbd5b5
Brace Sproul <braceasproul@gmail.com>
2023-10-10 19:33:41 -0700

    feat: added with sql output example

--- examples/src/chains/sql_db_sql_output.ts
+++ examples/src/chains/sql_db_sql_output.ts
@@ -2,6 +2,10 @@ import { DataSource } from "typeorm";
 import { OpenAI } from "langchain/llms/openai";
 import { SqlDatabase } from "langchain/sql_db";
 import { SqlDatabaseChain } from "langchain/chains/sql_db";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { RunnableSequence } from "langchain/schema/runnable";
+import { StringOutputParser } from "langchain/schema/output_parser";
 
 /**
  * This example uses Chinook database, which is a sample database available for SQL Server, Oracle, MySQL, etc.
@@ -17,17 +21,68 @@ const db = await SqlDatabase.fromDataSourceParams({
   appDataSource: datasource,
 });
 
-const chain = new SqlDatabaseChain({
-  llm: new OpenAI({ temperature: 0 }),
-  database: db,
-  sqlOutputKey: "sql",
+const llm = new ChatOpenAI();
+
+const prompt =
+  PromptTemplate.fromTemplate(`Based on the provided SQL table schema below, write a SQL query that would answer the user's question.
+------------
+SCHEMA: {schema}
+------------
+QUESTION: {question}
+------------
+SQL QUERY:`);
+
+const sqlQueryChain = RunnableSequence.from([
+  {
+    schema: async () => db.getTableInfo(),
+    question: (input: { question: string }) => input.question,
+  },
+  prompt,
+  llm.bind({ stop: ["\nSQLResult:"] }),
+  new StringOutputParser(),
+]);
+
+const finalResponsePrompt =
+  PromptTemplate.fromTemplate(`Based on the table schema below, question, SQL query, and SQL response, write a natural language response:
+------------
+SCHEMA: {schema}
+------------
+QUESTION: {question}
+------------
+SQL QUERY: {query}
+------------
+SQL RESPONSE: {response}
+------------
+NATURAL LANGUAGE RESPONSE:`);
+
+const finalChain = RunnableSequence.from([
+  {
+    question: (input) => input.question,
+    query: sqlQueryChain,
+  },
+  {
+    schema: async () => db.getTableInfo(),
+    question: (input) => input.question,
+    query: (input) => input.query,
+    response: (input) => db.run(input.query),
+  },
+  {
+    result: finalResponsePrompt.pipe(llm).pipe(new StringOutputParser()),
+    sql: (previousStepResult) => previousStepResult.query,
+  },
+]);
+
+const finalResponse = await finalChain.invoke({
+  question: "How many employees are there?",
 });
 
-const res = await chain.call({ query: "How many tracks are there?" });
-/* Expected result:
+console.log({ finalResponse });
+
+/**
  * {
- *   result: ' There are 3503 tracks.',
- *   sql: ' SELECT COUNT(*) FROM "Track";'
+ *   finalResponse: {
+ *     result: 'There are 8 employees.',
+ *     sql: 'SELECT COUNT(*) FROM tracks;'
+ *   }
  * }
  */
-console.log(res);
