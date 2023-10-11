da189ef96185e612a0af39f3c8845de586b244e8
Brace Sproul <braceasproul@gmail.com>
2023-10-10 19:18:32 -0700

    fix: added legacy files and updated base sql example to use runables

--- examples/src/chains/sql_db.ts
+++ examples/src/chains/sql_db.ts
@@ -2,6 +2,13 @@ import { DataSource } from "typeorm";
 import { OpenAI } from "langchain/llms/openai";
 import { SqlDatabase } from "langchain/sql_db";
 import { SqlDatabaseChain } from "langchain/chains/sql_db";
+import { PromptTemplate } from "langchain/prompts";
+import {
+  RunnablePassthrough,
+  RunnableSequence,
+} from "langchain/schema/runnable";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { StringOutputParser } from "langchain/schema/output_parser";
 
 /**
  * This example uses Chinook database, which is a sample database available for SQL Server, Oracle, MySQL, etc.
@@ -17,11 +24,71 @@ const db = await SqlDatabase.fromDataSourceParams({
   appDataSource: datasource,
 });
 
-const chain = new SqlDatabaseChain({
-  llm: new OpenAI({ temperature: 0 }),
-  database: db,
+const llm = new ChatOpenAI();
+
+const prompt =
+  PromptTemplate.fromTemplate(`Based on the provided SQL table schema below, write a SQL query that would answer the user's question.
+------------
+SCHEMA: {schema}
+------------
+QUESTION: {question}
+------------
+SQL QUERY:`);
+
+const sqlQueryChain = RunnableSequence.from([
+  {
+    schema: async () => db.getTableInfo(),
+    question: (input: { question: string }) => input.question,
+  },
+  prompt,
+  llm.bind({ stop: ["\nSQLResult:"] }),
+  new StringOutputParser(),
+]);
+
+const res = await sqlQueryChain.invoke({
+  question: "How many employees are there?",
+});
+console.log({ res });
+
+/**
+ * { res: 'SELECT COUNT(*) FROM tracks;' }
+ */
+
+const finalResponsePrompt =
+  PromptTemplate.fromTemplate(`Based on the table schema below, question, SQL query, and SQL response, write a natural language response:
+------------
+SCHEMA: {schema}
+------------
+QUESTION: {question}
+------------
+SQL QUERY: {query}
+------------
+SQL RESPONSE: {response}
+------------
+NATURAL LANGUAGE RESPONSE:`);
+
+const finalChain = RunnableSequence.from([
+  {
+    question: (input) => input.question,
+    query: sqlQueryChain,
+  },
+  {
+    schema: async () => db.getTableInfo(),
+    question: (input) => input.question,
+    query: (input) => input.query,
+    response: (input) => db.run(input.query),
+  },
+  finalResponsePrompt,
+  llm,
+  new StringOutputParser(),
+]);
+
+const finalResponse = await finalChain.invoke({
+  question: "How many employees are there?",
 });
 
-const res = await chain.run("How many tracks are there?");
-console.log(res);
-// There are 3503 tracks.
+console.log({ finalResponse });
+
+/**
+ * { finalResponse: 'There are 8 employees.' }
+ */
