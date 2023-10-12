0ff1edaf87e0fa965f5f830e05d0a0f572e91b0f
jacoblee93 <jacoblee93@gmail.com>
2023-10-12 09:40:52 -0700

    Allow backwards compatibility with docstore

--- langchain/src/retrievers/tests/parent_document.int.test.ts
+++ langchain/src/retrievers/tests/parent_document.int.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "@jest/globals";
 import { TextLoader } from "../../document_loaders/fs/text.js";
+import { InMemoryDocstore } from "../../stores/doc/in_memory.js";
 import { InMemoryStore } from "../../storage/in_memory.js";
 import { OpenAIEmbeddings } from "../../embeddings/openai.js";
 import { MemoryVectorStore } from "../../vectorstores/memory.js";
@@ -55,4 +56,25 @@ test("Should return a part of a document if a parent splitter is passed", async 
   console.log(retrievedDocs);
   expect(retrievedDocs.length).toBeGreaterThan(1);
   expect(retrievedDocs[0].pageContent.length).toBeGreaterThan(100);
+});
+
+test("Should work with a backwards compatible docstore too", async () => {
+  const vectorstore = new MemoryVectorStore(new OpenAIEmbeddings());
+  const retriever = new ParentDocumentRetriever({
+    vectorstore,
+    docstore: new InMemoryDocstore(),
+    childSplitter: new RecursiveCharacterTextSplitter({
+      chunkOverlap: 0,
+      chunkSize: 100,
+    }),
+  });
+  const docs = await new TextLoader(
+    "../examples/state_of_the_union.txt"
+  ).load();
+  await retriever.addDocuments(docs);
+
+  const query = "justice breyer";
+  const retrievedDocs = await retriever.getRelevantDocuments(query);
+  expect(retrievedDocs.length).toEqual(1);
+  expect(retrievedDocs[0].pageContent.length).toBeGreaterThan(1000);
 });
