cff094a74097265843085bf6f7cb02490c3c6f13
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2023-10-13 11:13:07 -0700

    improve code readability

--- langchain/src/vectorstores/cassandra.ts
+++ langchain/src/vectorstores/cassandra.ts
@@ -218,9 +218,8 @@ export class CassandraStore extends VectorStore {
 
       const metadataColNames = Object.keys(document.metadata);
       const metadataVals = Object.values(document.metadata);
-      const query = `INSERT INTO ${this.keyspace}.${this.table} (vector, text${
-        metadataColNames.length > 0 ? ", " + metadataColNames.join(", ") : ""
-      }) VALUES ([${vector}], '${document.pageContent}'${
+      const metadataInsert = metadataColNames.length > 0 ? ", " + metadataColNames.join(", ") : ""
+      const query = `INSERT INTO ${this.keyspace}.${this.table} (vector, text${metadataInsert}) VALUES ([${vector}], '${document.pageContent}'${
         metadataVals.length > 0
           ? ", " +
             metadataVals
