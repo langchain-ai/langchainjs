e9cce49139db2b56512ff885bec466523b2ab7b4
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-10-18 09:38:14 -0700

    add mmr retriever

--- examples/src/retrievers/zep.ts
+++ examples/src/retrievers/zep.ts
@@ -1,4 +1,4 @@
-import { ZepRetriever } from "langchain/retrievers/zep";
+import { ZepRetriever, ZepRetrieverConfig } from "langchain/retrievers/zep";
 import { ZepMemory } from "langchain/memory/zep";
 import { Memory as MemoryModel, Message } from "@getzep/zep-js";
 import { randomUUID } from "crypto";
@@ -11,14 +11,14 @@ function sleep(ms: number) {
 export const run = async () => {
   const zepConfig = {
     url: process.env.ZEP_URL || "http://localhost:8000",
-    sessionId: `session_${randomUUID()}`,
+    sessionId: `session_${randomUUID()}`
   };
 
   console.log(`Zep Config: ${JSON.stringify(zepConfig)}`);
 
   const memory = new ZepMemory({
     baseURL: zepConfig.url,
-    sessionId: zepConfig.sessionId,
+    sessionId: zepConfig.sessionId
   });
 
   // Generate chat messages
@@ -28,7 +28,11 @@ export const run = async () => {
     { role: "AI", message: "Great! What kind of car are you looking for?" },
     { role: "User", message: "I'm looking for a red car." },
     { role: "AI", message: "We have many red cars. Anything more specific?" },
-    { role: "User", message: "I'm looking for a red car with a sunroof." },
+    {
+      role: "User",
+      message:
+        "I'm looking for a red car with a sunroof that I can drive to Paris."
+    }
   ];
 
   const zepClient = await memory.zepClientPromise;
@@ -41,24 +45,46 @@ export const run = async () => {
     let m: MemoryModel;
     if (chatMessage.role === "AI") {
       m = new MemoryModel({
-        messages: [new Message({ role: "ai", content: chatMessage.message })],
+        messages: [new Message({ role: "ai", content: chatMessage.message })]
       });
     } else {
       m = new MemoryModel({
-        messages: [
-          new Message({ role: "human", content: chatMessage.message }),
-        ],
+        messages: [new Message({ role: "human", content: chatMessage.message })]
       });
     }
 
     await zepClient.memory.addMemory(zepConfig.sessionId, m);
   }
 
-  await sleep(2000);
+  await sleep(5000);
 
-  const query = "What type of car did the user ask for?";
-  const retriever = new ZepRetriever(zepConfig);
+  // Simple similarity search
+  const query = "Can I drive red cars in France?";
+  const retriever = new ZepRetriever({ ...zepConfig, topK: 3 });
   const docs = await retriever.getRelevantDocuments(query);
+  console.log("Simple similarity search");
+  console.log(JSON.stringify(docs, null, 2));
 
-  console.log(docs);
+  // mmr reranking search
+  const mmrRetriever = new ZepRetriever({
+    ...zepConfig,
+    topK: 3,
+    searchType: "mmr",
+    mmrLambda: 0.5
+  });
+  const mmrDocs = await mmrRetriever.getRelevantDocuments(query);
+  console.log("MMR reranking search");
+  console.log(JSON.stringify(mmrDocs, null, 2));
+
+  // Filtered search
+  const filteredRetriever = new ZepRetriever({
+    ...zepConfig,
+    topK: 3,
+    filter: {
+      where: { jsonpath: '$.system.entities[*] ? (@.Label == "GPE")' }
+    }
+  });
+  const filteredDocs = await filteredRetriever.getRelevantDocuments(query);
+  console.log("Filtered search");
+  console.log(JSON.stringify(filteredDocs, null, 2));
 };
