e87ee526442f811f59902d9f559914770bcb44b3
Brace Sproul <braceasproul@gmail.com>
2023-10-20 13:41:39 -0700

    Feat/upstash redis storage (#2963)
    * feat: upstash redis storage BaseStore
    
    * nit: rename file
    
    * nit: rename class & input interface
    
    * nit: remove unused input
    
    * feat: tests
    
    * fix: yield & tests yield
    
    * chore: lint files
    
    * nit
    
    * chore: added entrypoint
    
    * nit: tests
    
    * fix: added to requiresOptionalDependency

--- langchain/scripts/create-entrypoints.js
+++ langchain/scripts/create-entrypoints.js
@@ -257,6 +257,8 @@ const entrypoints = {
   "storage/in_memory": "storage/in_memory",
   "storage/ioredis": "storage/ioredis",
   "storage/vercel_kv": "storage/vercel_kv",
+  "storage/upstash_redis": "storage/upstash_redis",
+  // graphs
   "graphs/neo4j_graph": "graphs/neo4j_graph",
   // hub
   hub: "hub",
@@ -439,6 +441,7 @@ const requiresOptionalDependency = [
   "stores/message/xata",
   "storage/ioredis",
   "storage/vercel_kv",
+  "storage/upstash_redis",
   "graphs/neo4j_graph",
   // Prevent export due to circular dependency with "load" entrypoint
   "hub",
