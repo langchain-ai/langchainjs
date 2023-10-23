5b126d379ae53529ffc84c721e635e4dd8056690
jacoblee93 <jacoblee93@gmail.com>
2023-10-23 11:16:32 -0700

    Fix format and lint

--- langchain/src/vectorstores/cassandra.ts
+++ langchain/src/vectorstores/cassandra.ts
@@ -31,7 +31,9 @@ export interface CassandraLibArgs extends DseClientOptions {
  * texts or documents.
  */
 export class CassandraStore extends VectorStore {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   declare FilterType: Record<string, any>;
+
   private client: CassandraClient;
 
   private readonly dimensions: number;
@@ -273,8 +275,8 @@ export class CassandraStore extends VectorStore {
    */
   private buildSearchQuery(
     query: number[],
-    k: number = 1,
-    filter?: this["FilterType"]
+    k = 1,
+    filter: this["FilterType"] | undefined = undefined
   ): string {
     const whereClause = filter ? this.buildWhereClause(filter) : "";
 
