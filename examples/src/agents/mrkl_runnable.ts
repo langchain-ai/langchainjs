851529d5da76d58c6d0981d3ec6728e45020a156
Brace Sproul <braceasproul@gmail.com>
2023-10-24 15:10:25 -0700

    feat: runnable docs content

--- examples/src/agents/mrkl_runnable.ts
+++ examples/src/agents/mrkl_runnable.ts
@@ -1,10 +1,16 @@
-import {
-  ChatAgentOutputParser,
-  initializeAgentExecutorWithOptions,
-} from "langchain/agents";
+import { AgentExecutor, ChatAgentOutputParser } from "langchain/agents";
 import { OpenAI } from "langchain/llms/openai";
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  PromptTemplate,
+  SystemMessagePromptTemplate,
+} from "langchain/prompts";
+import { InputValues } from "langchain/schema";
+import { RunnableSequence } from "langchain/schema/runnable";
 import { SerpAPI } from "langchain/tools";
 import { Calculator } from "langchain/tools/calculator";
+import { renderTextDescription } from "langchain/tools/render";
 
 /** Define the model to be used */
 const model = new OpenAI({ temperature: 0 });
@@ -28,9 +34,9 @@ const outputParser = new ChatAgentOutputParser();
  * For this example we'll use the same default prompts
  * that the `ChatAgent` class uses.
  */
-export const PREFIX = `Answer the following questions as best you can. You have access to the following tools:
+const PREFIX = `Answer the following questions as best you can. You have access to the following tools:
 {tools}`;
-export const FORMAT_INSTRUCTIONS = `The way you use the tools is by specifying a json blob, denoted below by $JSON_BLOB
+const FORMAT_INSTRUCTIONS = `The way you use the tools is by specifying a json blob, denoted below by $JSON_BLOB
 Specifically, this $JSON_BLOB should have a "action" key (with the name of the tool to use) and a "action_input" key (with the input to the tool going here). 
 The $JSON_BLOB should only contain a SINGLE action, do NOT return a list of multiple actions. Here is an example of a valid $JSON_BLOB:
 
@@ -53,20 +59,67 @@ Observation: the result of the action
 ... (this Thought/Action/Observation can repeat N times)
 Thought: I now know the final answer
 Final Answer: the final answer to the original input question`;
-export const SUFFIX = `Begin! Reminder to always use the exact characters \`Final Answer\` when responding.`;
+const SUFFIX = `Begin! Reminder to always use the exact characters \`Final Answer\` when responding.`;
+const DEFAULT_HUMAN_MESSAGE_TEMPLATE = "{input}\n\n{agent_scratchpad}";
 /**
  * Now we can combine all our prompts together, passing
  * in the required input variables.
  */
-const createPrompt = () => {
-  const toolStrings = formatTo
-}
+const createPrompt = async () => {
+  // The `renderTextDescription` util function combines
+  // all tool names and descriptions into a single string.
+  const toolStrings = renderTextDescription(tools);
+  const prefixTemplate = PromptTemplate.fromTemplate(PREFIX);
+  const formattedPrefix = await prefixTemplate.format({ tools: toolStrings });
+  const template = [formattedPrefix, FORMAT_INSTRUCTIONS, SUFFIX].join("\n\n");
+  // Add the template, and human message template to an array of messages.
+  const messages = [
+    SystemMessagePromptTemplate.fromTemplate(template),
+    HumanMessagePromptTemplate.fromTemplate(DEFAULT_HUMAN_MESSAGE_TEMPLATE),
+  ];
+  // Return the final prompt as an instance of `ChatPromptTemplate`
+  return ChatPromptTemplate.fromMessages(messages);
+};
+const prompt = await createPrompt();
 
-const executor = await initializeAgentExecutorWithOptions(tools, model, {
-  agentType: "zero-shot-react-description",
-  verbose: true,
+/**
+ * Combine all our previous steps into a runnable agent.
+ * We'll use a `RunnableSequence` which takes in an input,
+ * and the previous steps. We then format the steps into a
+ * string so it can be passed to the prompt.
+ */
+const runnableAgent = RunnableSequence.from([
+  {
+    input: (i: InputValues) => i.input,
+    agent_scratchpad: (i: InputValues) => i.input,
+  },
+  prompt,
+  model,
+  outputParser,
+]);
+
+/**
+ * The last step is to pass our agent into the
+ * AgentExecutor along with the tools.
+ * The AgentExecutor is responsible for actually
+ * running the iterations.
+ */
+
+const executor = AgentExecutor.fromAgentAndTools({
+  agent: runnableAgent,
+  tools,
 });
 
-const input = `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`;
+console.log("Loaded agent executor");
 
+const input = `Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?`;
+console.log(`Calling agent with prompt: ${input}`);
 const result = await executor.call({ input });
+console.log(result);
+/**
+Loaded agent executor
+Calling agent with prompt: Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?
+{
+  output: "Jason Sudeikis, Olivia Wilde's boyfriend, is 45 years old and his age raised to the 0.23"
+}
+ */
