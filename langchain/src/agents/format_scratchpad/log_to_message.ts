834f8de95a8e4a945c8eb8429f96892966f46ca6
Brace Sproul <braceasproul@gmail.com>
2023-10-24 09:52:57 -0700

    fix: check input vars

--- langchain/src/agents/format_scratchpad/log_to_message.ts
+++ langchain/src/agents/format_scratchpad/log_to_message.ts
@@ -10,17 +10,23 @@ export function formatLogToMessage(
   intermediateSteps: AgentStep[],
   templateToolResponse = "{observation}"
 ): BaseMessage[] {
+  // Get all input variables, if there is more than one, throw an error.
+  const matches = [...templateToolResponse.matchAll(/{([^}]*)}/g)];
+  const stringsInsideBrackets = matches.map(match => match[1]);
+  if (stringsInsideBrackets.length > 1) {
+    throw new Error(
+      `templateToolResponse must contain one input variable: ${templateToolResponse}`
+    );
+  }
+
   const thoughts: BaseMessage[] = [];
-  const templateToolResponseWithoutBrackets = templateToolResponse.replace(
-    /{|}/g,
-    ""
-  );
+
   for (const step of intermediateSteps) {
     thoughts.push(new AIMessage(step.action.log));
     thoughts.push(
       new HumanMessage(
         renderTemplate(templateToolResponse, "f-string", {
-          [templateToolResponseWithoutBrackets]: step.observation,
+          [stringsInsideBrackets[0]]: step.observation,
         })
       )
     );
