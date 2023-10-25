4f56cf7ceb7f1abfc3f903c9d8a9bdb0c3d5b176
Jacob Lee <jacoblee93@gmail.com>
2023-10-24 23:19:17 -0700

    Add cookbook section and rewrite-retrieve-read example (#3039)
    * Add cookbook section
    
    * Add cookbook section and rewrite-retrieve-read example
    
    * Format
    
    * Adds noteable.io link

--- langchain/src/chains/graph_qa/tests/cypher.int.test.ts
+++ langchain/src/chains/graph_qa/tests/cypher.int.test.ts
@@ -5,7 +5,7 @@ import { Neo4jGraph } from "../../../graphs/neo4j_graph.js";
 import { OpenAI } from "../../../llms/openai.js";
 import { ChainValues } from "../../../schema/index.js";
 
-describe("testCypherGeneratingRun", () => {
+describe.skip("testCypherGeneratingRun", () => {
   const url = process.env.NEO4J_URI as string;
   const username = process.env.NEO4J_USERNAME as string;
   const password = process.env.NEO4J_PASSWORD as string;
