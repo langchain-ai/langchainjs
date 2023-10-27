a0f9b3abcafec013360ebee5f0d2824e06999116
Brace Sproul <braceasproul@gmail.com>
2023-10-27 13:11:43 -0700

    jsdoc

--- langchain/src/schema/prompt_template.ts
+++ langchain/src/schema/prompt_template.ts
@@ -1,6 +1,15 @@
 import { Document } from "../document.js";
 import { BasePromptTemplate } from "../prompts/base.js";
 
+/**
+ * Formats a document using a given prompt template.
+ * 
+ * @async
+ * @param {Document} document - The document to format.
+ * @param {BasePromptTemplate} prompt - The prompt template to use for formatting.
+ * @returns {Promise<string>} A Promise that resolves to the formatted document as a string.
+ * @throws {Error} If the document is missing required metadata variables specified in the prompt template.
+ */
 export const formatDocument = async (
   document: Document,
   prompt: BasePromptTemplate
