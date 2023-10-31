4d5514c813063e05874849ddd3ad993da12a25c2
Brace Sproul <braceasproul@gmail.com>
2023-10-31 16:56:38 -0700

    add back deleted tests

--- langchain/src/llms/tests/bedrock.int.test.ts
+++ langchain/src/llms/tests/bedrock.int.test.ts
@@ -47,6 +47,31 @@ test("Test Bedrock LLM: Claude-v2", async () => {
   console.log(res);
 });
 
+test("Test Bedrock LLM streaming: AI21", async () => {
+  const region = process.env.BEDROCK_AWS_REGION!;
+  const model = "ai21.j2-grande-instruct";
+  const prompt = "Human: What is your name?";
+
+  const bedrock = new Bedrock({
+    maxTokens: 20,
+    region,
+    model,
+    maxRetries: 0,
+    credentials: {
+      accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
+    },
+  });
+
+  const stream = await bedrock.stream(prompt);
+  const chunks = [];
+  for await (const chunk of stream) {
+    console.log(chunk);
+    chunks.push(chunk);
+  }
+  expect(chunks.length).toEqual(1);
+});
+
 test("Test Bedrock LLM handleLLMNewToken: Claude-v2", async () => {
   const region = process.env.BEDROCK_AWS_REGION!;
   const model = "anthropic.claude-v2";
