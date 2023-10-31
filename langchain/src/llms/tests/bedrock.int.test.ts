3a8faf54dd30edf3e019fe3935f27d316b9d27ef
Brace Sproul <braceasproul@gmail.com>
2023-10-31 16:58:20 -0700

    remove dup test

--- langchain/src/llms/tests/bedrock.int.test.ts
+++ langchain/src/llms/tests/bedrock.int.test.ts
@@ -102,27 +102,6 @@ test("Test Bedrock LLM handleLLMNewToken: Claude-v2", async () => {
   expect(stream).toEqual(tokens.join(""));
 });
 
-test("Test Bedrock LLM: Claude-v2", async () => {
-  const region = process.env.BEDROCK_AWS_REGION!;
-  const model = "anthropic.claude-v2";
-  const prompt = "Human: What is your name?\n\nAssistant:";
-
-  const bedrock = new Bedrock({
-    maxTokens: 20,
-    region,
-    model,
-    maxRetries: 0,
-    credentials: {
-      accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
-    },
-  });
-
-  const res = await bedrock.call(prompt);
-  expect(typeof res).toBe("string");
-  console.log(res);
-});
-
 test("Test Bedrock LLM streaming: Claude-v2", async () => {
   const region = process.env.BEDROCK_AWS_REGION!;
   const model = "anthropic.claude-v2";
