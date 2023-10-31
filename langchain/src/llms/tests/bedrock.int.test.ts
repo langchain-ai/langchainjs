c5c3c1dc7a2cac02d8a7c60022ec7e07c2417e7e
Brace Sproul <braceasproul@gmail.com>
2023-10-31 16:40:22 -0700

    normal llm streaming

--- langchain/src/llms/tests/bedrock.int.test.ts
+++ langchain/src/llms/tests/bedrock.int.test.ts
@@ -47,10 +47,40 @@ test("Test Bedrock LLM: Claude-v2", async () => {
   console.log(res);
 });
 
-test("Test Bedrock LLM streaming: AI21", async () => {
+test("Test Bedrock LLM handleLLMNewToken: Claude-v2", async () => {
   const region = process.env.BEDROCK_AWS_REGION!;
-  const model = "ai21.j2-grande-instruct";
-  const prompt = "Human: What is your name?";
+  const model = "anthropic.claude-v2";
+  const prompt = "Human: What is your name?\n\nAssistant:";
+  const tokens: string[] = [];
+
+  const bedrock = new Bedrock({
+    maxTokens: 20,
+    region,
+    model,
+    maxRetries: 0,
+    credentials: {
+      accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
+    },
+    streaming: true,
+    callbacks: [
+      {
+        handleLLMNewToken(token) {
+          tokens.push(token);
+        },
+      },
+    ],
+  });
+
+  const stream = await bedrock.call(prompt);
+  expect(tokens.length).toBeGreaterThan(1);
+  expect(stream).toEqual(tokens.join(""));
+});
+
+test("Test Bedrock LLM: Claude-v2", async () => {
+  const region = process.env.BEDROCK_AWS_REGION!;
+  const model = "anthropic.claude-v2";
+  const prompt = "Human: What is your name?\n\nAssistant:";
 
   const bedrock = new Bedrock({
     maxTokens: 20,
@@ -63,13 +93,9 @@ test("Test Bedrock LLM streaming: AI21", async () => {
     },
   });
 
-  const stream = await bedrock.stream(prompt);
-  const chunks = [];
-  for await (const chunk of stream) {
-    console.log(chunk);
-    chunks.push(chunk);
-  }
-  expect(chunks.length).toEqual(1);
+  const res = await bedrock.call(prompt);
+  expect(typeof res).toBe("string");
+  console.log(res);
 });
 
 test("Test Bedrock LLM streaming: Claude-v2", async () => {
