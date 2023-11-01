8a479be4a2a48a3568ffef47a1426067e143e719
Brace Sproul <braceasproul@gmail.com>
2023-11-01 16:12:34 -0700

    Fixed crypto could not be resolved warning on build

--- langchain/src/chat_models/iflytek_xinghuo/index.ts
+++ langchain/src/chat_models/iflytek_xinghuo/index.ts
@@ -4,6 +4,7 @@ import {
   BaseWebSocketStream,
   WebSocketStreamOptions,
 } from "../../util/iflytek_websocket_stream.js";
+import { getEnv } from "../../util/env.js";
 
 class WebSocketStream extends BaseWebSocketStream {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -17,10 +18,11 @@ export class ChatIflytekXinghuo extends BaseChatIflytekXinghuo {
   async openWebSocketStream<WebSocketStream>(
     options: WebSocketStreamOptions
   ): Promise<WebSocketStream> {
+    const { createHmac } = await ChatIflytekXinghuo.imports();
+
     const host = "spark-api.xf-yun.com";
     const date = new Date().toUTCString();
     const url = `GET /${this.version}/chat HTTP/1.1`;
-    const { createHmac } = await import("crypto");
     const hash = createHmac("sha256", this.iflytekApiSecret)
       .update(`host: ${host}\ndate: ${date}\n${url}`)
       .digest("base64");
@@ -31,5 +33,26 @@ export class ChatIflytekXinghuo extends BaseChatIflytekXinghuo {
     authWebSocketUrl += `&host=${encodeURIComponent(host)}`;
     authWebSocketUrl += `&date=${encodeURIComponent(date)}`;
     return new WebSocketStream(authWebSocketUrl, options) as WebSocketStream;
+  }
+
+  /**
+   * Static method that imports the `createHmac` function from the
+   * `crypto` module in Node.js. It is used to dynamically import the
+   * function when needed. If the import fails, it throws an error
+   * indicating that the `crypto` module is not available in the
+   * current environment.
+   * @returns Promise that resolves with an object containing the `createHmac` function.
+   */
+  static async imports() {
+    try {
+      const { createHmac } = await import("node:crypto");
+      return { createHmac };
+    } catch (e) {
+      console.error(e);
+
+      throw new Error(
+        `Failed to load crypto. 'createHmac' available only on environment 'node'. It appears you are running environment '${getEnv()}'. See https://<link to docs> for alternatives.`
+      );
+    }
   }
 }
--- langchain/src/chat_models/iflytek_xinghuo/web.ts
+++ langchain/src/chat_models/iflytek_xinghuo/web.ts
@@ -3,6 +3,7 @@ import {
   WebSocketStreamOptions,
   BaseWebSocketStream,
 } from "../../util/iflytek_websocket_stream.js";
+import { getEnv } from "../../util/env.js";
 
 class WebSocketStream extends BaseWebSocketStream<string> {
   openWebSocket(url: string, options: WebSocketStreamOptions): WebSocket {
@@ -14,6 +15,8 @@ export class ChatIflytekXinghuo extends BaseChatIflytekXinghuo {
   async openWebSocketStream<WebSocketStream>(
     options: WebSocketStreamOptions
   ): Promise<WebSocketStream> {
+    const { subtle } = await ChatIflytekXinghuo.imports();
+
     const host = "spark-api.xf-yun.com";
     const date = new Date().toUTCString();
     const url = `GET /${this.version}/chat HTTP/1.1`;
@@ -21,14 +24,14 @@ export class ChatIflytekXinghuo extends BaseChatIflytekXinghuo {
     const dataBuffer = new TextEncoder().encode(
       `host: ${host}\ndate: ${date}\n${url}`
     );
-    const cryptoKey = await crypto.subtle.importKey(
+    const cryptoKey = await subtle.importKey(
       "raw",
       keyBuffer,
       { name: "HMAC", hash: "SHA-256" },
       false,
       ["sign"]
     );
-    const signature = await crypto.subtle.sign("HMAC", cryptoKey, dataBuffer);
+    const signature = await subtle.sign("HMAC", cryptoKey, dataBuffer);
     const hash = window.btoa(String.fromCharCode(...new Uint8Array(signature)));
     const authorization_origin = `api_key="${this.iflytekApiKey}", algorithm="hmac-sha256", headers="host date request-line", signature="${hash}"`;
     const authorization = window.btoa(authorization_origin);
@@ -37,5 +40,26 @@ export class ChatIflytekXinghuo extends BaseChatIflytekXinghuo {
     authWebSocketUrl += `&host=${encodeURIComponent(host)}`;
     authWebSocketUrl += `&date=${encodeURIComponent(date)}`;
     return new WebSocketStream(authWebSocketUrl, options) as WebSocketStream;
+  }
+
+  /**
+   * Static method that imports the `subtle` function from the
+   * `crypto` module in Node.js. It is used to dynamically import the
+   * function when needed. If the import fails, it throws an error
+   * indicating that the `crypto` module is not available in the
+   * current environment.
+   * @returns Promise that resolves with an object containing the `subtle` function.
+   */
+  static async imports() {
+    try {
+      const { subtle } = await import("node:crypto");
+      return { subtle };
+    } catch (e) {
+      console.error(e);
+
+      throw new Error(
+        `Failed to load crypto. 'subtle' available only on environment 'node'. It appears you are running environment '${getEnv()}'. See https://<link to docs> for alternatives.`
+      );
+    }
   }
 }
