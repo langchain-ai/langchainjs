4ac9cfe75a3e0c79bbc7eddaf4a04d8431c807d8
Brace Sproul <braceasproul@gmail.com>
2023-11-10 12:50:46 -0800

    nits

--- api-docs/generate_typedoc.sh
+++ api-docs/generate_typedoc.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-# copy ../langchain/src into ../langchain/docs_dist/src
-
+# Create a directory to use for docs.
+# `docs_dist` is gitignored by default.
 mkdir -p ../langchain/docs_dist/
 cp -r ../langchain/src ../langchain/docs_dist/src
-echo "Copied ../langchain/src into ../langchain/docs_dist/src"
 
-# Check if NODE_ENV is set to production or staging
-if [[ "$NODE_ENV" == "production" || "$NODE_ENV" == "staging" ]]; then
-    echo "NODE_ENV is set to $NODE_ENV"
-    # Run the command if NODE_ENV is production or staging
-    yarn add-ignore-comments
-fi
+# Add `@ignore` to JSDoc comments for properties which should be ignored.
+yarn add-ignore-comments
 
 # Run this command regardless of the NODE_ENV value
+# This command will generate the docs
 yarn typedoc
 
-# delete ../langchain/docs_dist
-
-echo "Deleting ../langchain/docs_dist"
 rm -rf ../langchain/docs_dist
