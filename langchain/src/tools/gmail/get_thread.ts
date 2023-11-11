8cfc9b7da10448fce665e7db4c514506fa5fa178
saeedahsan <ahsan02@gmail.com>
2023-11-10 23:16:38 -0500

    Fix _call error

--- langchain/src/tools/gmail/get_thread.ts
+++ langchain/src/tools/gmail/get_thread.ts
@@ -38,65 +38,67 @@ export class GmailGetThread extends GmailBaseTool {
       throw new Error("No messages returned from Gmail");
     }
 
-    return messages.map((message) => {
-      const { payload } = message;
+    return `Result for the prompt ${threadId} \n${JSON.stringify(
+      messages.map((message) => {
+        const { payload } = message;
 
-      if (!payload) {
-        throw new Error("No payload returned from Gmail");
-      }
+        if (!payload) {
+          throw new Error("No payload returned from Gmail");
+        }
 
-      const { headers } = payload;
+        const { headers } = payload;
 
-      if (!headers) {
-        throw new Error("No headers returned from Gmail");
-      }
+        if (!headers) {
+          throw new Error("No headers returned from Gmail");
+        }
 
-      const subject = headers.find((header) => header.name === "Subject");
+        const subject = headers.find((header) => header.name === "Subject");
 
-      if (!subject) {
-        throw new Error("No subject returned from Gmail");
-      }
+        if (!subject) {
+          throw new Error("No subject returned from Gmail");
+        }
 
-      const body = headers.find((header) => header.name === "Body");
+        const body = headers.find((header) => header.name === "Body");
 
-      if (!body) {
-        throw new Error("No body returned from Gmail");
-      }
+        if (!body) {
+          throw new Error("No body returned from Gmail");
+        }
 
-      const from = headers.find((header) => header.name === "From");
+        const from = headers.find((header) => header.name === "From");
 
-      if (!from) {
-        throw new Error("No from returned from Gmail");
-      }
+        if (!from) {
+          throw new Error("No from returned from Gmail");
+        }
 
-      const to = headers.find((header) => header.name === "To");
+        const to = headers.find((header) => header.name === "To");
 
-      if (!to) {
-        throw new Error("No to returned from Gmail");
-      }
+        if (!to) {
+          throw new Error("No to returned from Gmail");
+        }
 
-      const date = headers.find((header) => header.name === "Date");
+        const date = headers.find((header) => header.name === "Date");
 
-      if (!date) {
-        throw new Error("No date returned from Gmail");
-      }
+        if (!date) {
+          throw new Error("No date returned from Gmail");
+        }
 
-      const messageIdHeader = headers.find(
-        (header) => header.name === "Message-ID"
-      );
+        const messageIdHeader = headers.find(
+          (header) => header.name === "Message-ID"
+        );
 
-      if (!messageIdHeader) {
-        throw new Error("No message id returned from Gmail");
-      }
+        if (!messageIdHeader) {
+          throw new Error("No message id returned from Gmail");
+        }
 
-      return {
-        subject: subject.value,
-        body: body.value,
-        from: from.value,
-        to: to.value,
-        date: date.value,
-        messageId: messageIdHeader.value,
-      };
-    });
+        return {
+          subject: subject.value,
+          body: body.value,
+          from: from.value,
+          to: to.value,
+          date: date.value,
+          messageId: messageIdHeader.value,
+        };
+      })
+    )}`;
   }
 }
