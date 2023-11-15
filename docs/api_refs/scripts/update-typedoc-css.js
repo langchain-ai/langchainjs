3cce781ab55ee195489b2322b3e271ed6d12cc8c
Brace Sproul <braceasproul@gmail.com>
2023-11-15 10:02:46 -0800

    fix: update api refs responsiveness

--- docs/api_refs/scripts/update-typedoc-css.js
+++ docs/api_refs/scripts/update-typedoc-css.js
@@ -3,6 +3,14 @@ const { readFile, writeFile } = require("fs/promises");
 async function main() {
   const css = `\n.tsd-navigation {
     word-break: break-word;
+}
+
+.col-content {
+    min-width: fit-content;
+}
+
+.page-menu {
+    display: none;
 }\n`;
 
   let file = await readFile("./public/assets/style.css", "utf-8");
