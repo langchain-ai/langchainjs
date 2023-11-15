2d79bbf1f0cae9807867c54babef8ef6dc59f86c
Brace Sproul <braceasproul@gmail.com>
2023-11-14 16:10:57 -0800

    cr

--- langchain/scripts/build-for-api-ref-docs.js
+++ langchain/scripts/build-for-api-ref-docs.js
@@ -4,39 +4,61 @@ async function main() {
   const project = new Project({
     tsConfigFilePath: "./tsconfig.json",
   });
-
   const sourceFiles = project.getSourceFiles();
-
+  /**
+   * @type {Array<Record<string, any>>}
+   */
   let changes = [];
 
   sourceFiles.forEach((sourceFile) => {
     sourceFile.getClasses().forEach((cls) => {
+      // Check instance members
       cls.getInstanceMembers().forEach((member) => {
-        if (
-          member.getKind() === SyntaxKind.MethodDeclaration ||
-          member.getKind() === SyntaxKind.PropertyDeclaration
-        ) {
-          const name = member.getName();
-          if (name.startsWith("_") || name.startsWith("lc_")) {
-            const jsDocs = member.getJsDocs();
-            const hasIgnoreTag = jsDocs.some(jsDoc => jsDoc.getTags().some(tag => tag.getTagName() === 'ignore'));
-            if (!hasIgnoreTag) {
-              console.log('FOUND ONE!')
-              const jsDocText = `/** @ignore */\n  `;
-              const start = member.getStart();
-              changes.push({sourceFile, start, jsDocText});
-            }
-          }
-        }
+        checkMember(member);
+      });
+
+      // Check static members
+      cls.getStaticMembers().forEach((member) => {
+        checkMember(member);
       });
     });
   });
 
-  // Sort changes in reverse order by start position
-  changes.sort((a, b) => b.start - a.start);
+  const syntaxTypes = [
+    SyntaxKind.MethodDeclaration,
+    SyntaxKind.PropertyDeclaration,
+    SyntaxKind.GetAccessor,
+    SyntaxKind.SetAccessor,
+    SyntaxKind.MethodSignature,
+    SyntaxKind.PropertySignature,
+  ];
 
+  function checkMember(member) {
+    if (syntaxTypes.includes(member.getKind())) {
+      const name = member.getName();
+      if (name.startsWith("_") || name.startsWith("lc_")) {
+        const jsDocs = member.getJsDocs();
+        const hasIgnoreTag = jsDocs.some((jsDoc) =>
+          jsDoc.getTags().some((tag) => tag.getTagName() === "ignore")
+        );
+        if (!hasIgnoreTag) {
+          const jsDocText = `/** @ignore */\n  `;
+          const start = member.getStart();
+          changes.push({
+            sourceFile: member.getSourceFile(),
+            start,
+            jsDocText,
+          });
+        }
+      }
+    }
+  }
+
+  // Sort changes in reverse order by start position so updating
+  // the source file doesn't mess up the start position of the next change
+  changes.sort((a, b) => b.start - a.start);
   // Apply changes after iterating over the AST
-  changes.forEach(({sourceFile, start, jsDocText}) => {
+  changes.forEach(({ sourceFile, start, jsDocText }) => {
     sourceFile.insertText(start, jsDocText);
   });
 
