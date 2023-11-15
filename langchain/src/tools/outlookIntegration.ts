3388d62bd0c9bb867c00f17bfc7be96e1358c934
SimonLi1020 <xiangyili1020@gmail.com>
2023-11-15 08:48:02 -0500

    Update outlookIntegration.ts
    Add description, name, and _call()

--- langchain/src/tools/outlookIntegration.ts
+++ langchain/src/tools/outlookIntegration.ts
@@ -1,19 +1,47 @@
 import { Tool, type ToolParams } from "./base.js";
 import fetch from 'node-fetch';
 
+
 export interface Email {
   sender: string;
   subject: string;
   // Add other properties as needed
 }
 
+interface SendEmailParams {
+  to: string;
+  subject: string;
+  content: string;
+}
+
+interface ReadEmailParams {
+  // Define parameters for reading emails if needed
+}
+
+// ... other parameter types for different actions
+
+
+
+/**
+ * The OutlookIntegration class is a tool used to send and 
+ * read emails. It extends the base Tool class.
+ */
 export class OutlookIntegration extends Tool {
   accessToken: string; // Store the OAuth2 access token
+
+  static lc_name() {
+    return "Outlook Integration";
+  }
+  name = "outlook-integration";
+  description = `Useful for sending and reading emails. The input to this tool should be a valid email address.`;
 
   constructor(params: ToolParams, accessToken: string) {
     super(params);
     this.accessToken = accessToken; // Initialize with an OAuth2 access token
+    this.
   }
+
+  
 
   async readEmails(): Promise<Email[]> {
     try {
@@ -73,6 +101,22 @@ export class OutlookIntegration extends Tool {
       throw error;
     }
   }
+
+  // Implement the abstract _call method
+  async _call(action: string, params: SendEmailParams | ReadEmailParams): Promise<void> {
+    switch (action) {
+      case "sendEmail":
+        const { to, subject, content } = params as SendEmailParams;
+        await this.sendEmail(to, subject, content);
+        break;
+      case "readEmails":
+        await this.readEmails();
+        break;
+      default:
+        throw new Error(`Action ${action} is not supported`);
+    }
+  }
+  
 
   // You can add more methods for other features like managing contacts, calendar, etc.
 
