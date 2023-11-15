9c293afd371c840aa7179f596b8cc8ef18c8a62c
Brace Sproul <braceasproul@gmail.com>
2023-11-15 15:50:18 -0800

    Add better docstrings for runnables

--- langchain/src/schema/runnable/passthrough.ts
+++ langchain/src/schema/runnable/passthrough.ts
@@ -34,7 +34,30 @@ export class RunnableAssign<
 }
 
 /**
- * A runnable that passes through the input.
+ * A runnable to passthrough inputs unchanged or with additional keys.
+ *
+ * This runnable behaves almost like the identity function, except that it
+ * can be configured to add additional keys to the output, if the input is
+ * an object.
+ *
+ * The example below demonstrates how to use `RunnablePassthrough to
+ * passthrough the input from the `.invoke()`
+ * 
+ * @example
+ * ```typescript
+ * const chain = RunnableSequence.from([
+ *   {
+ *     question: new RunnablePassthrough(),
+ *     context: async () => loadContextFromStore(),
+ *   },
+ *   prompt,
+ *   llm,
+ *   outputParser,
+ * ]);
+ * const response = await chain.invoke(
+ *   "I can pass a single string instead of an object since I'm using `RunnablePassthrough`."
+ * );
+ * ```
  */
 export class RunnablePassthrough<RunInput> extends Runnable<
   RunInput,
@@ -59,6 +82,37 @@ export class RunnablePassthrough<RunInput> extends Runnable<
     );
   }
 
+  /**
+   * A runnable that assigns key-value pairs to the input.
+   *
+   * The example below shows how you could use it with an inline function.
+   * 
+   * @example
+   * ```typescript
+   * const chain = RunnableSequence.from([
+   *   RunnablePassthrough.assign({
+   *     query: async () => getQuery(),
+   *   }),
+   *   {
+   *     question: (input) => {
+   *       if (
+   *         input.query ===
+   *         "SELECT COUNT(EmployeeId) AS TotalEmployees FROM Employee"
+   *       ) {
+   *         return "How many employees are there?";
+   *       } else {
+   *         return "What is the average salary?";
+   *       }
+   *     },
+   *   },
+   *   prompt,
+   *   llm,
+   *   outputParser,
+   * ]);
+   *
+   * const response = await chain.invoke({});
+   * ```
+   */
   static assign(
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     mapping: Record<string, RunnableLike<Record<string, unknown>, any>>
