acf4148c3e392049cdd2b6a764af5b8b8fcab77a
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2023-11-25 00:06:43 +0800

    Reformat the code again

--- examples/src/document_loaders/ppt.ts
+++ examples/src/document_loaders/ppt.ts
@@ -1,7 +1,9 @@
 import { PPTXLoader } from "langchain/document_loaders/fs/pptx";
 
 export const run = async () => {
-  const loader = new PPTXLoader("src/document_loaders/example_data/theikuntest.pptx");
+  const loader = new PPTXLoader(
+    "src/document_loaders/example_data/theikuntest.pptx"
+  );
 
   const docs = await loader.load();
 
