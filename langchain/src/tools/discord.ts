5d9bfea47f5b5e1ffd4d96e75be3ce8de9f4d5dc
slairu <sarah22liu@gmail.com>
2023-11-25 17:57:11 -0500

    Added the discord channel search tool.

--- langchain/src/tools/discord.ts
+++ langchain/src/tools/discord.ts
@@ -68,3 +68,76 @@ export class DiscordGetMessagesTool extends Tool {
     return JSON.stringify(results);
   }
 }
+
+/*
+ * A tool for searching for messages within a discord channel using a bot.
+ * It extends the base Tool class and implements the _call method to
+ * perform the retrieve operation. Requires an bot token which can be set
+ * in the environment variables, and the discord channel ID of the channel.
+ * The _call method takes the search term as the input argument.
+ * The bot must have read permissions to the given channel. It returns the
+ * message content, author, and time the message was created for each message.
+ */
+
+export class DiscordChannelSearchTool extends Tool {
+  static lc_name() {
+    return "DiscordChannelSearchTool";
+  }
+
+  name = "discord_channel_search_tool";
+
+  description = `a discord toolkit. useful for searching for messages 
+  within a discord channel. input should be the search term. the bot 
+  should have read permissions for the channel`;
+
+  protected botToken: string;
+  protected channelId: string;
+
+  client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+  });
+
+  constructor(
+    channelId: string,
+    botToken: string | undefined = getEnvironmentVariable("DiscordBotToken")
+  ) {
+    super(...arguments);
+
+    if (!botToken) {
+      throw new Error(
+        "Discord API key not set. You can set it as DiscordBotToken in your .env file."
+      );
+    }
+    this.botToken = botToken;
+    if (!channelId) {
+      throw new Error(
+        "Discord channel not set."
+      )
+    }
+    this.channelId = channelId;
+  }
+
+  /** @ignore */
+  async _call(searchTerm: string): Promise<string> {
+    await this.client.login(this.botToken);
+
+    const channel = (await this.client.channels.fetch(this.channelId)) as TextChannel;
+
+    if (!channel) {
+      return "Channel not found";
+    }
+
+    const messages = await channel.messages.fetch();
+    this.client.destroy();
+    const filtered = messages.filter((message) => message.content.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    const results =
+      filtered.map((message) => ({
+        author: message.author.tag,
+        content: message.content,
+        timestamp: message.createdAt,
+      })) ?? [];
+
+    return JSON.stringify(results);
+  }
+}
--- langchain/src/tools/tests/discord.test.ts
+++ langchain/src/tools/tests/discord.test.ts
@@ -1,10 +1,20 @@
 import { test } from "@jest/globals";
-import { DiscordGetMessagesTool } from "../discord.js";
+import { DiscordGetMessagesTool, DiscordChannelSearchTool } from "../discord.js";
 
 test("DiscordGetMessagesTool", async () => {
   const tool = new DiscordGetMessagesTool();
   try {
     const result = await tool.call('1153400523718938780')
+    console.log(result)
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+test("DiscordChannelSearchTool", async () => {
+  const tool = new DiscordChannelSearchTool('1153400523718938780');
+  try {
+    const result = await tool.call('Test')
     console.log(result)
   } catch (error) {
     console.error(error);
