7600b945c5e14c0b040b0319d8e489f5bf4e9d58
saeedahsan <ahsan02@gmail.com>
2023-11-26 22:02:22 -0500

    Add comment for default parameters, fix formatting

--- examples/src/tools/gmail.ts
+++ examples/src/tools/gmail.ts
@@ -3,43 +3,42 @@ import { OpenAI } from "langchain/llms/openai";
 import { GmailCreateDraft, GmailViewDraft } from "langchain/tools/gmail";
 
 export async function run() {
-    const model = new OpenAI({
-        temperature: 0,
-        openAIApiKey: process.env.OPENAI_API_KEY,
-    });
-    
-    const gmailParams = {
-        credentials: {
-        clientEmail: process.env.GMAIL_CLIENT_EMAIL,
-        privateKey: process.env.GMAIL_PRIVATE_KEY,
-        },
-        scopes: ["https://mail.google.com/"],
-        model,
-    };
-    
-    const tools = [
-        new GmailCreateDraft(gmailParams),
-        new GmailViewDraft(gmailParams),
-    ];
-    
-    const gmailAgent = await initializeAgentExecutorWithOptions(tools, model, {
-        agentType: "zero-shot-react-description",
-        verbose: true,
-    });
-    
-    const createInput = `Create a gmail draft for me to edit of a letter from the perspective of a sentient parrot who is looking to collaborate on some research with her estranged friend, a cat. Under no circumstances may you send the message, however.`;
-    
-    const createResult = await gmailAgent.call({ input: createInput });
-    //   Create Result {
-    //     output: 'I have created a draft email for you to edit. The draft Id is r5681294731961864018.'
-    //   }
-    console.log("Create Result", createResult);
-    
-    const viewInput = `Could you search in my drafts for the latest email?`;
-    
-    const viewResult = await gmailAgent.call({ input: viewInput });
-    //   View Result {
-    //     output: "The latest email in your drafts is from hopefulparrot@gmail.com with the subject 'Collaboration Opportunity'. The body of the email reads: 'Dear [Friend], I hope this letter finds you well. I am writing to you in the hopes of rekindling our friendship and to discuss the possibility of collaborating on some research together. I know that we have had our differences in the past, but I believe that we can put them aside and work together for the greater good. I look forward to hearing from you. Sincerely, [Parrot]'"
-    //   }
-    console.log("View Result", viewResult);
+  const model = new OpenAI({
+    temperature: 0,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  // These are the default parameters for the Gmail tools
+  //   const gmailParams = {
+  //     credentials: {
+  //       clientEmail: process.env.GMAIL_CLIENT_EMAIL,
+  //       privateKey: process.env.GMAIL_PRIVATE_KEY,
+  //     },
+  //     scopes: ["https://mail.google.com/"],
+  //     model,
+  //   };
+
+  // For custom parameters, uncomment the code above, replace the values with your own, and pass it to the tools below
+  const tools = [new GmailCreateDraft(), new GmailViewDraft()];
+
+  const gmailAgent = await initializeAgentExecutorWithOptions(tools, model, {
+    agentType: "zero-shot-react-description",
+    verbose: true,
+  });
+
+  const createInput = `Create a gmail draft for me to edit of a letter from the perspective of a sentient parrot who is looking to collaborate on some research with her estranged friend, a cat. Under no circumstances may you send the message, however.`;
+
+  const createResult = await gmailAgent.call({ input: createInput });
+  //   Create Result {
+  //     output: 'I have created a draft email for you to edit. The draft Id is r5681294731961864018.'
+  //   }
+  console.log("Create Result", createResult);
+
+  const viewInput = `Could you search in my drafts for the latest email?`;
+
+  const viewResult = await gmailAgent.call({ input: viewInput });
+  //   View Result {
+  //     output: "The latest email in your drafts is from hopefulparrot@gmail.com with the subject 'Collaboration Opportunity'. The body of the email reads: 'Dear [Friend], I hope this letter finds you well. I am writing to you in the hopes of rekindling our friendship and to discuss the possibility of collaborating on some research together. I know that we have had our differences in the past, but I believe that we can put them aside and work together for the greater good. I look forward to hearing from you. Sincerely, [Parrot]'"
+  //   }
+  console.log("View Result", viewResult);
 }
