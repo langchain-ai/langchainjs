7c3859e5d8f301f87e6a17dc24a48311e9dcd789
your_username <arielleramgoolie27@gmail.com>
2023-11-27 17:05:04 -0500

    Added send messages tool

--- langchain/src/tools/tests/discord.test.ts
+++ langchain/src/tools/tests/discord.test.ts
@@ -1,5 +1,5 @@
 import { test } from "@jest/globals";
-import { DiscordGetMessagesTool } from "../discord.js";
+import { DiscordGetMessagesTool, DiscordSendMessagesTool } from "../discord.js";
 
 test("DiscordGetMessagesTool", async () => {
   const tool = new DiscordGetMessagesTool();
@@ -8,5 +8,15 @@ test("DiscordGetMessagesTool", async () => {
     console.log(result)
   } catch (error) {
     console.error(error);
+  }
+});
+
+test("DiscordSendMessagesTool", async () => {
+  const tool = new DiscordSendMessagesTool("1153400523718938780");
+  try{  
+    const result = await tool.call("test message from new code");
+    console.log(result)
+  } catch(err){
+    console.log(err)
   }
 });
