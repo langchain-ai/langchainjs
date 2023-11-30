1f9e084833e972f8049dc580ffe01dc590a7fe63
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-29 21:55:50 -0500

    add example

---
+++ examples/src/llms/gradient_ai.ts
@@ -0,0 +1,17 @@
+import { GradientAI } from "langchain/llms/gradient_ai";
+
+export const run = async () => {
+  // Note that modelParameters are optional
+  const model = new GradientAI({
+    modelSlug: "llama2-7b-chat",
+    inferenceParameters: {
+        maxGeneratedTokenCount: 20,
+        temperature: 0,
+    },
+  });
+  const res = await model.predict(
+    "What would be a good company name for a company that makes colorful socks?"
+  );
+
+  console.log({ res });
+};
