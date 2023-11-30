bab4caecae4e321a82b8698431dd77f6822d5a02
maanethdesilva <maaneth.2002@gmail.com>
2023-11-30 17:00:28 -0500

    Passed yarn lint and yarn format

--- examples/src/tools/discord.ts
+++ examples/src/tools/discord.ts
@@ -7,27 +7,27 @@ import {
 } from "langchain/tools/discord";
 
 export async function run() {
-  //Get messages from a channel given channel ID
+  // Get messages from a channel given channel ID
   const getMessageTool = new DiscordGetMessagesTool();
   const messageResults = await getMessageTool.call("1153400523718938780");
   console.log(messageResults);
 
-  //Get guilds/servers
+  // Get guilds/servers
   const getGuildsTool = new DiscordGetGuildsTool();
   const guildResults = await getGuildsTool.call();
   console.log(guildResults);
 
-  //Search results in a given channel (case-insensitive)
+  // Search results in a given channel (case-insensitive)
   const searchTool = new DiscordChannelSearchTool("1153400523718938780");
   const searchResults = await searchTool.call("Test");
   console.log(searchResults);
 
-  //Get all text channels of a server
+  // Get all text channels of a server
   const getChannelsTool = new DiscordGetTextChannelsTool();
   const channelResults = await getChannelsTool.call("1153400523718938775");
   console.log(channelResults);
 
-  //Send a message 
+  // Send a message
   const sendMessageTool = new DiscordSendMessagesTool("1153400523718938780");
   const sendMessageResults = await sendMessageTool.call("test message");
   console.log(sendMessageResults);
