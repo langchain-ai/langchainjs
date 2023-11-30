e72b26d286afdca2eff9536764221409a631ca47
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-29 20:34:49 -0500

    yarn build

--- langchain/src/load/import_type.d.ts
+++ langchain/src/load/import_type.d.ts
@@ -97,6 +97,9 @@ export interface OptionalImportMap {
   "langchain/llms/sagemaker_endpoint"?:
     | typeof import("../llms/sagemaker_endpoint.js")
     | Promise<typeof import("../llms/sagemaker_endpoint.js")>;
+  "langchain/llms/gradient_ai"?:
+    | typeof import("../llms/gradient_ai.js")
+    | Promise<typeof import("../llms/gradient_ai.js")>;
   "langchain/llms/bedrock"?:
     | typeof import("../llms/bedrock/index.js")
     | Promise<typeof import("../llms/bedrock/index.js")>;
@@ -519,6 +522,8 @@ export interface SecretMap {
   GOOGLE_API_KEY?: string;
   GOOGLE_PALM_API_KEY?: string;
   GOOGLE_VERTEX_AI_WEB_CREDENTIALS?: string;
+  GRADIENT_ACCESS_TOKEN?: string;
+  GRADIENT_WORKSPACE_ID?: string;
   HUGGINGFACEHUB_API_KEY?: string;
   IFLYTEK_API_KEY?: string;
   IFLYTEK_API_SECRET?: string;
