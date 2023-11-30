1baeba86f96636a1f19a5fed33c1b60ee2420fdb
Jacob Lee <jacoblee93@gmail.com>
2023-11-30 13:56:26 -0800

    Update watsonx_ai.ts

--- langchain/src/llms/watsonx_ai.ts
+++ langchain/src/llms/watsonx_ai.ts
@@ -53,7 +53,7 @@ const endpointConstructor = (region: string, version: string) =>
 
 export class WatsonxAI extends LLM<BaseLLMCallOptions> {
   static lc_name() {
-    return "watsonx_ai";
+    return "WatsonxAI";
   }
 
   get lc_secrets(): { [key: string]: string } | undefined {
