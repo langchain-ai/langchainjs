5c259c7baea4bf1e72c620dccddee05c80128000
Qi123123Li <94654984+Qi123123Li@users.noreply.github.com>
2023-11-29 21:23:45 -0500

    reformat the read email

--- langchain/src/tools/outlook/readMail.ts
+++ langchain/src/tools/outlook/readMail.ts
@@ -1,6 +1,12 @@
 import { OutlookBase, OutlookCredentials } from "./base.js";
 import { READ_MAIL_TOOL_DESCRIPTION } from "./descriptions.js";
 
+interface Email {
+  subject: string;
+  body: { content: string; };
+  sender: { emailAddress: { name: string; address: string; } };
+}
+
 export class OutlookReadMailTool extends OutlookBase {
   name = "outlook_read_mail";
 
@@ -14,7 +20,7 @@ export class OutlookReadMailTool extends OutlookBase {
     console.log("query: ", query);
     const accessToken = await this.getAuth();
     try {
-        const response = await fetch("https://graph.microsoft.com/v1.0/me/messages", {
+        const response = await fetch("https://graph.microsoft.com/v1.0/me/messages?$select=sender,subject,body", {
           method: "GET",
           headers: {
             Authorization: `Bearer ${accessToken}`,
@@ -25,12 +31,16 @@ export class OutlookReadMailTool extends OutlookBase {
           throw new Error(`Error: ${response.status}`);
         }
 
-        
-        const data = await response.json();
-        const emails = data.value.map((email: any) => { JSON.stringify(email); });
-        return emails;
+        const data = await response.json() as { value: Email[] };
+        const formattedEmails = data.value.map(email => {
+          const { subject, body, sender } = email;
+          const senderInfo = `${sender.emailAddress.name} ${sender.emailAddress.address}`;
+          return `subject: ${subject}\nsender: ${senderInfo}\nbody: ${body.content}\n`;
+        }).join("\n");
+  
+        return formattedEmails;
     } catch (error) {
-        return `Failed to send email: ${error}`;
+        return `Failed to read email: ${error}`;
     }
     
   }
