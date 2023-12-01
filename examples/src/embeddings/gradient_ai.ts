5b3b005316f1b9f2d9d2ec9c9f625e48c6050658
Chase McDougall <chasemcdougall@hotmail.com>
2023-11-30 21:43:29 -0500

    add example

---
+++ examples/src/embeddings/gradient_ai.ts
@@ -0,0 +1,9 @@
+import { GradientEmbeddings } from "langchain/embeddings/gradient_ai";
+
+export const run = async () => {
+  const model = new GradientEmbeddings();
+  const res = await model.embedQuery(
+    "What would be a good company name a company that makes colorful socks?"
+  );
+  console.log({ res });
+};
