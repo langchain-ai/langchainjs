795b0d8bb9faa94192b6fdcf64d9ba47abc28224
Chase McDougall <chasemcdougall@hotmail.com>
2023-12-01 10:12:59 -0500

    remove run form example

--- examples/src/embeddings/gradient_ai.ts
+++ examples/src/embeddings/gradient_ai.ts
@@ -1,9 +1,7 @@
 import { GradientEmbeddings } from "langchain/embeddings/gradient_ai";
 
-export const run = async () => {
-  const model = new GradientEmbeddings();
-  const res = await model.embedQuery(
-    "What would be a good company name a company that makes colorful socks?"
-  );
-  console.log({ res });
-};
+const model = new GradientEmbeddings();
+const res = await model.embedQuery(
+  "What would be a good company name a company that makes colorful socks?"
+);
+console.log({ res });
