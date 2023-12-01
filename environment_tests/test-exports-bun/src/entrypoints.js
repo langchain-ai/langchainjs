d38f9f6a54dbdb2cb384a634cf34407a5a5846c8
Qi123123Li <94654984+Qi123123Li@users.noreply.github.com>
2023-11-30 22:15:24 -0500

    use typedoc

--- environment_tests/test-exports-bun/src/entrypoints.js
+++ environment_tests/test-exports-bun/src/entrypoints.js
@@ -13,6 +13,7 @@ export * from "langchain/agents/openai/output_parser";
 export * from "langchain/base_language";
 export * from "langchain/tools";
 export * from "langchain/tools/render";
+export * from "langchain/tools/outlook";
 export * from "langchain/tools/google_places";
 export * from "langchain/chains";
 export * from "langchain/chains/combine_documents/reduce";
