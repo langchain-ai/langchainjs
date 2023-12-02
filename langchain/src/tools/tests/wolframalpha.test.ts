cc015dcff0297efaa2bf007c3461ca9887dbfc28
AMellouki <abderrahim.mellouki.p@gmail.com>
2023-12-02 21:42:38 +0100

    fix: query parameters are not passed correctly to WolframAlpha API

---
+++ langchain/src/tools/tests/wolframalpha.test.ts
@@ -0,0 +1,46 @@
+import {afterEach, beforeEach, describe, expect} from "@jest/globals";
+import {WolframAlphaTool} from "../wolframalpha.js";
+
+const MOCK_APP_ID = '[MOCK_APP_ID]';
+const QUERY_1 = 'What is 2 + 2?';
+const MOCK_ANSWER = '[MOCK_ANSWER]';
+
+describe("wolfram alpha test suite", () => {
+
+  let fetchMock: jest.SpyInstance;
+
+  beforeEach(() => {
+    fetchMock = jest.spyOn(global, "fetch").mockImplementation(() => {
+      return Promise.resolve({
+        text: () => Promise.resolve(MOCK_ANSWER),
+      } as Response);
+    });
+  });
+
+  afterEach(() => {
+    fetchMock.mockRestore();
+  });
+
+  test("test query parameters passed correctly", async () => {
+    const wolframAlpha = new WolframAlphaTool({
+      appid: MOCK_APP_ID,
+    });
+    await wolframAlpha._call(QUERY_1);
+    const [url] = fetchMock.mock.calls[0];
+    const parsedUrl = new URL(url);
+    const params = new URLSearchParams(parsedUrl.search);
+
+    expect(fetchMock).toBeCalledTimes(1);
+    expect(params.get("appid")).toBe(MOCK_APP_ID);
+    expect(params.get("input")).toBe(QUERY_1);
+  });
+
+  test("test answer retrieved", async () => {
+    const wolframAlpha = new WolframAlphaTool({
+      appid: MOCK_APP_ID,
+    });
+
+    const answer = await wolframAlpha._call(QUERY_1);
+    expect(answer).toBe(MOCK_ANSWER);
+  });
+});
