d61dccc162485897969b65a419553b13d54dda10
Tsukasa OISHI <tsukasa.oishi@gmail.com>
2023-12-02 21:21:58 +0900

    Remove unused option

--- langchain-core/src/utils/tiktoken.ts
+++ langchain-core/src/utils/tiktoken.ts
@@ -13,16 +13,10 @@ const caller = /* #__PURE__ */ new AsyncCaller({});
 
 export async function getEncoding(
   encoding: TiktokenEncoding,
-  options?: {
-    signal?: AbortSignal;
-    extendedSpecialTokens?: Record<string, number>;
-  }
 ) {
   if (!(encoding in cache)) {
     cache[encoding] = caller
-      .fetch(`https://tiktoken.pages.dev/js/${encoding}.json`, {
-        signal: options?.signal,
-      })
+      .fetch(`https://tiktoken.pages.dev/js/${encoding}.json`)
       .then((res) => res.json())
       .catch((e) => {
         delete cache[encoding];
@@ -30,15 +24,11 @@ export async function getEncoding(
       });
   }
 
-  return new Tiktoken(await cache[encoding], options?.extendedSpecialTokens);
+  return new Tiktoken(await cache[encoding]);
 }
 
 export async function encodingForModel(
   model: TiktokenModel,
-  options?: {
-    signal?: AbortSignal;
-    extendedSpecialTokens?: Record<string, number>;
-  }
 ) {
-  return getEncoding(getEncodingNameForModel(model), options);
+  return getEncoding(getEncodingNameForModel(model));
 }
