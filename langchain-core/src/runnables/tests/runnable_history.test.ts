7b6db6b98d80e0d2914adaa8a7abe42ae180f053
Brace Sproul <braceasproul@gmail.com>
2023-12-03 15:34:44 -0800

    Added extra chat message class to types for history (#3510)
    * Added extra chat message class to types for history
    
    * chore: lint files

--- langchain-core/src/runnables/tests/runnable_history.test.ts
+++ langchain-core/src/runnables/tests/runnable_history.test.ts
@@ -4,9 +4,14 @@ import { RunnableConfig } from "../config.js";
 import { RunnableWithMessageHistory } from "../history.js";
 import {
   BaseChatMessageHistory,
-  FakeChatMessageHistory,
+  BaseListChatMessageHistory,
 } from "../../chat_history.js";
+import {
+  FakeChatMessageHistory,
+  FakeListChatMessageHistory,
+} from "../../utils/testing/index.js";
 
+// For `BaseChatMessageHistory`
 async function getGetSessionHistory(): Promise<
   (sessionId: string) => Promise<BaseChatMessageHistory>
 > {
@@ -17,6 +22,24 @@ async function getGetSessionHistory(): Promise<
   ): Promise<BaseChatMessageHistory> {
     if (!(sessionId in chatHistoryStore)) {
       chatHistoryStore[sessionId] = new FakeChatMessageHistory();
+    }
+    return chatHistoryStore[sessionId];
+  }
+
+  return getSessionHistory;
+}
+
+// Extends `BaseListChatMessageHistory`
+async function getListSessionHistory(): Promise<
+  (sessionId: string) => Promise<BaseListChatMessageHistory>
+> {
+  const chatHistoryStore: { [key: string]: BaseListChatMessageHistory } = {};
+
+  async function getSessionHistory(
+    sessionId: string
+  ): Promise<BaseListChatMessageHistory> {
+    if (!(sessionId in chatHistoryStore)) {
+      chatHistoryStore[sessionId] = new FakeListChatMessageHistory();
     }
     return chatHistoryStore[sessionId];
   }
@@ -38,6 +61,28 @@ test("Runnable with message history", async () => {
     runnable,
     config: {},
     getMessageHistory,
+  });
+  const config: RunnableConfig = { configurable: { sessionId: "1" } };
+  let output = await withHistory.invoke([new HumanMessage("hello")], config);
+  expect(output).toBe("you said: hello");
+  output = await withHistory.invoke([new HumanMessage("good bye")], config);
+  expect(output).toBe("you said: hello\ngood bye");
+});
+
+test("Runnable with message history work with chat list memory", async () => {
+  const runnable = new RunnableLambda({
+    func: (messages: BaseMessage[]) =>
+      `you said: ${messages
+        .filter((m) => m._getType() === "human")
+        .map((m) => m.content)
+        .join("\n")}`,
+  });
+
+  const getListMessageHistory = await getListSessionHistory();
+  const withHistory = new RunnableWithMessageHistory({
+    runnable,
+    config: {},
+    getMessageHistory: getListMessageHistory,
   });
   const config: RunnableConfig = { configurable: { sessionId: "1" } };
   let output = await withHistory.invoke([new HumanMessage("hello")], config);
