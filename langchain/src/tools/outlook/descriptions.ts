767acbccdfdcd66b3f35be842f5d912b4452201c
Qi123123Li <94654984+Qi123123Li@users.noreply.github.com>
2023-12-06 18:48:17 -0500

    Update langchain/src/tools/outlook/descriptions.ts
    Co-authored-by: Brace Sproul <braceasproul@gmail.com>

--- langchain/src/tools/outlook/descriptions.ts
+++ langchain/src/tools/outlook/descriptions.ts
@@ -1,14 +1,14 @@
 export const SEND_MAIL_TOOL_DESCRIPTION = `
-  A tool for sending emails.
-  input instructions:
-  input a JSON formatted email message with the following four keys:
-  'subject', 'content', 'to', and 'cc'.
-  The 'subject' should be a brief title for the email,
-  'content' should contain the body of the email,
-  'to' should be an array of the recipient's email address,
-  and 'cc' should be an array of any additional recipient's email address.
-  The 'cc' key is optional, just give an empty array if no cc.
-  Ensure that the JSON object is correctly formatted and includes all four specified keys.
+A tool for sending emails.
+input instructions:
+input a JSON formatted email message with the following four keys:
+'subject', 'content', 'to', and 'cc'.
+The 'subject' should be a brief title for the email,
+'content' should contain the body of the email,
+'to' should be an array of the recipient's email address,
+and 'cc' should be an array of any additional recipient's email address.
+The 'cc' key is optional, just give an empty array if no cc.
+Ensure that the JSON object is correctly formatted and includes all four specified keys.
 `;
 
 export const READ_MAIL_TOOL_DESCRIPTION = `A tool for reading emails.
