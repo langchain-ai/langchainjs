729fbeddb27ef3cb2d072a3b56cd0832c2a2a356
jacoblee93 <jacoblee93@gmail.com>
2023-12-08 14:36:19 -0800

    Fixes

--- environment_tests/scripts/docker-ci-entrypoint.sh
+++ environment_tests/scripts/docker-ci-entrypoint.sh
@@ -16,30 +16,16 @@ mkdir -p ./libs/langchain-anthropic/
 mkdir -p ./libs/langchain-community/
 mkdir -p ./libs/langchain/
 
-ls ./libs/
-
 cp -r ../langchain-core ./libs/
 cp -r ../langchain-openai ./libs/
 cp -r ../langchain-anthropic ./libs/
 cp -r ../langchain-community ./libs/
 cp -r ../langchain ./libs/
 
-ls ../langchain-anthropic
-ls ./libs/langchain-anthropic/
-
 # copy cache
 mkdir -p ./.yarn
 cp -r ../root/.yarn/!(berry|cache) ./.yarn
 cp ../root/yarn.lock ../root/.yarnrc.yml .
-
-# Replace the workspace dependency with the local copy, and install all others
-# Avoid calling "yarn add ../langchain" as yarn berry does seem to hang for ~30s
-# before installation actually occurs
-# sed -i 's/"@langchain\/core": "workspace:\*"/"@langchain\/core": "..\/langchain-core"/g' package.json
-# sed -i 's/"@langchain\/community": "workspace:\*"/"@langchain\/community": "..\/langchain-community"/g' package.json
-# sed -i 's/"@langchain\/anthropic": "workspace:\*"/"@langchain\/anthropic": "..\/langchain-anthropic"/g' package.json
-# sed -i 's/"@langchain\/openai": "workspace:\*"/"@langchain\/openai": "..\/langchain-openai"/g' package.json
-# sed -i 's/"langchain": "workspace:\*"/"langchain": "..\/langchain"/g' package.json
 
 yarn install --no-immutable
 
