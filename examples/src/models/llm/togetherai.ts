850028158b20bdd231da425f960c0a372efbf551
bracesproul <braceasproul@gmail.com>
2023-12-12 12:18:34 -0800

    added docs & created entrypoint

--- examples/src/models/llm/togetherai.ts
+++ examples/src/models/llm/togetherai.ts
@@ -2,14 +2,22 @@ import { TogetherAI } from "@langchain/community/llms/togetherai";
 import { PromptTemplate } from "langchain/prompts";
 
 const model = new TogetherAI({
+<<<<<<< HEAD
   modelName: "togethercomputer/StripedHyena-Nous-7B",
+=======
+  modelName: "togethercomputer/StripedHyena-Nous-7B"
+>>>>>>> 076acc1c (added docs & created entrypoint)
 });
 const prompt = PromptTemplate.fromTemplate(`System: You are a helpful assistant.
 User: {input}.
 Assistant:`);
 const chain = prompt.pipe(model);
 const response = await chain.invoke({
+<<<<<<< HEAD
   input: `Tell me a joke about bears`,
+=======
+  input: `Tell me a joke about bears`
+>>>>>>> 076acc1c (added docs & created entrypoint)
 });
 console.log("response", response);
 /**
