7b228ffb9dcb067aa611fdb4ab385405b98eddd9
jacoblee93 <jacoblee93@gmail.com>
2023-12-14 16:17:35 -0800

    Format

--- libs/langchain-community/src/vectorstores/elasticsearch.ts
+++ libs/langchain-community/src/vectorstores/elasticsearch.ts
@@ -137,7 +137,9 @@ export class ElasticVectorSearch extends VectorStore {
     ]);
     const results = await this.client.bulk({ refresh: true, operations });
     if (results.errors) {
-      const reasons = results.items.map((result) => result.index?.error?.reason);
+      const reasons = results.items.map(
+        (result) => result.index?.error?.reason
+      );
       throw new Error(`Failed to insert documents:\n${reasons.join("\n")}`);
     }
     return documentIds;
