0f8565ddc94657951abc875d9cd6379ca88699cc
Volodymyr Machula <machulav@gmail.com>
2023-12-18 00:01:53 +0100

    Update tool, toolkit, docs and examples

--- examples/src/agents/connery_mrkl.ts
+++ examples/src/agents/connery_mrkl.ts
@@ -1,23 +1,35 @@
-import { OpenAI } from "langchain/llms/openai";
+import { ConneryService } from "@langchain/community/tools/connery";
+import { ConneryToolkit } from "@langchain/community/agents/toolkits/connery";
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
-import { ConneryToolkit } from "langchain/agents/toolkits/connery";
-import { ConneryService } from "langchain/tools/connery";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 
+// Specify your Connery Runner credentials.
+process.env.CONNERY_RUNNER_URL = "";
+process.env.CONNERY_RUNNER_API_KEY = "";
+
+// Specify OpenAI API key.
+process.env.OPENAI_API_KEY = "";
+
+// Specify your email address to receive the emails from examples below.
+const recepientEmail = "test@example.com";
+
+// Create a Connery Toolkit with all the available actions from the Connery Runner.
 const conneryService = new ConneryService();
 const conneryToolkit = await ConneryToolkit.createInstance(conneryService);
 
-const llm = new OpenAI({ temperature: 0 });
+// Use OpenAI Functions agent to execute the prompt using actions from the Connery Toolkit.
+const llm = new ChatOpenAI({ temperature: 0 });
 const agent = await initializeAgentExecutorWithOptions(
   conneryToolkit.tools,
   llm,
   {
-    agentType: "zero-shot-react-description",
+    agentType: "openai-functions",
     verbose: true,
   }
 );
-
-const input =
-  "Make a short summary of the webpage http://www.paulgraham.com/vb.html in three sentences " +
-  "and send it to test@example.com. Include the link to the webpage into the body of the email.";
-const result = await agent.invoke({ input });
+const result = await agent.invoke({
+  input:
+    `Make a short summary of the webpage http://www.paulgraham.com/vb.html in three sentences ` +
+    `and send it to ${recepientEmail}. Include the link to the webpage into the body of the email.`,
+});
 console.log(result.output);
--- examples/src/tools/connery.ts
+++ examples/src/tools/connery.ts
@@ -1,12 +1,40 @@
-import { ConneryService } from "langchain/tools/connery";
+import { ConneryService } from "@langchain/community/tools/connery";
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 
+// Specify your Connery Runner credentials.
+process.env.CONNERY_RUNNER_URL = "";
+process.env.CONNERY_RUNNER_API_KEY = "";
+
+// Specify OpenAI API key.
+process.env.OPENAI_API_KEY = "";
+
+// Specify your email address to receive the emails from examples below.
+const recepientEmail = "test@example.com";
+
+// Get the SendEmail action from the Connery Runner by ID.
 const conneryService = new ConneryService();
 const sendEmailAction = await conneryService.getAction(
   "CABC80BB79C15067CA983495324AE709"
 );
 
-const result = await sendEmailAction.call(
-  "Send an email to test@example.com with the subject 'Test email' and the body 'This is a test email sent from Connery.'"
-);
+// Run the action manually.
+const manualRunResult = await sendEmailAction.call({
+  recipient: recepientEmail,
+  subject: "Test email",
+  body: "This is a test email sent by Connery.",
+});
+console.log(manualRunResult);
 
-console.log(result);
+// Run the action using the OpenAI Functions agent.
+const llm = new ChatOpenAI({ temperature: 0 });
+const agent = await initializeAgentExecutorWithOptions([sendEmailAction], llm, {
+  agentType: "openai-functions",
+  verbose: true,
+});
+const agentRunResult = await agent.invoke({
+  input:
+    `Send an email to ${recepientEmail} with the subject 'Test email' ` +
+    `and the body 'This is a test email sent by Connery using the OpenAI Functions agent.'`,
+});
+console.log(agentRunResult);
