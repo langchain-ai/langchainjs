0bc2b66b96e7cf8d9af6dd46dbc740df4cb00c0c
jacoblee93 <jacoblee93@gmail.com>
2023-12-21 15:37:52 -0800

    Format

--- langchain/src/index.ts
+++ langchain/src/index.ts
@@ -1,1 +1,3 @@
-console.warn(`[WARNING]: The root "langchain" entrypoint is empty. Please use a specific entrypoint instead.`)
+console.warn(
+  `[WARNING]: The root "langchain" entrypoint is empty. Please use a specific entrypoint instead.`
+);
