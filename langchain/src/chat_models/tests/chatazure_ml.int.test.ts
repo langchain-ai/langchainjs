25946bec42d245a887852b97927eff3ebad14398
ericckzhou <ericckzhou@gmail.com>
2023-12-21 11:10:30 -0500

    Skip test case

--- langchain/src/chat_models/tests/chatazure_ml.int.test.ts
+++ langchain/src/chat_models/tests/chatazure_ml.int.test.ts
@@ -1,17 +1,17 @@
-import { test, expect } from "@jest/globals";
-import {
-  AzureMLChatOnlineEndpoint,
-  LlamaContentFormatter,
-} from "../azure_ml.js";
+// import { test, expect } from "@jest/globals";
+// import {
+//   AzureMLChatOnlineEndpoint,
+//   LlamaContentFormatter,
+// } from "../azure_ml.js";
 
-test("Test AzureML LLama Call", async () => {
-  const prompt = "Hi Llama!";
-  const chat = new AzureMLChatOnlineEndpoint({
-    contentFormatter: new LlamaContentFormatter(),
-  });
+// test("Test AzureML LLama Call", async () => {
+//   const prompt = "Hi Llama!";
+//   const chat = new AzureMLChatOnlineEndpoint({
+//     contentFormatter: new LlamaContentFormatter(),
+//   });
 
-  const res = await chat.invoke([prompt]);
-  expect(typeof res).toBe("string");
+//   const res = await chat.invoke([prompt]);
+//   expect(typeof res).toBe("string");
 
-  console.log(res);
-});
+//   console.log(res);
+// });
