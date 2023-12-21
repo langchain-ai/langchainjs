42d7f5ffbf686680c2514f2533854b5a8ebaa8c8
jacoblee93 <jacoblee93@gmail.com>
2023-12-20 17:55:49 -0800

    Change port to integration test default

--- libs/langchain-community/src/vectorstores/tests/pgvector/pgvector.int.test.ts
+++ libs/langchain-community/src/vectorstores/tests/pgvector/pgvector.int.test.ts
@@ -12,7 +12,7 @@ describe("PGVectorStore", () => {
       postgresConnectionOptions: {
         type: "postgres",
         host: "127.0.0.1",
-        port: 5432,
+        port: 5433,
         user: "myuser",
         password: "ChangeMe",
         database: "api",
