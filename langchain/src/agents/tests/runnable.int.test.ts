7b37d43b4197293eaf04f5bb34cb445441f46380
jacoblee93 <jacoblee93@gmail.com>
2023-12-22 15:57:48 -0800

    Support stream log for agent executors

--- langchain/src/agents/tests/runnable.int.test.ts
+++ langchain/src/agents/tests/runnable.int.test.ts
@@ -102,3 +102,64 @@ test("Runnable variant works with executor", async () => {
 
   console.log(result);
 });
+
+test("Runnable variant executor astream log", async () => {
+  const tools = [new Calculator(), new SerpAPI()];
+  const model = new ChatOpenAI({
+    modelName: "gpt-4",
+    temperature: 0,
+    streaming: true,
+  });
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    ["ai", "You are a helpful assistant"],
+    ["human", "{input}"],
+    new MessagesPlaceholder("agent_scratchpad"),
+  ]);
+
+  const modelWithTools = model.bind({
+    functions: [...tools.map((tool) => formatToOpenAIFunction(tool))],
+  });
+
+  const formatAgentSteps = (steps: AgentStep[]): BaseMessage[] =>
+    steps.flatMap(({ action, observation }) => {
+      if ("messageLog" in action && action.messageLog !== undefined) {
+        const log = action.messageLog as BaseMessage[];
+        return log.concat(new FunctionMessage(observation, action.tool));
+      } else {
+        return [new AIMessage(action.log)];
+      }
+    });
+
+  const runnableAgent = RunnableSequence.from([
+    {
+      input: (i: { input: string; steps: AgentStep[] }) => i.input,
+      agent_scratchpad: (i: { input: string; steps: AgentStep[] }) =>
+        formatAgentSteps(i.steps),
+    },
+    prompt,
+    modelWithTools,
+    new OpenAIFunctionsAgentOutputParser(),
+  ]);
+
+  const executor = AgentExecutor.fromAgentAndTools({
+    agent: runnableAgent,
+    tools,
+  });
+
+  console.log("Loaded agent executor");
+
+  const query = "What is the weather in New York?";
+  console.log(`Calling agent executor with query: ${query}`);
+  const stream = await executor.streamLog({
+    input: query,
+  });
+  let hasSeenLLMLogPatch = false;
+  for await (const chunk of stream) {
+    console.log(JSON.stringify(chunk));
+    if (chunk.ops[0].path.includes("ChatOpenAI")) {
+      hasSeenLLMLogPatch = true;
+    }
+  }
+  expect(hasSeenLLMLogPatch).toBe(true);
+});
