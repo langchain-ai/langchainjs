7aba6888616406055f0f5a569f5714fdaaa27566
hughcrt <chocart.hugues@icloud.com>
2023-12-23 22:28:05 +0100

    Better examples

--- examples/src/callbacks/lunary_tags.ts
+++ examples/src/callbacks/lunary_tags.ts
@@ -1,7 +1,11 @@
 import { LunaryHandler } from "@langchain/community/callbacks/handlers/lunary";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
-const chat = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
+const chat = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo",
+  temperature: 0,
+  callbacks: [new LunaryHandler()],
+});
 
 await chat.invoke("Hello", {
   tags: ["greeting"],
--- examples/src/callbacks/lunary_users.ts
+++ examples/src/callbacks/lunary_users.ts
@@ -4,7 +4,11 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import { Calculator } from "langchain/tools/calculator";
 
 const tools = [new Calculator()];
-const chat = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
+const chat = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo",
+  temperature: 0,
+  callbacks: [new LunaryHandler()],
+});
 
 const executor = await initializeAgentExecutorWithOptions(tools, chat, {
   agentType: "openai-functions",
