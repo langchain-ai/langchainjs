7e1f8e1f0bc25feb456b1fa39e9bdf99999c3edb
bracesproul <braceasproul@gmail.com>
2023-12-26 18:28:04 -0500

    auto ignore generated files

---
+++ docs/core_docs/scripts/ignore_autogenerated_md.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+
+const PATH_TO_DOCS = "./docs/";
+
+function main() {
+  /**
+   * @type {string[]}
+   */
+  const pathToIPYNBFiles = [];
+  const getIPYNBFilesInDir = (dir) => {
+    const dirContents = fs.readdirSync(dir);
+    dirContents.forEach((item) => {
+      if (item.endsWith(".ipynb")) {
+        pathToIPYNBFiles.push(path.join(dir, item));
+      } else if (fs.statSync(path.join(dir, item)).isDirectory()) {
+        getIPYNBFilesInDir(path.join(dir, item));
+      }
+    });
+  };
+  getIPYNBFilesInDir(PATH_TO_DOCS);
+  // read the existing gitignore
+  const existingGitignore = fs.readFileSync("./.gitignore", "utf8");
+  // split at the # notebook comment
+  const [existing] = existingGitignore.split("# notebooks");
+  // rewrite the after
+  const newGitignore =
+    existing +
+    "\n# notebooks\n" +
+    pathToIPYNBFiles.map((file) => file.replace(".ipynb", ".md")).join("\n");
+  fs.writeFileSync("./.gitignore", newGitignore);
+}
+main();
