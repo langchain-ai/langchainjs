60dfd74d7427ac8c6ee5023c600f096fc051fff1
bracesproul <braceasproul@gmail.com>
2023-12-26 17:48:07 -0500

    chore: lint files

--- docs/core_docs/helpers/create_global_node_vars.js
+++ docs/core_docs/helpers/create_global_node_vars.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 function createGlobalNodeVars() {
   const process = {
     cwd: Deno.cwd,
     env: Deno.env.toObject(),
   };
-  (globalThis).process = process;
-  (globalThis).readFile = Deno.readFile;
+  globalThis.process = process;
+  globalThis.readFile = Deno.readFile;
 }
 
 createGlobalNodeVars();
