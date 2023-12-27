41514ef231f7cb50ac1feca4e3168a58792887a6
Mish Ushakov <mishushakov@users.noreply.github.com>
2023-12-27 12:12:24 +0100

    Implementing @bracesproul's comments

--- langchain/src/experimental/tools/graphql.ts
+++ langchain/src/experimental/tools/graphql.ts
@@ -1,6 +1,17 @@
 import { z } from "zod";
 import { StructuredTool } from "../../tools/base.js";
 
+/**
+ * Class that represents a GraphQL client in the LangChain framework.
+ *
+ * @security **Security Notice**
+ * This class sends HTTP requests to a given endpoint.
+ * The GraphQLClientTool class provides a _call method that can be used
+ * to make a GraphQL query or mutation by sending an HTTP request.
+ *
+ * @link See https://js.langchain.com/docs/security for more information.
+ */
+
 export class GraphQLClientTool extends StructuredTool {
   static lc_name() {
     return "GraphQLClientTool";
@@ -10,9 +21,9 @@ export class GraphQLClientTool extends StructuredTool {
 
   description = `You can make a GraphQL request with this tool`;
 
-  _endpoint: string;
+  private endpoint: string;
 
-  _headers: HeadersInit | undefined;
+  private headers: HeadersInit | undefined;
 
   schema = z.object({
     query: z.string(),
@@ -28,20 +39,20 @@ export class GraphQLClientTool extends StructuredTool {
   }) {
     super();
 
-    this._endpoint = endpoint;
-    this._headers = headers;
+    this.endpoint = endpoint;
+    this.headers = headers;
   }
 
   async _call({ query, variables }: z.infer<typeof this.schema>) {
-    const response = await fetch(this._endpoint, {
+    const response = await fetch(this.endpoint, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        ...this._headers,
+        ...this.headers,
       },
       body: JSON.stringify({ query, variables }),
     });
 
-    return await response.text();
+    return response.text();
   }
 }
