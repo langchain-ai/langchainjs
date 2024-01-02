dd35083812c1643c26c4a72567a9e9394768a4fe
jacoblee93 <jacoblee93@gmail.com>
2024-01-02 11:50:48 -0800

    Adds entrypoint deprecation log method

---
+++ langchain/src/util/entrypoint_deprecation.ts
@@ -0,0 +1,23 @@
+export function logVersion010MigrationWarning(
+  {
+    oldEntrypointName,
+    newEntrypointName,
+    newPackageName = "@langchain/community",
+  }: {
+    oldEntrypointName: string,
+    newEntrypointName?: string,
+    newPackageName?: string,
+  }) {
+  /*#__PURE__*/ console.warn(
+    [
+      `[WARNING]: Importing from "langchain/${oldEntrypointName}" is deprecated.\n`,
+      `Instead, please add the "${newPackageName}" package to your project with e.g.`,
+      ``,
+      `    $ npm install ${newPackageName}`,
+      ``,
+      `and import from "${newPackageName}${newEntrypointName === undefined ? `/${oldEntrypointName}` : ""}".`,
+      ``,
+      `This will be mandatory after the next "langchain" minor version bump to 0.2.`,
+    ].join("\n")
+  ); 
+}
