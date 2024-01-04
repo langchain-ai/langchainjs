9585b9dc1ad838a49a8c3eba54023a23754b8673
Chris <chris.zirkel@gmail.com>
2024-01-04 18:17:39 +0100

    core[patch]: Add generics to `RunnablePassthrough.assign` (#3844)
    * add generics to passtrough assign
    
    * use unknown instead of any
    
    * format
    
    * Adds test
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- langchain-core/src/runnables/tests/runnable_passthrough.test.ts
+++ langchain-core/src/runnables/tests/runnable_passthrough.test.ts
@@ -2,6 +2,7 @@ import { PromptTemplate } from "../../prompts/prompt.js";
 import { FakeChatModel } from "../../utils/testing/index.js";
 import { RunnablePassthrough } from "../passthrough.js";
 import { JsonOutputParser } from "../../output_parsers/json.js";
+import { RunnableSequence } from "../base.js";
 
 test("RunnablePassthrough can call .assign and pass prev result through", async () => {
   const promptTemplate = PromptTemplate.fromTemplate("{input}");
@@ -22,4 +23,26 @@ test("RunnablePassthrough can call .assign and pass prev result through", async 
   const result = await chainWithAssign.invoke({ input: text });
   console.log(result);
   expect(result).toEqual({ outputValue: "testing" });
+});
+
+test("RunnablePassthrough can call .assign as the first step with proper typing", async () => {
+  const promptTemplate = PromptTemplate.fromTemplate("{input}");
+  const llm = new FakeChatModel({});
+  const parser = new JsonOutputParser();
+  const text = `\`\`\`
+{"outputValue": "testing2"}
+\`\`\``;
+
+  const chain = RunnableSequence.from([
+    RunnablePassthrough.assign({
+      input: (input) => input.otherProp,
+    }),
+    promptTemplate,
+    llm,
+    parser,
+  ]);
+
+  const result = await chain.invoke({ otherProp: text });
+  console.log(result);
+  expect(result).toEqual({ outputValue: "testing2" });
 });
