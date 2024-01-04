532cdf7e38102e4ebe2a01181f131c04cca91f6c
Jacob Lee <jacoblee93@gmail.com>
2024-01-03 19:16:44 -0500

    community[patch]: Move all tests to community (#3893)
    * Move GCal to community
    
    * Move tests to community
    
    * Fix typo

--- libs/langchain-community/src/load/import_constants.ts
+++ libs/langchain-community/src/load/import_constants.ts
@@ -5,6 +5,7 @@ export const optionalImportEntrypoints = [
   "langchain_community/tools/aws_sfn",
   "langchain_community/tools/discord",
   "langchain_community/tools/gmail",
+  "langchain_community/tools/google_calendar",
   "langchain_community/agents/toolkits/aws_sfn",
   "langchain_community/embeddings/bedrock",
   "langchain_community/embeddings/cloudflare_workersai",
