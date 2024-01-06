dc4013eba8ac21e5ab83f2fd81d1b3b83d0d2708
Kimi Lu <kimi.lu@aident.ai>
2024-01-05 19:51:25 -0800

    Add streaming for HuggingFace Inference (also add the support of stop-sequences)

--- libs/langchain-community/src/llms/hf.ts
+++ libs/langchain-community/src/llms/hf.ts
@@ -1,4 +1,6 @@
+import { CallbackManagerForLLMRun } from "@langchain/core/callbacks/manager";
 import { LLM, type BaseLLMParams } from "@langchain/core/language_models/llms";
+import { GenerationChunk } from "@langchain/core/outputs";
 import { getEnvironmentVariable } from "@langchain/core/utils/env";
 
 /**
@@ -19,6 +21,11 @@ export interface HFInput {
    * Maximum number of tokens to generate in the completion.
    */
   maxTokens?: number;
+
+  /**
+   * The model will stop generating text when one of the strings in the list is generated.
+   */
+  stopSequences?: string[];
 
   /** Total probability mass of tokens to consider at each step */
   topP?: number;
@@ -70,6 +77,8 @@ export class HuggingFaceInference extends LLM implements HFInput {
 
   maxTokens: number | undefined = undefined;
 
+  stopSequences: string[] | undefined = undefined;
+
   topP: number | undefined = undefined;
 
   topK: number | undefined = undefined;
@@ -88,6 +97,7 @@ export class HuggingFaceInference extends LLM implements HFInput {
     this.model = fields?.model ?? this.model;
     this.temperature = fields?.temperature ?? this.temperature;
     this.maxTokens = fields?.maxTokens ?? this.maxTokens;
+    this.stopSequences = fields?.stopSequences ?? this.stopSequences;
     this.topP = fields?.topP ?? this.topP;
     this.topK = fields?.topK ?? this.topK;
     this.frequencyPenalty = fields?.frequencyPenalty ?? this.frequencyPenalty;
@@ -98,7 +108,7 @@ export class HuggingFaceInference extends LLM implements HFInput {
 
     if (!this.apiKey) {
       throw new Error(
-        "Please set an API key for HuggingFace Hub in the environment variable HUGGINGFACEHUB_API_KEY or in the apiKey field of the HuggingFaceInference constructor."
+        `Please set an API key for HuggingFace Hub in the environment variable "HUGGINGFACEHUB_API_KEY" or in the apiKey field of the HuggingFaceInference constructor.`
       );
     }
   }
@@ -107,38 +117,70 @@ export class HuggingFaceInference extends LLM implements HFInput {
     return "hf";
   }
 
+  invocationParams(_options?: this["ParsedCallOptions"]) {
+    return {
+      model: this.model,
+      parameters: {
+        // make it behave similar to openai, returning only the generated text
+        return_full_text: false,
+        temperature: this.temperature,
+        max_new_tokens: this.maxTokens,
+        stop_sequences: this.stopSequences,
+        top_p: this.topP,
+        top_k: this.topK,
+        repetition_penalty: this.frequencyPenalty,
+      },
+    };
+  }
+
+  async *_streamResponseChunks(
+    prompt: string,
+    options: this["ParsedCallOptions"],
+    runManager?: CallbackManagerForLLMRun
+  ): AsyncGenerator<GenerationChunk> {
+    const hfi = await this._prepareHFInference();
+    const stream = await this.caller.call(async () =>
+      hfi.textGenerationStream({
+        ...this.invocationParams(options),
+        inputs: prompt,
+      })
+    );
+    for await (const chunk of stream) {
+      const token = chunk.token.text;
+      yield new GenerationChunk({ text: token, generationInfo: chunk });
+      await runManager?.handleLLMNewToken(token ?? "");
+
+      // stream is done
+      if (chunk.generated_text)
+        yield new GenerationChunk({
+          text: "",
+          generationInfo: { finished: true },
+        });
+    }
+  }
+
   /** @ignore */
   async _call(
     prompt: string,
     options: this["ParsedCallOptions"]
   ): Promise<string> {
-    const { HfInference } = await HuggingFaceInference.imports();
-    const hf = this.endpointUrl
-      ? new HfInference(this.apiKey, {
-          includeCredentials: this.includeCredentials,
-        }).endpoint(this.endpointUrl)
-      : new HfInference(this.apiKey, {
-          includeCredentials: this.includeCredentials,
-        });
-
+    const hfi = await this._prepareHFInference();
+    const args = { ...this.invocationParams(options), inputs: prompt };
     const res = await this.caller.callWithOptions(
       { signal: options.signal },
-      hf.textGeneration.bind(hf),
-      {
-        model: this.model,
-        parameters: {
-          // make it behave similar to openai, returning only the generated text
-          return_full_text: false,
-          temperature: this.temperature,
-          max_new_tokens: this.maxTokens,
-          top_p: this.topP,
-          top_k: this.topK,
-          repetition_penalty: this.frequencyPenalty,
-        },
-        inputs: prompt,
-      }
+      hfi.textGeneration.bind(hfi),
+      args
     );
     return res.generated_text;
+  }
+
+  /** @ignore */
+  private async _prepareHFInference() {
+    const { HfInference } = await HuggingFaceInference.imports();
+    const hfi = new HfInference(this.apiKey, {
+      includeCredentials: this.includeCredentials,
+    });
+    return this.endpointUrl ? hfi.endpoint(this.endpointUrl) : hfi;
   }
 
   /** @ignore */
--- libs/langchain-community/src/llms/tests/huggingface_hub.int.test.ts
+++ libs/langchain-community/src/llms/tests/huggingface_hub.int.test.ts
@@ -6,3 +6,13 @@ test("Test HuggingFace", async () => {
   const res = await model.call("1 + 1 =");
   console.log(res);
 }, 50000);
+
+test("Test HuggingFace with streaming", async () => {
+  const model = new HuggingFaceInference({ temperature: 0.1, topP: 0.5 });
+  const stream = await model.stream("1 + 1 =");
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  console.log(chunks.join(""));
+}, 50000);
