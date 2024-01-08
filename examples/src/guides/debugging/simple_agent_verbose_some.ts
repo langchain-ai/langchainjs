dfdec18551e30bbd83c9f1b53c3b2dcec7292c19
bracesproul <braceasproul@gmail.com>
2024-01-08 14:33:46 -0800

    docs[patch]: Guide for debugging

---
+++ examples/src/guides/debugging/simple_agent_verbose_some.ts
@@ -0,0 +1,24 @@
+import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
+import { pull } from "langchain/hub";
+import { ChatOpenAI } from "@langchain/openai";
+import type { ChatPromptTemplate } from "@langchain/core/prompts";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
+import { Calculator } from "langchain/tools/calculator";
+
+const tools = [new TavilySearchResults({ verbose: true }), new Calculator({ verbose: true })];
+const prompt = await pull<ChatPromptTemplate>("hwchase17/openai-tools-agent");
+const llm = new ChatOpenAI({
+  modelName: "gpt-4-1106-preview",
+  temperature: 0,
+  verbose: false,
+});
+const agent = await createOpenAIToolsAgent({
+  llm,
+  tools,
+  prompt,
+});
+const agentExecutor = new AgentExecutor({
+  agent,
+  tools,
+  verbose: false,
+});
