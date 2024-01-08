f2b0ec2a817a656f90b48162b7812b05f0470a59
bracesproul <braceasproul@gmail.com>
2024-01-08 14:38:26 -0800

    chore: lint files

--- examples/src/guides/debugging/simple_agent_verbose.ts
+++ examples/src/guides/debugging/simple_agent_verbose.ts
@@ -5,7 +5,10 @@ import type { ChatPromptTemplate } from "@langchain/core/prompts";
 import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
 import { Calculator } from "langchain/tools/calculator";
 
-const tools = [new TavilySearchResults({ verbose: true }), new Calculator({ verbose: true })];
+const tools = [
+  new TavilySearchResults({ verbose: true }),
+  new Calculator({ verbose: true }),
+];
 const prompt = await pull<ChatPromptTemplate>("hwchase17/openai-tools-agent");
 const llm = new ChatOpenAI({
   modelName: "gpt-4-1106-preview",
