bd2b2290c6745e8a0dd323857e7f3a4493f96c5d
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2024-01-17 22:33:08 +0100

    community[patch]: Add neo4j timeout option (#4057)
    * Add neo4j timeout option
    
    * Narrow type
    
    * fix
    
    * switch to timeout in ms
    
    ---------
    
    Co-authored-by: Oskar Hane <oh@oskarhane.com>

--- libs/langchain-community/src/graphs/neo4j_graph.ts
+++ libs/langchain-community/src/graphs/neo4j_graph.ts
@@ -5,6 +5,7 @@ interface Neo4jGraphConfig {
   username: string;
   password: string;
   database?: string;
+  timeoutMs?: number;
 }
 
 interface StructuredSchema {
@@ -42,6 +43,8 @@ export class Neo4jGraph {
 
   private database: string;
 
+  private timeoutMs: number | undefined;
+
   private schema = "";
 
   private structuredSchema: StructuredSchema = {
@@ -55,10 +58,12 @@ export class Neo4jGraph {
     username,
     password,
     database = "neo4j",
+    timeoutMs,
   }: Neo4jGraphConfig) {
     try {
       this.driver = neo4j.driver(url, neo4j.auth.basic(username, password));
       this.database = database;
+      this.timeoutMs = timeoutMs;
     } catch (error) {
       throw new Error(
         "Could not create a Neo4j driver instance. Please check the connection details."
@@ -107,6 +112,7 @@ export class Neo4jGraph {
     try {
       const result = await this.driver.executeQuery(query, params, {
         database: this.database,
+        transactionConfig: { timeout: this.timeoutMs },
       });
       return toObjects(result.records);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
