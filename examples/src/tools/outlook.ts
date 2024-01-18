d1e961e23457fb664246b2ce0ff726de015f702b
oscarchen178 <65813281+oscarchen178@users.noreply.github.com>
2024-01-18 16:55:30 -0500

    putting example in the examples/

---
+++ examples/src/tools/outlook.ts
@@ -0,0 +1,64 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { AgentExecutor, createOpenAIFunctionsAgent } from "langchain/agents";
+import {
+  OutlookReadMailTool,
+  OutlookSendMailTool,
+} from "@langchain/community/tools/outlook";
+import { pull } from "langchain/hub";
+import type { ChatPromptTemplate } from "@langchain/core/prompts";
+
+async function AgentRead() {
+  const llm = new ChatOpenAI({
+    modelName: "gpt-3.5-turbo-1106",
+  });
+
+  const outlookReadMail = new OutlookReadMailTool(undefined, "token");
+  const tools = [outlookReadMail];
+  const prompt = await pull<ChatPromptTemplate>("hwchase17/openai-tools-agent");
+
+  const agent = await createOpenAIFunctionsAgent({
+    llm,
+    tools,
+    prompt,
+  });
+
+  const agentExecutor = new AgentExecutor({
+    agent,
+    tools,
+  });
+
+  const input = "show my emails";
+  const result = await agentExecutor.invoke({ input });
+
+  console.log(result);
+}
+
+AgentRead();
+
+async function AgentSend() {
+  const llm = new ChatOpenAI({
+    modelName: "gpt-4",
+  });
+
+  const sendMailTool = new OutlookSendMailTool(undefined, "token");
+  const tools = [sendMailTool];
+  const prompt = await pull<ChatPromptTemplate>("hwchase17/openai-tools-agent");
+
+  const agent = await createOpenAIFunctionsAgent({
+    llm,
+    tools,
+    prompt,
+  });
+  const agentExecutor = new AgentExecutor({
+    agent,
+    tools,
+  });
+
+  const input =
+    "send an email to YOUR_EMAIL and invite him to a meeting at 3pm tomorrow";
+  const result = await agentExecutor.invoke({ input });
+
+  console.log(result);
+}
+
+AgentSend();
