d6d08a52b26e51c1d2accb32899ceb710135130f
bracesproul <braceasproul@gmail.com>
2024-01-19 16:43:59 -0800

    prefix node modules w/ node:

--- libs/langchain-scripts/src/create-entrypoints.ts
+++ libs/langchain-scripts/src/create-entrypoints.ts
@@ -1,5 +1,5 @@
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import { identifySecrets } from "./identify-secrets.js";
 
 export interface ExtraImportMapEntry {
