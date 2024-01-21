d25eaf7526f59868da3a161e3df8d56d60f95989
bracesproul <braceasproul@gmail.com>
2024-01-21 14:39:34 -0800

    modify script

--- dependency_range_tests/scripts/test-langchain-with-latest-deps.sh
+++ dependency_range_tests/scripts/test-langchain-with-latest-deps.sh
@@ -11,9 +11,9 @@ shopt -s extglob
 cp -r ../langchain/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
 
 # Copy the yarn.lock file from the host
+cp -r ../langchain-core/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
+cp -r ../libs/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
 cp ../yarn.lock ./
-cp ../langchain-core/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
-cp ../libs/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
 cp ../package.json ./
 
 yarn
