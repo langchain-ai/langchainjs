cbf6d266cc6b440c4e0f8bf6a01091e479e29cc8
jacoblee93 <jacoblee93@gmail.com>
2024-01-23 22:07:05 -0800

    Lint

--- examples/src/guides/langsmith/eval_walkthrough.ts
+++ examples/src/guides/langsmith/eval_walkthrough.ts
@@ -1,4 +1,8 @@
+/* eslint-disable import/first */
+/* eslint-disable arrow-body-style */
+/* eslint-disable import/no-duplicates */
 import { v4 as uuidv4 } from "uuid";
+
 const uniqueId = uuidv4().slice(0, 8);
 
 import { Client } from "langsmith";
