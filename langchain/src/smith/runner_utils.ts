8620addc9ab12ef034f097dbbeff75d5c7efacee
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-01-26 18:40:10 -0800

    Trace in Evaluators Project

--- langchain/src/smith/runner_utils.ts
+++ langchain/src/smith/runner_utils.ts
@@ -49,9 +49,13 @@ class RunIdExtractor {
   handleChainStart = (
     _chain: Serialized,
     _inputs: ChainValues,
-    runId: string
+    runId: string,
+    parentRunId?: string
   ) => {
-    this.runIdPromiseResolver(runId);
+    if (!parentRunId) {
+      // Only resolve root runs
+      this.runIdPromiseResolver(runId);
+    }
   };
 
   async extract(): Promise<string> {
@@ -77,6 +81,7 @@ class DynamicRunEvaluator implements RunEvaluator {
    */
   async evaluateRun(run: Run, example?: Example): Promise<EvaluationResult> {
     const extractor = new RunIdExtractor();
+    const tracer = new LangChainTracer({ projectName: "evaluators" });
     const result = await this.evaluator.invoke(
       {
         run,
@@ -86,7 +91,7 @@ class DynamicRunEvaluator implements RunEvaluator {
         reference: example?.outputs,
       },
       {
-        callbacks: [extractor],
+        callbacks: [extractor, tracer],
       }
     );
     const runId = await extractor.extract();
@@ -169,6 +174,7 @@ class PreparedRunEvaluator implements RunEvaluator {
       run,
     });
     const extractor = new RunIdExtractor();
+    const tracer = new LangChainTracer({ projectName: "evaluators" });
     if (this.isStringEvaluator) {
       const evalResult = await this.evaluator.evaluateStrings(
         {
@@ -177,7 +183,7 @@ class PreparedRunEvaluator implements RunEvaluator {
           input: input as string,
         },
         {
-          callbacks: [extractor],
+          callbacks: [extractor, tracer],
         }
       );
       const runId = await extractor.extract();
