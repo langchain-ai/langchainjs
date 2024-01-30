00af82e8b50b83a307551b5bfba7017f7d1ac295
bracesproul <braceasproul@gmail.com>
2024-01-29 18:11:21 -0800

    cr

--- examples/src/use_cases/sql/agents/index.ts
+++ examples/src/use_cases/sql/agents/index.ts
@@ -5,11 +5,7 @@ import {
 } from "@langchain/core/prompts";
 import { ChatOpenAI } from "@langchain/openai";
 import { createOpenAIToolsAgent, AgentExecutor } from "langchain/agents";
-import {
-  SqlToolkit,
-  SQL_PREFIX,
-  SQL_SUFFIX,
-} from "langchain/agents/toolkits/sql";
+import { SqlToolkit } from "langchain/agents/toolkits/sql";
 import { AIMessage } from "langchain/schema";
 import { SqlDatabase } from "langchain/sql_db";
 import { DataSource } from "typeorm";
@@ -24,6 +20,25 @@ const db = await SqlDatabase.fromDataSourceParams({
 const llm = new ChatOpenAI({ modelName: "gpt-3.5-turbo", temperature: 0 });
 const sqlToolKit = new SqlToolkit(db, llm);
 const tools = sqlToolKit.getTools();
+const SQL_PREFIX = `You are an agent designed to interact with a SQL database.
+Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
+Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results using the LIMIT clause.
+You can order the results by a relevant column to return the most interesting examples in the database.
+Never query for all the columns from a specific table, only ask for a the few relevant columns given the question.
+You have access to tools for interacting with the database.
+Only use the below tools.
+Only use the information returned by the below tools to construct your final answer.
+You MUST double check your query before executing it. If you get an error while executing a query, rewrite the query and try again.
+
+DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
+
+If the question does not seem related to the database, just return "I don't know" as the answer.`;
+const SQL_SUFFIX =
+  `Begin!
+
+Question: {input}
+Thought: I should look at the tables in the database to see what I can query.
+{agent_scratchpad}`;
 const prompt = ChatPromptTemplate.fromMessages([
   ["system", SQL_PREFIX],
   HumanMessagePromptTemplate.fromTemplate("{input}"),
