841f0c7da911508080d5dc9444d2d9d2480bd09f
Phil Miesle <mieslep@users.noreply.github.com>
2024-01-30 02:15:54 +0000

    community[patch]: adding cassandra utils (#4193)
    * adding cassandra utils
    
    * fixing typo in factory name
    
    * Fix flake?
    
    * Revert
    
    * Flake?
    
    * Remove flaky dep
    
    * Update lock
    
    * Run build :facepalm:
    
    ---------
    
    Co-authored-by: Phil Miesle <phil.miesle@datastax.com>
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/langchain.config.js
+++ libs/langchain-community/langchain.config.js
@@ -175,7 +175,6 @@ export const config = {
     // graphs
     "graphs/neo4j_graph": "graphs/neo4j_graph",
     "graphs/memgraph_graph": "graphs/memgraph_graph",
-    "utils/event_source_parse": "utils/event_source_parse",
     // document transformers
     "document_transformers/html_to_text": "document_transformers/html_to_text",
     "document_transformers/mozilla_readability":
@@ -205,11 +204,14 @@ export const config = {
     "memory/chat_memory": "memory/chat_memory",
     "memory/motorhead_memory": "memory/motorhead_memory",
     "memory/zep": "memory/zep",
-    "util/convex": "utils/convex",
     // indexes
     "indexes/base": "indexes/base",
     "indexes/postgres": "indexes/postgres",
     "indexes/memory": "indexes/memory",
+    // utils
+    "util/convex": "utils/convex",
+    "utils/event_source_parse": "utils/event_source_parse",
+    "utils/cassandra": "utils/cassandra",
   },
   requiresOptionalDependency: [
     "tools/aws_sfn",
