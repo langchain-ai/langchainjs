c10e8bf3eb5ae5d39b8e6e126c6856c6c58c0588
bracesproul <braceasproul@gmail.com>
2024-01-31 13:22:06 -0800

    chore: lint files

--- examples/src/models/chat/integration_togetherai_tools.ts
+++ examples/src/models/chat/integration_togetherai_tools.ts
@@ -12,7 +12,8 @@ const modelWithCalculator = new ChatTogetherAI({
   togetherAIApiKey: process.env.TOGETHER_AI_API_KEY,
   // Together JSON mode/tool calling only supports a select number of models
   modelName: "mistralai/Mixtral-8x7B-Instruct-v0.1",
-}).bind({ // Bind the tool to the model.
+}).bind({
+  // Bind the tool to the model.
   tools: [calculatorTool],
   tool_choice: calculatorTool, // Specify what tool the model should use
 });
