0a4851aae1b2ca25f78190f3bd4f10fea71393aa
jacoblee93 <jacoblee93@gmail.com>
2024-01-31 16:57:06 -0800

    Update docs + examples

--- examples/src/embeddings/azure_openai.ts
+++ examples/src/embeddings/azure_openai.ts
@@ -1,9 +1,7 @@
 import { AzureOpenAIEmbeddings } from "@langchain/azure-openai";
 
-export const run = async () => {
-  const model = new AzureOpenAIEmbeddings();
-  const res = await model.embedQuery(
-    "What would be a good company name for a company that makes colorful socks?"
-  );
-  console.log({ res });
-};
+const model = new AzureOpenAIEmbeddings();
+const res = await model.embedQuery(
+  "What would be a good company name for a company that makes colorful socks?"
+);
+console.log({ res });
