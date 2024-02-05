7c1620a7e3aaee84aebc48e40c5854fe3ca073ad
bracesproul <braceasproul@gmail.com>
2024-02-04 17:50:51 -0800

    impriove test

--- langchain/src/vectorstores/tests/memory.test.ts
+++ langchain/src/vectorstores/tests/memory.test.ts
@@ -2,6 +2,7 @@ import { test, expect } from "@jest/globals";
 
 import { Document, DocumentInterface } from "@langchain/core/documents";
 import { SyntheticEmbeddings } from "@langchain/core/utils/testing";
+import { similarity } from "ml-distance";
 import { MemoryVectorStore } from "../memory.js";
 
 test("MemoryVectorStore with external ids", async () => {
@@ -36,7 +37,6 @@ test("MemoryVectorStore as retriever can filter metadata", async () => {
   const store = new MemoryVectorStore(embeddings);
 
   const filterFunc = (doc: DocumentInterface): boolean => {
-    console.log("running filter in test");
     const { metadata } = doc;
     if (metadata.namespace <= 2) {
       return true;
@@ -66,4 +66,34 @@ test("MemoryVectorStore as retriever can filter metadata", async () => {
     new Document({ metadata: { namespace: 1 }, pageContent: "hello" }),
     new Document({ metadata: { namespace: 2 }, pageContent: "hello" }),
   ]);
+});
+
+test("MemoryVectorStore with custom similarity", async () => {
+  const embeddings = new SyntheticEmbeddings({
+    vectorSize: 1536,
+  });
+  let similarityCalled = false;
+  let similarityCalledCount = 0;
+  const store = new MemoryVectorStore(embeddings, {
+    similarity: (a: number, b: number) => {
+      similarityCalledCount += 1;
+      similarityCalled = true;
+      return similarity.cosine(a, b);
+    }
+  });
+
+  expect(store).toBeDefined();
+
+  await store.addDocuments([
+    { pageContent: "hello", metadata: { a: 1 } },
+    { pageContent: "hi", metadata: { a: 1 } },
+    { pageContent: "bye", metadata: { a: 1 } },
+    { pageContent: "what's this", metadata: { a: 1 } },
+  ]);
+
+  const results = await store.similaritySearch("hello", 3);
+
+  expect(similarityCalled).toBe(true);
+  expect(similarityCalledCount).toBe(4);
+  expect(results).toHaveLength(3);
 });
