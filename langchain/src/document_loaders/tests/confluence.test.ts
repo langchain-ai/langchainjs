12204d6b2dab8ed4dc2162441b7b9747e91e177c
Chris Chen <fuleinist@outlook.com>
2024-02-07 11:54:02 +1100

    langchain[patch]: update metadata and default expand param for confluence loader (#4220)
    * update metadata and default expand param for confluence loader
    
    * format
    
    * Fix test
    
    * Format
    
    ---------
    
    Co-authored-by: Chris Chen <fuleinist@gmail.com>
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- langchain/src/document_loaders/tests/confluence.test.ts
+++ langchain/src/document_loaders/tests/confluence.test.ts
@@ -36,11 +36,15 @@ test("Test ConfluenceLoader and fetchConfluenceData calls", async () => {
   // until the size of the response is 0
   const fetchConfluenceDataMock = jest
     .spyOn(loader, "fetchConfluenceData")
-    .mockImplementationOnce(() =>
-      Promise.resolve({ size: 2, results: fakeResponse })
+    .mockImplementationOnce(
+      () =>
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        Promise.resolve({ size: 2, results: fakeResponse }) as any
     )
-    .mockImplementationOnce(() =>
-      Promise.resolve({ size: 2, results: fakeResponse })
+    .mockImplementationOnce(
+      () =>
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        Promise.resolve({ size: 2, results: fakeResponse }) as any
     )
     .mockImplementationOnce(() => Promise.resolve({ size: 0, results: [] }));
 
@@ -57,15 +61,15 @@ test("Test ConfluenceLoader and fetchConfluenceData calls", async () => {
   // Ensure the arguments are correct for each call
   expect(fetchConfluenceDataMock).toHaveBeenNthCalledWith(
     1,
-    "https://example.atlassian.net/wiki/rest/api/content?spaceKey=SPACEKEY&limit=25&start=0&expand=body.storage"
+    "https://example.atlassian.net/wiki/rest/api/content?spaceKey=SPACEKEY&limit=25&start=0&expand=body.storage,version"
   );
   expect(fetchConfluenceDataMock).toHaveBeenNthCalledWith(
     2,
-    "https://example.atlassian.net/wiki/rest/api/content?spaceKey=SPACEKEY&limit=25&start=2&expand=body.storage"
+    "https://example.atlassian.net/wiki/rest/api/content?spaceKey=SPACEKEY&limit=25&start=2&expand=body.storage,version"
   );
   expect(fetchConfluenceDataMock).toHaveBeenNthCalledWith(
     3,
-    "https://example.atlassian.net/wiki/rest/api/content?spaceKey=SPACEKEY&limit=25&start=4&expand=body.storage"
+    "https://example.atlassian.net/wiki/rest/api/content?spaceKey=SPACEKEY&limit=25&start=4&expand=body.storage,version"
   );
 
   // Check if the generated URLs in the metadata are correct
