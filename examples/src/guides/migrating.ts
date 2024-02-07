ae9af9b72d5478a9fd82e7321ce9fea37d9859b4
Brace Sproul <braceasproul@gmail.com>
2024-02-07 09:45:04 -0800

    Revert "langchain[minor]: Add imports migration script (#4103)"
    This reverts commit d4c5dd5a05e0ead85aa663580984b378f26fe5a0.

--- examples/src/guides/migrating.ts
+++
@@ -1,8 +0,0 @@
-import { updateEntrypointsFrom0_0_xTo0_1_x } from "langchain/util/migrations/0_1";
-
-updateEntrypointsFrom0_0_xTo0_1_x({
-  localLangChainPath: "/Users/my-profile/langchainjs",
-  codePath: "/Users/my-profile/langchainjs-project",
-})
-  .then(() => console.log("done"))
-  .catch(console.error);
