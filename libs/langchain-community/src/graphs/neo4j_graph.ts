905b721eccc3dcb6b6acbaae2580768306cac100
Adam Cowley <adam@adamcowley.co.uk>
2024-02-16 21:03:33 +0000

    community[patch]: add neo4j-graph generics, don't swallow query execution errors (#4434)
    * fix: add neo4j-graph generics, don't swallow query execution errors
    
    * Update lock
    
    * Formatting
    
    * Remove unused import
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/src/graphs/neo4j_graph.ts
+++ libs/langchain-community/src/graphs/neo4j_graph.ts
@@ -1,4 +1,4 @@
-import neo4j, { Neo4jError } from "neo4j-driver";
+import neo4j, { RoutingControl } from "neo4j-driver";
 
 interface Neo4jGraphConfig {
   url: string;
@@ -108,25 +108,17 @@ export class Neo4jGraph {
   }
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async query(query: string, params: any = {}): Promise<any[] | undefined> {
-    try {
-      const result = await this.driver.executeQuery(query, params, {
-        database: this.database,
-        transactionConfig: { timeout: this.timeoutMs },
-      });
-      return toObjects(result.records);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      if (
-        // eslint-disable-next-line
-        error instanceof Neo4jError &&
-        error.code === "Neo.ClientError.Procedure.ProcedureNotFound"
-      ) {
-        throw new Error("Procedure not found in Neo4j.");
-      }
-    }
-    /** @TODO Propagate error instead of returning undefined */
-    return undefined;
+  async query<RecordShape extends Record<string, any> = Record<string, any>>(
+    query: string,
+    params: Record<string, any> = {},
+    routing: RoutingControl = neo4j.routing.WRITE
+  ): Promise<RecordShape[] | undefined> {
+    const result = await this.driver.executeQuery<RecordShape>(query, params, {
+      database: this.database,
+      routing,
+      transactionConfig: { timeout: this.timeoutMs },
+    });
+    return toObjects<RecordShape>(result.records);
   }
 
   async verifyConnectivity() {
@@ -160,16 +152,16 @@ export class Neo4jGraph {
 
     // Assuming query method is defined and returns a Promise
     const nodeProperties: NodeType[] | undefined = (
-      await this.query(nodePropertiesQuery)
-    )?.map((el: { output: NodeType }) => el.output);
+      await this.query<{ output: NodeType }>(nodePropertiesQuery)
+    )?.map((el) => el.output);
 
     const relationshipsProperties: RelType[] | undefined = (
-      await this.query(relPropertiesQuery)
-    )?.map((el: { output: RelType }) => el.output);
+      await this.query<{ output: RelType }>(relPropertiesQuery)
+    )?.map((el) => el.output);
 
     const relationships: PathType[] | undefined = (
-      await this.query(relQuery)
-    )?.map((el: { output: PathType }) => el.output);
+      await this.query<{ output: PathType }>(relQuery)
+    )?.map((el) => el.output);
 
     // Structured schema similar to Python's dictionary comprehension
     this.structuredSchema = {
@@ -219,18 +211,19 @@ export class Neo4jGraph {
   }
 }
 
-function toObjects(records: neo4j.Record[]) {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const recordValues: Record<string, any>[] = records.map((record) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function toObjects<
+  RecordShape extends Record<string, any> = Record<string, any>
+>(records: neo4j.Record<RecordShape>): RecordShape[] {
+  return records.map((record) => {
     const rObj = record.toObject();
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const out: { [key: string]: any } = {};
-    Object.keys(rObj).forEach((key) => {
+    const out: Partial<RecordShape> = {};
+    Object.keys(rObj).forEach((key: keyof RecordShape) => {
       out[key] = itemIntToString(rObj[key]);
     });
-    return out;
+    return out as RecordShape;
   });
-  return recordValues;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
