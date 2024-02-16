a11a911fa1dac3387d46fb69b43bfebba24b5247
Phil Miesle <mieslep@users.noreply.github.com>
2024-02-16 21:38:50 +0000

    community[minor]: add CassandraKVStore (#4418)
    * add CassandraKVStore
    
    * fixing (?) doc build problem
    
    * re-exporting for backwards compatibility
    
    * Update langchain.config.js
    
    * Format
    
    ---------
    
    Co-authored-by: Phil Miesle <phil.miesle@datastax.com>
    Co-authored-by: Jacob Lee <jacoblee93@gmail.com>

--- libs/langchain-community/langchain.config.js
+++ libs/langchain-community/langchain.config.js
@@ -182,6 +182,7 @@ export const config = {
     "document_transformers/mozilla_readability":
       "document_transformers/mozilla_readability",
     // storage
+    "storage/cassandra": "storage/cassandra",
     "storage/convex": "storage/convex",
     "storage/ioredis": "storage/ioredis",
     "storage/upstash_redis": "storage/upstash_redis",
@@ -315,6 +316,7 @@ export const config = {
     "document_transformers/html_to_text",
     "document_transformers/mozilla_readability",
     // storage
+    "storage/cassandra",
     "storage/convex",
     "storage/ioredis",
     "storage/upstash_redis",
@@ -335,7 +337,9 @@ export const config = {
     // memory
     "memory/motorhead_memory",
     "memory/zep",
+    // utils
     "util/convex",
+    "utils/cassandra",
     // indexes
     "indexes/postgres",
     "indexes/sqlite",
