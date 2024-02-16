a30541ec3cafcc2e2934ec339164fd84fe570360
jacoblee93 <jacoblee93@gmail.com>
2024-02-15 16:55:58 -0800

    Fix lint

--- examples/src/agents/quickstart.ts
+++ examples/src/agents/quickstart.ts
@@ -44,7 +44,7 @@ console.log(retrieverResult[0]);
 
 /*
   Document {
-    pageContent: "your application progresses through the beta testing phase, it's essential to continue collecting data to refine and improve its performance. LangSmith enables you to add runs as examples to datasets (from both the project page and within an annotation queue), expanding your test coverage on real-world scenarios. This is a key benefit in having your logging system and your evaluation/testing system in the same platform.Production​Closely inspecting key data points, growing benchmarking datasets, annotating traces, and drilling down into important data in trace view are workflows you’ll also want to do once your app hits production. However, especially at the production stage, it’s crucial to get a high-level overview of application performance with respect to latency, cost, and feedback scores. This ensures that it's delivering desirable results at scale.Monitoring and A/B Testing​LangSmith provides monitoring charts that allow you to track key metrics over time. You can expand to",
+    pageContent: "your application progresses through the beta testing phase, it's essential to continue collecting data to refine and improve...",
     metadata: {
       source: 'https://docs.smith.langchain.com/user_guide',
       loc: { lines: [Object] }
