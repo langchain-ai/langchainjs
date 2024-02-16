56ee9c654de51a4fb1cd028227a08390d2fb7fe0
bracesproul <braceasproul@gmail.com>
2024-02-16 15:15:41 -0800

    chore: lint files

--- examples/src/models/embeddings/nomic.ts
+++ examples/src/models/embeddings/nomic.ts
@@ -5,5 +5,8 @@ const nomicEmbeddings = new NomicEmbeddings();
 const res = await nomicEmbeddings.embedQuery("Hello world");
 console.log(res);
 /* Embed documents */
-const documentRes = await nomicEmbeddings.embedDocuments(["Hello world", "Bye bye"]);
+const documentRes = await nomicEmbeddings.embedDocuments([
+  "Hello world",
+  "Bye bye",
+]);
 console.log(documentRes);
