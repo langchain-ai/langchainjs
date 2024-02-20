3e67b315f942602c2626c9b030ec84d3f22cff13
bracesproul <braceasproul@gmail.com>
2024-02-20 15:57:38 -0800

    [WIP]community[minor]: ChatGroq

---
+++ libs/langchain-community/src/chat_models/tests/chatgroq.int.test.ts
@@ -0,0 +1,32 @@
+import { describe, test } from "@jest/globals";
+import { ChatMessage, HumanMessage } from "@langchain/core/messages";
+import { ChatGroq } from "../groq.js";
+
+describe.skip("ChatGroq", () => {
+  test("invoke", async () => {
+    const chat = new ChatGroq({
+      maxRetries: 0
+    });
+    const message = new HumanMessage("What color is the sky?");
+    const res = await chat.invoke([message]);
+    console.log({ res });
+    expect(res.content.length).toBeGreaterThan(10);
+  });
+
+  test("generate", async () => {
+    const chat = new ChatGroq();
+    const message = new HumanMessage("Hello!");
+    const res = await chat.generate([[message]]);
+    console.log(JSON.stringify(res, null, 2));
+    expect(res.generations[0][0].text.length).toBeGreaterThan(10);
+  });
+
+  test("custom messages", async () => {
+    const chat = new ChatGroq();
+    const res = await chat.invoke([new ChatMessage("Hello!", "user")]);
+    console.log({ res });
+    expect(res.content.length).toBeGreaterThan(10);
+  });
+
+  // From their API docs it appears you can pass functions through the messages field, test function calling
+});
