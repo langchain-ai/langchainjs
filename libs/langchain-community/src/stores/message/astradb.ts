b3701224d6e352b7c6531a91def112d3743761cc
Mike Fortman <michael.fortman@datastax.com>
2024-02-19 21:57:28 -0600

    documentation

--- libs/langchain-community/src/stores/message/astradb.ts
+++ libs/langchain-community/src/stores/message/astradb.ts
@@ -21,6 +21,31 @@ export interface AstraDBChatMessageHistoryProps {
   sessionId: string;
 }
 
+
+/**
+ * Class for storing chat message history with Astra DB. It extends the
+ * BaseListChatMessageHistory class and provides methods to get, add, and
+ * clear messages.
+ * @example
+ * 
+ * ```typescript
+ * const client = new AstraDB(
+ *   process.env.ASTRA_DB_APPLICATION_TOKEN,
+ *   process.env.ASTRA_DB_ENDPOINT,
+ *   process.env.ASTRA_DB_NAMESPACE
+ * );
+ * 
+ * const collection = await client.collection("test_chat");
+ * 
+ * const chatHistory = new AstraDBChatMessageHistory({
+ *   collection,
+ *   sessionId: "YOUR_SESSION_ID",
+ * });
+ * 
+ * const messages = await chatHistory.getMessages();
+ * 
+ * await chatHistory.clear();
+ */
 export class AstraDBChatMessageHistory extends BaseListChatMessageHistory {
   lc_namespace = ["langchain", "stores", "message", "astradb"];
 
@@ -34,6 +59,20 @@ export class AstraDBChatMessageHistory extends BaseListChatMessageHistory {
     this.collection = collection;
   }
 
+  /**
+   * async initializer function to return a new instance of AstraDBChatMessageHistory in a single step
+   * @param AstraDBChatMessageHistoryInput 
+   * @returns Promise<AstraDBChatMessageHistory>
+   * 
+   * @example
+   * const chatHistory = await AstraDBChatMessageHistory.initialize({
+   *  token: process.env.ASTRA_DB_APPLICATION_TOKEN,
+   *  endpoint: process.env.ASTRA_DB_ENDPOINT,
+   *  namespace: process.env.ASTRA_DB_NAMESPACE,
+   *  collectionName:"test_chat",
+   *  sessionId: "YOUR_SESSION_ID"
+   * });
+   */
   static async initialize({ token, endpoint, collectionName, namespace, sessionId }: AstraDBChatMessageHistoryInput): Promise<AstraDBChatMessageHistory> {
     const client = new AstraDB(token, endpoint, namespace);
     const collection = await client.collection(collectionName);
