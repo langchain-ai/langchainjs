86d1e55f3c80b605dc54427f8526661000352cde
Yufei (Benny) Chen <1585539+benjibc@users.noreply.github.com>
2024-02-20 21:38:25 -0800

    community[minor]: Basic Fireworks function calling notebook + embeddings code (#4447)
    * Basic Fireworks function calling notebook + embeddings code
    
    * Add entrypoint, update notebook
    
    * Remove log
    
    * typo
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/langchain.config.js
+++ libs/langchain-community/langchain.config.js
@@ -58,6 +58,7 @@ export const config = {
     "embeddings/bedrock": "embeddings/bedrock",
     "embeddings/cloudflare_workersai": "embeddings/cloudflare_workersai",
     "embeddings/cohere": "embeddings/cohere",
+    "embeddings/fireworks": "embeddings/fireworks",
     "embeddings/googlepalm": "embeddings/googlepalm",
     "embeddings/googlevertexai": "embeddings/googlevertexai",
     "embeddings/gradient_ai": "embeddings/gradient_ai",
