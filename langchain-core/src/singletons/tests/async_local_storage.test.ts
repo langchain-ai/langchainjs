55f662be84930ce47f4259c59df780fd312e0edc
jacoblee93 <jacoblee93@gmail.com>
2024-02-23 11:30:27 -0800

    Adds test, build

---
+++ langchain-core/src/singletons/tests/async_local_storage.test.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@jest/globals";
+import { AsyncLocalStorage } from "node:async_hooks";
+import { AsyncLocalStorageProviderSingleton } from "../index.js";
+import { RunnableLambda } from "../../runnables/base.js";
+
+test("Config should be automatically populated after setting global async local storage", async () => {
+  const inner = RunnableLambda.from((_, config) => {
+    return config;
+  });
+  const outer = RunnableLambda.from(async (input) => {
+    const res = await inner.invoke(input);
+    return res;
+  });
+  const res1 = await outer.invoke(
+    { hi: true },
+    {
+      configurable: {
+        sampleKey: "sampleValue",
+      },
+      tags: ["tester"],
+    }
+  );
+  expect(res1?.tags).toEqual([]);
+  AsyncLocalStorageProviderSingleton.initializeGlobalInstance(
+    new AsyncLocalStorage()
+  );
+  const res2 = await outer.invoke(
+    { hi: true },
+    {
+      configurable: {
+        sampleKey: "sampleValue",
+      },
+      tags: ["tester"],
+    }
+  );
+  expect(res2?.tags).toEqual(["tester"]);
+});
