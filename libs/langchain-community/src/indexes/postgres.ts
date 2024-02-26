243aaea57cdcb889313da4ea3d3aac0b1c6c0d7f
Alan Bentley <57491268+alan-bentley-dsg@users.noreply.github.com>
2024-02-26 15:32:20 -0500

    community[patch]: Remove quotes around PostgresRecordManager table create statement (#4480)
    * Remove quotes around PostgresRecordManager table create statement
    
    Wrapping the table name in quotes locks the creation of tables to the default schema in postgres. Removing the quotes enables the creation of tables in other schemes enabling flexibility
    
    * replaced quotes with optional param to use schema and table name to avoid breakage and enable schema flexibility
    
    * removed typo with double quotes, also added smoke test for creating table under new schema
    
    * refactor optional param to optional constructor argument, applied same logic to remaining queries
    
    * move schema argument out of constructor and into PostgresRecordManagerOptions, refactor  into class variable to avoid logic repetition, updated test to use new instance of PostgresRecordManager to avoid reinitialization every test
    
    * Format
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/src/indexes/postgres.ts
+++ libs/langchain-community/src/indexes/postgres.ts
@@ -8,6 +8,7 @@ import {
 export type PostgresRecordManagerOptions = {
   postgresConnectionOptions: PoolConfig;
   tableName?: string;
+  schema?: string;
 };
 
 export class PostgresRecordManager implements RecordManagerInterface {
@@ -19,28 +20,34 @@ export class PostgresRecordManager implements RecordManagerInterface {
 
   namespace: string;
 
+  finalTableName: string;
+
   constructor(namespace: string, config: PostgresRecordManagerOptions) {
     const { postgresConnectionOptions, tableName } = config;
     this.namespace = namespace;
     this.pool = new pg.Pool(postgresConnectionOptions);
     this.tableName = tableName || "upsertion_records";
+    this.finalTableName = config.schema
+      ? `"${config.schema}"."${tableName}"`
+      : `"${tableName}"`;
   }
 
   async createSchema(): Promise<void> {
     try {
       await this.pool.query(`
-CREATE TABLE IF NOT EXISTS "${this.tableName}" (
-  uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  key TEXT NOT NULL,
-  namespace TEXT NOT NULL,
-  updated_at Double PRECISION NOT NULL,
-  group_id TEXT,
-  UNIQUE (key, namespace)
-);
-CREATE INDEX IF NOT EXISTS updated_at_index ON "${this.tableName}" (updated_at);
-CREATE INDEX IF NOT EXISTS key_index ON "${this.tableName}" (key);
-CREATE INDEX IF NOT EXISTS namespace_index ON "${this.tableName}" (namespace);
-CREATE INDEX IF NOT EXISTS group_id_index ON "${this.tableName}" (group_id);`);
+        CREATE TABLE IF NOT EXISTS ${this.finalTableName} (
+          uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+          key TEXT NOT NULL,
+          namespace TEXT NOT NULL,
+          updated_at Double PRECISION NOT NULL,
+          group_id TEXT,
+          UNIQUE (key, namespace)
+        );
+        CREATE INDEX IF NOT EXISTS updated_at_index ON ${this.finalTableName} (updated_at);
+        CREATE INDEX IF NOT EXISTS key_index ON ${this.finalTableName} (key);
+        CREATE INDEX IF NOT EXISTS namespace_index ON ${this.finalTableName} (namespace);
+        CREATE INDEX IF NOT EXISTS group_id_index ON ${this.finalTableName} (group_id);`);
+
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
       // This error indicates that the table already exists
@@ -114,7 +121,7 @@ CREATE INDEX IF NOT EXISTS group_id_index ON "${this.tableName}" (group_id);`);
       )
       .join(", ");
 
-    const query = `INSERT INTO "${this.tableName}" (key, namespace, updated_at, group_id) VALUES ${valuesPlaceholders} ON CONFLICT (key, namespace) DO UPDATE SET updated_at = EXCLUDED.updated_at;`;
+    const query = `INSERT INTO ${this.finalTableName} (key, namespace, updated_at, group_id) VALUES ${valuesPlaceholders} ON CONFLICT (key, namespace) DO UPDATE SET updated_at = EXCLUDED.updated_at;`;
     await this.pool.query(query, recordsToUpsert.flat());
   }
 
@@ -129,7 +136,7 @@ CREATE INDEX IF NOT EXISTS group_id_index ON "${this.tableName}" (group_id);`);
       .join(", ");
 
     const query = `
-      SELECT k, (key is not null) ex from unnest(ARRAY[${arrayPlaceholders}]) k left join "${this.tableName}" on k=key and namespace = $1;
+      SELECT k, (key is not null) ex from unnest(ARRAY[${arrayPlaceholders}]) k left join ${this.finalTableName} on k=key and namespace = $1;
       `;
     const res = await this.pool.query(query, [this.namespace, ...keys.flat()]);
     return res.rows.map((row: { ex: boolean }) => row.ex);
@@ -137,7 +144,8 @@ CREATE INDEX IF NOT EXISTS group_id_index ON "${this.tableName}" (group_id);`);
 
   async listKeys(options?: ListKeyOptions): Promise<string[]> {
     const { before, after, limit, groupIds } = options ?? {};
-    let query = `SELECT key FROM "${this.tableName}" WHERE namespace = $1`;
+
+    let query = `SELECT key FROM ${this.finalTableName} WHERE namespace = $1`;
     const values: (string | number | (string | null)[])[] = [this.namespace];
 
     let index = 2;
@@ -175,7 +183,7 @@ CREATE INDEX IF NOT EXISTS group_id_index ON "${this.tableName}" (group_id);`);
       return;
     }
 
-    const query = `DELETE FROM "${this.tableName}" WHERE namespace = $1 AND key = ANY($2);`;
+    const query = `DELETE FROM ${this.finalTableName} WHERE namespace = $1 AND key = ANY($2);`;
     await this.pool.query(query, [this.namespace, keys]);
   }
 
