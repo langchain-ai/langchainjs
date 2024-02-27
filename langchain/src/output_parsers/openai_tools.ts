d487962ea64589e90ef741eeb62fbdfcfdcd3c7d
bracesproul <braceasproul@gmail.com>
2024-02-26 19:17:00 -0800

    drop warning

--- langchain/src/output_parsers/openai_tools.ts
+++ langchain/src/output_parsers/openai_tools.ts
@@ -1,9 +1,1 @@
-import { logVersion010MigrationWarning } from "../util/entrypoint_deprecation.js";
-
-/* #__PURE__ */ logVersion010MigrationWarning({
-  oldEntrypointName: "output_parsers",
-  newEntrypointName: "output_parsers/openai_tools",
-  newPackageName: "@langchain/core",
-});
-
 export * from "@langchain/core/output_parsers/openai_tools";
