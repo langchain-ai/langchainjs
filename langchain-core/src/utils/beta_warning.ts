c7009562861986423d0bc01ce9d2bcd1adb3c086
bracesproul <braceasproul@gmail.com>
2024-02-29 13:09:25 -0800

    cr

--- langchain-core/src/utils/beta_warning.ts
+++ langchain-core/src/utils/beta_warning.ts
@@ -1,7 +1,7 @@
 /**
  * Decorator for logging a warning when a method is called.
  */
-export function betaWarning(reason: string) {
-  console.warn(`The function '${reason}' is in beta. It is actively being worked on, so the API may change.`);
+export function betaWarning(func: string) {
+  console.warn(`The function '${func}' is in beta. It is actively being worked on, so the API may change.`);
 }
 
