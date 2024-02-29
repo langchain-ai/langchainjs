c35abbc52b49d76399b38376266a0a09bc80de0b
bracesproul <braceasproul@gmail.com>
2024-02-29 13:09:53 -0800

    chore: lint files

--- langchain-core/src/utils/beta_warning.ts
+++ langchain-core/src/utils/beta_warning.ts
@@ -2,6 +2,7 @@
  * Decorator for logging a warning when a method is called.
  */
 export function betaWarning(func: string) {
-  console.warn(`The function '${func}' is in beta. It is actively being worked on, so the API may change.`);
+  console.warn(
+    `The function '${func}' is in beta. It is actively being worked on, so the API may change.`
+  );
 }
-
