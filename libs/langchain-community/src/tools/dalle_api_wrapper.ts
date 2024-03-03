ddd9c28bb429f65dffbb7f0f3639b9d6bdf4a1e8
David Fekke <david@fekke.com>
2024-03-03 16:52:14 -0500

    Adding documentation for the DallE

--- libs/langchain-community/src/tools/dalle_api_wrapper.ts
+++ libs/langchain-community/src/tools/dalle_api_wrapper.ts
@@ -2,26 +2,54 @@ import { getEnvironmentVariable } from "@langchain/core/utils/env";
 import { type ClientOptions, OpenAI as OpenAIClient } from "openai";
 import { Tool, ToolParams } from "@langchain/core/tools";
 
+/*
+    An enumerator for the Dall-E model to use with the OpenAI.
+    Dall-E 2 and 3 API
+    https://beta.openai.com/docs/api-reference/images/generate
+ */
 export enum DalleApiWrapperModelEnum {
     DALLE_2 = "dall-e-2",
     DALLE_3 = "dall-e-3"
 }
 
+/*
+    An enumerator for the style of the generated images.
+    natural: causes the model to produce more natural, less hyper-real looking images.
+    vivid: causes the model to lean towards generating hyper-real and dramatic images.
+ */
 export enum styleEnum {
     NATURAL = "natural",
     VIVID = "vivid"
 }
 
-export enum qualityEnum {
+/*
+    An enumerator for the quality of the image that will be generated.
+    standard: creates images with finer details and greater consistency across the image.
+    hd: creates images with finer details and greater consistency across the image.
+ */
+export enum qualityEnum { 
     STANDARD = "standard",
     HD = "hd"
 }
 
+/*
+    An enumerator for the format in which the generated images are returned.
+    url: The format in which the generated images are returned.
+    b64_json: The format in which the generated images are returned.
+ */
 export enum responseFormatEnum {
     URL = "url",
     B64_JSON = "b64_json"
 }
 
+/*
+    An enumerator for the size of the generated images.
+    256x256: The size of the generated images.
+    512x512: The size of the generated images.
+    1024x1024: The size of the generated images.
+    1792x1024: The size of the generated images.
+    1024x1792: The size of the generated images.
+ */
 export enum sizeEnum {
     _256x256 = "256x256",
     _512x512 = "512x512",
@@ -31,31 +59,45 @@ export enum sizeEnum {
 }
 
 /*
-New parameters:
-model (‘dall-e-2’ or ‘dall-e-3’): This is the model you’re generating with. Be careful to set it to ‘dall-e-3’ as it defaults to ‘dall-e-2’ if empty.
-style (‘natural’ or ‘vivid’): The style of the generated images. Must be one of vivid or natural. Vivid causes the model to lean towards generating hyper-real and dramatic images. Natural causes the model to produce more natural, less hyper-real looking images. Defaults to ‘vivid’.
-quality (‘standard’ or ‘hd’): The quality of the image that will be generated. ‘hd’ creates images with finer details and greater consistency across the image. Defaults to ‘standard’.
+    An interface for the Dall-E API Wrapper.
 
-Other parameters:
-prompt (str): A text description of the desired image(s). The maximum length is 1000 characters. Required field.
-n (int): The number of images to generate. Must be between 1 and 10. Defaults to 1. For dall-e-3, only n=1 is supported.
-size (...): The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024 for DALL·E-2 models. Must be one of 1024x1024, 1792x1024, or 1024x1792 for DALL·E-3 models.
-response_format ('url' or 'b64_json'): The format in which the generated images are returned. Must be one of "url" or "b64_json". Defaults to "url".
-user (str): A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse. Learn more.
+    New parameters:
+    model (‘dall-e-2’ or ‘dall-e-3’): This is the model you’re generating with. Be careful to set it to ‘dall-e-3’ as it defaults to ‘dall-e-2’ if empty.
+    style (‘natural’ or ‘vivid’): The style of the generated images. Must be one of vivid or natural. Vivid causes the model to lean towards generating hyper-real and dramatic images. Natural causes the model to produce more natural, less hyper-real looking images. Defaults to ‘vivid’.
+    quality (‘standard’ or ‘hd’): The quality of the image that will be generated. ‘hd’ creates images with finer details and greater consistency across the image. Defaults to ‘standard’.
+
+    Other parameters:
+    prompt (str): A text description of the desired image(s). The maximum length is 1000 characters. Required field.
+    n (int): The number of images to generate. Must be between 1 and 10. Defaults to 1. For dall-e-3, only n=1 is supported.
+    size (...): The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024 for DALL·E-2 models. Must be one of 1024x1024, 1792x1024, or 1024x1792 for DALL·E-3 models.
+    response_format ('url' or 'b64_json'): The format in which the generated images are returned. Must be one of "url" or "b64_json". Defaults to "url".
+    user (str): A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse. Learn more.
 */
 export interface DalleApiWrapperParams extends ToolParams {
     openAIApiKey?: string;
+
     model?: DalleApiWrapperModelEnum;
+
     style?: styleEnum;
+
     quality?: qualityEnum;
+
     prompt: string;
+
     n?: number;
+
     size?: sizeEnum;
+
     response_format?: responseFormatEnum;
+
     user?: string;
+
     organization?: string;
 }
 
+/**
+ * A tool for generating images with Open AIs Dall-E 2 or 3 API.
+ */
 export class DalleApiWrapper extends Tool {
     static lc_name() {
         return "DalleApiWrapper";
@@ -106,5 +148,4 @@ export class DalleApiWrapper extends Tool {
         
         return urls[0];
     }
-    
 }
