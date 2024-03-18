e8ecaca00d207d39c9b2f82b903bd506fb242d75
easwee <easwee@gmx.com>
2024-03-18 21:53:18 +0100

    Fix Serializable extension

--- libs/langchain-community/src/graphs/graph_document.ts
+++ libs/langchain-community/src/graphs/graph_document.ts
@@ -1,27 +1,32 @@
 import { Serializable } from "@langchain/core/load/serializable";
 import { Document } from "@langchain/core/documents";
 
-export class Node implements Serializable {
+export class Node extends Serializable {
     id: string | number;
     type: string;
     properties: Record<string, any>;
+
+    lc_namespace = ["langchain", "graph", "document_node"];
 
     constructor(
         id: string | number,
         type: string = "Node",
         properties: Record<string, any> = {}
     ) {
+        super()
         this.id = id;
         this.type = type;
         this.properties = properties;
     }
 }
 
-export class Relationship implements Serializable {
+export class Relationship extends Serializable {
     source: Node;
     target: Node;
     type: string;
     properties: Record<string, any>;
+
+    lc_namespace = ["langchain", "graph", "document_relationship"];
 
     constructor(
         source: Node, 
@@ -29,6 +34,7 @@ export class Relationship implements Serializable {
         type: string, 
         properties: Record<string, any> = {}
     ) {
+        super()
         this.source = source;
         this.target = target;
         this.type = type;
@@ -36,16 +42,19 @@ export class Relationship implements Serializable {
     }
 }
 
-export class GraphDocument implements Serializable {
+export class GraphDocument extends Serializable {
     nodes: Node[];
     relationships: Relationship[];
     source: Document;
+
+    lc_namespace = ["langchain", "graph", "document_node"];
 
     constructor(
         nodes: Node[],
         relationships: Relationship[],
         source: Document
     ) {
+        super()
         this.nodes = nodes;
         this.relationships = relationships;
         this.source = source;
