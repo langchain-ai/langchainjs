ac460d22815c19b9cd2b2d4e0fc34a50df374fe8
bracesproul <braceasproul@gmail.com>
2024-03-19 16:15:50 -0700

    docs[minor]: Add model selection tabs component

---
+++ docs/core_docs/src/theme/ChatModelTabs.js
@@ -0,0 +1,111 @@
+import React from "react";
+import Tabs from "@theme/Tabs";
+import TabItem from "@theme/TabItem";
+import CodeBlock from "@theme-original/CodeBlock";
+import Npm2Yarn from "@theme/Npm2Yarn";
+import Admonition from '@theme/Admonition';
+
+function InstallationInfo({ children }) {
+  return (
+    <>
+    <Admonition type="tip">
+      <p>
+        See <a href="/docs/get_started/installation#installing-integration-packages">this section for general instructions on installing integration packages</a>.
+      </p>
+    </Admonition>
+    <Npm2Yarn>
+      {children}
+    </Npm2Yarn>
+    </>
+  )
+}
+
+/**
+ * @param {{ openaiParams?: string, anthropicParams?: string, fireworksParams?: string, mistralParams?: string, hideOpenai?: boolean, hideAnthropic?: boolean, hideFireworks?: boolean, hideMistral?: boolean }} props 
+ */
+export default function ChatModelTabs(props) {
+  // OpenAI
+  const openAIText = `import { ChatOpenAI } from "@langchain/openai";
+  
+const model = new ChatOpenAI(${props.openaiParams ?? "{}"});`;
+  const openAIEnvText = `OPENAI_API_KEY=your-api-key`
+  const openAIProps = { value: "OpenAI", label: "OpenAI", default: true };
+
+  // Anthropic
+  const anthropicText = `import { ChatAnthropic } from "@langchain/anthropic";
+  
+const model = new ChatAnthropic(${props.anthropicParams ?? "{}"});`
+  const anthropicEnvText = `ANTHROPIC_API_KEY=your-api-key`
+  const anthropicProps = { value: "Anthropic", label: "Anthropic" };
+
+  // FireworksAI
+  const fireworksText = `import { ChatFireworks } from "@langchain/community/chat_models/fireworks";
+  
+const model = new ChatFireworks(${props.fireworksParams ?? "{}"});`;
+  const fireworksEnvText = `FIREWORKS_API_KEY=your-api-key`
+  const fireworksProps = { value: "FireworksAI", label: "FireworksAI" };
+
+  // MistralAI
+  const mistralText = `import { ChatMistralAI } from "@langchain/mistralai";
+  
+const model = new ChatMistralAI(${props.mistralParams ?? "{}"});`;
+  const mistralEnvText = `MISTRAL_API_KEY=your-api-key`
+  const mistralProps = { value: "MistralAI", label: "MistralAI" };
+
+  return (
+    <Tabs groupId="modelTabs">
+      {props.hideOpenai ? null : (
+        <TabItem {...openAIProps}>
+          <InstallationInfo>
+            @langchain/openai
+          </InstallationInfo>
+          <CodeBlock language="bash">
+              {openAIEnvText}
+          </CodeBlock>
+          <CodeBlock language="typescript">
+              {openAIText}
+          </CodeBlock>
+        </TabItem>
+      )}
+      {props.hideAnthropic ? null : (
+        <TabItem {...anthropicProps}>
+          <InstallationInfo>
+            @langchain/anthropic
+          </InstallationInfo>
+          <CodeBlock language="bash">
+              {anthropicEnvText}
+          </CodeBlock>
+          <CodeBlock language="typescript">
+            {anthropicText}
+          </CodeBlock>
+        </TabItem>
+      )}
+      {props.hideFireworks ? null : (
+        <TabItem {...fireworksProps}>
+          <InstallationInfo>
+            @langchain/community
+          </InstallationInfo>
+          <CodeBlock language="bash">
+              {fireworksEnvText}
+          </CodeBlock>
+          <CodeBlock language="typescript">
+            {fireworksText}
+          </CodeBlock>
+        </TabItem>
+      )}
+      {props.hideMistral ? null : (
+        <TabItem {...mistralProps}>
+          <InstallationInfo>
+            @langchain/mistralai
+          </InstallationInfo>
+          <CodeBlock language="bash">
+              {mistralEnvText}
+          </CodeBlock>
+          <CodeBlock language="typescript">
+              {mistralText}
+          </CodeBlock>
+        </TabItem>
+      )}
+    </Tabs>
+  );
+}
