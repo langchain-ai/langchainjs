a3d9ed3da27948e666a4b89a25fd0b6a82df3404
Xingqi Cheng <xingqi.cheng@sap.com>
2024-03-19 12:24:16 +0000

    update documentations

--- examples/src/indexes/vector_stores/hana_vector/basics.ts
+++ examples/src/indexes/vector_stores/hana_vector/basics.ts
@@ -1,34 +1,38 @@
-import hanaClient from "@sap/hana-client";
 import { OpenAIEmbeddings } from "@langchain/openai";
+import hanaClient from "@sap/hana-client";
 import { setTimeout } from "timers/promises";
-import { HanaDB, HanaDBArgs} from "@langchain/community/vectorstores/hanavector";
-import { setTimeout } from "timers/promises";
+import { Document } from "@langchain/core/documents";
+import {
+  HanaDB,
+  HanaDBArgs,
+} from "@langchain/community/vectorstores/hanavector";
+
 const connectionParams = {
-    host: process.env.HANA_HOST,
-    port: process.env.HANA_PORT,
-    uid: process.env.HANA_UID,
-    pwd: process.env.HANA_PWD,
+  host: process.env.HANA_HOST,
+  port: process.env.HANA_PORT,
+  uid: process.env.HANA_UID,
+  pwd: process.env.HANA_PWD,
 };
 const embeddings = new OpenAIEmbeddings();
-//connet to hanaDB
+// connet to hanaDB
 const client = hanaClient.createConnection();
 client.connect(connectionParams);
 // define instance args
 const args: HanaDBArgs = {
-connection: client,
-tableName: "testBasics",
+  connection: client,
+  tableName: "testBasics",
 };
 
-//Add documents with metadata.
+// Add documents with metadata.
 const docs: Document[] = [
-    {
-        pageContent: "foo",
-        metadata: { start: 100, end: 150, docName: "foo.txt", quality: "bad" },
-    },
-    {
-        pageContent: "bar",
-        metadata: { start: 200, end: 250, docName: "bar.txt", quality: "good" },
-    },
+  {
+    pageContent: "foo",
+    metadata: { start: 100, end: 150, docName: "foo.txt", quality: "bad" },
+  },
+  {
+    pageContent: "bar",
+    metadata: { start: 200, end: 250, docName: "bar.txt", quality: "good" },
+  },
 ];
 
 // Create a LangChain VectorStore interface for the HANA database and specify the table (collection) to use in args.
@@ -36,15 +40,15 @@ const vectorStore = new HanaDB(embeddings, args);
 // sleep 5 seconds to make sure the table is created.
 await setTimeout(5000);
 // Delete already existing documents from the table
-await vectorStore.delete({filter : {}})
+await vectorStore.delete({ filter: {} });
 await vectorStore.addDocuments(docs);
 // sleep 5 seconds to make sure the documents are indexed.
 await setTimeout(5000);
 // Query documents with specific metadata.
-const filter = {"quality": "bad"};
-const query = "foobar"
+const filterF = { quality: "bad" };
+const query = "foobar";
 // With filtering on {"quality": "bad"}, only one document should be returned
-const results = await vectorStore.similaritySearch(query, 1, filter)
+const results = await vectorStore.similaritySearch(query, 1, filter);
 console.log(results);
 /*
     [  {
@@ -54,9 +58,9 @@ console.log(results);
     ]
 */
 // Delete documents with specific metadata.
-await vectorStore.delete({filter : filter});
+await vectorStore.delete({ filter: filterF });
 // Now the similarity search with the same filter will return no results
-const results1 = await vectorStore.similaritySearch(query, 1, filter)
+const results1 = await vectorStore.similaritySearch(query, 1, filter);
 console.log(results1);
 /*
     []
--- examples/src/indexes/vector_stores/hana_vector/fromTexts.ts
+++ examples/src/indexes/vector_stores/hana_vector/fromTexts.ts
@@ -1,33 +1,38 @@
-import hanaClient from "@sap/hana-client";
 import { OpenAIEmbeddings } from "@langchain/openai";
+import hanaClient from "@sap/hana-client";
 import { setTimeout } from "timers/promises";
-import { HanaDB, HanaDBArgs} from "@langchain/community/vectorstores/hanavector";
+import {
+  HanaDB,
+  HanaDBArgs,
+} from "@langchain/community/vectorstores/hanavector";
 
 const connectionParams = {
-    host: process.env.HANA_HOST,
-    port: process.env.HANA_PORT,
-    uid: process.env.HANA_UID,
-    pwd: process.env.HANA_PWD,
+  host: process.env.HANA_HOST,
+  port: process.env.HANA_PORT,
+  uid: process.env.HANA_UID,
+  pwd: process.env.HANA_PWD,
 };
 const embeddings = new OpenAIEmbeddings();
-//connet to hanaDB
+// connet to hanaDB
 const client = hanaClient.createConnection();
 client.connect(connectionParams);
 // define instance args
 const args: HanaDBArgs = {
-connection: client,
-tableName: "test",
+  connection: client,
+  tableName: "test_fromTexts",
 };
+// This function will create a table "test_fromTexts" if not exist, if exists,
+// then the value will be appended to the table.
 const vectorStore = await HanaDB.fromTexts(
-    ["Bye bye", "Hello world", "hello nice world"],
-    [
-      { id: 2, name: "2" },
-      { id: 1, name: "1" },
-      { id: 3, name: "3" },
-    ],
-    embeddings,
-    args
-    );
+  ["Bye bye", "Hello world", "hello nice world"],
+  [
+    { id: 2, name: "2" },
+    { id: 1, name: "1" },
+    { id: 3, name: "3" },
+  ],
+  embeddings,
+  args
+);
 
 // sleep 5 seconds to make sure the documents are indexed.
 await setTimeout(5000);
@@ -36,9 +41,9 @@ const response = await vectorStore.similaritySearch("hello world", 2);
 
 console.log(response);
 
-/*
-[
-  { pageContent: 'Hello world', metadata: { id: 1, name: '1' } },
-  { pageContent: 'hello nice world', metadata: { id: 3, name: '3' } }
-]
+/* This result is based on no table "test_fromTexts" existing in the database.
+  [
+    { pageContent: 'Hello world', metadata: { id: 1, name: '1' } },
+    { pageContent: 'hello nice world', metadata: { id: 3, name: '3' } }
+  ]
 */
