d688ac9f93c9a901b7d9c711bd02638b508d271f
afirstenberg <github@addventure.com>
2024-03-23 13:47:14 -0400

    Fixes for testing against AI Studio, since it is more strict with safety settings

--- libs/langchain-google-webauth/src/tests/chat_models.int.test.ts
+++ libs/langchain-google-webauth/src/tests/chat_models.int.test.ts
@@ -11,13 +11,14 @@ import {
 } from "@langchain/core/messages";
 import { BaseLanguageModelInput } from "@langchain/core/language_models/base";
 import { ChatPromptValue } from "@langchain/core/prompt_values";
+import { GoogleAISafetySetting } from "@langchain/google-common";
 import { ChatGoogle } from "../chat_models.js";
 
 describe("Google APIKey Chat", () => {
   test("invoke", async () => {
     const model = new ChatGoogle();
     try {
-      const res = await model.invoke("What is 1 + 1?");
+      const res = await model.invoke("What is the answer to life the universe and everything? Answer briefly.");
       expect(res).toBeDefined();
       expect(res._getType()).toEqual("ai");
 
@@ -32,7 +33,7 @@ describe("Google APIKey Chat", () => {
 
       const textContent = content as MessageContentText;
       expect(textContent.text).toBeDefined();
-      expect(textContent.text).toEqual("2");
+      expect(textContent.text).toEqual("42");
     } catch (e) {
       console.error(e);
       throw e;
@@ -40,15 +41,33 @@ describe("Google APIKey Chat", () => {
   });
 
   test("generate", async () => {
-    const model = new ChatGoogle();
+    const safetySettings: GoogleAISafetySetting[] = [
+      {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+      {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+      {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+      {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+    ];
+    const model = new ChatGoogle({safetySettings});
     try {
       const messages: BaseMessage[] = [
         new SystemMessage(
-          "You will reply to all requests to flip a coin with either H, indicating heads, or T, indicating tails."
+          "You will reply to all requests to toss a coin with either H, indicating heads, or T, indicating tails."
         ),
-        new HumanMessage("Flip it"),
+        new HumanMessage("Toss the coin"),
         new AIMessage("T"),
-        new HumanMessage("Flip the coin again"),
+        new HumanMessage("Toss the coin again"),
       ];
       const res = await model.predictMessages(messages);
       expect(res).toBeDefined();
@@ -67,19 +86,37 @@ describe("Google APIKey Chat", () => {
       expect(textContent.text).toBeDefined();
       expect(["H", "T"]).toContainEqual(textContent.text);
     } catch (e) {
-      console.error(e);
+      console.error(JSON.stringify(e,null,1));
       throw e;
     }
   });
 
   test("stream", async () => {
-    const model = new ChatGoogle();
+    const safetySettings: GoogleAISafetySetting[] = [
+      {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+      {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+      {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+      {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_ONLY_HIGH"
+      },
+    ];
+    const model = new ChatGoogle({safetySettings});
     try {
       const input: BaseLanguageModelInput = new ChatPromptValue([
         new SystemMessage(
           "You will reply to all requests to flip a coin with either H, indicating heads, or T, indicating tails."
         ),
-        new HumanMessage("Flip it"),
+        new HumanMessage("Flip the coin"),
         new AIMessage("T"),
         new HumanMessage("Flip the coin again"),
       ]);
@@ -99,7 +136,7 @@ describe("Google APIKey Chat", () => {
 
       console.log(JSON.stringify(resArray, null, 2));
     } catch (e) {
-      console.error(e);
+      console.error(JSON.stringify(e,null,1));
       throw e;
     }
   });
