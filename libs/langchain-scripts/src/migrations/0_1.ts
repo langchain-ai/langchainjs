f603b4de82416a28fbebe5fdef6ec263e44c5f5f
Tomoaki Imai <tomoima525@gmail.com>
2024-04-05 15:51:14 -0700

    Some symbols have empty declarations

--- libs/langchain-scripts/src/migrations/0_1.ts
+++ libs/langchain-scripts/src/migrations/0_1.ts
@@ -37,7 +37,9 @@ async function getEntrypointsFromFile(
       path.join(packagePath, "src", `${value}.ts`)
     );
     const exportedSymbolsMap = newFile.getExportedDeclarations();
-    const exportedSymbols = Array.from(exportedSymbolsMap.entries()).map(
+    const exportedSymbols = Array.from(exportedSymbolsMap.entries())
+      .filter(([_, declarations]) => declarations.length > 0)
+      .map(
       ([symbol, declarations]) => ({
         kind: declarations[0].getKind(),
         symbol,
