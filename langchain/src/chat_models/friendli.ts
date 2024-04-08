0ef9b5cf43a05eb5f7770694e1107144990971b5
Seungha Jeon <seungha@friendli.ai>
2024-04-08 14:55:52 +0900

    add configurations

---
+++ langchain/src/chat_models/friendli.ts
@@ -0,0 +1,6 @@
+import { logVersion010MigrationWarning } from "../util/entrypoint_deprecation.js";
+
+/* #__PURE__ */ logVersion010MigrationWarning({
+  oldEntrypointName: "chat_models/friendli",
+});
+export * from "@langchain/community/chat_models/friendli";
---
+++ langchain/src/llms/friendli.ts
@@ -0,0 +1,6 @@
+import { logVersion010MigrationWarning } from "../util/entrypoint_deprecation.js";
+
+/* #__PURE__ */ logVersion010MigrationWarning({
+  oldEntrypointName: "llms/friendli",
+});
+export * from "@langchain/community/llms/friendli";
