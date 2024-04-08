291d6b72be478b3f89e6b0fd4f6b1c427c83ad7e
bracesproul <braceasproul@gmail.com>
2024-04-08 16:06:02 -0700

    chore: lint files

--- libs/langchain-community/src/vectorstores/tests/zep.test.ts
+++ libs/langchain-community/src/vectorstores/tests/zep.test.ts
@@ -177,7 +177,7 @@ describe("ZepVectorStore", () => {
     const originalFromDocuments = ZepVectorStore.fromDocuments;
     ZepVectorStore.fromDocuments = jest.fn(
       async (docs, embeddings, zepConfig) => {
-        const zepVectorStore = await originalFromDocuments.invoke(
+        const zepVectorStore = await originalFromDocuments.call(
           ZepVectorStore,
           docs as Document[],
           embeddings as EmbeddingsInterface,
@@ -189,7 +189,7 @@ describe("ZepVectorStore", () => {
     );
 
     const zepVectorStore = await ZepVectorStore.fromTexts(
-      texts,.
+      texts,
       metadatas,
       embeddings,
       zepConfig
