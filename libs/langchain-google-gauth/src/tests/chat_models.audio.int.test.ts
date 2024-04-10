1be1fd3b170718ab765d8dc3fc6b124c48253d02
bracesproul <braceasproul@gmail.com>
2024-04-10 12:46:15 -0700

    chore: lint files

--- libs/langchain-google-gauth/src/tests/chat_models.audio.int.test.ts
+++ libs/langchain-google-gauth/src/tests/chat_models.audio.int.test.ts
@@ -1,5 +1,8 @@
 import fs from "fs";
-import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
+import {
+  ChatPromptTemplate,
+  MessagesPlaceholder,
+} from "@langchain/core/prompts";
 import { HumanMessage } from "@langchain/core/messages";
 import { ChatGoogle } from "../chat_models.js";
 
@@ -18,20 +21,25 @@ test("Gemini can understand audio", async () => {
   const audioPath = "./src/tests/data/audio.mp3";
   const audioBase64 = convertMp3ToBase64(audioPath);
 
-  const prompt = ChatPromptTemplate.fromMessages([new MessagesPlaceholder("audio")]);
+  const prompt = ChatPromptTemplate.fromMessages([
+    new MessagesPlaceholder("audio"),
+  ]);
 
   const chain = prompt.pipe(model);
   const response = await chain.invoke({
     audio: new HumanMessage({
-      content: [{
-        type: "audio",
-        data: {
-          url: `data:audio/mp3;base64,${audioBase64}`
+      content: [
+        {
+          type: "audio",
+          data: {
+            url: `data:audio/mp3;base64,${audioBase64}`,
+          },
         },
-      }, {
-        type: "text",
-        text: "Summarize this audio. Be very concise.",
-      }]
+        {
+          type: "text",
+          text: "Summarize this audio. Be very concise.",
+        },
+      ],
     }),
   });
   console.log("response", response);
