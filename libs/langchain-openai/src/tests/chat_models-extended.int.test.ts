1af75d9af21f08893af674e0e1209a465db398ee
jacoblee93 <jacoblee93@gmail.com>
2024-04-09 20:14:33 -0700

    Lint

--- libs/langchain-openai/src/tests/chat_models-extended.int.test.ts
+++ libs/langchain-openai/src/tests/chat_models-extended.int.test.ts
@@ -64,14 +64,14 @@ test("Test ChatOpenAI tool calling", async () => {
   ]);
   console.log(JSON.stringify(res));
   expect(res.additional_kwargs.tool_calls?.length).toEqual(3);
-  expect(res.tool_calls[0].args).toEqual(
-    JSON.parse(res.additional_kwargs.tool_calls[0].function.arguments)
+  expect(res.tool_calls?.[0].args).toEqual(
+    JSON.parse(res.additional_kwargs.tool_calls?.[0].function.arguments)
   );
-  expect(res.tool_calls[1].args).toEqual(
-    JSON.parse(res.additional_kwargs.tool_calls[1].function.arguments)
+  expect(res.tool_calls?.[1].args).toEqual(
+    JSON.parse(res.additional_kwargs.tool_calls?.[1].function.arguments)
   );
-  expect(res.tool_calls[2].args).toEqual(
-    JSON.parse(res.additional_kwargs.tool_calls[2].function.arguments)
+  expect(res.tool_calls?.[2].args).toEqual(
+    JSON.parse(res.additional_kwargs.tool_calls?.[2].function.arguments)
   );
 });
 
