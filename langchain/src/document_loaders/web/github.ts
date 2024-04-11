6dbd961f5194b1d77a7131208f887bd1799690d5
Alexander Claus <alexander.claus@sap.com>
2024-04-11 10:11:48 +0200

    fixed linting issue: no parameter reassign

--- langchain/src/document_loaders/web/github.ts
+++ langchain/src/document_loaders/web/github.ts
@@ -312,9 +312,10 @@ export class GithubRepoLoader
   private async parseGitmodules(
     gitmodulesContent: string
   ): Promise<SubmoduleInfo[]> {
+    let validGitmodulesContent = gitmodulesContent;
     // in case the .gitmodules file does not end with a newline, we add one to make the regex work
-    if (!gitmodulesContent.endsWith("\n")) {
-      gitmodulesContent += "\n";
+    if (!validGitmodulesContent.endsWith("\n")) {
+      validGitmodulesContent += "\n";
     }
     // catches the initial line of submodule entries
     const submodulePattern = /\[submodule "(.*?)"]\n((\s+.*?\s*=\s*.*?\n)*)/g;
@@ -322,7 +323,7 @@ export class GithubRepoLoader
     const keyValuePattern = /\s+(.*?)\s*=\s*(.*?)\s/g;
 
     const submoduleInfos = [];
-    for (const [, name, propertyLines] of gitmodulesContent.matchAll(
+    for (const [, name, propertyLines] of validGitmodulesContent.matchAll(
       submodulePattern
     )) {
       if (!name || !propertyLines) {
