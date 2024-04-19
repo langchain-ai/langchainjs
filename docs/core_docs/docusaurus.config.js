ae455b1bc88228fa7a6678986e4fb8a18a589ccc
bracesproul <braceasproul@gmail.com>
2024-04-19 13:33:26 -0700

    trying something

--- docs/core_docs/docusaurus.config.js
+++ docs/core_docs/docusaurus.config.js
@@ -75,7 +75,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          showLastUpdateTime: true,
+          // showLastUpdateTime: true,
           sidebarPath: require.resolve("./sidebars.js"),
           remarkPlugins: [
             [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
