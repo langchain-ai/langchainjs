9b0516c96527bc34afd973dd9bfeb14c80820f2b
bracesproul <braceasproul@gmail.com>
2024-04-19 14:58:41 -0700

    custom component to handle it

--- docs/core_docs/src/theme/DocItem/Content/index.js
+++ docs/core_docs/src/theme/DocItem/Content/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Content from "@theme-original/DocItem/Content";
 import Feedback from "../../Feedback";
+import LastUpdatedComponent from "../../LastUpdatedComponent";
 
 export default function ContentWrapper(props) {
   return (
@@ -8,9 +9,7 @@ export default function ContentWrapper(props) {
       {/* eslint-disable react/jsx-props-no-spreading */}
       <Content {...props} />
       <Feedback />
-      <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <p style={{ fontSize: 13.3333 }}><i>Last updated on <strong>Jan 1, 2024</strong></i></p>
-      </div>
+      <LastUpdatedComponent />
     </>
   );
 }
--- libs/langchain-scripts/src/docs_last_updated.ts
+++
@@ -1,6 +0,0 @@
-import { glob } from 'glob';
-
-function updateDocsLastUpdated(pathToDocs: string) {
-  const allIpynbFiles = glob.sync(`${pathToDocs}/**/*.ipynb`);
-  // Iterate over all, get the last modified date, and ensure the date in docs matches this.
-}
