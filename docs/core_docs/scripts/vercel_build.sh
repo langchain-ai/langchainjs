36a38c176f4771a0e1bde48394708138c9ccba77
jacoblee93 <jacoblee93@gmail.com>
2024-04-22 10:54:51 -0700

    Attempt to fix docs build

--- docs/core_docs/scripts/vercel_build.sh
+++ docs/core_docs/scripts/vercel_build.sh
@@ -5,6 +5,15 @@ yum -y update
 yum install wget tar -y
 # amazon-linux-extras install python3.8 -y
 
+# Install pip for Python 3.11
+python3.11 -m ensurepip
+
+# Upgrade pip to the latest version
+python3.11 -m pip install --upgrade pip
+
+# Install setuptools using pip
+python3.11 -m pip install setuptools distutils
+
 # install quarto
 wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.450/quarto-1.3.450-linux-amd64.tar.gz
 tar -xzf quarto-1.3.450-linux-amd64.tar.gz
