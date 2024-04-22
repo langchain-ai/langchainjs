81abb045ba31e0ce55f448df59b1144dcb666c6d
jacoblee93 <jacoblee93@gmail.com>
2024-04-22 11:30:08 -0700

    Bump HNSWLib and node-gyp deps

--- docs/core_docs/scripts/vercel_build.sh
+++ docs/core_docs/scripts/vercel_build.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-yum -y update
-# yum install gcc bzip2-devel libffi-devel zlib-devel wget tar gzip -y
-yum install wget tar -y
-# amazon-linux-extras install python3.8 -y
-
-# Install pip for Python 3.11
-python3.11 -m ensurepip
-
-# Upgrade pip to the latest version
-python3.11 -m pip install --upgrade pip
-
-# Install setuptools using pip
-python3.11 -m pip install setuptools distutils
-
 # install quarto
 wget -q https://github.com/quarto-dev/quarto-cli/releases/download/v1.3.450/quarto-1.3.450-linux-amd64.tar.gz
 tar -xzf quarto-1.3.450-linux-amd64.tar.gz
