b60e91c77033d16f2c2c3bf821310b0b39299d11
bracesproul <braceasproul@gmail.com>
2024-04-26 12:03:48 -0700

    chore: lint files

--- langchain/src/chains/llm_chain.ts
+++ langchain/src/chains/llm_chain.ts
@@ -74,7 +74,7 @@ function _getLanguageModel(llmLike: RunnableInterface): BaseLanguageModel {
 /**
  * @deprecated This class will be removed in 0.3.0. Use the LangChain Expression Language (LCEL) instead.
  * See the example below for how to use LCEL with the LLMChain class:
- * 
+ *
  * Chain to run queries against LLMs.
  *
  * @example
