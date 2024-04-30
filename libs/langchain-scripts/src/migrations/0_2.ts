08fae65ac72e9d96e0fe796d6046d56ab1c54f35
bracesproul <braceasproul@gmail.com>
2024-04-30 14:04:28 -0700

    updated & jsdoc

--- libs/langchain-scripts/src/migrations/0_2.ts
+++ libs/langchain-scripts/src/migrations/0_2.ts
@@ -1,7 +1,31 @@
 import { glob } from "glob";
 import { Project } from "ts-morph";
-import fs from "fs";
-import path from "path";
+import fs from "node:fs";
+import path from "node:path";
+
+type MigrationUpdate = {
+  /**
+   * The path of the file which was updated.
+   */
+  path: string;
+  /**
+   * The original import statement.
+   */
+  oldImport: string;
+  /**
+   * The updated import statement.
+   */
+  updatedImport: string;
+};
+
+type DeprecatedEntrypoint = { old: string; new: string, symbol: string | null }
+
+function matchOldEntrypoints(oldEntrypoint: string, newEntrypoint: string) {
+  if (oldEntrypoint.endsWith("*")) {
+    return newEntrypoint.startsWith(oldEntrypoint.replace("/*", ""));
+  }
+  return oldEntrypoint === newEntrypoint;
+}
 
 export interface UpdateLangChainFields {
   /**
@@ -29,49 +53,33 @@ export interface UpdateLangChainFields {
   shouldLog?: boolean;
 }
 
-type MigrationUpdate = {
-  /**
-   * The path of the file which was updated.
-   */
-  path: string;
-  /**
-   * The original import statement.
-   */
-  oldImport: string;
-  /**
-   * The updated import statement.
-   */
-  updatedImport: string;
-};
-
-type DeprecatedEntrypoint = { old: string; new: string, symbol: string | null }
-
-function matchOldEntrypoints(oldEntrypoint: string, newEntrypoint: string) {
-  if (oldEntrypoint.endsWith("*")) {
-    return newEntrypoint.startsWith(oldEntrypoint.replace("/*", ""));
-  }
-  return oldEntrypoint === newEntrypoint;
-}
-
 /**
- * Edge cases to cover:
- * double exports
- * - tools - DONE
- * - memory - DONE
- * - schema - DONE
- * - agents (toolkits?) - DONE
- * - stores
- * - llms/fake
- * - prompts/load
- * - retrievers/remote
- * Moving all the @langchain/community/retrievers/self_query/x to @langchain/community/structured_query/x
+ * Migrates a project's LangChain imports from version 0.0.x or 0.1.x to 0.2.x.
+ * This function updates the import statements in the specified project files
+ * based on the provided import map.
+ *
+ * @param {UpdateLangChainFields} fields - The configuration object for the migration.
+ * @param {string} [fields.projectPath] - The path to the project to check. TypeScript files will be extracted
+ *   via this glob pattern: `${projectPath}/{star-star}/{star}.{ts,tsx,js,jsx}`. Not required if `files` or `tsConfigPath` is provided.
+ * @param {string[]} [fields.files] - A list of .ts file paths to check. Not required if `projectPath` or `tsConfigPath` is provided.
+ * @param {string} [fields.tsConfigPath] - Path to the tsConfig file. This will be used to load
+ *   all the project files into the script. Not required if `projectPath` or `files` is provided.
+ * @param {boolean} [fields.shouldLog=false] - Whether or not to log a message when an import is updated.
+ * @returns {Promise<Array<MigrationUpdate> | null>} - A promise that resolves to an array of migration updates if successful, or null if an error occurs.
+ * @throws {Error} - If more than one of `projectPath`, `tsConfigPath`, or `files` is provided, or if none of them are provided.
  * 
+ * @example
+ * ```typescript
+ * import { updateEntrypointsFrom0_x_xTo0_2_x } from "@langchain/scripts/migrations";
  * 
+ * const pathToMyProject = "...";
  * 
- * importMap now contains `symbol: string | null`
- * if import map old path ends with `/*` then it can be any path after
+ * updateEntrypointsFrom0_x_xTo0_2_x({
+ *   projectPath: pathToMyProject,
+ *   shouldLog: true,
+ * });
+ * ```
  */
-
 export async function updateEntrypointsFrom0_x_xTo0_2_x(
   fields: UpdateLangChainFields
 ): Promise<Array<MigrationUpdate> | null> {
@@ -96,7 +104,7 @@ export async function updateEntrypointsFrom0_x_xTo0_2_x(
   }
 
   const importMap: Array<DeprecatedEntrypoint> = JSON.parse(
-    fs.readFileSync("importMap_new.json", "utf-8")
+    fs.readFileSync("importMap.json", "utf-8")
   );
 
   let projectFiles: string[] | null = null;
@@ -138,6 +146,7 @@ export async function updateEntrypointsFrom0_x_xTo0_2_x(
           // no-op
           return;
         }
+
         // if deprecatedEntrypoint.symbol is NOT null then verify the named imports match
         if (deprecatedEntrypoint.symbol) {
           const namedImports = importDeclaration.getNamedImports();
