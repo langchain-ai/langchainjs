afd4c52b000b1efb56ad24d363cb242f4cf938dc
guyifeng <santree122@gmail.com>
2024-04-30 17:55:01 +0800

    fix: document-loaders[patch] #5253 show full message when S3Loader.load get error

--- langchain/src/document_loaders/web/s3.ts
+++ langchain/src/document_loaders/web/s3.ts
@@ -159,9 +159,10 @@ export class S3Loader extends BaseDocumentLoader {
       const docs = await unstructuredLoader.load();
 
       return docs;
-    } catch {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
       throw new Error(
-        `Failed to load file ${filePath} using unstructured loader.`
+        `Failed to load file ${filePath} using unstructured loader : ${e.message}`
       );
     }
   }
