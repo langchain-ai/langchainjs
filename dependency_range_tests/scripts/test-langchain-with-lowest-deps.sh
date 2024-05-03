bb3d20f3b8e4cde5b877fd248df3739c741ded18
bracesproul <braceasproul@gmail.com>
2024-05-03 12:01:57 -0700

    cr

--- dependency_range_tests/scripts/test-langchain-with-lowest-deps.sh
+++ dependency_range_tests/scripts/test-langchain-with-lowest-deps.sh
@@ -23,7 +23,9 @@ node /updater_script/update_resolutions_lowest.js
 
 yarn
 
-cd ../langchain-core && yarn build
+pwd
+
+ls -l
 
 # Check the test command completes successfully
 NODE_OPTIONS=--experimental-vm-modules yarn run jest --testPathIgnorePatterns=\\.int\\.test.ts --testTimeout 30000 --maxWorkers=50%
