4b8e66a459b90eba0ef209ff82f0cd93747fd580
bracesproul <braceasproul@gmail.com>
2024-05-03 11:44:42 -0700

    drop bad

--- dependency_range_tests/scripts/test-langchain-with-latest-deps.sh
+++ dependency_range_tests/scripts/test-langchain-with-latest-deps.sh
@@ -10,8 +10,6 @@ shopt -s extglob
 # avoid copying build artifacts from the host
 cp -r ../langchain/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
 
-yarn build --filter=@langchain/cohere
-
 yarn
 
 # Check the test command completes successfully
