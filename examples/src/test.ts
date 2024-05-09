060b67998d833e5b54c9a66501365435b012a491
bracesproul <braceasproul@gmail.com>
2024-05-09 13:47:36 -0700

    promps

---
+++ examples/src/test.ts
@@ -0,0 +1,25 @@
+import { RunnableSequence } from "@langchain/core/runnables";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatAnthropic } from "@langchain/anthropic";
+
+const model = new ChatAnthropic({
+  temperature: 0,
+})
+
+const prompt = ChatPromptTemplate.fromTemplate("tell me a joke about {topic}")
+
+const chain = prompt.pipe(model).pipe(new StringOutputParser())
+
+const analysisPrompt = ChatPromptTemplate.fromTemplate("is this a funny joke? {joke}")
+
+const composedChainWithLambda = RunnableSequence.from([
+    chain,
+    (input) => ({ joke: input }),
+    analysisPrompt,
+    model,
+    new StringOutputParser()
+])
+
+console.log("composedChainWithLambda\n\n")
+console.log(await composedChainWithLambda.invoke({ topic: "beets" }))
