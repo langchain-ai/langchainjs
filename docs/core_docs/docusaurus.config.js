eaafa157db7ff9a24dd72ec114e7a0f11fe09137
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-05-15 10:53:24 -0700

    docs: feedback link in banner (#5387)

--- docs/core_docs/docusaurus.config.js
+++ docs/core_docs/docusaurus.config.js
@@ -134,7 +134,7 @@ const config = {
     ({
       announcementBar: {
         content:
-          'You are viewing the <strong>preview</strong> LangChain v0.2 docs. View the <a href="/v0.1/docs/get_started/introduction/">stable 0.1 docs here</a>.',
+          'You are viewing the <strong>preview</strong> v0.2 docs. View the <strong>stable</strong> v0.1 docs <a href="/v0.1/docs/get_started/introduction/">here</a>. Leave feedback on the v0.2 docs <a href="https://github.com/langchain-ai/langchainjs/discussions/5386">here</a>.',
         isCloseable: true,
       },
       prism: {
