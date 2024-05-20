a94fdb74eb0c66dda1240c4888242419f5870660
Yohan Lasorsa <noda@free.fr>
2024-05-20 23:52:44 +0200

    docs[patch]: update Azure documentation and deprecate @langchain/azure-openai (#5477)
    * docs: update Azure OpenAI chat documentation
    
    * docs: update azure chat openai
    
    * docs: update Azure OpenAI LLM docs
    
    * docs: update docs for Azure OpenAI embeddings
    
    * docs: update wording
    
    * docs: fix example path
    
    * docs: add deprecation notice
    
    * docs: update Microsoft integrations docs
    
    * chore: fix typo
    
    * docs: fix install
    
    * Format
    
    ---------
    
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- examples/src/models/embeddings/azure_openai-base_path.ts
+++ examples/src/models/embeddings/azure_openai-base_path.ts
@@ -1,9 +1,8 @@
-import { AzureChatOpenAI } from "@langchain/openai";
+import { AzureOpenAIEmbeddings } from "@langchain/openai";
 
-const model = new AzureChatOpenAI({
-  temperature: 0.9,
+const model = new AzureOpenAIEmbeddings({
   azureOpenAIApiKey: "<your_key>", // In Node.js defaults to process.env.AZURE_OPENAI_API_KEY
-  azureOpenAIApiDeploymentName: "<your_deployment_name>", // In Node.js defaults to process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME
+  azureOpenAIApiEmbeddingsDeploymentName: "<your_embedding_deployment_name>", // In Node.js defaults to process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME
   azureOpenAIApiVersion: "<api_version>", // In Node.js defaults to process.env.AZURE_OPENAI_API_VERSION
   azureOpenAIBasePath:
     "https://westeurope.api.microsoft.com/openai/deployments", // In Node.js defaults to process.env.AZURE_OPENAI_BASE_PATH
--- examples/src/models/llm/azure_openai-base_path.ts
+++ examples/src/models/llm/azure_openai-base_path.ts
@@ -1,7 +1,6 @@
-import { AzureChatOpenAI } from "@langchain/openai";
+import { AzureOpenAI } from "@langchain/openai";
 
-const model = new AzureChatOpenAI({
-  temperature: 0.9,
+const model = new AzureOpenAI({
   azureOpenAIApiKey: "<your_key>", // In Node.js defaults to process.env.AZURE_OPENAI_API_KEY
   azureOpenAIApiDeploymentName: "<your_deployment_name>", // In Node.js defaults to process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME
   azureOpenAIApiVersion: "<api_version>", // In Node.js defaults to process.env.AZURE_OPENAI_API_VERSION
