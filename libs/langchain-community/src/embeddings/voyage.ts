04f3bc86705e5d9e123a5e26eccac3b2e3d3d922
jacoblee93 <jacoblee93@gmail.com>
2024-05-20 17:51:39 -0700

    Fix types

--- libs/langchain-community/src/embeddings/voyage.ts
+++ libs/langchain-community/src/embeddings/voyage.ts
@@ -63,6 +63,8 @@ export class VoyageEmbeddings
 
   headers?: Record<string, string>;
 
+  inputType?: string;
+
   /**
    * Constructor for the VoyageEmbeddings class.
    * @param fields - An optional object with properties to configure the instance.
