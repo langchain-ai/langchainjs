c3e8d3a2e2b8b3d2d6f64e1fe321227170a6746f
Nicolas <8360521+nicolas-geysse@users.noreply.github.com>
2024-05-21 02:26:08 +0200

    Update voyage.int.test.ts
    With document to make sure it works

--- libs/langchain-community/src/embeddings/tests/voyage.int.test.ts
+++ libs/langchain-community/src/embeddings/tests/voyage.int.test.ts
@@ -2,14 +2,14 @@ import { test, expect } from "@jest/globals";
 import { VoyageEmbeddings } from "../voyage.js";
 
 test.skip("Test VoyageEmbeddings.embedQuery with input_type", async () => {
-  const embeddings = new VoyageEmbeddings({ inputType: "None" });
+  const embeddings = new VoyageEmbeddings({ inputType: "document" });
   const res = await embeddings.embedQuery("Hello world");
 
   expect(typeof res[0]).toBe("number");
 });
 
 test.skip("Test VoyageEmbeddings.embedDocuments with input_type", async () => {
-  const embeddings = new VoyageEmbeddings({ inputType: "None" });
+  const embeddings = new VoyageEmbeddings({ inputType: "document" });
   const res = await embeddings.embedDocuments(["Hello world", "Bye bye"]);
   expect(res).toHaveLength(2);
   expect(typeof res[0][0]).toBe("number");
@@ -20,7 +20,7 @@ test.skip("Test VoyageEmbeddings concurrency with input_type", async () => {
   const embeddings = new VoyageEmbeddings({
     batchSize: 1,
     maxConcurrency: 2,
-    inputType: "None",
+    inputType: "document",
   });
   const res = await embeddings.embedDocuments([
     "Hello world",
