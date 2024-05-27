424d5598a8161433dd1130d8a2ecdf40c53146b9
jeasonnow <guyf@seeyon.com>
2024-05-27 22:19:57 +0800

    langchain[patch]: add support to merge retrievers

---
+++ langchain/src/retrievers/tests/multi_retriever.int.test.ts
@@ -0,0 +1,64 @@
+import { expect, test } from "@jest/globals";
+import { CohereEmbeddings } from "@langchain/cohere";
+import { MemoryVectorStore } from "../../vectorstores/memory.js";
+import { MultiRetriever } from "../multi_retriever.js";
+
+test("Should work with a question input", async () => {
+  const vectorstore = await MemoryVectorStore.fromTexts(
+    [
+      "Buildings are made out of brick",
+      "Buildings are made out of wood",
+      "Buildings are made out of stone",
+      "Cars are made out of metal",
+      "Cars are made out of plastic",
+      "mitochondria is the powerhouse of the cell",
+      "mitochondria is made of lipids",
+    ],
+    [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+    new CohereEmbeddings()
+  );
+  const retriever = new MultiRetriever({
+    retrivers: [vectorstore.asRetriever()],
+  });
+
+  const query = "What are mitochondria made of?";
+  const retrievedDocs = await retriever.invoke(query);
+  console.log(retrievedDocs);
+  expect(retrievedDocs[0].pageContent).toContain("mitochondria");
+});
+
+test("Should work with a keyword", async () => {
+  const vectorstore = await MemoryVectorStore.fromTexts(
+    [
+      "Buildings are made out of brick",
+      "Buildings are made out of wood",
+      "Buildings are made out of stone",
+      "Cars are made out of metal",
+      "Cars are made out of plastic",
+      "mitochondria is the powerhouse of the cell",
+      "mitochondria is made of lipids",
+    ],
+    [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
+    new CohereEmbeddings()
+  );
+  const vectorstore2 = await MemoryVectorStore.fromTexts(
+    [
+      "Buildings are made out of brick",
+      "Buildings are made out of wood",
+      "Buildings are made out of stone",
+      "Cars are made out of metal",
+      "Cars are made out of plastic",
+      "mitochondria is the powerhouse of the cell",
+      "mitochondria is made of lipids",
+    ],
+    [{ id: 6 }, { id: 7 }, { id: 8 }, { id: 9}, { id: 10 }],
+    new CohereEmbeddings()
+  );
+  const retriever = new MultiRetriever({
+    retrivers: [vectorstore.asRetriever(), vectorstore2.asRetriever()],
+  });
+
+  const query = "cars";
+  const retrievedDocs = await retriever.invoke(query);
+  expect(retrievedDocs.filter(item => item.pageContent.includes('Cars')).length).toBe(4);
+});
