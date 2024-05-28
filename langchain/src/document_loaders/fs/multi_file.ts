a44b7ff2bab2fb4bf2f54287fee0e0313f9b3d53
Theo Gravity <theo@suteki.nu>
2024-05-28 14:11:47 -0700

    Multi-file loader

---
+++ langchain/src/document_loaders/fs/multi_file.ts
@@ -0,0 +1,113 @@
+import type { extname as ExtnameT, resolve as ResolveT } from "node:path";
+import type { stat as StatT } from "node:fs/promises";
+import { Document } from "@langchain/core/documents";
+import { getEnv } from "@langchain/core/utils/env";
+import { BaseDocumentLoader } from "../base.js";
+import { type LoadersMapping, UnknownHandling } from "./directory.js";
+
+/**
+ * A document loader that loads documents from multiple files. It extends the
+ * `BaseDocumentLoader` class and implements the `load()` method.
+ * @example
+ * ```typescript
+ *
+ * const multiFileLoader = new MultiFileLoader(
+ *   ["path/to/file1.pdf", "path/to/file2.txt"],
+ *   {
+ *     ".pdf": (path: string) => new PDFLoader(path),
+ *   },
+ * );
+ *
+ * const docs = await multiFileLoader.load();
+ * console.log({ docs });
+ *
+ * ```
+ */
+export class MultiFileLoader extends BaseDocumentLoader {
+  constructor(
+    public filePaths: string[],
+    public loaders: LoadersMapping,
+    public unknown: UnknownHandling = UnknownHandling.Warn
+  ) {
+    super();
+
+    if (Object.keys(loaders).length === 0) {
+      throw new Error("Must provide at least one loader");
+    }
+    for (const extension in loaders) {
+      if (Object.hasOwn(loaders, extension)) {
+        if (extension[0] !== ".") {
+          throw new Error(`Extension must start with a dot: ${extension}`);
+        }
+      }
+    }
+  }
+
+  /**
+   * Loads the documents from the provided file paths. It checks if the file
+   * is a directory and ignores it. If a file is a file, it checks if there
+   * is a corresponding loader function for the file extension in the `loaders`
+   * mapping. If there is, it loads the documents. If there is no
+   * corresponding loader function and `unknown` is set to `Warn`, it logs a
+   * warning message. If `unknown` is set to `Error`, it throws an error.
+   * @returns A promise that resolves to an array of loaded documents.
+   */
+  public async load(): Promise<Document[]> {
+    const { stat, extname, resolve } = await MultiFileLoader.imports();
+    const documents: Document[] = [];
+
+    for (const filePath of this.filePaths) {
+      const fullPath = resolve(filePath);
+      const fileStat = await stat(fullPath);
+
+      if (fileStat.isDirectory()) {
+        console.warn(`Ignoring directory: ${fullPath}`);
+        continue;
+      }
+
+      const loaderFactory = this.loaders[extname(fullPath)];
+      if (loaderFactory) {
+        const loader = loaderFactory(fullPath);
+        documents.push(...(await loader.load()));
+      } else {
+        switch (this.unknown) {
+          case UnknownHandling.Ignore:
+            break;
+          case UnknownHandling.Warn:
+            console.warn(`Unknown file type: ${fullPath}`);
+            break;
+          case UnknownHandling.Error:
+            throw new Error(`Unknown file type: ${fullPath}`);
+          default:
+            throw new Error(`Unknown unknown handling: ${this.unknown}`);
+        }
+      }
+    }
+
+    return documents;
+  }
+
+  /**
+   * Imports the necessary functions from the `node:path` and
+   * `node:fs/promises` modules. It is used to dynamically import the
+   * functions when needed. If the import fails, it throws an error
+   * indicating that the modules failed to load.
+   * @returns A promise that resolves to an object containing the imported functions.
+   */
+  static async imports(): Promise<{
+    stat: typeof StatT;
+    extname: typeof ExtnameT;
+    resolve: typeof ResolveT;
+  }> {
+    try {
+      const { extname, resolve } = await import("node:path");
+      const { stat } = await import("node:fs/promises");
+      return { stat, extname, resolve };
+    } catch (e) {
+      console.error(e);
+      throw new Error(
+        `Failed to load fs/promises. MultiFileLoader available only on environment 'node'. It appears you are running environment '${getEnv()}'. See https://<link to docs> for alternatives.`
+      );
+    }
+  }
+}
