400b04b716446f33ad106b6745c3f8214290ce6a
bracesproul <braceasproul@gmail.com>
2024-06-04 11:41:02 -0700

    google genai

---
+++ libs/langchain-google-genai/src/tests/chat_models.standard.int.test.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-process-env */
+import { test, expect } from "@jest/globals";
+import { ChatModelIntegrationTests } from "@langchain/standard-tests";
+import { AIMessageChunk } from "@langchain/core/messages";
+import { ChatGoogleGenerativeAI, GoogleGenerativeAIChatCallOptions } from "../chat_models.js";
+
+class ChatGoogleGenerativeAIStandardIntegrationTests extends ChatModelIntegrationTests<
+GoogleGenerativeAIChatCallOptions,
+  AIMessageChunk
+> {
+  constructor() {
+    super({
+      Cls: ChatGoogleGenerativeAI,
+      chatModelHasToolCalling: true,
+      chatModelHasStructuredOutput: true,
+      constructorArgs: {
+        maxRetries: 1,
+      },
+    });
+  }
+}
+
+const testClass = new ChatGoogleGenerativeAIStandardIntegrationTests();
+
+test("ChatGoogleGenerativeAIStandardIntegrationTests", async () => {
+  const testResults = await testClass.runTests();
+  expect(testResults).toBe(true);
+});
