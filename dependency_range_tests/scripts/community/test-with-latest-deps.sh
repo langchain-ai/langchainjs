56663e9860581bfd6890a3e8851dff4eefc66843
bracesproul <braceasproul@gmail.com>
2024-06-07 17:34:11 -0700

    cr

--- dependency_range_tests/scripts/community/test-with-latest-deps.sh
+++ dependency_range_tests/scripts/community/test-with-latest-deps.sh
@@ -8,9 +8,34 @@ export CI=true
 shopt -s extglob
 
 # avoid copying build artifacts from the host
-cp -r ../libs/langchain-community/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
+mkdir -p ./libs/langchain-community/ ./libs/langchain-standard-tests/
+cp -r ../libs/langchain-community/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./libs/langchain-community/
+cp -r ../libs/langchain-standard-tests ./libs/langchain-standard-tests/
+cp ../turbo.json ./
+cp ../package.json ./
+
+ls ./libs
+# ls ./libs/langchain-community
+ls ./libs/langchain-standard-tests/langchain-standard-tests
+
+# Copy all contents from ./libs/langchain-standard-tests/langchain-standard-tests to ./libs/langchain-standard-tests
+cp -r ./libs/langchain-standard-tests/langchain-standard-tests/* ./libs/langchain-standard-tests/
+
+# Delete the ./libs/langchain-standard-tests/langchain-standard-tests directory
+rm -rf ./libs/langchain-standard-tests/langchain-standard-tests
+
+ls ./libs/langchain-standard-tests
+
+# Replace any workspace dependencies in `@langchain/standard-tests`
+# with "latest" for the version.
+mkdir -p /updater_script
+cp /scripts/community/node/update_workspace_dependencies.js /updater_script/
+node /updater_script/update_workspace_dependencies.js
 
 yarn
 
 # Check the test command completes successfully
-NODE_OPTIONS=--experimental-vm-modules yarn run jest --testPathIgnorePatterns=\\.int\\.test.ts --testTimeout 30000 --maxWorkers=50%
+ls
+ls ./libs
+cd ./libs/langchain-community && yarn test
+# yarn run jest --testPathIgnorePatterns=\\.int\\.test.ts --testTimeout 30000 --maxWorkers=50%
