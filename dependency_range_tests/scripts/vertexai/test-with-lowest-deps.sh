99608992d7660e9b59c0ba615e539dff56421906
Brace Sproul <braceasproul@gmail.com>
2024-06-11 11:28:17 -0700

    infra[minor]: Add vertexai to latest/lowest ci (#5727)

---
+++ dependency_range_tests/scripts/vertexai/test-with-lowest-deps.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+export CI=true
+
+# enable extended globbing for omitting build artifacts
+shopt -s extglob
+
+# avoid copying build artifacts from the host
+cp -r ../libs/langchain-google-vertexai/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) ./
+
+mkdir -p /updater_script
+cp -r /scripts/vertexai/node/!(node_modules|dist|dist-cjs|dist-esm|build|.next|.turbo) /updater_script/
+
+cd /updater_script
+
+yarn
+
+cd /app
+
+node /updater_script/update_resolutions_lowest.js
+
+yarn
+
+# Check the test command completes successfully
+NODE_OPTIONS=--experimental-vm-modules yarn run jest --testPathIgnorePatterns=\\.int\\.test.ts --testTimeout 30000 --maxWorkers=50%
