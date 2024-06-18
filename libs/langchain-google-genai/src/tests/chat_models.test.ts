bb3f2772d6b254c6e516c434bdbc073bff8a0630
bracesproul <braceasproul@gmail.com>
2024-06-18 13:33:32 -0700

    google-genai[patch]: Update tool message parsing

--- libs/langchain-google-genai/src/tests/chat_models.test.ts
+++ libs/langchain-google-genai/src/tests/chat_models.test.ts
@@ -2,8 +2,18 @@ import { test } from "@jest/globals";
 import type { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
+import {
+  AIMessage,
+  HumanMessage,
+  SystemMessage,
+  ToolMessage,
+} from "@langchain/core/messages";
 import { ChatGoogleGenerativeAI } from "../chat_models.js";
 import { removeAdditionalProperties } from "../utils/zod_to_genai_parameters.js";
+import {
+  convertBaseMessagesToContent,
+  convertMessageContentToParts,
+} from "../utils/common.js";
 
 test("Google AI - `temperature` must be in range [0.0,1.0]", async () => {
   expect(
@@ -127,4 +137,99 @@ test("removeAdditionalProperties can remove all instances of additionalPropertie
   expect(
     arrSchemaObj.find((key) => key === "additionalProperties")
   ).toBeUndefined();
+});
+
+test("convertMessageContentToParts correctly handles message types", () => {
+  const messages = [
+    new SystemMessage("You are a helpful assistant"),
+    new HumanMessage("What's the weather like in new york?"),
+    new AIMessage({
+      content: "",
+      tool_calls: [
+        {
+          name: "get_current_weather",
+          args: {
+            location: "New York",
+          },
+          id: "123",
+        },
+      ],
+    }),
+    new ToolMessage(
+      "{ weather: '28 °C', location: 'New York, NY' }",
+      "get_current_weather",
+      "123"
+    ),
+  ];
+  const messagesAsGoogleParts = messages
+    .map((msg) => convertMessageContentToParts(msg, false))
+    .flat();
+  console.log(messagesAsGoogleParts);
+  expect(messagesAsGoogleParts).toEqual([
+    { text: "You are a helpful assistant" },
+    { text: "What's the weather like in new york?" },
+    {
+      functionCall: {
+        name: "get_current_weather",
+        args: {
+          location: "New York",
+        },
+      },
+    },
+    { text: "{ weather: '28 °C', location: 'New York, NY' }" },
+  ]);
+});
+
+test("convertBaseMessagesToContent correctly creates properly formatted content", async () => {
+  const toolResponse = "{ weather: '28 °C', location: 'New York, NY' }";
+  const toolName = "get_current_weather";
+  const toolId = "123";
+  const toolArgs = {
+    location: "New York",
+  };
+  const messages = [
+    new SystemMessage("You are a helpful assistant"),
+    new HumanMessage("What's the weather like in new york?"),
+    new AIMessage({
+      content: "",
+      tool_calls: [
+        {
+          name: toolName,
+          args: toolArgs,
+          id: toolId,
+        },
+      ],
+    }),
+    new ToolMessage(toolResponse, toolName, toolId),
+  ];
+
+  const messagesAsGoogleContent = convertBaseMessagesToContent(messages, false);
+  console.log(messagesAsGoogleContent);
+  // Google Generative AI API only allows for 'model' and 'user' roles
+  // This means that 'system', 'human' and 'tool' messages are converted
+  // to 'user' messages, and ai messages are converted to 'model' messages.
+  expect(messagesAsGoogleContent).toEqual([
+    {
+      role: "user",
+      parts: [
+        { text: "You are a helpful assistant" },
+        { text: "What's the weather like in new york?" },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
+        {
+          functionCall: {
+            name: toolName,
+            args: toolArgs,
+          },
+        },
+      ],
+    },
+    {
+      role: "user",
+      parts: [{ text: toolResponse }],
+    },
+  ]);
 });
