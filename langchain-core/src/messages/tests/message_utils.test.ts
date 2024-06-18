dfec099f755698d6658c50583e1b2863234773c1
bracesproul <braceasproul@gmail.com>
2024-06-18 15:28:20 -0700

    implemented rest of code and tests

--- langchain-core/src/messages/tests/message_utils.test.ts
+++ langchain-core/src/messages/tests/message_utils.test.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@jest/globals";
-import { filterMessages, mergeMessageRuns } from "../utils.js";
+import { filterMessages, mergeMessageRuns, trimMessages } from "../utils.js";
 import { AIMessage } from "../ai.js";
 import { HumanMessage } from "../human.js";
 import { SystemMessage } from "../system.js";
+import { BaseMessage } from "../base.js";
 
 test("filterMessages works", () => {
   const messages = [
@@ -69,4 +70,221 @@ test("mergeMessageRuns works", () => {
       id: "baz",
     }),
   ]);
+});
+
+describe.only("trimMessages can trim", () => {
+  const messages = [
+    new SystemMessage("This is a 4 token text. The full message is 10 tokens."),
+    new HumanMessage({
+      content: "This is a 4 token text. The full message is 10 tokens.",
+      id: "first",
+    }),
+    new AIMessage({
+      content: [
+        { type: "text", text: "This is the FIRST 4 token block." },
+        { type: "text", text: "This is the SECOND 4 token block." },
+      ],
+      id: "second",
+    }),
+    new HumanMessage({
+      content: "This is a 4 token text. The full message is 10 tokens.",
+      id: "third",
+    }),
+    new AIMessage({
+      content: "This is a 4 token text. The full message is 10 tokens.",
+      id: "fourth",
+    }),
+  ];
+
+  function dummyTokenCounter(messages: BaseMessage[]): number {
+    // treat each message like it adds 3 default tokens at the beginning
+    // of the message and at the end of the message. 3 + 4 + 3 = 10 tokens
+    // per message.
+
+    const defaultContentLen = 4;
+    const defaultMsgPrefixLen = 3;
+    const defaultMsgSuffixLen = 3;
+
+    let count = 0;
+    for (const msg of messages) {
+      if (typeof msg.content === "string") {
+        count += defaultMsgPrefixLen + defaultContentLen + defaultMsgSuffixLen;
+      }
+      if (Array.isArray(msg.content)) {
+        count +=
+          defaultMsgPrefixLen +
+          msg.content.length * defaultContentLen +
+          defaultMsgSuffixLen;
+      }
+    }
+    return count;
+  }
+
+  it.only("First 30 tokens, not allowing partial messages", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 30,
+      tokenCounter: dummyTokenCounter,
+      strategy: "first",
+    });
+    expect(trimmedMessages).toHaveLength(2);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "first",
+      }),
+    ]);
+  });
+
+  it.only("First 30 tokens, allowing partial messages", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 30,
+      tokenCounter: dummyTokenCounter,
+      strategy: "first",
+      allowPartial: true,
+    });
+
+    expect(trimmedMessages).toHaveLength(3);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "first",
+      }),
+      new AIMessage({
+        content: [{ type: "text", text: "This is the FIRST 4 token block." }],
+        id: "second",
+      }),
+    ]);
+  });
+
+  it.only("First 30 tokens, allowing partial messages, have to end on HumanMessage", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 30,
+      tokenCounter: dummyTokenCounter,
+      strategy: "first",
+      allowPartial: true,
+      endOn: "human",
+    });
+
+    expect(trimmedMessages).toHaveLength(2);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "first",
+      }),
+    ]);
+  });
+
+  it.only("Last 30 tokens, including system message, not allowing partial messages", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 30,
+      includeSystem: true,
+      tokenCounter: dummyTokenCounter,
+      strategy: "last",
+    });
+
+    expect(trimmedMessages).toHaveLength(3);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "third",
+      }),
+      new AIMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "fourth",
+      }),
+    ]);
+  });
+
+  it("Last 40 tokens, including system message, allowing partial messages", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 40,
+      tokenCounter: dummyTokenCounter,
+      strategy: "last",
+      allowPartial: true,
+      includeSystem: true,
+    });
+
+    expect(trimmedMessages).toHaveLength(4);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new AIMessage({
+        content: [{ type: "text", text: "This is the FIRST 4 token block." }],
+        id: "second",
+      }),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "third",
+      }),
+      new AIMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "fourth",
+      }),
+    ]);
+  });
+
+  it("Last 30 tokens, including system message, allowing partial messages, end on HumanMessage", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 30,
+      tokenCounter: dummyTokenCounter,
+      strategy: "last",
+      endOn: "human",
+      includeSystem: true,
+      allowPartial: true,
+    });
+
+    expect(trimmedMessages).toHaveLength(3);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new AIMessage({
+        content: [{ type: "text", text: "This is the FIRST 4 token block." }],
+        id: "second",
+      }),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "third",
+      }),
+    ]);
+  });
+
+  it("Last 40 tokens, including system message, allowing partial messages, start on HumanMessage", async () => {
+    const trimmedMessages = await trimMessages(messages, {
+      maxTokens: 40,
+      tokenCounter: dummyTokenCounter,
+      strategy: "last",
+      includeSystem: true,
+      allowPartial: true,
+      startOn: "human",
+    });
+
+    expect(trimmedMessages).toHaveLength(3);
+    expect(trimmedMessages).toEqual([
+      new SystemMessage(
+        "This is a 4 token text. The full message is 10 tokens."
+      ),
+      new HumanMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "third",
+      }),
+      new AIMessage({
+        content: "This is a 4 token text. The full message is 10 tokens.",
+        id: "fourth",
+      }),
+    ]);
+  });
 });
--- langchain-core/src/messages/utils.ts
+++ langchain-core/src/messages/utils.ts
@@ -26,6 +26,9 @@ import {
   ToolMessageChunk,
   ToolMessageFieldsWithToolCallId,
 } from "./tool.js";
+import { BaseLanguageModel } from "../language_models/base.js";
+import { BaseDocumentTransformer } from "../documents/transformers.js";
+import { Runnable, RunnableLambda } from "../../runnables.js";
 
 export function coerceMessageLikeToMessage(
   messageLike: BaseMessageLike
@@ -384,13 +387,581 @@ export function mergeMessageRuns(messages: BaseMessage[]): BaseMessage[] {
       const lastChunk = msgToChunk(last);
       const currChunk = msgToChunk(curr);
       const mergedChunks = lastChunk.concat(currChunk);
-      if (typeof lastChunk.content === "string" && typeof currChunk.content === "string") {
+      if (
+        typeof lastChunk.content === "string" &&
+        typeof currChunk.content === "string"
+      ) {
         mergedChunks.content = `${lastChunk.content}\n${currChunk.content}`;
       }
       merged.push(chunkToMsg(mergedChunks));
     }
   }
   return merged;
+}
+
+/**
+ * Since we can not import from `@langchain/textsplitters` we need
+ * to reconstruct the interface here.
+ */
+interface TextSplitterInterface extends BaseDocumentTransformer {
+  splitText(text: string): Promise<string[]>;
+}
+
+export interface TrimMessagesFields {
+  /**
+   * @param {number} maxTokens Max token count of trimmed messages.
+   */
+  maxTokens: number;
+  /**
+   * @param {((messages: BaseMessage[]) => number) | ((messages: BaseMessage[]) => Promise<number>) | BaseLanguageModel} tokenCounter
+   * Function or LLM for counting tokens in an array of `BaseMessage`s.
+   * If a `BaseLanguageModel` is passed in then `BaseLanguageModel.getNumTokens()` will be used.
+   */
+  tokenCounter:
+    | ((messages: BaseMessage[]) => number)
+    | ((messages: BaseMessage[]) => Promise<number>)
+    | BaseLanguageModel;
+  /**
+   * @param {"first" | "last"} [strategy="last"] Strategy for trimming.
+   * - "first": Keep the first <= n_count tokens of the messages.
+   * - "last": Keep the last <= n_count tokens of the messages.
+   * @default "last"
+   */
+  strategy?: "first" | "last";
+  /**
+   * @param {boolean} [allowPartial=false] Whether to split a message if only part of the message can be included.
+   * If `strategy: "last"` then the last partial contents of a message are included.
+   * If `strategy: "first"` then the first partial contents of a message are included.
+   * @default false
+   */
+  allowPartial?: boolean;
+  /**
+   * @param {MessageType | BaseMessage | (MessageType | BaseMessage)[]} [endOn] The message type to end on.
+   * If specified then every message after the last occurrence of this type is ignored.
+   * If `strategy === "last"` then this is done before we attempt to get the last `maxTokens`.
+   * If `strategy === "first"` then this is done after we get the first `maxTokens`.
+   * Can be specified as string names (e.g. "system", "human", "ai", ...) or as `BaseMessage` classes
+   * (e.g. `SystemMessage`, `HumanMessage`, `AIMessage`, ...). Can be a single type or an array of types.
+   */
+  endOn?: MessageType | BaseMessage | (MessageType | BaseMessage)[];
+  /**
+   * @param {MessageType | BaseMessage | (MessageType | BaseMessage)[]} [startOn] The message type to start on.
+   * Should only be specified if `strategy: "last"`. If specified then every message before the first occurrence
+   * of this type is ignored. This is done after we trim the initial messages to the last `maxTokens`.
+   * Does not apply to a `SystemMessage` at index 0 if `includeSystem: true`.
+   * Can be specified as string names (e.g. "system", "human", "ai", ...) or as `BaseMessage` classes
+   * (e.g. `SystemMessage`, `HumanMessage`, `AIMessage`, ...). Can be a single type or an array of types.
+   */
+  startOn?: MessageType | BaseMessage | (MessageType | BaseMessage)[];
+  /**
+   * @param {boolean} [includeSystem=false] Whether to keep the `SystemMessage` if there is one at index 0.
+   * Should only be specified if `strategy: "last"`.
+   * @default false
+   */
+  includeSystem?: boolean;
+  /**
+   * @param {((text: string) => string[]) | BaseDocumentTransformer} [textSplitter] Function or `BaseDocumentTransformer` for
+   * splitting the string contents of a message. Only used if `allowPartial: true`.
+   * If `strategy: "last"` then the last split tokens from a partial message will be included.
+   * If `strategy: "first"` then the first split tokens from a partial message will be included.
+   * Token splitter assumes that separators are kept, so that split contents can be directly concatenated
+   * to recreate the original text. Defaults to splitting on newlines.
+   */
+  textSplitter?:
+    | ((text: string) => string[])
+    | ((text: string) => Promise<string[]>)
+    | TextSplitterInterface;
+}
+
+/**
+ * Trim messages to be below a token count.
+ *
+ * @param {BaseMessage[]} messages Array of `BaseMessage` instances to trim.
+ * @param {TrimMessagesFields} options Trimming options.
+ * @returns An array of trimmed `BaseMessage`s or a `Runnable` that takes a sequence of `BaseMessage`-like objects and returns
+ *     an array of trimmed `BaseMessage`s.
+ * @throws {Error} If two incompatible arguments are specified or an unrecognized `strategy` is specified.
+ *
+ * @example
+ * ```typescript
+ * import { trimMessages, AIMessage, BaseMessage, HumanMessage, SystemMessage } from "@langchain/core/messages";
+ *
+ * const messages = [
+ *   new SystemMessage("This is a 4 token text. The full message is 10 tokens."),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "first",
+ *   }),
+ *   new AIMessage({
+ *     content: [
+ *       { type: "text", text: "This is the FIRST 4 token block." },
+ *       { type: "text", text: "This is the SECOND 4 token block." },
+ *     ],
+ *     id: "second",
+ *   }),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "third",
+ *   }),
+ *   new AIMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "fourth",
+ *   }),
+ * ];
+ *
+ * function dummyTokenCounter(messages: BaseMessage[]): number {
+ *   // treat each message like it adds 3 default tokens at the beginning
+ *   // of the message and at the end of the message. 3 + 4 + 3 = 10 tokens
+ *   // per message.
+ *
+ *   const defaultContentLen = 4;
+ *   const defaultMsgPrefixLen = 3;
+ *   const defaultMsgSuffixLen = 3;
+ *
+ *   let count = 0;
+ *   for (const msg of messages) {
+ *     if (typeof msg.content === "string") {
+ *       count += defaultMsgPrefixLen + defaultContentLen + defaultMsgSuffixLen;
+ *     }
+ *     if (Array.isArray(msg.content)) {
+ *       count +=
+ *         defaultMsgPrefixLen +
+ *         msg.content.length * defaultContentLen +
+ *         defaultMsgSuffixLen;
+ *     }
+ *   }
+ *   return count;
+ * }
+ * ```
+ *
+ * First 30 tokens, not allowing partial messages:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 30,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "first",
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "first",
+ *   }),
+ * ]
+ * ```
+ *
+ * First 30 tokens, allowing partial messages:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 30,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "first",
+ *   allowPartial: true,
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "first",
+ *   }),
+ *   new AIMessage({
+ *     content: [{ type: "text", text: "This is the FIRST 4 token block." }],
+ *     id: "second",
+ *   }),
+ * ]
+ * ```
+ *
+ * First 30 tokens, allowing partial messages, have to end on HumanMessage:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 30,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "first",
+ *   allowPartial: true,
+ *   endOn: "human",
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "first",
+ *   }),
+ * ]
+ * ```
+ *
+ * Last 30 tokens, including system message, not allowing partial messages:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 30,
+ *   includeSystem: true,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "last",
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "third",
+ *   }),
+ *   new AIMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "fourth",
+ *   }),
+ * ]
+ * ```
+ *
+ * Last 40 tokens, including system message, allowing partial messages:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 40,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "last",
+ *   allowPartial: true,
+ *   includeSystem: true,
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new AIMessage({
+ *     content: [{ type: "text", text: "This is the FIRST 4 token block." }],
+ *     id: "second",
+ *   }),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "third",
+ *   }),
+ *   new AIMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "fourth",
+ *   }),
+ * ]
+ * ```
+ *
+ * Last 30 tokens, including system message, allowing partial messages, end on HumanMessage:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 30,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "last",
+ *   endOn: "human",
+ *   includeSystem: true,
+ *   allowPartial: true,
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new AIMessage({
+ *     content: [{ type: "text", text: "This is the FIRST 4 token block." }],
+ *     id: "second",
+ *   }),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "third",
+ *   }),
+ * ]
+ * ```
+ *
+ * Last 40 tokens, including system message, allowing partial messages, start on HumanMessage:
+ * ```typescript
+ * await trimMessages(messages, {
+ *   maxTokens: 40,
+ *   tokenCounter: dummyTokenCounter,
+ *   strategy: "last",
+ *   includeSystem: true,
+ *   allowPartial: true,
+ *   startOn: "human",
+ * });
+ * ```
+ *
+ * Output:
+ * ```typescript
+ * [
+ *   new SystemMessage(
+ *     "This is a 4 token text. The full message is 10 tokens."
+ *   ),
+ *   new HumanMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "third",
+ *   }),
+ *   new AIMessage({
+ *     content: "This is a 4 token text. The full message is 10 tokens.",
+ *     id: "fourth",
+ *   }),
+ * ]
+ * ```
+ */
+export async function trimMessages(
+  messages: BaseMessage[],
+  options: TrimMessagesFields
+): Promise<BaseMessage[] | Runnable<BaseMessage[], BaseMessage[]>> {
+  if (messages.length) {
+    return _trimMessagesHelper(messages, options);
+  }
+  const trimmer = (input: BaseMessage[]) => _trimMessagesHelper(input, options);
+  return RunnableLambda.from(trimmer);
+}
+
+async function _trimMessagesHelper(
+  messages: BaseMessage[],
+  options: TrimMessagesFields
+): Promise<Array<BaseMessage>> {
+  const {
+    maxTokens,
+    tokenCounter,
+    strategy = "last",
+    allowPartial = false,
+    endOn,
+    startOn,
+    includeSystem = false,
+    textSplitter,
+  } = options;
+  if (startOn && strategy === "first") {
+    throw new Error(
+      "`startOn` should only be specified if `strategy` is 'last'."
+    );
+  }
+  if (includeSystem && strategy === "first") {
+    throw new Error(
+      "`includeSystem` should only be specified if `strategy` is 'last'."
+    );
+  }
+
+  let listTokenCounter: (msgs: BaseMessage[]) => Promise<number>;
+  if ("getNumTokens" in tokenCounter) {
+    listTokenCounter = async (msgs: BaseMessage[]): Promise<number> => {
+      const tokenCounts = await Promise.all(
+        msgs.map((msg) => tokenCounter.getNumTokens(msg.content))
+      );
+      return tokenCounts.reduce((sum, count) => sum + count, 0);
+    };
+  } else {
+    listTokenCounter = async (msgs: BaseMessage[]): Promise<number> =>
+      tokenCounter(msgs);
+  }
+
+  let textSplitterFunc: (text: string) => Promise<string[]> =
+    defaultTextSplitter;
+  if (textSplitter) {
+    if ("splitText" in textSplitter) {
+      textSplitterFunc = textSplitter.splitText;
+    } else {
+      textSplitterFunc = async (text: string): Promise<string[]> =>
+        textSplitter(text);
+    }
+  }
+
+  if (strategy === "first") {
+    return _firstMaxTokens(messages, {
+      maxTokens,
+      tokenCounter: listTokenCounter,
+      textSplitter: textSplitterFunc,
+      partialStrategy: allowPartial ? "first" : undefined,
+      endOn,
+    });
+  } else if (strategy === "last") {
+    return _lastMaxTokens(messages, {
+      maxTokens,
+      tokenCounter: listTokenCounter,
+      textSplitter: textSplitterFunc,
+      allowPartial,
+      includeSystem,
+      startOn,
+      endOn,
+    });
+  } else {
+    throw new Error(
+      `Unrecognized strategy: '${strategy}'. Must be one of 'first' or 'last'.`
+    );
+  }
+}
+
+async function _firstMaxTokens(
+  messages: BaseMessage[],
+  options: {
+    maxTokens: number;
+    tokenCounter: (messages: BaseMessage[]) => Promise<number>;
+    textSplitter: (text: string) => Promise<string[]>;
+    partialStrategy?: "first" | "last";
+    endOn?: MessageType | BaseMessage | (MessageType | BaseMessage)[];
+  }
+): Promise<BaseMessage[]> {
+  const { maxTokens, tokenCounter, textSplitter, partialStrategy, endOn } =
+    options;
+  let messagesCopy = [...messages];
+  let idx = 0;
+  for (let i = 0; i < messagesCopy.length; i++) {
+    const remainingMessages = i > 0 ? messagesCopy.slice(0, -i) : messagesCopy;
+    if ((await tokenCounter(remainingMessages)) <= maxTokens) {
+      idx = messagesCopy.length - i;
+      break;
+    }
+  }
+
+  if (idx < messagesCopy.length - 1 && partialStrategy) {
+    let includedPartial = false;
+    if (Array.isArray(messagesCopy[idx].content)) {
+      const excluded = messagesCopy[idx];
+      if (typeof excluded.content === "string") {
+        throw new Error("Expected content to be an array.");
+      }
+
+      const numBlock = excluded.content.length;
+      if (partialStrategy === "last") {
+        excluded.content = [...excluded.content].reverse();
+      }
+      for (let _ = 1; _ < numBlock; _++) {
+        excluded.content = excluded.content.slice(0, -1);
+        if (
+          (await tokenCounter([...messagesCopy.slice(0, idx), excluded])) <=
+          maxTokens
+        ) {
+          messagesCopy = [...messagesCopy.slice(0, idx), excluded];
+          idx++;
+          includedPartial = true;
+          break;
+        }
+      }
+      if (includedPartial && partialStrategy === "last") {
+        excluded.content = [...excluded.content].reverse();
+      }
+    }
+    if (!includedPartial) {
+      const excluded = messagesCopy[idx];
+      let text: string | undefined;
+      if (
+        Array.isArray(excluded.content) &&
+        excluded.content.some(
+          (block) => typeof block === "string" || block.type === "text"
+        )
+      ) {
+        const textBlock = excluded.content.find(
+          (block) => block.type === "text" && block.text
+        ) as { type: "text"; text: string } | undefined;
+        text = textBlock?.text;
+      } else if (typeof excluded.content === "string") {
+        text = excluded.content;
+      }
+      if (text) {
+        const splitTexts = await textSplitter(text);
+        const numSplits = splitTexts.length;
+        if (partialStrategy === "last") {
+          splitTexts.reverse();
+        }
+        for (let _ = 0; _ < numSplits - 1; _++) {
+          splitTexts.pop();
+          excluded.content = splitTexts.join("");
+          if (
+            (await tokenCounter([...messagesCopy.slice(0, idx), excluded])) <=
+            maxTokens
+          ) {
+            if (partialStrategy === "last") {
+              excluded.content = [...splitTexts].reverse().join("");
+            }
+            messagesCopy = [...messagesCopy.slice(0, idx), excluded];
+            idx++;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  if (endOn) {
+    const endOnArr = Array.isArray(endOn) ? endOn : [endOn];
+    while (idx > 0 && !_isMessageType(messagesCopy[idx - 1], endOnArr)) {
+      idx--;
+    }
+  }
+
+  return messagesCopy.slice(0, idx);
+}
+
+async function _lastMaxTokens(
+  messages: BaseMessage[],
+  options: {
+    maxTokens: number;
+    tokenCounter: (messages: BaseMessage[]) => Promise<number>;
+    textSplitter: (text: string) => Promise<string[]>;
+    /**
+     * @default {false}
+     */
+    allowPartial?: boolean;
+    /**
+     * @default {false}
+     */
+    includeSystem?: boolean;
+    startOn?: MessageType | BaseMessage | (MessageType | BaseMessage)[];
+    endOn?: MessageType | BaseMessage | (MessageType | BaseMessage)[];
+  }
+): Promise<BaseMessage[]> {
+  const {
+    allowPartial = false,
+    includeSystem = false,
+    endOn,
+    startOn,
+    ...rest
+  } = options;
+
+  if (endOn) {
+    const endOnArr = Array.isArray(endOn) ? endOn : [endOn];
+    while (
+      messages &&
+      !_isMessageType(messages[messages.length - 1], endOnArr)
+    ) {
+      messages.pop();
+    }
+  }
+
+  const swappedSystem = includeSystem && messages[0]._getType() === "system";
+  let reversed_ = swappedSystem
+    ? messages.slice(0, 1).concat(messages.slice(1).reverse())
+    : messages.reverse();
+
+  reversed_ = await _firstMaxTokens(reversed_, {
+    ...rest,
+    partialStrategy: allowPartial ? "last" : undefined,
+    endOn: startOn,
+  });
+
+  if (swappedSystem) {
+    return [reversed_[0], ...reversed_.slice(1).reverse()];
+  } else {
+    return reversed_.reverse();
+  }
 }
 
 type MessageUnion =
@@ -592,7 +1163,10 @@ function chunkToMsg(chunk: BaseMessageChunk): BaseMessage {
   return msg;
 }
 
-// const defaultTextSplitter = (text: string): string[] => {
-//   const splits = text.split("\n");
-//   return [...splits.slice(0, -1).map((s) => s + "\n"), splits[splits.length - 1]];
-// };
+const defaultTextSplitter = (text: string): Promise<string[]> => {
+  const splits = text.split("\n");
+  return Promise.resolve([
+    ...splits.slice(0, -1).map((s) => s + "\n"),
+    splits[splits.length - 1],
+  ]);
+};
