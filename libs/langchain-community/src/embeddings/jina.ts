a8f470d061c598431206a8751b53ac9cd4d949d3
AvaterClasher <avater.clasher@gmail.com>
2024-06-21 20:27:27 +0530

    docs(jina): added doc comments

--- libs/langchain-community/src/embeddings/jina.ts
+++ libs/langchain-community/src/embeddings/jina.ts
@@ -31,21 +31,59 @@ function getBytesStr(filePath: string): string {
   return Buffer.from(imageFile).toString("base64");
 }
 
+/**
+ * Input parameters for the Jina embeddings
+ */
 export interface JinaEmbeddingsParams extends EmbeddingsParams {
+  /**
+   * The API key to use for authentication.
+   * If not provided, it will be read from the `JINA_API_KEY` environment variable.
+   */
   apiKey?: string;
+
+  /**
+   * The model ID to use for generating embeddings.
+   * Default: `jina-embeddings-v2-base-en`
+   */
   modelName?: string;
 }
 
+/**
+ * Response from the Jina embeddings API.
+ */
 export interface JinaEmbeddingsResponse {
+  /**
+   * The embeddings generated for the input texts.
+   */
   data: { index: number; embedding: number[] }[];
+
+  /**
+   * The detail of the response e.g usage, model used etc.
+   */
   detail?: string;
 }
 
+/**
+ * A class for generating embeddings using the Jina API.
+ * @example
+ * ```typescript
+ * // Embed a query using the JinaEmbeddings class
+ * const model = new JinaEmbeddings();
+ * const res = await model.embedQuery(
+ *   "What would be a good name for a semantic search engine ?",
+ * );
+ * console.log({ res });
+ * ```
+ */
 export class JinaEmbeddings extends Embeddings implements JinaEmbeddingsParams {
   apiKey: string;
 
   modelName: string;
 
+  /**
+   * Constructor for the JinaEmbeddings class.
+   * @param fields - An optional object with properties to configure the instance.
+   */
   constructor(fields?: Partial<JinaEmbeddingsParams> & { verbose?: boolean }) {
     const fieldsWithDefaults = {
       modelName: "jina-embeddings-v2-base-en",
