89425b690885ec116a5d2e5dfbb3e758128003c6
jacoblee93 <jacoblee93@gmail.com>
2024-06-25 10:24:01 -0700

    Format

--- langchain-core/src/tests/document.test.ts
+++ langchain-core/src/tests/document.test.ts
@@ -1,7 +1,7 @@
-import {test, expect} from '@jest/globals'
-import {Document} from '../documents/document.js'
+import { test, expect } from "@jest/globals";
+import { Document } from "../documents/document.js";
 
-test('Document should handle empty pageContent', () => {
-  const doc = new Document({pageContent: ''})
-  expect(doc.pageContent).toEqual('')
-})
+test("Document should handle empty pageContent", () => {
+  const doc = new Document({ pageContent: "" });
+  expect(doc.pageContent).toEqual("");
+});
