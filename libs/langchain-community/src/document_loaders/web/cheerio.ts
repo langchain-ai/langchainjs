7beac60f4e37b52467b8882d12ef0722f7dc32c2
xujingzhu <xujingzhu@youzan.com>
2024-06-26 15:35:09 +0800

    fix: cheerio loader fetch headers type error

--- libs/langchain-community/src/document_loaders/web/cheerio.ts
+++ libs/langchain-community/src/document_loaders/web/cheerio.ts
@@ -62,7 +62,7 @@ export class CheerioWebBaseLoader
 
   textDecoder?: TextDecoder;
 
-  headers?: Headers;
+  headers?: HeadersInit;
 
   constructor(public webPath: string, fields?: WebBaseLoaderParams) {
     super();
@@ -86,7 +86,7 @@ export class CheerioWebBaseLoader
     timeout: number | undefined,
     textDecoder?: TextDecoder,
     options?: CheerioOptions & {
-      headers?: Headers;
+      headers?: HeadersInit;
     }
   ): Promise<CheerioAPI[]> {
     return Promise.all(
@@ -102,7 +102,7 @@ export class CheerioWebBaseLoader
     timeout: number | undefined,
     textDecoder?: TextDecoder,
     options?: CheerioOptions & {
-      headers?: Headers;
+      headers?: HeadersInit;
     }
   ): Promise<CheerioAPI> {
     const { headers, ...cheerioOptions } = options ?? {};
