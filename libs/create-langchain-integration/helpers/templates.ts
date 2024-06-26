a4b08a2bd4517d89c51fba742ca2f1e1fa999a6d
bracesproul <braceasproul@gmail.com>
2024-06-26 12:06:36 -0700

    chore: lint files

--- libs/create-langchain-integration/helpers/templates.ts
+++ libs/create-langchain-integration/helpers/templates.ts
@@ -33,7 +33,7 @@ export async function installTemplate({ appName, root }: any) {
   if (appName.startsWith("@langchain/")) {
     const integrationName = appName.replace("@langchain/", "");
     packageJson.description = `Integration for LangChain ${integrationName}`;
-    packageJson.homepage = `https://github.com/langchain-ai/langchainjs/tree/main/libs/langchain-${integrationName}/`
+    packageJson.homepage = `https://github.com/langchain-ai/langchainjs/tree/main/libs/langchain-${integrationName}/`;
     packageJson.scripts.build = `yarn turbo:command build:internal --filter=${appName}`;
   }
 
