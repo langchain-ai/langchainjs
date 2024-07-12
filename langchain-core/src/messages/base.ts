6d5f184ec813269a3695349bee5b7540770236e6
bracesproul <braceasproul@gmail.com>
2024-07-12 14:47:03 -0700

    core[minor]: Allow concatenation of messages with multi part content

--- langchain-core/src/messages/base.ts
+++ langchain-core/src/messages/base.ts
@@ -127,7 +127,13 @@ export function mergeContent(
     }
     // If both are arrays
   } else if (Array.isArray(secondContent)) {
-    return [...firstContent, ...secondContent];
+    // find and merge all objects with the same "index" and "type" fields.
+    return (
+      _mergeLists(firstContent, secondContent) ?? [
+        ...firstContent,
+        ...secondContent,
+      ]
+    );
     // If the first content is a list and second is a string
   } else {
     // Otherwise, add the second content as a new element of the list
@@ -258,8 +264,16 @@ export function _mergeDicts(
       throw new Error(
         `field[${key}] already exists in the message chunk, but with a different type.`
       );
-    } else if (typeof merged[key] === "string") {
-      merged[key] = (merged[key] as string) + value;
+    } else if (
+      typeof merged[key] === "string" &&
+      !(key === "type" && merged[key] === value)
+    ) {
+      if (key === "type") {
+        // Do not merge 'type' fields
+        continue;
+      } else {
+        merged[key] = (merged[key] as string) + value;
+      }
     } else if (!Array.isArray(merged[key]) && typeof merged[key] === "object") {
       merged[key] = _mergeDicts(merged[key], value);
     } else if (Array.isArray(merged[key])) {
@@ -297,6 +311,12 @@ export function _mergeLists(left?: any[], right?: any[]) {
         } else {
           merged.push(item);
         }
+      } else if (
+        typeof item === "object" &&
+        "text" in item &&
+        item.text === ""
+      ) {
+        // Skip empty text blocks
       } else {
         merged.push(item);
       }
