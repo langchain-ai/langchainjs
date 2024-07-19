326cb85fcdb25c7bdd3db277355a8ddf1d7b7b3e
Srijan Dubey <87586713+Srijan-D@users.noreply.github.com>
2024-07-19 22:41:25 +0530

    docs[minor]: Replace deprecated code examples with LCEL & AgentExecutor (#6149)
    * adding v0.2 langchainjs support replaced ZeroShotAgent with createReactAgent
    
    * Using the LangChain Expression Language (LCEL) instead of LLMChain deprecated class
    
    * fixing farmat ci check
    
    ---------
    
    Co-authored-by: Brace Sproul <braceasproul@gmail.com>

--- examples/src/chains/llm_chain.ts
+++ examples/src/chains/llm_chain.ts
@@ -1,5 +1,4 @@
 import { OpenAI } from "@langchain/openai";
-import { LLMChain } from "langchain/chains";
 import { PromptTemplate } from "@langchain/core/prompts";
 
 // We can construct an LLMChain from a PromptTemplate and an LLM.
@@ -7,7 +6,8 @@ const model = new OpenAI({ temperature: 0 });
 const prompt = PromptTemplate.fromTemplate(
   "What is a good name for a company that makes {product}?"
 );
-const chainA = new LLMChain({ llm: model, prompt });
+
+const chainA = prompt.pipe({ llm: model });
 
 // The result is an object with a `text` property.
 const resA = await chainA.invoke({ product: "colorful socks" });
