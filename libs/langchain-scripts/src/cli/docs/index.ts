24c73f2c7dcca1b8f2fb1d02f515941a81a97b8d
bracesproul <braceasproul@gmail.com>
2024-07-30 11:33:37 -0700

    implemented script

--- libs/langchain-scripts/src/cli/docs/index.ts
+++ libs/langchain-scripts/src/cli/docs/index.ts
@@ -8,15 +8,18 @@ type CLIInput = {
   package: string;
   module: string;
   type: string;
-}
+};
 
 async function main() {
   const program = new Command();
   program
     .description("Create a new chat model integration docs.")
-    .option("--package <package>", "Package name, eg openai. Should be value of @langchain/<package>")
+    .option(
+      "--package <package>",
+      "Package name, eg openai. Should be value of @langchain/<package>"
+    )
     .option("--module <module>", "Module name, e.g ChatOpenAI")
-    .option("--type <type>", "Type of integration, e.g. 'chat'")
+    .option("--type <type>", "Type of integration, e.g. 'chat'");
 
   program.parse();
 
@@ -33,11 +36,11 @@ async function main() {
       await fillChatIntegrationDocTemplate({ packageName, moduleName });
       break;
     default:
-      console.error(`Invalid type: ${type}.\nOnly 'chat' is supported at this time.`);
+      console.error(
+        `Invalid type: ${type}.\nOnly 'chat' is supported at this time.`
+      );
       process.exit(1);
-    }
+  }
 }
 
-if (require.main === module) {
-  main();
-}
+main();
