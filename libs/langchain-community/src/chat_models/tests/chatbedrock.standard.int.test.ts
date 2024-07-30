5ed8753b69526ff9c3afb5f34d0729f785161e9e
Brace Sproul <braceasproul@gmail.com>
2024-07-29 17:54:14 -0700

    community[patch]: Fix handling parallel tool call results in bedrock (#6266)

--- libs/langchain-community/src/chat_models/tests/chatbedrock.standard.int.test.ts
+++ libs/langchain-community/src/chat_models/tests/chatbedrock.standard.int.test.ts
@@ -15,6 +15,7 @@ class BedrockChatStandardIntegrationTests extends ChatModelIntegrationTests<
       Cls: BedrockChat,
       chatModelHasToolCalling: true,
       chatModelHasStructuredOutput: true,
+      supportsParallelToolCalls: true,
       constructorArgs: {
         region,
         model: "anthropic.claude-3-sonnet-20240229-v1:0",
@@ -56,6 +57,13 @@ class BedrockChatStandardIntegrationTests extends ChatModelIntegrationTests<
       "BedrockChat",
       "Flaky test with Bedrock not consistently returning tool calls. TODO: Fix prompting."
     );
+  }
+
+  async testParallelToolCalling() {
+    // Anthropic is very flaky when calling multiple tools in parallel.
+    // Because of this, we pass `true` as the second arg to only verify
+    // it can handle parallel tools in the message history.
+    await super.testParallelToolCalling(undefined, true);
   }
 }
 
