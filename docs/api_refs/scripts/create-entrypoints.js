e9f2da85e3d78e6e4a07c85de50e1fd744a29e6e
bracesproul <braceasproul@gmail.com>
2024-08-07 13:39:53 -0700

    api_refs[minor]: Improve DX

--- docs/api_refs/scripts/create-entrypoints.js
+++ docs/api_refs/scripts/create-entrypoints.js
@@ -2,30 +2,29 @@ const fs = require("fs");
 const path = require("path");
 
 const BASE_TYPEDOC_CONFIG = {
-  $schema: "https://typedoc.org/schema.json",
-  out: "public",
-  sort: [
+  "$schema": "https://typedoc.org/schema.json",
+  "out": "public",
+  "sort": [
     "kind",
     "visibility",
     "instance-first",
     "required-first",
-    "alphabetical",
+    "alphabetical"
   ],
-  plugin: ["./typedoc_plugins/hide_underscore_lc.js"],
-  tsconfig: "../../tsconfig.json",
-  readme: "none",
-  excludePrivate: true,
-  excludeInternal: true,
-  excludeExternals: true,
-  excludeNotDocumented: false,
-  includeVersion: true,
-  sourceLinkTemplate:
-    "https://github.com/langchain-ai/langchainjs/blob/{gitRevision}/{path}#L{line}",
-  logLevel: "Error",
-  name: "LangChain.js",
-  skipErrorChecking: true,
-  exclude: ["dist"],
-  hostedBaseUrl: "https://v02.api.js.langchain.com/",
+  "plugin": ["./typedoc_plugins/hide_underscore_lc.js"],
+  "tsconfig": "../../tsconfig.json",
+  "readme": "none",
+  "excludePrivate": true,
+  "excludeInternal": true,
+  "excludeExternals": false, // trying this
+  "excludeNotDocumented": false,
+  "includeVersion": true,
+  "sourceLinkTemplate": "https://github.com/langchain-ai/langchainjs/blob/{gitRevision}/{path}#L{line}",
+  "logLevel": "Error",
+  "name": "LangChain.js",
+  "skipErrorChecking": true,
+  "exclude": ["dist"],
+  "hostedBaseUrl": "https://v02.api.js.langchain.com/",
 };
 
 /**
