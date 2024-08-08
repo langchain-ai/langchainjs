b59a8492291d1021f9ddcee615f7b6e4d28202a1
bracesproul <braceasproul@gmail.com>
2024-08-08 13:28:26 -0700

    community[patch]: Update JSDoc on bedrock chat

--- libs/langchain-community/src/chat_models/bedrock/web.ts
+++ libs/langchain-community/src/chat_models/bedrock/web.ts
@@ -154,100 +154,310 @@ export interface BedrockChatFields
     BaseChatModelParams {}
 
 /**
- * A type of Large Language Model (LLM) that interacts with the Bedrock
- * service. It extends the base `LLM` class and implements the
- * `BaseBedrockInput` interface. The class is designed to authenticate and
- * interact with the Bedrock service, which is a part of Amazon Web
- * Services (AWS). It uses AWS credentials for authentication and can be
- * configured with various parameters such as the model to use, the AWS
- * region, and the maximum number of tokens to generate.
+ * AWS Bedrock chat model integration.
  *
- * The `BedrockChat` class supports both synchronous and asynchronous interactions with the model,
- * allowing for streaming responses and handling new token callbacks. It can be configured with
- * optional parameters like temperature, stop sequences, and guardrail settings for enhanced control
- * over the generated responses.
+ * Setup:
+ * Install `@langchain/community` and set the following environment variables:
  *
- * @example
- * ```typescript
- * import { BedrockChat } from 'path-to-your-bedrock-chat-module';
- * import { HumanMessage } from '@langchain/core/messages';
- *
- * async function run() {
- *   // Instantiate the BedrockChat model with the desired configuration
- *   const model = new BedrockChat({
- *     model: "anthropic.claude-v2",
- *     region: "us-east-1",
- *     credentials: {
- *       accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
- *       secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
- *     },
- *     maxTokens: 150,
- *     temperature: 0.7,
- *     stopSequences: ["\n", " Human:", " Assistant:"],
- *     streaming: false,
- *     trace: "ENABLED",
- *     guardrailIdentifier: "your-guardrail-id",
- *     guardrailVersion: "1.0",
- *     guardrailConfig: {
- *       tagSuffix: "example",
- *       streamProcessingMode: "SYNCHRONOUS",
- *     },
- *   });
- *
- *   // Prepare the message to be sent to the model
- *   const message = new HumanMessage("Tell me a joke");
- *
- *   // Invoke the model with the message
- *   const res = await model.invoke([message]);
- *
- *   // Output the response from the model
- *   console.log(res);
- * }
- *
- * run().catch(console.error);
+ * ```bash
+ * npm install @langchain/openai
+ * export BEDROCK_AWS_REGION="your-aws-region"
+ * export BEDROCK_AWS_SECRET_ACCESS_KEY="your-aws-secret-access-key"
+ * export BEDROCK_AWS_ACCESS_KEY_ID="your-aws-access-key-id"
  * ```
  *
- * For streaming responses, use the following example:
- * @example
+ * ## Key args
+ *
+ * ### [Init args](/classes/langchain_community_chat_models_bedrock.BedrockChat.html#constructor)
+ *
+ * ### [Runtime args](/interfaces/langchain_community_chat_models_bedrock.BedrockChatCallOptions.html)
+ *
+ * > See full list of supported init args and their descriptions in the [`constructor`](/classes/langchain_community_chat_models_bedrock.BedrockChat.html#constructor) section.
+ *
+ * ## Examples
+ *
+ * <details open>
+ * <summary><strong>Instantiate</strong></summary>
+ *
  * ```typescript
- * import { BedrockChat } from 'path-to-your-bedrock-chat-module';
- * import { HumanMessage } from '@langchain/core/messages';
+ * import { BedrockChat } from '@langchain/community/chat_models/bedrock';
  *
- * async function runStreaming() {
- *   // Instantiate the BedrockChat model with the desired configuration
- *   const model = new BedrockChat({
- *     model: "anthropic.claude-3-sonnet-20240229-v1:0",
- *     region: "us-east-1",
- *     credentials: {
- *       accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
- *       secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
- *     },
- *     maxTokens: 150,
- *     temperature: 0.7,
- *     stopSequences: ["\n", " Human:", " Assistant:"],
- *     streaming: true,
- *     trace: "ENABLED",
- *     guardrailIdentifier: "your-guardrail-id",
- *     guardrailVersion: "1.0",
- *     guardrailConfig: {
- *       tagSuffix: "example",
- *       streamProcessingMode: "SYNCHRONOUS",
- *     },
- *   });
+ * const llm = new BedrockChat({
+ *   region: process.env.BEDROCK_AWS_REGION,
+ *   maxRetries: 0,
+ *   credentials: {
+ *     secretAccessKey: process.env.BEDROCK_AWS_SECRET_ACCESS_KEY!,
+ *     accessKeyId: process.env.BEDROCK_AWS_ACCESS_KEY_ID!,
+ *   },
+ *   model: "anthropic.claude-3-5-sonnet-20240620-v1:0",
+ *   temperature: 0,
+ *   maxTokens: undefined,
+ *   // other params...
+ * });
+ * ```
+ * </details>
  *
- *   // Prepare the message to be sent to the model
- *   const message = new HumanMessage("Tell me a joke");
+ * <br />
  *
- *   // Stream the response from the model
- *   const stream = await model.stream([message]);
- *   for await (const chunk of stream) {
- *     // Output each chunk of the response
- *     console.log(chunk);
+ * <details>
+ * <summary><strong>Invoking</strong></summary>
+ *
+ * ```typescript
+ * const messages = [
+ *   {
+ *     type: "system" as const,
+ *     content: "You are a helpful translator. Translate the user sentence to French.",
+ *   },
+ *   {
+ *     type: "human" as const,
+ *     content: "I love programming.",
+ *   },
+ * ];
+ * const result = await llm.invoke(messages);
+ * console.log(result);
+ * ```
+ * 
+ * ```txt
+ * AIMessage {
+ *   "content": "Here's the translation to French:\n\nJ'adore la programmation.",
+ *   "additional_kwargs": {
+ *     "id": "msg_bdrk_01HCZHa2mKbMZeTeHjLDd286"
+ *   },
+ *   "response_metadata": {
+ *     "type": "message",
+ *     "role": "assistant",
+ *     "model": "claude-3-5-sonnet-20240620",
+ *     "stop_reason": "end_turn",
+ *     "stop_sequence": null,
+ *     "usage": {
+ *       "input_tokens": 25,
+ *       "output_tokens": 19
+ *     }
+ *   },
+ *   "tool_calls": [],
+ *   "invalid_tool_calls": []
+ * }
+ * ```
+ * </details>
+ *
+ * <br />
+ *
+ * <details>
+ * <summary><strong>Streaming Chunks</strong></summary>
+ *
+ * ```typescript
+ * for await (const chunk of await llm.stream(messages)) {
+ *   console.log(chunk);
+ * }
+ * ```
+ * AIMessageChunk {
+ *   "content": "",
+ *   "additional_kwargs": {
+ *     "id": "msg_bdrk_01RhFuGR9uJ2bj5GbdAma4y6"
+ *   },
+ *   "response_metadata": {
+ *     "type": "message",
+ *     "role": "assistant",
+ *     "model": "claude-3-5-sonnet-20240620",
+ *     "stop_reason": null,
+ *     "stop_sequence": null
+ *   },
+ * }
+ * AIMessageChunk {
+ *   "content": "J",
+ * }
+ * AIMessageChunk {
+ *   "content": "'adore la",
+ * }
+ * AIMessageChunk {
+ *   "content": " programmation.",
+ * }
+ * AIMessageChunk {
+ *   "content": "",
+ *   "additional_kwargs": {
+ *     "stop_reason": "end_turn",
+ *     "stop_sequence": null
+ *   },
+ * }
+ * AIMessageChunk {
+ *   "content": "",
+ *   "response_metadata": {
+ *     "amazon-bedrock-invocationMetrics": {
+ *       "inputTokenCount": 25,
+ *       "outputTokenCount": 11,
+ *       "invocationLatency": 659,
+ *       "firstByteLatency": 506
+ *     }
+ *   },
+ *   "usage_metadata": {
+ *     "input_tokens": 25,
+ *     "output_tokens": 11,
+ *     "total_tokens": 36
  *   }
  * }
- *
- * runStreaming().catch(console.error);
+ * ```txt
+ * 
  * ```
+ * </details>
+ *
+ * <br />
+ *
+ * <details>
+ * <summary><strong>Aggregate Streamed Chunks</strong></summary>
+ *
+ * ```typescript
+ * import { AIMessageChunk } from '@langchain/core/messages';
+ * import { concat } from '@langchain/core/utils/stream';
+ *
+ * const stream = await llm.stream(messages);
+ * let full: AIMessageChunk | undefined;
+ * for await (const chunk of stream) {
+ *   full = !full ? chunk : concat(full, chunk);
+ * }
+ * console.log(full);
+ * ```
+ * 
+ * ```txt
+ * AIMessageChunk {
+ *   "content": "J'adore la programmation.",
+ *   "additional_kwargs": {
+ *     "id": "msg_bdrk_017b6PuBybA51P5LZ9K6gZHm",
+ *     "stop_reason": "end_turn",
+ *     "stop_sequence": null
+ *   },
+ *   "response_metadata": {
+ *     "type": "message",
+ *     "role": "assistant",
+ *     "model": "claude-3-5-sonnet-20240620",
+ *     "stop_reason": null,
+ *     "stop_sequence": null,
+ *     "amazon-bedrock-invocationMetrics": {
+ *       "inputTokenCount": 25,
+ *       "outputTokenCount": 11,
+ *       "invocationLatency": 1181,
+ *       "firstByteLatency": 1177
+ *     }
+ *   },
+ *   "usage_metadata": {
+ *     "input_tokens": 25,
+ *     "output_tokens": 11,
+ *     "total_tokens": 36
+ *   }
+ * }
+ * ```
+ * </details>
+ *
+ * <br />
+ *
+ * <details>
+ * <summary><strong>Bind tools</strong></summary>
+ *
+ * ```typescript
+ * import { z } from 'zod';
+ * import { AIMessage } from '@langchain/core/messages';
+ *
+ * const GetWeather = {
+ *   name: "GetWeather",
+ *   description: "Get the current weather in a given location",
+ *   schema: z.object({
+ *     location: z.string().describe("The city and state, e.g. San Francisco, CA")
+ *   }),
+ * }
+ *
+ * const GetPopulation = {
+ *   name: "GetPopulation",
+ *   description: "Get the current population in a given location",
+ *   schema: z.object({
+ *     location: z.string().describe("The city and state, e.g. San Francisco, CA")
+ *   }),
+ * }
+ *
+ * const llmWithTools = llm.bindTools([GetWeather, GetPopulation]);
+ * const aiMsg: AIMessage = await llmWithTools.invoke(
+ *   "Which city is hotter today and which is bigger: LA or NY?"
+ * );
+ * console.log(aiMsg.tool_calls);
+ * ```
+ * 
+ * ```txt
+ * [
+ *   {
+ *     name: 'GetWeather',
+ *     args: { location: 'Los Angeles, CA' },
+ *     id: 'toolu_bdrk_01R2daqwHR931r4baVNzbe38',
+ *     type: 'tool_call'
+ *   },
+ *   {
+ *     name: 'GetWeather',
+ *     args: { location: 'New York, NY' },
+ *     id: 'toolu_bdrk_01WDadwNc7PGqVZvCN7Dr7eD',
+ *     type: 'tool_call'
+ *   },
+ *   {
+ *     name: 'GetPopulation',
+ *     args: { location: 'Los Angeles, CA' },
+ *     id: 'toolu_bdrk_014b8zLkpAgpxrPfewKinJFc',
+ *     type: 'tool_call'
+ *   },
+ *   {
+ *     name: 'GetPopulation',
+ *     args: { location: 'New York, NY' },
+ *     id: 'toolu_bdrk_01Tt8K2MUP15kNuMDFCLEFKN',
+ *     type: 'tool_call'
+ *   }
+ * ]
+ * ```
+ * </details>
+ *
+ * <br />
+ *
+ * <details>
+ * <summary><strong>Structured Output</strong></summary>
+ *
+ * ```typescript
+ * const Joke = z.object({
+ *   setup: z.string().describe("The setup of the joke"),
+ *   punchline: z.string().describe("The punchline to the joke"),
+ *   rating: z.number().optional().describe("How funny the joke is, from 1 to 10")
+ * }).describe('Joke to tell user.');
+ *
+ * const structuredLlm = llm.withStructuredOutput(Joke);
+ * const jokeResult = await structuredLlm.invoke("Tell me a joke about cats");
+ * console.log(jokeResult);
+ * ```
+ * 
+ * ```txt
+ * {
+ *   setup: "Why don't cats play poker in the jungle?",
+ *   punchline: 'Too many cheetahs!'
+ * }
+ * ```
+ * </details>
+ *
+ * <br />
+ *
+ * <details>
+ * <summary><strong>Response Metadata</strong></summary>
+ *
+ * ```typescript
+ * const aiMsgForResponseMetadata = await llm.invoke(messages);
+ * console.log(aiMsgForResponseMetadata.response_metadata);
+ * ```
+ * 
+ * ```txt
+ * "response_metadata": {
+ *   "type": "message",
+ *   "role": "assistant",
+ *   "model": "claude-3-5-sonnet-20240620",
+ *   "stop_reason": "end_turn",
+ *   "stop_sequence": null,
+ *   "usage": {
+ *     "input_tokens": 25,
+ *     "output_tokens": 19
+ *   }
+ * }
+ * ```
+ * </details>
  */
 export class BedrockChat
   extends BaseChatModel<BedrockChatCallOptions, AIMessageChunk>
