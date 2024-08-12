b44740fed015646121530b69bda7939b6be5b73a
bracesproul <braceasproul@gmail.com>
2024-08-12 12:48:20 -0700

    cr

--- docs/api_refs/scripts/update-typedoc-css.js
+++ docs/api_refs/scripts/update-typedoc-css.js
@@ -1,55 +1,52 @@
 const { readFile, writeFile } = require("fs/promises");
 
-async function main() {
-  const css = `\n.tsd-navigation {
-    word-break: break-word;
-}
-
-.col-content {
-    min-width: fit-content;
+const CSS = `\n.tsd-navigation {
+  word-break: break-word;
 }
 
 .page-menu {
-    display: none;
+  display: none;
 }
 
 .deprecation-warning {
-  background-color: #ef4444;
-  border-radius: 0.375rem;
-  display: flex;
-  flex-direction: column;
-  padding: 12px;
-  text-align: left;
+background-color: #ef4444;
+border-radius: 0.375rem;
+display: flex;
+flex-direction: column;
+padding: 12px;
+text-align: left;
 }
 
 .version-select {
-  display: inline-block;
-  margin-left: 10px;
-  z-index: 1;
+display: inline-block;
+margin-left: 10px;
+z-index: 1;
 }
 
 .version-select select {
-  padding: 2.5px 5px;
-  font-size: 14px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  background-color: #fff;
-  color: #333;
-  cursor: pointer;
+padding: 2.5px 5px;
+font-size: 14px;
+border: 1px solid #ccc;
+border-radius: 4px;
+background-color: #fff;
+color: #333;
+cursor: pointer;
 }
 
 .version-select select:hover {
-  border-color: #999;
+border-color: #999;
 }
 
 .version-select select:focus {
-  outline: none;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+outline: none;
+box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
 }
 `;
 
-  let file = await readFile("./public/assets/style.css", "utf-8");
-  file += css;
-  await writeFile("./public/assets/style.css", file);
+async function main() {
+  let cssContents = await readFile("./public/assets/style.css", "utf-8");
+  cssContents += CSS;
+  await writeFile("./public/assets/style.css", cssContents);
 }
+
 main();
