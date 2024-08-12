66ef96c7bb5883048fa7d662590275cafb6564fc
jacoblee93 <jacoblee93@gmail.com>
2024-08-12 05:42:21 -0700

    Remove signal event listener when completed

--- langchain-core/src/utils/signal.ts
+++ langchain-core/src/utils/signal.ts
@@ -5,6 +5,7 @@ export async function raceWithSignal<T>(
   if (signal === undefined) {
     return promise;
   }
+  let listener: () => void;
   return Promise.race([
     promise.catch<T>((err) => {
       if (!signal?.aborted) {
@@ -14,13 +15,14 @@ export async function raceWithSignal<T>(
       }
     }),
     new Promise<never>((_, reject) => {
-      signal.addEventListener("abort", () => {
+      listener = () => {
         reject(new Error("Aborted"));
-      });
+      };
+      signal.addEventListener("abort", listener);
       // Must be here inside the promise to avoid a race condition
       if (signal.aborted) {
         reject(new Error("Aborted"));
       }
     }),
-  ]);
+  ]).finally(() => signal.removeEventListener("abort", listener));
 }
