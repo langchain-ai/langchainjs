76dbe6033543be7c6023c1c3cee50876f290fe7a
bracesproul <braceasproul@gmail.com>
2024-08-12 13:53:39 -0700

    cr

--- docs/api_refs/scripts/update-typedoc-css.js
+++ docs/api_refs/scripts/update-typedoc-css.js
@@ -9,37 +9,37 @@ const CSS = `\n.tsd-navigation {
 }
 
 .deprecation-warning {
-background-color: #ef4444;
-border-radius: 0.375rem;
-display: flex;
-flex-direction: column;
-padding: 12px;
-text-align: left;
+  background-color: #ef4444;
+  border-radius: 0.375rem;
+  display: flex;
+  flex-direction: column;
+  padding: 12px;
+  text-align: left;
 }
 
 .version-select {
-display: inline-block;
-margin-left: 10px;
-z-index: 1;
+  display: inline-block;
+  margin-left: 10px;
+  z-index: 1;
 }
 
 .version-select select {
-padding: 2.5px 5px;
-font-size: 14px;
-border: 1px solid #ccc;
-border-radius: 4px;
-background-color: #fff;
-color: #333;
-cursor: pointer;
+  padding: 2.5px 5px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  color: #333;
+  cursor: pointer;
 }
 
 .version-select select:hover {
-border-color: #999;
+  border-color: #999;
 }
 
 .version-select select:focus {
-outline: none;
-box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+  outline: none;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
 }
 `;
 
