2619253bb18f84c01168190f521c0d9d4dbc0984
bracesproul <braceasproul@gmail.com>
2024-08-15 14:44:15 -0700

    revert non web test changes

--- libs/langchain-google-vertexai/src/tests/chat_models.standard.int.test.ts
+++ libs/langchain-google-vertexai/src/tests/chat_models.standard.int.test.ts
@@ -3,7 +3,6 @@ import { test, expect } from "@jest/globals";
 import { ChatModelIntegrationTests } from "@langchain/standard-tests";
 import { AIMessageChunk } from "@langchain/core/messages";
 import { GoogleAIBaseLanguageModelCallOptions } from "@langchain/google-common";
-import fs from "fs";
 import { ChatVertexAI } from "../chat_models.js";
 
 class ChatVertexAIStandardIntegrationTests extends ChatModelIntegrationTests<
@@ -11,7 +10,12 @@ class ChatVertexAIStandardIntegrationTests extends ChatModelIntegrationTests<
   AIMessageChunk
 > {
   constructor() {
-    const superValues = {
+    if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+      throw new Error(
+        "GOOGLE_APPLICATION_CREDENTIALS must be set to run standard integration tests."
+      );
+    }
+    super({
       Cls: ChatVertexAI,
       chatModelHasToolCalling: true,
       chatModelHasStructuredOutput: true,
@@ -20,39 +24,7 @@ class ChatVertexAIStandardIntegrationTests extends ChatModelIntegrationTests<
       constructorArgs: {
         model: "gemini-1.5-pro",
       },
-    };
-    if (process.env.GOOGLE_APPLICATION_CREDENTIALS === "true") {
-      super({
-        ...superValues,
-        constructorArgs: {
-          ...superValues.constructorArgs,
-          keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-        },
-      });
-    } else if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
-      try {
-        // If this env var is set we're running in a CI environment and need to
-        // write the credentials to a file. The `credentials.json` file will be
-        // removed at the end of the test.
-        const newCredentialsPath = "./credentials.json";
-        fs.writeFileSync(
-          newCredentialsPath,
-          process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON
-        );
-        super({
-          ...superValues,
-          constructorArgs: {
-            ...superValues.constructorArgs,
-            keyFile: newCredentialsPath,
-          },
-        });
-      } catch (e) {
-        console.error("Error writing credentials file.");
-        throw e;
-      }
-    } else {
-      throw new Error("Missing secrets for Google VertexAI standard tests.");
-    }
+    });
   }
 
   async testToolMessageHistoriesListContent() {
@@ -77,23 +49,11 @@ class ChatVertexAIStandardIntegrationTests extends ChatModelIntegrationTests<
     // This is because the model struggles to actually call parallel tools.
     await super.testParallelToolCalling(undefined, true);
   }
-
-  cleanup() {
-    if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
-      // If this env var is present, it means we're running in a CI
-      // environment and we need to clean up the credentials file.
-      fs.unlinkSync("./credentials.json");
-    }
-  }
 }
 
 const testClass = new ChatVertexAIStandardIntegrationTests();
 
 test("ChatVertexAIStandardIntegrationTests", async () => {
-  try {
-    const testResults = await testClass.runTests();
-    expect(testResults).toBe(true);
-  } finally {
-    testClass.cleanup();
-  }
+  const testResults = await testClass.runTests();
+  expect(testResults).toBe(true);
 });
