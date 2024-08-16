f1b7ad74f91d6a199b0b0981be3f2fe362a67b93
choeller <christoph.hoeller@googlemail.com>
2024-08-16 11:02:52 +0200

    community[patch]: fix: ElasticVectorSearch: exclude metadata filters … (#6536)
    * community[patch]: fix: ElasticVectorSearch: exclude metadata filters not working due to syntax error in filter creation
    
    * Adds integration test
    
    ---------
    
    Co-authored-by: Christoph Höller <christoph.hoeller@redbull.com>
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/src/vectorstores/tests/elasticsearch.int.test.ts
+++ libs/langchain-community/src/vectorstores/tests/elasticsearch.int.test.ts
@@ -106,6 +106,14 @@ describe("ElasticVectorSearch", () => {
     ]);
     const results = await store.similaritySearch("*", 11);
     expect(results).toHaveLength(11);
+    const results2 = await store.similaritySearch("*", 11, [
+      {
+        field: "a",
+        value: createdAt,
+        operator: "exclude",
+      },
+    ]);
+    expect(results2).toHaveLength(1);
   });
 
   test.skip("ElasticVectorSearch integration with text splitting metadata", async () => {
