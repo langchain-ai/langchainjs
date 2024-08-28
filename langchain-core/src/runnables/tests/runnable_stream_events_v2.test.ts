0c003d8d602cf7ac396053ac59b22deb2b95f1bb
David Duong <david@duong.cz>
2024-08-28 20:24:00 +0200

    feat(core): always provide message ID inferred from run ID if not present while streaming (#6646)

--- langchain-core/src/runnables/tests/runnable_stream_events_v2.test.ts
+++ langchain-core/src/runnables/tests/runnable_stream_events_v2.test.ts
@@ -90,6 +90,10 @@ test("Runnable streamEvents method on a chat model", async () => {
   for await (const event of eventStream) {
     events.push(event);
   }
+
+  // used here to avoid casting every ID
+  const anyString = expect.any(String) as unknown as string;
+
   expect(events).toMatchObject([
     {
       data: { input: "hello" },
@@ -100,7 +104,7 @@ test("Runnable streamEvents method on a chat model", async () => {
       tags: [],
     },
     {
-      data: { chunk: new AIMessageChunk({ content: "a" }) },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "a" }) },
       event: "on_chat_model_stream",
       name: "FakeListChatModel",
       metadata: expect.any(Object),
@@ -108,7 +112,7 @@ test("Runnable streamEvents method on a chat model", async () => {
       tags: [],
     },
     {
-      data: { chunk: new AIMessageChunk({ content: "b" }) },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "b" }) },
       event: "on_chat_model_stream",
       name: "FakeListChatModel",
       metadata: expect.any(Object),
@@ -116,7 +120,7 @@ test("Runnable streamEvents method on a chat model", async () => {
       tags: [],
     },
     {
-      data: { chunk: new AIMessageChunk({ content: "c" }) },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "c" }) },
       event: "on_chat_model_stream",
       name: "FakeListChatModel",
       metadata: expect.any(Object),
@@ -124,7 +128,7 @@ test("Runnable streamEvents method on a chat model", async () => {
       tags: [],
     },
     {
-      data: { output: new AIMessageChunk({ content: "abc" }) },
+      data: { output: new AIMessageChunk({ id: anyString, content: "abc" }) },
       event: "on_chat_model_end",
       name: "FakeListChatModel",
       metadata: expect.any(Object),
@@ -748,6 +752,9 @@ test("Runnable streamEvents method with chat model chain", async () => {
   for await (const event of eventStream) {
     events.push(event);
   }
+
+  // used here to avoid casting every ID
+  const anyString = expect.any(String) as unknown as string;
   expect(events).toEqual([
     {
       run_id: expect.any(String),
@@ -816,7 +823,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("R") },
+      data: { chunk: new AIMessageChunk({ content: "R", id: anyString }) },
     },
     {
       event: "on_chain_stream",
@@ -826,7 +833,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         foo: "bar",
       },
       name: "my_chain",
-      data: { chunk: new AIMessageChunk("R") },
+      data: { chunk: new AIMessageChunk({ content: "R", id: anyString }) },
     },
     {
       event: "on_chat_model_stream",
@@ -839,7 +846,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("O") },
+      data: { chunk: new AIMessageChunk({ content: "O", id: anyString }) },
     },
     {
       event: "on_chain_stream",
@@ -849,7 +856,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         foo: "bar",
       },
       name: "my_chain",
-      data: { chunk: new AIMessageChunk("O") },
+      data: { chunk: new AIMessageChunk({ content: "O", id: anyString }) },
     },
     {
       event: "on_chat_model_stream",
@@ -862,7 +869,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("A") },
+      data: { chunk: new AIMessageChunk({ content: "A", id: anyString }) },
     },
     {
       event: "on_chain_stream",
@@ -872,7 +879,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         foo: "bar",
       },
       name: "my_chain",
-      data: { chunk: new AIMessageChunk("A") },
+      data: { chunk: new AIMessageChunk({ content: "A", id: anyString }) },
     },
     {
       event: "on_chat_model_stream",
@@ -885,7 +892,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("R") },
+      data: { chunk: new AIMessageChunk({ content: "R", id: anyString }) },
     },
     {
       event: "on_chain_stream",
@@ -895,7 +902,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
         foo: "bar",
       },
       name: "my_chain",
-      data: { chunk: new AIMessageChunk("R") },
+      data: { chunk: new AIMessageChunk({ content: "R", id: anyString }) },
     },
     {
       event: "on_chat_model_end",
@@ -914,7 +921,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
             [new SystemMessage("You are Godzilla"), new HumanMessage("hello")],
           ],
         },
-        output: new AIMessageChunk("ROAR"),
+        output: new AIMessageChunk({ content: "ROAR", id: anyString }),
       },
     },
     {
@@ -925,9 +932,7 @@ test("Runnable streamEvents method with chat model chain", async () => {
       metadata: {
         foo: "bar",
       },
-      data: {
-        output: new AIMessageChunk("ROAR"),
-      },
+      data: { output: new AIMessageChunk({ content: "ROAR", id: anyString }) },
     },
   ]);
 });
@@ -965,6 +970,10 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
   for await (const event of eventStream) {
     events.push(event);
   }
+
+  // used here to avoid casting every ID
+  const anyString = expect.any(String) as unknown as string;
+
   expect(events).toEqual([
     {
       run_id: expect.any(String),
@@ -1043,7 +1052,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("R") },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "R" }) },
     },
     {
       event: "on_chat_model_stream",
@@ -1056,7 +1065,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("O") },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "O" }) },
     },
     {
       event: "on_chat_model_stream",
@@ -1069,7 +1078,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("A") },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "A" }) },
     },
     {
       event: "on_chat_model_stream",
@@ -1082,7 +1091,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("R") },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "R" }) },
     },
     {
       event: "on_chat_model_end",
@@ -1101,7 +1110,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
             [new SystemMessage("You are Godzilla"), new HumanMessage("hello")],
           ],
         },
-        output: new AIMessageChunk("ROAR"),
+        output: new AIMessageChunk({ id: anyString, content: "ROAR" }),
       },
     },
     {
@@ -1112,7 +1121,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         foo: "bar",
       },
       name: "RunnableLambda",
-      data: { chunk: new AIMessageChunk("ROAR") },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "ROAR" }) },
     },
     {
       event: "on_chain_stream",
@@ -1122,7 +1131,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         foo: "bar",
       },
       name: "my_chain",
-      data: { chunk: new AIMessageChunk("ROAR") },
+      data: { chunk: new AIMessageChunk({ id: anyString, content: "ROAR" }) },
     },
     {
       event: "on_chain_end",
@@ -1134,7 +1143,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
       },
       data: {
         input: await template.invoke({ question: "hello" }),
-        output: new AIMessageChunk("ROAR"),
+        output: new AIMessageChunk({ id: anyString, content: "ROAR" }),
       },
     },
     {
@@ -1146,7 +1155,7 @@ test("Chat model that supports streaming, but is invoked, should still emit on_s
         foo: "bar",
       },
       data: {
-        output: new AIMessageChunk("ROAR"),
+        output: new AIMessageChunk({ id: anyString, content: "ROAR" }),
       },
     },
   ]);
@@ -1183,6 +1192,8 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
   for await (const event of eventStream) {
     events.push(event);
   }
+
+  const anyString = expect.any(String) as unknown as string;
   expect(events).toEqual([
     {
       run_id: expect.any(String),
@@ -1261,7 +1272,12 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
         ls_stop: undefined,
       },
       name: "my_model",
-      data: { chunk: new AIMessageChunk("You are Godzilla\nhello") },
+      data: {
+        chunk: new AIMessageChunk({
+          id: anyString,
+          content: "You are Godzilla\nhello",
+        }),
+      },
     },
     {
       event: "on_chat_model_end",
@@ -1280,7 +1296,10 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
             [new SystemMessage("You are Godzilla"), new HumanMessage("hello")],
           ],
         },
-        output: new AIMessage("You are Godzilla\nhello"),
+        output: new AIMessage({
+          id: anyString,
+          content: "You are Godzilla\nhello",
+        }),
       },
     },
     {
@@ -1291,7 +1310,12 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
         foo: "bar",
       },
       name: "RunnableLambda",
-      data: { chunk: new AIMessage("You are Godzilla\nhello") },
+      data: {
+        chunk: new AIMessage({
+          id: anyString,
+          content: "You are Godzilla\nhello",
+        }),
+      },
     },
     {
       event: "on_chain_stream",
@@ -1301,7 +1325,12 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
         foo: "bar",
       },
       name: "my_chain",
-      data: { chunk: new AIMessage("You are Godzilla\nhello") },
+      data: {
+        chunk: new AIMessage({
+          id: anyString,
+          content: "You are Godzilla\nhello",
+        }),
+      },
     },
     {
       event: "on_chain_end",
@@ -1313,7 +1342,10 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
       },
       data: {
         input: await template.invoke({ question: "hello" }),
-        output: new AIMessage("You are Godzilla\nhello"),
+        output: new AIMessage({
+          id: anyString,
+          content: "You are Godzilla\nhello",
+        }),
       },
     },
     {
@@ -1325,7 +1357,10 @@ test("Chat model that doesn't support streaming, but is invoked, should emit one
         foo: "bar",
       },
       data: {
-        output: new AIMessage("You are Godzilla\nhello"),
+        output: new AIMessage({
+          id: anyString,
+          content: "You are Godzilla\nhello",
+        }),
       },
     },
   ]);
