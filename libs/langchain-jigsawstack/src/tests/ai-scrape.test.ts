8a9fff739156317d6b308ddf9020dcf1bc10c220
Narcisse <mechikaegon@gmail.com>
2024-08-29 21:31:22 +0100

    clean up

--- libs/langchain-jigsawstack/src/tests/ai-scrape.test.ts
+++ libs/langchain-jigsawstack/src/tests/ai-scrape.test.ts
@@ -6,8 +6,6 @@ it("should run success scrape result", async () => {
     params: {
       element_prompts: ["Pro plan"],
     },
-    apiKey:
-      "sk_0b9b4fab624794c93fd423d4c079738a8bf0eed4b5c93dd4ac0d77b155c3d7ce91a572f60d1ac4d00a799d0f924d431f6ab42c4a128d689bdae69db59cf67b25024fS7GiEZWQ2HvXOHcdt",
   });
   const metadata = await tool.invoke("https://jigsawstack.com/pricing");
   const jsonData = JSON.parse(metadata);
--- libs/langchain-jigsawstack/src/tests/integration.int.test.ts
+++ libs/langchain-jigsawstack/src/tests/integration.int.test.ts
@@ -1,14 +1,12 @@
 import { expect, test } from "@jest/globals";
 import { JigsawStackAIScrape } from "../tools/ai-scrape.js";
-import { JigsawStackVOCR } from "../tools/vocr.js";
+import { JigsawStackTextToSQL } from "../tools/text-to-sql.js";
 
 test("JigsawStackAIScrape can scrape a website given a url and prompt", async () => {
   const tool = new JigsawStackAIScrape({
     params: {
       element_prompts: ["Pro plan"],
     },
-    apiKey:
-      "sk_0b9b4fab624794c93fd423d4c079738a8bf0eed4b5c93dd4ac0d77b155c3d7ce91a572f60d1ac4d00a799d0f924d431f6ab42c4a128d689bdae69db59cf67b25024fS7GiEZWQ2HvXOHcdt",
   });
 
   const toolData = await tool.invoke("https://jigsawstack.com/pricing");
@@ -23,8 +21,6 @@ test("JigsawStackVocr can perform vision ocr", async () => {
     params: {
       prompt: "Describe the image in detail",
     },
-    apiKey:
-      "sk_0b9b4fab624794c93fd423d4c079738a8bf0eed4b5c93dd4ac0d77b155c3d7ce91a572f60d1ac4d00a799d0f924d431f6ab42c4a128d689bdae69db59cf67b25024fS7GiEZWQ2HvXOHcdt",
   });
 
   const toolData = await tool.invoke(
