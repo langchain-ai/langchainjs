5c6c4b409902f3e4912ec9ff5a4cd3ef87038d85
jacoblee93 <jacoblee93@gmail.com>
2024-09-06 15:21:12 -0700

    Format

--- libs/langchain-community/src/vectorstores/chroma.ts
+++ libs/langchain-community/src/vectorstores/chroma.ts
@@ -4,7 +4,7 @@ import type {
   Collection,
   ChromaClientParams,
   CollectionMetadata,
-  Where
+  Where,
 } from "chromadb";
 
 import type { EmbeddingsInterface } from "@langchain/core/embeddings";
