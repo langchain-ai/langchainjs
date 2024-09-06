728ddc726177d05b6da28ca1aba0553cbdfb58c8
jacoblee93 <jacoblee93@gmail.com>
2024-09-06 14:20:23 -0700

    Remove document loaders

--- langchain/src/document_loaders/tests/multi_file.test.ts
+++ langchain/src/document_loaders/tests/multi_file.test.ts
@@ -2,8 +2,6 @@ import * as url from "node:url";
 import * as path from "node:path";
 import { test, expect } from "@jest/globals";
 import { MultiFileLoader } from "../fs/multi_file.js";
-import { CSVLoader } from "../fs/csv.js";
-import { PDFLoader } from "../fs/pdf.js";
 import { TextLoader } from "../fs/text.js";
 import { JSONLoader } from "../fs/json.js";
 import { UnknownHandling } from "../fs/directory.js";
@@ -33,13 +31,6 @@ test("Test MultiFileLoader", async () => {
   const loader = new MultiFileLoader(
     filePaths,
     {
-      ".csv": (p) => {
-        if (p.includes("separator.csv")) {
-          return new CSVLoader(p, { column: "html", separator: "｜" });
-        }
-        return new CSVLoader(p, "html");
-      },
-      ".pdf": (p) => new PDFLoader(p),
       ".txt": (p) => new TextLoader(p),
       ".json": (p) => new JSONLoader(p),
     },
@@ -50,18 +41,6 @@ test("Test MultiFileLoader", async () => {
   expect(docs.length).toBe(123);
 
   const expectedSources = [
-    // PDF
-    ...Array.from({ length: 15 }, (_) =>
-      path.resolve(baseDirectory, "1706.03762.pdf")
-    ),
-    path.resolve(baseDirectory, "Jacob_Lee_Resume_2023.pdf"),
-    // CSV
-    ...Array.from({ length: 32 }, (_) =>
-      path.resolve(
-        baseDirectory,
-        "Star_Wars_The_Clone_Wars_S06E07_Crisis_at_the_Heart.csv"
-      )
-    ),
     // JSON
     ...Array.from({ length: 32 }, (_) =>
       path.resolve(
@@ -74,10 +53,6 @@ test("Test MultiFileLoader", async () => {
     ),
     // TXT
     path.resolve(baseDirectory, "example.txt"),
-    // CSV
-    ...Array.from({ length: 32 }, (_) =>
-      path.resolve(baseDirectory, "example_separator.csv")
-    ),
   ];
 
   expect(docs.map((d) => d.metadata.source).sort()).toEqual(expectedSources);
