34421e04e6b0aea8f84bdd2bb9c74e35f206214a
Raj Mehta <rajmehta231@gmail.com>
2024-09-10 15:38:42 -0700

    feat(community): BM25 Keyword Search Retriever (#6719)
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/langchain.config.js
+++ libs/langchain-community/langchain.config.js
@@ -196,6 +196,7 @@ export const config = {
     // retrievers
     "retrievers/amazon_kendra": "retrievers/amazon_kendra",
     "retrievers/amazon_knowledge_base": "retrievers/amazon_knowledge_base",
+    "retrievers/bm25": "retrievers/bm25",
     "retrievers/chaindesk": "retrievers/chaindesk",
     "retrievers/databerry": "retrievers/databerry",
     "retrievers/dria": "retrievers/dria",
