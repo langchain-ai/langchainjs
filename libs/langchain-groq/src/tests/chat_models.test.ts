66474854efed15d8df691417b7e063fae7a55fa1
Bagatur <22008038+baskaryan@users.noreply.github.com>
2024-09-23 13:47:11 -0700

    groq[patch]: set api key attr (#6864)
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-groq/src/tests/chat_models.test.ts
+++ libs/langchain-groq/src/tests/chat_models.test.ts
@@ -10,3 +10,11 @@ test("Serialization", () => {
     `{"lc":1,"type":"constructor","id":["langchain","chat_models","groq","ChatGroq"],"kwargs":{"api_key":{"lc":1,"type":"secret","id":["GROQ_API_KEY"]}}}`
   );
 });
+
+test("Serialization with no params", () => {
+  process.env.GROQ_API_KEY = "foo";
+  const model = new ChatGroq();
+  expect(JSON.stringify(model)).toEqual(
+    `{"lc":1,"type":"constructor","id":["langchain","chat_models","groq","ChatGroq"],"kwargs":{"api_key":{"lc":1,"type":"secret","id":["GROQ_API_KEY"]}}}`
+  );
+});
