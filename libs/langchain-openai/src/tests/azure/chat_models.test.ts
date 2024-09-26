407b3985c23b2bd7211284ed5d5d945063edf0b9
Bagatur <baskaryan@gmail.com>
2024-09-26 09:27:16 -0700

    fix test

--- libs/langchain-openai/src/tests/azure/chat_models.test.ts
+++ libs/langchain-openai/src/tests/azure/chat_models.test.ts
@@ -5,6 +5,7 @@ test("Test Azure OpenAI serialization from azure endpoint", async () => {
     azureOpenAIEndpoint: "https://foobar.openai.azure.com/",
     azureOpenAIApiDeploymentName: "gpt-4o",
     azureOpenAIApiVersion: "2024-08-01-preview",
+    azureOpenAIApiKey: "foo",
   });
   expect(JSON.stringify(chat)).toEqual(
     `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"azure_endpoint":"https://foobar.openai.azure.com/","openai_api_key":{"lc":1,"type":"secret","id":["OPENAI_API_KEY"]},"deployment_name":"gpt-4o"}}`
@@ -16,6 +17,7 @@ test("Test Azure OpenAI serialization from base path", async () => {
     azureOpenAIBasePath:
       "https://foobar.openai.azure.com/openai/deployments/gpt-4o",
     azureOpenAIApiVersion: "2024-08-01-preview",
+    azureOpenAIApiKey: "foo",
   });
   expect(JSON.stringify(chat)).toEqual(
     `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"openai_api_key":{"lc":1,"type":"secret","id":["OPENAI_API_KEY"]},"azure_endpoint":"https://langchain-ai.openai.azure.com/","deployment_name":"gpt-4o"}}`
@@ -27,6 +29,7 @@ test("Test Azure OpenAI serialization from instance name", async () => {
     azureOpenAIApiInstanceName: "foobar",
     azureOpenAIApiDeploymentName: "gpt-4o",
     azureOpenAIApiVersion: "2024-08-01-preview",
+    azureOpenAIApiKey: "foo",
   });
   expect(JSON.stringify(chat)).toEqual(
     `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"openai_api_key":{"lc":1,"type":"secret","id":["OPENAI_API_KEY"]},"azure_endpoint":"https://foobar.openai.azure.com/","deployment_name":"gpt-4o"}}`
