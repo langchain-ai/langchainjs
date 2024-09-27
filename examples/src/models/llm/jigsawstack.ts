272b757d2723771db7d21b0f24bb54f61775a6be
jacoblee93 <jacoblee93@gmail.com>
2024-09-27 13:52:13 -0700

    Minimize changes

--- examples/src/models/llm/jigsawstack.ts
+++
@@ -1,9 +0,0 @@
-import { JigsawStackPromptEngine } from "@langchain/jigsawstack";
-
-export const run = async () => {
-  const model = new JigsawStackPromptEngine();
-  const res = await model.invoke(
-    "Tell me about the leaning tower of pisa?\nAnswer:"
-  );
-  console.log({ res });
-};
