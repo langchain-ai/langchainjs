2001727218a1bf8d45f8fe6c45b1694cd7c186f8
FilipZmijewski <filip.zmijewski@ibm.com>
2024-10-01 14:56:44 +0200

    Prepare implementation IBM WatsonxAI to langchain community package.
    Provide solutions for:
    
    - LLMs
    - embeddings
    - tests

---
+++ libs/langchain-community/src/embeddings/ibm.ts
@@ -0,0 +1,131 @@
+import { Embeddings } from "@langchain/core/embeddings";
+import {
+  EmbeddingParameters,
+  TextEmbeddingsParams,
+} from "@ibm-cloud/watsonx-ai/dist/watsonx-ai-ml/vml_v1.js";
+import { WatsonXAI } from "@ibm-cloud/watsonx-ai";
+import { AsyncCaller } from "@langchain/core/utils/async_caller";
+import { WatsonxAuth, WatsonxParams } from "../types/watsonx_ai.js";
+import { authenticateAndSetInstance } from "../utils/ibm.js";
+
+export interface WatsonxEmbeddingsParams
+  extends Omit<EmbeddingParameters, "return_options">,
+    Pick<TextEmbeddingsParams, "headers"> {}
+
+export class WatsonxEmbeddings
+  extends Embeddings
+  implements WatsonxEmbeddingsParams, WatsonxParams
+{
+  modelId = "ibm/slate-125m-english-rtrvr";
+
+  serviceUrl: string;
+
+  version: string;
+
+  spaceId?: string;
+
+  projectId?: string;
+
+  truncate_input_tokens?: number;
+
+  maxRetries?: number;
+
+  maxConcurrency?: number;
+
+  private service: WatsonXAI;
+
+  constructor(fields: WatsonxEmbeddingsParams & WatsonxAuth & WatsonxParams) {
+    const superProps = { maxConcurrency: 2, ...fields };
+    super(superProps);
+    this.modelId = fields?.modelId ? fields.modelId : this.modelId;
+    this.version = fields.version;
+    this.serviceUrl = fields.serviceUrl;
+    this.truncate_input_tokens = fields.truncate_input_tokens;
+    this.maxConcurrency = fields.maxConcurrency;
+    this.maxRetries = fields.maxRetries;
+    if (fields.projectId && fields.spaceId)
+      throw new Error("Maximum 1 id type can be specified per instance");
+    else if (!fields.projectId && !fields.spaceId && !fields.idOrName)
+      throw new Error(
+        "No id specified! At least id of 1 type has to be specified"
+      );
+    this.projectId = fields?.projectId;
+    this.spaceId = fields?.spaceId;
+    this.serviceUrl = fields?.serviceUrl;
+    const {
+      watsonxAIApikey,
+      watsonxAIAuthType,
+      watsonxAIBearerToken,
+      watsonxAIUsername,
+      watsonxAIPassword,
+      watsonxAIUrl,
+      version,
+      serviceUrl,
+    } = fields;
+    const auth = authenticateAndSetInstance({
+      watsonxAIApikey,
+      watsonxAIAuthType,
+      watsonxAIBearerToken,
+      watsonxAIUsername,
+      watsonxAIPassword,
+      watsonxAIUrl,
+      version,
+      serviceUrl,
+    });
+    if (auth) this.service = auth;
+    else throw new Error("You have not provided one type of authentication");
+  }
+
+  scopeId() {
+    if (this.projectId) return { projectId: this.projectId };
+    else return { spaceId: this.spaceId };
+  }
+
+  ivocationParams(): EmbeddingParameters {
+    return {
+      truncate_input_tokens: this.truncate_input_tokens,
+    };
+  }
+
+  async completionWithRetry<T>(callback: () => T) {
+    const caller = new AsyncCaller({
+      maxConcurrency: this.maxConcurrency,
+      maxRetries: this.maxRetries,
+    });
+    return caller.call(async () => callback());
+  }
+
+  async listModels() {
+    const listModelParams = {
+      filters: "function_embedding",
+    };
+    const listModels = await this.completionWithRetry(() =>
+      this.service.listFoundationModelSpecs(listModelParams)
+    );
+    return listModels.result.resources?.map((item) => item.model_id);
+  }
+
+  private async embedSingleText(inputs: string[]) {
+    const textEmbeddingParams: TextEmbeddingsParams = {
+      inputs,
+      modelId: this.modelId,
+      ...this.scopeId(),
+      parameters: this.ivocationParams(),
+    };
+
+    const embeddings = await this.completionWithRetry(() =>
+      this.service.embedText(textEmbeddingParams)
+    );
+    return embeddings.result.results.map((item) => item.embedding);
+  }
+
+  async embedDocuments(documents: string[]): Promise<number[][]> {
+    const data = await this.embedSingleText(documents);
+    return data;
+  }
+
+  async embedQuery(document: string): Promise<number[]> {
+    const data = await this.embedSingleText([document]);
+    return data[0];
+  }
+}
---
+++ libs/langchain-community/src/llms/tests/ibm.test.ts
@@ -0,0 +1,207 @@
+/* eslint-disable no-process-env */
+import WatsonxAiMlVml_v1 from "@ibm-cloud/watsonx-ai/dist/watsonx-ai-ml/vml_v1.js";
+import { WatsonxLLM, WatsonxInputLLM } from "../ibm.js";
+import { authenticateAndSetInstance } from "../../utils/ibm.js";
+import {
+  WatsonxEmbeddings,
+  WatsonxEmbeddingsParams,
+} from "../../embeddings/ibm.js";
+
+const fakeAuthProp = {
+  watsonxAIAuthType: "iam",
+  watsonxAIApikey: "fake_key",
+};
+export function getKey<K>(key: K): K {
+  return key;
+}
+export const testProperties = (
+  instance: WatsonxLLM | WatsonxEmbeddings,
+  testProps: WatsonxInputLLM,
+  notExTestProps?: { [key: string]: any }
+) => {
+  const checkProperty = <T extends { [key: string]: any }>(
+    testProps: T,
+    instance: T,
+    existing = true
+  ) => {
+    Object.keys(testProps).forEach((key) => {
+      const keys = getKey<keyof T>(key);
+      type Type = Pick<T, typeof keys>;
+
+      if (typeof testProps[key as keyof T] === "object")
+        checkProperty<Type>(testProps[key as keyof T], instance[key], existing);
+      else {
+        if (existing)
+          expect(instance[key as keyof T]).toBe(testProps[key as keyof T]);
+        else if (instance) expect(instance[key as keyof T]).toBeUndefined();
+      }
+    });
+  };
+  checkProperty<WatsonxEmbeddingsParams>(testProps, instance);
+  if (notExTestProps)
+    checkProperty<typeof notExTestProps>(notExTestProps, instance, false);
+};
+
+describe("LLM unit tests", () => {
+  describe("Positive tests", () => {
+    test("Test authentication function", () => {
+      const instance = authenticateAndSetInstance({
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+        ...fakeAuthProp,
+      });
+      expect(instance).toBeInstanceOf(WatsonxAiMlVml_v1);
+    });
+
+    test("Test basic properties after init", async () => {
+      const testProps = {
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+        projectId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+      };
+      const instance = new WatsonxLLM({ ...testProps, ...fakeAuthProp });
+
+      testProperties(instance, testProps);
+    });
+
+    test("Test methods after init", () => {
+      const testProps: WatsonxInputLLM = {
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+        projectId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+      };
+      const instance = new WatsonxLLM({
+        ...testProps,
+        ...fakeAuthProp,
+      });
+      expect(instance.getNumTokens).toBeDefined();
+      expect(instance._generate).toBeDefined();
+      expect(instance._streamResponseChunks).toBeDefined();
+      expect(instance.invocationParams).toBeDefined();
+    });
+
+    test("Test properties after init", async () => {
+      const testProps = {
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+        projectId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+        modelId: "ibm/granite-13b-chat-v2",
+        max_new_tokens: 100,
+        decoding_method: "sample",
+        length_penalty: { decay_factor: 1, start_index: 1 },
+        min_new_tokens: 10,
+        random_seed: 1,
+        stop_sequences: ["hello"],
+        temperature: 0.1,
+        time_limit: 10000,
+        top_k: 1,
+        top_p: 1,
+        repetition_penalty: 1,
+        truncate_input_tokens: 1,
+        return_options: {
+          input_text: true,
+          generated_tokens: true,
+          input_tokens: true,
+          token_logprobs: true,
+          token_ranks: true,
+
+          top_n_tokens: 2,
+        },
+        include_stop_sequence: false,
+        maxRetries: 3,
+        maxConcurrency: 3,
+      };
+      const instance = new WatsonxLLM({ ...testProps, ...fakeAuthProp });
+
+      testProperties(instance, testProps);
+    });
+  });
+
+  describe("Negative tests", () => {
+    test("Missing id", async () => {
+      const testProps: WatsonxInputLLM = {
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+      };
+      expect(
+        () =>
+          new WatsonxLLM({
+            ...testProps,
+            ...fakeAuthProp,
+          })
+      ).toThrowError();
+    });
+
+    test("Missing other props", async () => {
+      // @ts-expect-error Intentionally passing not enough parameters
+      const testPropsProjectId: WatsonxInputLLM = {
+        projectId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+      };
+
+      expect(
+        () =>
+          new WatsonxLLM({
+            ...testPropsProjectId,
+            ...fakeAuthProp,
+          })
+      ).toThrowError();
+      // @ts-expect-error Intentionally passing not enough parameters
+      const testPropsServiceUrl: WatsonxInputLLM = {
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+      };
+      expect(
+        () =>
+          new WatsonxLLM({
+            ...testPropsServiceUrl,
+            ...fakeAuthProp,
+          })
+      ).toThrowError();
+      const testPropsVersion = {
+        version: "2024-05-31",
+      };
+      expect(
+        () =>
+          new WatsonxLLM({
+            // @ts-expect-error Intentionally passing wrong type of an object
+            testPropsVersion,
+          })
+      ).toThrowError();
+    });
+
+    test("Passing more than one id", async () => {
+      const testProps: WatsonxInputLLM = {
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+        projectId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+        spaceId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+      };
+      expect(
+        () =>
+          new WatsonxLLM({
+            ...testProps,
+            ...fakeAuthProp,
+          })
+      ).toThrowError();
+    });
+
+    test("Not existing property passed", async () => {
+      const testProps = {
+        version: "2024-05-31",
+        serviceUrl: process.env.WATSONX_AI_SERVICE_URL as string,
+        projectId: process.env.WATSONX_AI_PROJECT_ID || "testString",
+      };
+      const notExTestProps = {
+        notExisting: 12,
+        notExObj: {
+          notExProp: 12,
+        },
+      };
+      const instance = new WatsonxLLM({
+        ...testProps,
+        ...notExTestProps,
+        ...fakeAuthProp,
+      });
+      testProperties(instance, testProps, notExTestProps);
+    });
+  });
+});
