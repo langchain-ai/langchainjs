ca4a432bc321f4ada73d97bef46281cf4a099343
William Fu-Hinthorn <13333726+hinthornw@users.noreply.github.com>
2024-10-03 14:35:08 -0700

    ooooooo foooooorammmmmmaaaaat

--- langchain-core/src/messages/utils.ts
+++ langchain-core/src/messages/utils.ts
@@ -33,8 +33,8 @@ function _constructMessageFromParams(
     return new SystemMessage(rest);
   } else {
     throw new Error(
-      `Unable to coerce message from array: only human, AI, or system message coercion is currently supported.`
-      + `Got: ${params}`
+      `Unable to coerce message from array: only human, AI, or system message coercion is currently supported.` +
+        `Got: ${params}`
     );
   }
 }
