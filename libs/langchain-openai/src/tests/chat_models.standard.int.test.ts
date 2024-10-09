002c70dc7d5b00cfdcb72b5e5347d5d36fe1617d
bracesproul <braceasproul@gmail.com>
2024-10-09 14:55:28 -0700

    fix oai lint

--- libs/langchain-openai/src/tests/chat_models.standard.int.test.ts
+++ libs/langchain-openai/src/tests/chat_models.standard.int.test.ts
@@ -2,11 +2,11 @@
 import { test, expect } from "@jest/globals";
 import { ChatModelIntegrationTests } from "@langchain/standard-tests";
 import { AIMessage, AIMessageChunk } from "@langchain/core/messages";
-import { ChatOpenAI, ChatOpenAICallOptions } from "../chat_models.js";
 import { readFileSync } from "fs";
 import { join } from "path";
 import { concat } from "@langchain/core/utils/stream";
 import { BaseLanguageModelInput } from "@langchain/core/language_models/base";
+import { ChatOpenAI, ChatOpenAICallOptions } from "../chat_models.js";
 
 const REPO_ROOT_DIR = process.cwd();
 
