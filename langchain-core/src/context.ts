444e1a13b8ce20e303c09ff446047917b7c79db6
jacoblee93 <jacoblee93@gmail.com>
2024-10-10 18:08:49 -0700

    Adds docstring

--- langchain-core/src/context.ts
+++ langchain-core/src/context.ts
@@ -8,18 +8,54 @@ AsyncLocalStorageProviderSingleton.initializeGlobalInstance(
   new AsyncLocalStorage()
 );
 
+/**
+ * Set a context variable. Context variables are scoped to any
+ * child runnables called by the current runnable, or globally if set outside
+ * of any runnable.
+ *
+ * @remarks
+ * This function is only supported in environments that support AsyncLocalStorage,
+ * including Node.js, Deno, and Cloudflare Workers.
+ *
+ * @example
+ * ```ts
+ * const nested = RunnableLambda.from(() => {
+ *   // "bar" because it was set by a parent
+ *   console.log(getContextVariable("foo"));
+ *
+ *   // Override to "baz", but only for child runnables
+ *   setContextVariable("foo", "baz");
+ *
+ *   // Now "baz", but only for child runnables
+ *   return getContextVariable("foo");
+ * });
+ *
+ * const runnable = RunnableLambda.from(async () => {
+ *   // Set a context variable named "foo"
+ *   setContextVariable("foo", "bar");
+ *
+ *   const res = await nested.invoke({});
+ *
+ *   // Still "bar" since child changes do not affect parents
+ *   console.log(getContextVariable("foo"));
+ *
+ *   return res;
+ * });
+ *
+ * // undefined, because context variable has not been set yet
+ * console.log(getContextVariable("foo"));
+ *
+ * // Final return value is "baz"
+ * const result = await runnable.invoke({});
+ * ```
+ *
+ * @param name The name of the context variable.
+ * @param value The value to set.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function setContextVariable(name: PropertyKey, value: any) {
   const runTree = AsyncLocalStorageProviderSingleton.getInstance().getStore();
-  if (runTree === undefined) {
-    throw new Error(
-      [
-        "This function can only be called from within an existing run (e.g.,",
-        "inside a tool, RunnableLambda, or LangGraph.js node).",
-      ].join("")
-    );
-  }
-  const contextVars = { ...runTree[_CONTEXT_VARIABLES_KEY] };
+  const contextVars = { ...runTree?.[_CONTEXT_VARIABLES_KEY] };
   contextVars[name] = value;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (AsyncLocalStorageProviderSingleton.getInstance() as any).enterWith({
@@ -28,7 +64,50 @@ export function setContextVariable(name: PropertyKey, value: any) {
   });
 }
 
-export function getContextVariable(name: string) {
+/**
+ * Get the value of a previously set context variable. Context variables
+ * are scoped to any child runnables called by the current runnable,
+ * or globally if set outside of any runnable.
+ *
+ * @remarks
+ * This function is only supported in environments that support AsyncLocalStorage,
+ * including Node.js, Deno, and Cloudflare Workers.
+ *
+ * @example
+ * ```ts
+ * const nested = RunnableLambda.from(() => {
+ *   // "bar" because it was set by a parent
+ *   console.log(getContextVariable("foo"));
+ *
+ *   // Override to "baz", but only for child runnables
+ *   setContextVariable("foo", "baz");
+ *
+ *   // Now "baz", but only for child runnables
+ *   return getContextVariable("foo");
+ * });
+ *
+ * const runnable = RunnableLambda.from(async () => {
+ *   // Set a context variable named "foo"
+ *   setContextVariable("foo", "bar");
+ *
+ *   const res = await nested.invoke({});
+ *
+ *   // Still "bar" since child changes do not affect parents
+ *   console.log(getContextVariable("foo"));
+ *
+ *   return res;
+ * });
+ *
+ * // undefined, because context variable has not been set yet
+ * console.log(getContextVariable("foo"));
+ *
+ * // Final return value is "baz"
+ * const result = await runnable.invoke({});
+ * ```
+ *
+ * @param name The name of the context variable.
+ */
+export function getContextVariable(name: PropertyKey) {
   const runTree = AsyncLocalStorageProviderSingleton.getInstance().getStore();
   return runTree?.[_CONTEXT_VARIABLES_KEY]?.[name];
 }
--- langchain-core/src/tests/context.test.ts
+++ langchain-core/src/tests/context.test.ts
@@ -4,17 +4,22 @@ import { getContextVariable, setContextVariable } from "../context.js";
 test("RunnableLambda that returns a runnable should invoke the runnable", async () => {
   const nested = RunnableLambda.from(() => {
     expect(getContextVariable("foo")).toEqual("bar");
+    expect(getContextVariable("toplevel")).toEqual(9);
     setContextVariable("foo", "baz");
     return getContextVariable("foo");
   });
   const runnable = RunnableLambda.from(async () => {
     setContextVariable("foo", "bar");
     expect(getContextVariable("foo")).toEqual("bar");
+    expect(getContextVariable("toplevel")).toEqual(9);
     const res = await nested.invoke({});
     expect(getContextVariable("foo")).toEqual("bar");
     return res;
   });
   expect(getContextVariable("foo")).toEqual(undefined);
+  setContextVariable("toplevel", 9);
+  expect(getContextVariable("toplevel")).toEqual(9);
   const result = await runnable.invoke({});
+  expect(getContextVariable("toplevel")).toEqual(9);
   expect(result).toEqual("baz");
 });
