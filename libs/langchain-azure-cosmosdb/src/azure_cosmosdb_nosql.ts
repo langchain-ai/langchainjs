ceb00afde731707c644a175a84fb29aced9e3772
aditishree1 <aditishree.git@gmail.com>
2024-10-20 21:48:48 +0530

    add test

--- libs/langchain-azure-cosmosdb/src/azure_cosmosdb_nosql.ts
+++ libs/langchain-azure-cosmosdb/src/azure_cosmosdb_nosql.ts
@@ -78,7 +78,7 @@ export interface AzureCosmosDBNoSQLConfig
   readonly metadataKey?: string;
 }
 
-const USER_AGENT_SUFFIX = "langchainjs-azure-cosmosdb-nosql";
+const USER_AGENT_SUFFIX = "LangChain-CDBNoSQL-VectorStore-JavaScript";
 
 /**
  * Azure Cosmos DB for NoSQL vCore vector store.
