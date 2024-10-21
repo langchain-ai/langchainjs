421a60ca272d1096d3b63f9b5820449f6af9e6ab
aditishree1 <aditishree.git@gmail.com>
2024-10-21 23:10:31 +0530

    fix int test

--- libs/langchain-azure-cosmosdb/src/tests/caches.int.test.ts
+++ libs/langchain-azure-cosmosdb/src/tests/caches.int.test.ts
@@ -1,11 +1,16 @@
 /* eslint-disable no-process-env */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { IndexingMode, VectorEmbeddingPolicy } from "@azure/cosmos";
+import {
+  CosmosClient,
+  IndexingMode,
+  VectorEmbeddingPolicy,
+} from "@azure/cosmos";
 import { FakeEmbeddings, FakeLLM } from "@langchain/core/utils/testing";
+import { DefaultAzureCredential } from "@azure/identity";
 import { AzureCosmosDBNoSQLSemanticCache } from "../caches.js";
 
-const DATABASE_NAME = "langchainTestDB";
+const DATABASE_NAME = "langchainTestCacheDB";
 const CONTAINER_NAME = "testContainer";
 
 function indexingPolicy(indexType: any) {
@@ -17,14 +22,16 @@ function indexingPolicy(indexType: any) {
   };
 }
 
-function vectorEmbeddingPolicy(distanceFunction: any): VectorEmbeddingPolicy {
+function vectorEmbeddingPolicy(
+  distanceFunction: "euclidean" | "cosine" | "dotproduct"
+): VectorEmbeddingPolicy {
   return {
     vectorEmbeddings: [
       {
         path: "/embedding",
         dataType: "float32",
         distanceFunction,
-        dimensions: 1536,
+        dimensions: 0,
       },
     ],
   };
@@ -73,6 +80,31 @@ function initializeCache(
  * - AZURE_COSMOSDB_NOSQL_CONNECTION_STRING or AZURE_COSMOSDB_NOSQL_ENDPOINT
  */
 describe("Azure CosmosDB NoSQL Semantic Cache", () => {
+  beforeEach(async () => {
+    let client: CosmosClient;
+
+    if (process.env.AZURE_COSMOSDB_NOSQL_CONNECTION_STRING) {
+      client = new CosmosClient(
+        process.env.AZURE_COSMOSDB_NOSQL_CONNECTION_STRING
+      );
+    } else if (process.env.AZURE_COSMOSDB_NOSQL_ENDPOINT) {
+      client = new CosmosClient({
+        endpoint: process.env.AZURE_COSMOSDB_NOSQL_ENDPOINT,
+        aadCredentials: new DefaultAzureCredential(),
+      });
+    } else {
+      throw new Error(
+        "Please set the environment variable AZURE_COSMOSDB_NOSQL_CONNECTION_STRING or AZURE_COSMOSDB_NOSQL_ENDPOINT"
+      );
+    }
+
+    // Make sure the database does not exists
+    try {
+      await client.database(DATABASE_NAME).delete();
+    } catch {
+      // Ignore error if the database does not exist
+    }
+  });
   it("test AzureCosmosDBNoSqlSemanticCache with cosine quantizedFlat", async () => {
     const cache = initializeCache("quantizedFlat", "cosine");
     const llm = new FakeLLM({});
@@ -92,7 +124,7 @@ describe("Azure CosmosDB NoSQL Semantic Cache", () => {
     await cache.clear(llmString);
   });
   it("test AzureCosmosDBNoSqlSemanticCache with dotProduct quantizedFlat", async () => {
-    const cache = initializeCache("quantizedFlat", "dotProduct");
+    const cache = initializeCache("quantizedFlat", "dotproduct");
     const llm = new FakeLLM({});
     const llmString = JSON.stringify(llm._identifyingParams());
     await cache.update("foo", llmString, [{ text: "fizz" }, { text: "Buzz" }]);
@@ -101,7 +133,7 @@ describe("Azure CosmosDB NoSQL Semantic Cache", () => {
     await cache.clear(llmString);
   });
   it("test AzureCosmosDBNoSqlSemanticCache with dotProduct flat", async () => {
-    const cache = initializeCache("flat", "dotProduct");
+    const cache = initializeCache("flat", "dotproduct");
     const llm = new FakeLLM({});
     const llmString = JSON.stringify(llm._identifyingParams());
     await cache.update("foo", llmString, [{ text: "fizz" }, { text: "Buzz" }]);
