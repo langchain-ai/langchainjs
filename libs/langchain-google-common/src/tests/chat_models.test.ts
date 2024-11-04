b5d8225e10368297a083e20517c38d095d090b53
afirstenberg <github@addventure.com>
2024-11-03 23:01:15 -0500

    Add enums for the various Gemini safety settings

--- libs/langchain-google-common/src/tests/chat_models.test.ts
+++ libs/langchain-google-common/src/tests/chat_models.test.ts
@@ -19,7 +19,9 @@ import { authOptions, MockClient, MockClientAuthInfo, mockId } from "./mock.js";
 import {
   GeminiTool,
   GoogleAIBaseLLMInput,
+  GoogleAISafetyCategory,
   GoogleAISafetyHandler,
+  GoogleAISafetyThreshold,
 } from "../types.js";
 import { GoogleAbstractedClient } from "../auth.js";
 import { GoogleAISafetyError } from "../utils/safety.js";
@@ -476,6 +478,48 @@ describe("Mock ChatGoogle - Gemini", () => {
       caught = true;
     }
     expect(caught).toBeTruthy();
+  });
+
+  test("2. Safety - settings", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "chat-2-mock.json",
+    };
+    const model = new ChatGoogle({
+      authOptions,
+      safetySettings: [
+        {
+          category: GoogleAISafetyCategory.Harassment,
+          threshold: GoogleAISafetyThreshold.Most,
+        },
+      ],
+    });
+    const messages: BaseMessageLike[] = [
+      new HumanMessage("Flip a coin and tell me H for heads and T for tails"),
+      new AIMessage("H"),
+      new HumanMessage("Flip it again"),
+    ];
+    let caught = false;
+    try {
+      await model.invoke(messages);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (xx: any) {
+      caught = true;
+    }
+
+    const settings = record?.opts?.data?.safetySettings;
+    expect(settings).toBeDefined();
+    expect(Array.isArray(settings)).toEqual(true);
+    expect(settings).toHaveLength(1);
+    expect(settings[0].category).toEqual("HARM_CATEGORY_HARASSMENT");
+    expect(settings[0].threshold).toEqual("BLOCK_LOW_AND_ABOVE");
+
+    expect(caught).toEqual(true);
   });
 
   test("2. Safety - default", async () => {
