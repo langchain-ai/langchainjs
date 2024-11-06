580950feaf2383d17e00c81fd5e89193d44c523e
Jacky Chen <jackychen4@gmail.com>
2024-11-06 01:41:28 +0000

    fixed build errors

--- examples/src/models/chat/integration_llama_cpp_stream.ts
+++ examples/src/models/chat/integration_llama_cpp_stream.ts
@@ -2,7 +2,7 @@ import { ChatLlamaCpp } from "@langchain/community/chat_models/llama_cpp";
 
 const llamaPath = "/Replace/with/path/to/your/model/gguf-llama2-q4_0.bin";
 
-const model = await ChatLlamaCpp.chatInit({
+const model = await ChatLlamaCpp.initialize({
   modelPath: llamaPath,
   temperature: 0.7,
 });
--- examples/src/models/chat/integration_llama_cpp_stream_invoke.ts
+++ examples/src/models/chat/integration_llama_cpp_stream_invoke.ts
@@ -3,7 +3,7 @@ import { SystemMessage, HumanMessage } from "@langchain/core/messages";
 
 const llamaPath = "/Replace/with/path/to/your/model/gguf-llama2-q4_0.bin";
 
-const model = await ChatLlamaCpp.chatInit({
+const model = await ChatLlamaCpp.initialize({
   modelPath: llamaPath,
   temperature: 0.7,
 });
