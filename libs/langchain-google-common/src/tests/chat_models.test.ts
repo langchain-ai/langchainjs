b55f8cb2e4edce685791a533ba6ae900973a7dd6
jacoblee93 <jacoblee93@gmail.com>
2024-11-11 14:03:23 -0800

    Fix lint

--- libs/langchain-google-common/src/tests/chat_models.test.ts
+++ libs/langchain-google-common/src/tests/chat_models.test.ts
@@ -1,4 +1,4 @@
-/* eslint disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { expect, test } from "@jest/globals";
 import {
   AIMessage,
