58aa4156030b54bf6fab833020fc0ad27448b52d
xnnxnyx <annanya21sharma@gmail.com>
2024-11-17 15:05:41 -0500

    chore: added google_scholar example

---
+++ examples/src/tools/google_scholar.ts
@@ -0,0 +1,15 @@
+import { GoogleScholarAPI } from "./google_scholar";
+
+const scholar = new GoogleScholarAPI({
+  apiKey: "YOUR_SERP_API_KEY", // Alternatively, set this as an environment variable.
+});
+
+(async () => {
+  try {
+    const query = "machine learning applications in healthcare";
+    const results = await scholar._call(query);
+    console.log("Search Results:", results);
+  } catch (error) {
+    console.error("Error querying Google Scholar via SerpApi:", error);
+  }
+})();
