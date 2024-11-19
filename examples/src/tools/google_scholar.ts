e3172bbf8a236730747cb0d394b03bf1d1a15b30
xnnxnyx <annanya21sharma@gmail.com>
2024-11-19 12:18:49 -0500

    chore: updated google_scholar import in examples

--- examples/src/tools/google_scholar.ts
+++ examples/src/tools/google_scholar.ts
@@ -1,4 +1,4 @@
-import { GoogleScholarAPI } from "./google_scholar";
+import { GoogleScholarAPI } from "@langchain/community/tools/google_scholar";
 
 const scholar = new GoogleScholarAPI({
   apiKey: "YOUR_SERP_API_KEY", // Alternatively, set this as an environment variable.
