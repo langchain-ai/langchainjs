54b692a88b91e3f9cb42300affe29dae52ba442c
Jason <ggbbddjm@gmail.com>
2024-12-04 07:34:39 +0800

    feat(community): Add new LLM provider Novita AI (#7231)
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- libs/langchain-community/langchain.config.js
+++ libs/langchain-community/langchain.config.js
@@ -178,6 +178,7 @@ export const config = {
     "chat_models/llama_cpp": "chat_models/llama_cpp",
     "chat_models/minimax": "chat_models/minimax",
     "chat_models/moonshot": "chat_models/moonshot",
+    "chat_models/novita": "chat_models/novita",
     "chat_models/ollama": "chat_models/ollama",
     "chat_models/portkey": "chat_models/portkey",
     "chat_models/premai": "chat_models/premai",
