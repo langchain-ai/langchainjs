2f40ce31abfc133e5d095bd93963235c38bc90c5
jacoblee93 <jacoblee93@gmail.com>
2024-12-09 15:49:57 -0800

    Fix import

--- libs/langchain-community/src/embeddings/tests/jina.int.test.ts
+++ libs/langchain-community/src/embeddings/tests/jina.int.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@jest/globals";
-import { JinaEmbeddings } from "../jina";
+import { JinaEmbeddings } from "../jina.js";
 
 test("Test JinaEmbeddings.embedQuery", async () => {
   const embeddings = new JinaEmbeddings();
