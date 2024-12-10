0b3ebbc1741f79490cd30db427c297df2b62c252
jacoblee93 <jacoblee93@gmail.com>
2024-12-10 15:53:10 -0800

    Preserve export

--- langchain-core/src/singletons/index.ts
+++ langchain-core/src/singletons/index.ts
@@ -1,12 +1,13 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   type AsyncLocalStorageInterface,
   AsyncLocalStorageProviderSingleton,
   MockAsyncLocalStorage,
 } from "./async_local_storage/index.js";
+import { _CONTEXT_VARIABLES_KEY } from "./async_local_storage/globals.js";
 
 export {
   type AsyncLocalStorageInterface,
   AsyncLocalStorageProviderSingleton,
   MockAsyncLocalStorage,
 };
+export { _CONTEXT_VARIABLES_KEY };
