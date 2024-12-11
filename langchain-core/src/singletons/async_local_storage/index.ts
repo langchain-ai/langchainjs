edea078248f95c40914b73a3741d01b71eccf2d9
Olmo Maldonado <olmo.maldonado@gmail.com>
2024-12-10 18:08:56 -0600

    feat(core): add registerConfigureHook (#7330)
    Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

--- langchain-core/src/singletons/async_local_storage/index.ts
+++ langchain-core/src/singletons/async_local_storage/index.ts
@@ -4,6 +4,7 @@ import {
   AsyncLocalStorageInterface,
   getGlobalAsyncLocalStorageInstance,
   setGlobalAsyncLocalStorageInstance,
+  _CONTEXT_VARIABLES_KEY,
 } from "./globals.js";
 import { CallbackManager } from "../../callbacks/manager.js";
 import { LangChainTracer } from "../../tracers/tracer_langchain.js";
@@ -25,8 +26,6 @@ export class MockAsyncLocalStorage implements AsyncLocalStorageInterface {
 const mockAsyncLocalStorage = new MockAsyncLocalStorage();
 
 const LC_CHILD_KEY = Symbol.for("lc:child_config");
-
-export const _CONTEXT_VARIABLES_KEY = Symbol.for("lc:context_variables");
 
 class AsyncLocalStorageProvider {
   getInstance(): AsyncLocalStorageInterface {
