305abf53fc7ee9f4cab06852a586b4e814e36a4b
Chester Curme <chester.curme@gmail.com>
2024-12-11 14:37:22 -0500

    update

--- docs/core_docs/src/theme/VectorStoreTabs.js
+++ docs/core_docs/src/theme/VectorStoreTabs.js
@@ -6,20 +6,25 @@ import CodeBlock from "@theme-original/CodeBlock";
 export default function VectorStoreTabs(props) {
   const customVarName = props;
 
-  const vectorStoreVarName = customVarName ?? "vectorStore";
-
   const tabItems = [
+    {
+      value: "Memory",
+      label: "Memory",
+      text: `import { MemoryVectorStore } from "langchain/vectorstores/memory";\n\nconst vectorStore = new MemoryVectorStore(embeddings);`,
+      packageName: "@langchain/community",
+      default: true,
+      },
     {
       value: "Chroma",
       label: "Chroma",
-      text: `import { Chroma } from "@langchain/community/vectorstores/chroma";\n\nconst ${vectorStoreVarName} = new Chroma(embeddings, {\n  collectionName: "a-test-collection",\n});`,
+      text: `import { Chroma } from "@langchain/community/vectorstores/chroma";\n\nconst vectorStore = new Chroma(embeddings, {\n  collectionName: "a-test-collection",\n});`,
       packageName: "@langchain/community",
       default: true,
     },
     {
       value: "FAISS",
       label: "FAISS",
-      text: `import { FaissStore } from "@langchain/community/vectorstores/faiss";\n\nconst ${vectorStoreVarName} = new FaissStore(embeddings, {});`,
+      text: `import { FaissStore } from "@langchain/community/vectorstores/faiss";\n\nconst vectorStore = new FaissStore(embeddings, {});`,
       packageName: "@langchain/community",
       default: false,
     },
@@ -27,19 +32,19 @@ export default function VectorStoreTabs(props) {
       value: "MongoDB",
       label: "MongoDB",
       text: `import { MongoDBAtlasVectorSearch } from "@langchain/mongodb"
-            import { MongoClient } from "mongodb";
-            
-            const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
-            const collection = client
-              .db(process.env.MONGODB_ATLAS_DB_NAME)
-              .collection(process.env.MONGODB_ATLAS_COLLECTION_NAME);
-            
-            const ${vectorStoreVarName} = new MongoDBAtlasVectorSearch(embeddings, {
-              collection: collection,
-              indexName: "vector_index",
-              textKey: "text",
-              embeddingKey: "embedding",
-            });`,
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_ATLAS_URI || "");
+const collection = client
+  .db(process.env.MONGODB_ATLAS_DB_NAME)
+  .collection(process.env.MONGODB_ATLAS_COLLECTION_NAME);
+
+const vectorStore = new MongoDBAtlasVectorSearch(embeddings, {
+  collection: collection,
+  indexName: "vector_index",
+  textKey: "text",
+  embeddingKey: "embedding",
+});`,
       packageName: "@langchain/mongodb",
       default: false,
     },
@@ -47,8 +52,8 @@ export default function VectorStoreTabs(props) {
       value: "PGVector",
       label: "PGVector",
       text: `import PGVectorStore from "@langchain/community/vectorstores/pgvector";
-            
-            const ${vectorStoreVarName} = await PGVectorStore.initialize(embeddings, {})`,
+
+  const vectorStore = await PGVectorStore.initialize(embeddings, {})`,
       packageName: "@langchain/community",
       default: false,
     },
@@ -56,13 +61,13 @@ export default function VectorStoreTabs(props) {
       value: "Pinecone",
       label: "Pinecone",
       text: `import { PineconeStore } from "@langchain/pinecone";
-            import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
-            
-            const pinecone = new PineconeClient();
-            const ${vectorStoreVarName} = new PineconeStore(embeddings, {
-              pineconeIndex,
-              maxConcurrency: 5,
-            });`,
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+
+const pinecone = new PineconeClient();
+const vectorStore = new PineconeStore(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});`,
       packageName: "@langchain/pinecone",
       default: false,
     },
@@ -70,11 +75,11 @@ export default function VectorStoreTabs(props) {
       value: "Qdrant",
       label: "Qdrant",
       text: `import { QdrantVectorStore } from "@langchain/qdrant";
-            
-            const ${vectorStoreVarName} = await QdrantVectorStore.fromExistingCollection(embeddings, {
-              url: process.env.QDRANT_URL,
-              collectionName: "langchainjs-testing",
-            });`,
+
+const vectorStore = await QdrantVectorStore.fromExistingCollection(embeddings, {
+  url: process.env.QDRANT_URL,
+  collectionName: "langchainjs-testing",
+});`,
       packageName: "@langchain/qdrant",
       default: false,
     },
