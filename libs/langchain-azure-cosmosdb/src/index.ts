0bf7cab781e6c59235a801b08930e3283dc11236
crisjc <cjy1994116@163.com>
2024-12-16 05:24:36 +0000

    resolve comments

--- libs/langchain-azure-cosmosdb/src/index.ts
+++ libs/langchain-azure-cosmosdb/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./azure_cosmosdb_mongodb.js";
 export * from "./azure_cosmosdb_nosql.js";
-export * from "./caches.js";
 export * from "./chat_histories.js";
+export * from "./caches/caches_nosql.js"
+export * from "./caches/caches_mongodb.js"
