262623e8186554881bf72c62e30d79c7773b11ab
crisjc <cjy1994116@163.com>
2024-12-18 10:34:59 +0000

    add mongodb cache test and resolve comment

--- libs/langchain-azure-cosmosdb/src/index.ts
+++ libs/langchain-azure-cosmosdb/src/index.ts
@@ -1,5 +1,5 @@
 export * from "./azure_cosmosdb_mongodb.js";
 export * from "./azure_cosmosdb_nosql.js";
 export * from "./chat_histories.js";
-export * from "./caches/caches_nosql.js"
-export * from "./caches/caches_mongodb.js"
+export * from "./caches/caches_nosql.js";
+export * from "./caches/caches_mongodb.js";
