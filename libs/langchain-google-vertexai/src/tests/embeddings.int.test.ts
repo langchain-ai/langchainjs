604219b0a0aca4d6c3d8e0c3630d5912f14bc9c0
jacoblee93 <jacoblee93@gmail.com>
2024-12-19 14:05:33 -0800

    Format

--- libs/langchain-google-vertexai/src/tests/embeddings.int.test.ts
+++ libs/langchain-google-vertexai/src/tests/embeddings.int.test.ts
@@ -14,24 +14,27 @@ const testModelsLocations = [
   ["text-multilingual-embedding-002", "us-central1"],
   ["text-embedding-005", "europe-west9"],
   ["text-multilingual-embedding-002", "europe-west9"],
-]
+];
 
-test.each(testModelsLocations)("VertexAIEmbeddings.embedDocuments %s %s", async (model, location) => {
-  const embeddings = new VertexAIEmbeddings({
-    model,
-    location,
-  });
-  const res = await embeddings.embedDocuments([
-    "Hello world",
-    "Bye bye",
-    "we need",
-    "at least",
-    "six documents",
-    "to test pagination",
-  ]);
-  // console.log(res);
-  expect(res).toHaveLength(6);
-  res.forEach((r) => {
-    expect(typeof r[0]).toBe("number");
-  });
-});
+test.each(testModelsLocations)(
+  "VertexAIEmbeddings.embedDocuments %s %s",
+  async (model, location) => {
+    const embeddings = new VertexAIEmbeddings({
+      model,
+      location,
+    });
+    const res = await embeddings.embedDocuments([
+      "Hello world",
+      "Bye bye",
+      "we need",
+      "at least",
+      "six documents",
+      "to test pagination",
+    ]);
+    // console.log(res);
+    expect(res).toHaveLength(6);
+    res.forEach((r) => {
+      expect(typeof r[0]).toBe("number");
+    });
+  }
+);
