83a9db64540a3758a26fe5d1d39df357f4d08838
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2024-12-21 00:55:50 -0500

    made comparison generic with a backwards compatible default

--- langchain-core/src/structured_query/ir.ts
+++ langchain-core/src/structured_query/ir.ts
@@ -149,13 +149,13 @@ export abstract class FilterDirective extends Expression {}
  * Class representing a comparison filter directive. It extends the
  * FilterDirective class.
  */
-export class Comparison extends FilterDirective {
+export class Comparison<ValueTypes = string | number> extends FilterDirective {
   exprName = "Comparison" as const;
 
   constructor(
     public comparator: Comparator,
     public attribute: string,
-    public value: string | number | boolean
+    public value: ValueTypes
   ) {
     super();
   }
