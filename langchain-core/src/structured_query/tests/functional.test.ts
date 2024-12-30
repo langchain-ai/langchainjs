79c243e1a0d3f33d4ccfb5650104920e4faf669f
Jacob Lee <jacoblee93@gmail.com>
2024-12-29 18:38:23 -0800

    Fix missing import

--- langchain-core/src/structured_query/tests/functional.test.ts
+++ langchain-core/src/structured_query/tests/functional.test.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@jest/globals";
+import { test, expect, describe } from "@jest/globals";
 import { Document } from "../../documents/document.js";
 import { FunctionalTranslator } from "../functional.js";
 import { Comparators, Visitor } from "../ir.js";
