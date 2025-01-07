988678386a97870a066e7f9e2741dcbc54b98392
bracesproul <braceasproul@gmail.com>
2025-01-06 17:04:49 -0800

    fix: Add test for running google web initChatModel

--- langchain/src/chat_models/tests/universal.int.test.ts
+++ langchain/src/chat_models/tests/universal.int.test.ts
@@ -421,6 +421,17 @@ describe("Works with all model providers", () => {
     const togetherResult = await together.invoke("what's your name");
     expect(togetherResult).toBeDefined();
     expect(togetherResult.content.length).toBeGreaterThan(0);
+    });
+
+  it("Can invoke google-vertexai-web", async () => {
+    const vertexAIWeb = await initChatModel(undefined, {
+      modelProvider: "google-vertexai-web",
+      temperature: 0,
+    });
+
+    const vertexAIWebResult = await vertexAIWeb.invoke("what's your name? Use the 'name' tool to respond.");
+    expect(vertexAIWebResult).toBeDefined();
+    expect(vertexAIWebResult.content.length).toBeGreaterThan(0);
   });
 });
 
