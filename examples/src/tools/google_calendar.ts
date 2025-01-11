f94be5fefd9c3f4c1c69f6558c887af1a9edd590
ucev <zhangshuaiyf@icloud.com>
2025-01-12 00:03:57 +0800

    fix: 7274

--- examples/src/tools/google_calendar.ts
+++ examples/src/tools/google_calendar.ts
@@ -1,10 +1,12 @@
-import { initializeAgentExecutorWithOptions } from "langchain/agents";
-import { OpenAI } from "@langchain/openai";
+import { AgentExecutor, createOpenAIToolsAgent } from "langchain/agents";
+import { pull } from "langchain/hub";
+import { OpenAI, ChatOpenAI } from "@langchain/openai";
 import { Calculator } from "@langchain/community/tools/calculator";
 import {
   GoogleCalendarCreateTool,
   GoogleCalendarViewTool,
 } from "@langchain/community/tools/google_calendar";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 export async function run() {
   const model = new OpenAI({
@@ -31,14 +33,24 @@ export async function run() {
     new GoogleCalendarViewTool(googleCalendarParams),
   ];
 
-  const calendarAgent = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: "zero-shot-react-description",
-    verbose: true,
+  const llm = new ChatOpenAI({
+    temperature: 0,
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const prompt: ChatPromptTemplate = await pull("hwchase17/openai-tools-agent");
+  const calendarAgent = await createOpenAIToolsAgent({
+    llm,
+    tools,
+    prompt,
+  });
+  const agentExecutor = new AgentExecutor({
+    agent: calendarAgent,
+    tools,
   });
 
   const createInput = `Create a meeting with John Doe next Friday at 4pm - adding to the agenda of it the result of 99 + 99`;
 
-  const createResult = await calendarAgent.invoke({ input: createInput });
+  const createResult = await agentExecutor.invoke({ input: createInput });
   //   Create Result {
   //     output: 'A meeting with John Doe on 29th September at 4pm has been created and the result of 99 + 99 has been added to the agenda.'
   //   }
@@ -46,7 +58,7 @@ export async function run() {
 
   const viewInput = `What meetings do I have this week?`;
 
-  const viewResult = await calendarAgent.invoke({ input: viewInput });
+  const viewResult = await agentExecutor.invoke({ input: viewInput });
   //   View Result {
   //     output: "You have no meetings this week between 8am and 8pm."
   //   }
