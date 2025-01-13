langchain-ai/langchainjs

commit 5030088c78f4837d2de6d06138ee9695738a9639

google-common/webauth/gauth [patch]: Fixes for error, safety, and image handling (#4583)
* Better handling when API call makes an error. Tailored to how Google will respond.

* Handle finish reason or prompt block reason.

* Fixes for image types

* Support for GoogleBaseLLM .invoke() and .stream() that can handle messages with image parts.

* formatting

* Refactor safety handler to be a class

* formatting

* Fix bug turning a chunk into a string when streaming an LLM response

* Refactor DefaultGeminiSafetyHandler.
Add MessageGeminiSafetyHandler that will mask exceptions with a custom message.

* Adding tests for invoke()

* Explicitly type safety handler.
Code cleanup.

* Change from overriding stream() to overriding _streamIterator() as discussed in #4583

* Formatting

* Refactor LLM methods to support callbacks

* Fix mock

* Test everything

* Lint

* Resolve circular deps

* Fix

* Fix

---------

Co-authored-by: jacoblee93 <jacoblee93@gmail.com>

libs/langchain-google-common/src/utils/failed_handler.ts
@@ -1,0 +1,39 @@
+import { AsyncCallerParams } from "@langchain/core/utils/async_caller";
+
+const STATUS_NO_RETRY = [
+  400,
+  401,
+  402,
+  403,
+  404,
+  405,
+  406,
+  407,
+  408,
+  409, // Conflict
+];
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function failedAttemptHandler(error: any) {
+  const status = error?.response?.status ?? 0;
+
+  if (status === 0) {
+    // What is this?
+    console.error("failedAttemptHandler", error);
+  }
+
+  // What errors shouldn't be retried?
+  if (STATUS_NO_RETRY.includes(+status)) {
+    throw error;
+  }
+
+  throw error;
+}
+
+export function ensureParams(params?: AsyncCallerParams): AsyncCallerParams {
+  const base: AsyncCallerParams = params ?? {};
+  return {
+    onFailedAttempt: failedAttemptHandler,
+    ...base,
+  };
+}

libs/langchain-google-common/src/tests/chat_models.test.ts
@@ -1,8 +1,10 @@
 import { expect, test } from "@jest/globals";
 import {
   AIMessage,
+  BaseMessage,
   BaseMessageLike,
   HumanMessage,
+  HumanMessageChunk,
   MessageContentComplex,
   MessageContentText,
   SystemMessage,
@@ -11,6 +13,7 @@ import { ChatGoogleBase, ChatGoogleBaseInput } from "../chat_models.js";
 import { authOptions, MockClient, MockClientAuthInfo, mockId } from "./mock.js";
 import { GoogleAIBaseLLMInput } from "../types.js";
 import { GoogleAbstractedClient } from "../auth.js";
+import { GoogleAISafetyError } from "../utils/safety.js";
 
 class ChatGoogle extends ChatGoogleBase<MockClientAuthInfo> {
   constructor(fields?: ChatGoogleBaseInput<MockClientAuthInfo>) {
@@ -147,6 +150,41 @@ describe("Mock ChatGoogle", () => {
     expect(data.contents[1].parts.length).toBeGreaterThanOrEqual(1);
   });
 
+  test("1. Invoke request format", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "chat-1-mock.json",
+    };
+    const model = new ChatGoogle({
+      authOptions,
+    });
+    const messages: BaseMessageLike[] = [
+      new HumanMessage("Flip a coin and tell me H for heads and T for tails"),
+      new AIMessage("H"),
+      new HumanMessage("Flip it again"),
+    ];
+    const result = await model.invoke(messages);
+    console.log("record", JSON.stringify(record, null, 1));
+    console.log("result", JSON.stringify(result, null, 1));
+
+    expect(record.opts).toBeDefined();
+    expect(record.opts.data).toBeDefined();
+    const { data } = record.opts;
+    expect(data.contents).toBeDefined();
+    expect(data.contents.length).toEqual(3);
+    expect(data.contents[0].role).toEqual("user");
+    expect(data.contents[0].parts).toBeDefined();
+    expect(data.contents[0].parts.length).toBeGreaterThanOrEqual(1);
+    expect(data.contents[0].parts[0].text).toBeDefined();
+    expect(data.contents[1].role).toEqual("model");
+    expect(data.contents[1].parts).toBeDefined();
+    expect(data.contents[1].parts.length).toBeGreaterThanOrEqual(1);
+  });
+
   test("1. Response format", async () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const record: Record<string, any> = {};
@@ -165,6 +203,37 @@ describe("Mock ChatGoogle", () => {
       new HumanMessage("Flip it again"),
     ];
     const result = await model.call(messages);
+
+    expect(result._getType()).toEqual("ai");
+    const aiMessage = result as AIMessage;
+    expect(aiMessage.content).toBeDefined();
+    expect(aiMessage.content.length).toBeGreaterThanOrEqual(1);
+    expect(aiMessage.content[0]).toHaveProperty("type");
+
+    const complexContent = aiMessage.content[0] as MessageContentComplex;
+    expect(complexContent.type).toEqual("text");
+    const content = complexContent as MessageContentText;
+    expect(content.text).toEqual("T");
+  });
+
+  test("1. Invoke response format", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "chat-1-mock.json",
+    };
+    const model = new ChatGoogle({
+      authOptions,
+    });
+    const messages: BaseMessageLike[] = [
+      new HumanMessage("Flip a coin and tell me H for heads and T for tails"),
+      new AIMessage("H"),
+      new HumanMessage("Flip it again"),
+    ];
+    const result = await model.invoke(messages);
 
     expect(result._getType()).toEqual("ai");
     const aiMessage = result as AIMessage;
@@ -219,5 +288,101 @@ describe("Mock ChatGoogle", () => {
     expect(data.contents[1].parts).toBeDefined();
     expect(data.contents[1].parts.length).toBeGreaterThanOrEqual(1);
     expect(data.contents[1].parts[0].text).toEqual("Ok");
+  });
+
+  test("2. Response format - safety", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "chat-2-mock.json",
+    };
+    const model = new ChatGoogle({
+      authOptions,
+    });
+    const messages: BaseMessageLike[] = [
+      new HumanMessage("Flip a coin and tell me H for heads and T for tails"),
+      new AIMessage("H"),
+      new HumanMessage("Flip it again"),
+    ];
+    let caught = false;
+    try {
+      await model.call(messages);
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (xx: any) {
+      caught = true;
+      expect(xx).toBeInstanceOf(GoogleAISafetyError);
+
+      const result = xx?.reply.generations[0].message;
+
+      expect(result._getType()).toEqual("ai");
+      const aiMessage = result as AIMessage;
+      expect(aiMessage.content).toBeDefined();
+      expect(aiMessage.content.length).toBeGreaterThanOrEqual(1);
+      expect(aiMessage.content[0]).toHaveProperty("type");
+
+      const complexContent = aiMessage.content[0] as MessageContentComplex;
+      expect(complexContent.type).toEqual("text");
+      const content = complexContent as MessageContentText;
+      expect(content.text).toEqual("T");
+    }
+
+    expect(caught).toEqual(true);
+  });
+
+  /*
+   * Images aren't supported (yet) by Gemini, but a one-round with
+   * image should work ok.
+   */
+  test("3. invoke - images", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "chat-3-mock.json",
+    };
+    const model = new ChatGoogle({
+      authOptions,
+      model: "gemini-pro-vision",
+    });
+
+    const message: MessageContentComplex[] = [
+      {
+        type: "text",
+        text: "What is in this image?",
+      },
+      {
+        type: "image_url",
+        image_url: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH6AIbFwQSRaexCAAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAJklEQVQY02P8//8/A27AxIAXsEAor31f0CS2OfEQ1j2Q0owU+RsAGNUJD2/04PgAAAAASUVORK5CYII=`,
+      },
+    ];
+
+    const messages: BaseMessage[] = [
+      new HumanMessageChunk({ content: message }),
+    ];
+
+    const result = await model.invoke(messages);
+
+    expect(record.opts).toHaveProperty("data");
+    expect(record.opts.data).toHaveProperty("contents");
+    expect(record.opts.data.contents).toHaveLength(1);
+    expect(record.opts.data.contents[0]).toHaveProperty("parts");
+
+    const parts = record?.opts?.data?.contents[0]?.parts;
+    expect(parts).toHaveLength(2);
+    expect(parts[0]).toHaveProperty("text");
+    expect(parts[1]).toHaveProperty("inlineData");
+    expect(parts[1].inlineData).toHaveProperty("mimeType");
+    expect(parts[1].inlineData).toHaveProperty("data");
+
+    expect(result.content[0]).toHaveProperty("text");
+    expect((result.content[0] as MessageContentText).text).toEqual(
+      "A blue square."
+    );
   });
 });

libs/langchain-google-common/src/tests/llms.test.ts
@@ -1,4 +1,10 @@
 import { expect, test } from "@jest/globals";
+import {
+  BaseMessage,
+  HumanMessageChunk,
+  MessageContentComplex,
+} from "@langchain/core/messages";
+import { ChatPromptValue } from "@langchain/core/prompt_values";
 import { GoogleBaseLLM, GoogleBaseLLMInput } from "../llms.js";
 import {
   authOptions,
@@ -7,6 +13,8 @@ import {
   mockFile,
   mockId,
 } from "./mock.js";
+import { GoogleAISafetyError } from "../utils/safety.js";
+import { MessageGeminiSafetyHandler } from "../utils/gemini.js";
 
 class GoogleLLM extends GoogleBaseLLM<MockClientAuthInfo> {
   constructor(fields?: GoogleBaseLLMInput<MockClientAuthInfo>) {
@@ -186,6 +194,36 @@ describe("Mock Google LLM", () => {
     console.log("record", JSON.stringify(record, null, 2));
   });
 
+  test("1: invoke", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-1-mock.json",
+    };
+    const model = new GoogleLLM({
+      authOptions,
+    });
+    const response = await model.invoke("Hello world");
+    expect(response).toEqual(
+      "1. Sock it to Me!\n2. Heel Yeah Socks\n3. Sole Mates\n4. Happy Soles\n5. Toe-tally Awesome Socks\n6. Sock Appeal\n7. Footsie Wootsies\n8. Thread Heads\n9. Sock Squad\n10. Sock-a-licious\n11. Darn Good Socks\n12. Sockcessories\n13. Sole Searching\n14. Sockstar\n15. Socktopia\n16. Sockology\n17. Elevated Toes\n18. The Urban Sole\n19. The Hippie Sole\n20. Sole Fuel"
+    );
+    // expect(record.opts.url).toEqual(`https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/publishers/google/models/gemini-pro:generateContent`)
+    console.log("record", JSON.stringify(record, null, 2));
+    expect(record.opts).toHaveProperty("data");
+    expect(record.opts.data).toHaveProperty("contents");
+    expect(record.opts.data.contents).toHaveLength(1);
+    expect(record.opts.data.contents[0]).toHaveProperty("parts");
+
+    const parts = record?.opts?.data?.contents[0]?.parts;
+    console.log(parts);
+    expect(parts).toHaveLength(1);
+    expect(parts[0]).toHaveProperty("text");
+    expect(parts[0].text).toEqual("Hello world");
+  });
+
   test("2: streamGenerateContent - non-streaming", async () => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const record: Record<string, any> = {};
@@ -223,10 +261,264 @@ describe("Mock Google LLM", () => {
     const response = await model.stream("Hello world");
     const responseArray: string[] = [];
     for await (const value of response) {
+      expect(typeof value).toEqual("string");
       responseArray.push(value);
     }
 
     expect(responseArray).toHaveLength(6);
+    console.log("record", JSON.stringify(record, null, 2));
+  });
+
+  test("4: streamGenerateContent - non-streaming - safety exception", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-4-mock.json",
+    };
+    const model = new GoogleLLM({
+      authOptions,
+    });
+    let caught = false;
+    try {
+      await model.call("Hello world");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (xx: any) {
+      caught = true;
+      expect(xx).toBeInstanceOf(GoogleAISafetyError);
+
+      const reply = xx?.reply;
+      const expectedReply = await mockFile("llm-4-mock.txt");
+      expect(reply).toEqual(expectedReply);
+    }
+    expect(caught).toEqual(true);
+  });
+
+  test("4: streamGenerateContent - non-streaming - safety message", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-4-mock.json",
+    };
+    const safetyHandler = new MessageGeminiSafetyHandler({
+      msg: "I'm sorry Dave, but I can't do that.",
+    });
+    const model = new GoogleLLM({
+      authOptions,
+      safetyHandler,
+    });
+    const reply = await model.call("Hello world");
+    expect(reply).toContain("I'm sorry Dave, but I can't do that.");
+  });
+
+  test("5: streamGenerateContent - streaming - safety exception", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-5-mock.json",
+    };
+    const model = new GoogleLLM({
+      authOptions,
+    });
+    const response = await model.stream("Hello world");
+    const responseArray: string[] = [];
+    let caught = false;
+    try {
+      for await (const value of response) {
+        expect(typeof value).toEqual("string");
+        responseArray.push(value);
+      }
+    } catch (xx) {
+      caught = true;
+      expect(xx).toBeInstanceOf(GoogleAISafetyError);
+    }
+
+    expect(responseArray).toHaveLength(4);
+    console.log("record", JSON.stringify(record, null, 2));
+
+    expect(caught).toEqual(true);
+  });
+
+  test("5: streamGenerateContent - streaming - safety message", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-5-mock.json",
+    };
+    const safetyHandler = new MessageGeminiSafetyHandler({
+      msg: "I'm sorry Dave, but I can't do that.",
+    });
+    const model = new GoogleLLM({
+      authOptions,
+      safetyHandler,
+    });
+    const response = await model.stream("Hello world");
+    const responseArray: string[] = [];
+    for await (const value of response) {
+      expect(typeof value).toEqual("string");
+      responseArray.push(value);
+    }
+
+    expect(responseArray).toHaveLength(6);
+    expect(responseArray[4]).toEqual("I'm sorry Dave, but I can't do that.");
+    console.log("record", JSON.stringify(record, null, 2));
+  });
+
+  test("6: predictMessages image blue-square", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-6-mock.json",
+    };
+
+    const model = new GoogleLLM({
+      authOptions,
+      model: "gemini-pro-vision",
+    });
+
+    const message: MessageContentComplex[] = [
+      {
+        type: "text",
+        text: "What is in this image?",
+      },
+      {
+        type: "image_url",
+        image_url: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH6AIbFwQSRaexCAAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAJklEQVQY02P8//8/A27AxIAXsEAor31f0CS2OfEQ1j2Q0owU+RsAGNUJD2/04PgAAAAASUVORK5CYII=`,
+      },
+    ];
+
+    const messages: BaseMessage[] = [
+      new HumanMessageChunk({ content: message }),
+    ];
+    const res = await model.predictMessages(messages);
+
+    console.log("record", record);
+    expect(record.opts).toHaveProperty("data");
+    expect(record.opts.data).toHaveProperty("contents");
+    expect(record.opts.data.contents).toHaveLength(1);
+    expect(record.opts.data.contents[0]).toHaveProperty("parts");
+
+    const parts = record?.opts?.data?.contents[0]?.parts;
+    console.log(parts);
+    expect(parts).toHaveLength(2);
+    expect(parts[0]).toHaveProperty("text");
+    expect(parts[1]).toHaveProperty("inlineData");
+    expect(parts[1].inlineData).toHaveProperty("mimeType");
+    expect(parts[1].inlineData).toHaveProperty("data");
+
+    expect(res?.content?.[0]).toEqual({ text: "A blue square.", type: "text" });
+  });
+
+  /*
+   * This test is skipped because .invoke() converts everything to text
+   * only at the moment.
+   */
+  test("6: invoke image blue-square", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-6-mock.json",
+    };
+
+    const model = new GoogleLLM({
+      authOptions,
+      model: "gemini-pro-vision",
+    });
+
+    const message: MessageContentComplex[] = [
+      {
+        type: "text",
+        text: "What is in this image?",
+      },
+      {
+        type: "image_url",
+        image_url: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH6AIbFwQSRaexCAAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAJklEQVQY02P8//8/A27AxIAXsEAor31f0CS2OfEQ1j2Q0owU+RsAGNUJD2/04PgAAAAASUVORK5CYII=`,
+      },
+    ];
+
+    const messages: BaseMessage[] = [
+      new HumanMessageChunk({ content: message }),
+    ];
+    const input = new ChatPromptValue(messages);
+    const res = await model.invoke(input);
+
+    console.log("record", record);
+    expect(record.opts).toHaveProperty("data");
+    expect(record.opts.data).toHaveProperty("contents");
+    expect(record.opts.data.contents).toHaveLength(1);
+    expect(record.opts.data.contents[0]).toHaveProperty("parts");
+
+    const parts = record?.opts?.data?.contents[0]?.parts;
+    console.log(parts);
+    expect(parts).toHaveLength(2);
+    expect(parts[0]).toHaveProperty("text");
+    expect(parts[1]).toHaveProperty("inlineData");
+    expect(parts[1].inlineData).toHaveProperty("mimeType");
+    expect(parts[1].inlineData).toHaveProperty("data");
+
+    expect(res).toEqual("A blue square.");
+  });
+
+  /*
+   * This test is skipped because .stream() converts everything to text
+   * only at the moment.
+   */
+  test("7: stream image blue-square", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-7-mock.json",
+    };
+    const model = new GoogleLLM({
+      authOptions,
+      model: "gemini-pro-image",
+    });
+
+    const message: MessageContentComplex[] = [
+      {
+        type: "text",
+        text: "What is in this image?",
+      },
+      {
+        type: "image_url",
+        image_url: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH6AIbFwQSRaexCAAAAB1pVFh0Q29tbWVudAAAAAAAQ3JlYXRlZCB3aXRoIEdJTVBkLmUHAAAAJklEQVQY02P8//8/A27AxIAXsEAor31f0CS2OfEQ1j2Q0owU+RsAGNUJD2/04PgAAAAASUVORK5CYII=`,
+      },
+    ];
+
+    const messages: BaseMessage[] = [
+      new HumanMessageChunk({ content: message }),
+    ];
+
+    // const input: BaseLanguageModelInput = [["human", message]]
+    const input = new ChatPromptValue(messages);
+
+    const response = await model.stream(input);
+    const responseArray: string[] = [];
+    for await (const value of response) {
+      responseArray.push(value);
+    }
+
+    expect(responseArray).toHaveLength(3);
     console.log("record", JSON.stringify(record, null, 2));
   });
 });

libs/langchain-google-common/src/chat_models.ts
@@ -24,12 +24,18 @@ import {
 import { AbstractGoogleLLMConnection } from "./connection.js";
 import {
   baseMessageToContent,
-  responseToChatGeneration,
-  responseToChatResult,
+  safeResponseToChatGeneration,
+  safeResponseToChatResult,
+  DefaultGeminiSafetyHandler,
 } from "./utils/gemini.js";
 import { ApiKeyGoogleAuth, GoogleAbstractedClient } from "./auth.js";
-import { GoogleBaseLLMInput } from "./llms.js";
 import { JsonStream } from "./utils/stream.js";
+import { ensureParams } from "./utils/failed_handler.js";
+import type {
+  GoogleBaseLLMInput,
+  GoogleAISafetyHandler,
+  GoogleAISafetyParams,
+} from "./types.js";
 
 class ChatConnection<AuthOptions> extends AbstractGoogleLLMConnection<
   BaseMessage[],
@@ -51,7 +57,8 @@ class ChatConnection<AuthOptions> extends AbstractGoogleLLMConnection<
 export interface ChatGoogleBaseInput<AuthOptions>
   extends BaseChatModelParams,
     GoogleConnectionParams<AuthOptions>,
-    GoogleAIModelParams {}
+    GoogleAIModelParams,
+    GoogleAISafetyParams {}
 
 /**
  * Integration with a chat model.
@@ -81,14 +88,18 @@ export abstract class ChatGoogleBase<AuthOptions>
 
   safetySettings: GoogleAISafetySetting[] = [];
 
+  safetyHandler: GoogleAISafetyHandler;
+
   protected connection: ChatConnection<AuthOptions>;
 
   protected streamedConnection: ChatConnection<AuthOptions>;
 
   constructor(fields?: ChatGoogleBaseInput<AuthOptions>) {
-    super(fields ?? {});
+    super(ensureParams(fields));
 
     copyAndValidateModelParamsInto(fields, this);
+    this.safetyHandler =
+      fields?.safetyHandler ?? new DefaultGeminiSafetyHandler();
 
     const client = this.buildClient(fields);
     this.buildConnection(fields ?? {}, client);
@@ -156,7 +167,7 @@ export abstract class ChatGoogleBase<AuthOptions>
       parameters,
       options
     );
-    const ret = responseToChatResult(response);
+    const ret = safeResponseToChatResult(response, this.safetyHandler);
     return ret;
   }
 
@@ -183,7 +194,7 @@ export abstract class ChatGoogleBase<AuthOptions>
       const output = await stream.nextChunk();
       const chunk =
         output !== null
-          ? responseToChatGeneration({ data: output })
+          ? safeResponseToChatGeneration({ data: output }, this.safetyHandler)
           : new ChatGenerationChunk({
               text: "",
               generationInfo: { finishReason: "stop" },

commit 7aa77866d301146adb80f6be5037ec805810c5a0

fix(google-common,google-*): fix retryable errors
Introduced in 5030088c; where all errors are thrown even though certain
statuses are checked.

libs/langchain-google-common/src/utils/failed_handler.ts
@@ -20,14 +20,13 @@ export function failedAttemptHandler(error: any) {
   if (status === 0) {
     // What is this?
     console.error("failedAttemptHandler", error);
+    throw error;
   }
 
   // What errors shouldn't be retried?
   if (STATUS_NO_RETRY.includes(+status)) {
     throw error;
   }
-
-  throw error;
 }
 
 export function ensureParams(params?: AsyncCallerParams): AsyncCallerParams {

libs/langchain-google-common/src/tests/chat_models.test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { expect, test } from "@jest/globals";
+import { expect, test, jest } from "@jest/globals";
 import {
   AIMessage,
   BaseMessage,
@@ -16,7 +16,13 @@ import { Serialized } from "@langchain/core/load/serializable";
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import { ChatGoogleBase, ChatGoogleBaseInput } from "../chat_models.js";
-import { authOptions, MockClient, MockClientAuthInfo, mockId } from "./mock.js";
+import {
+  authOptions,
+  MockClient,
+  MockClientAuthInfo,
+  MockClientError,
+  mockId,
+} from "./mock.js";
 import {
   GeminiTool,
   GoogleAIBaseLLMInput,
@@ -167,6 +173,39 @@ describe("Mock ChatGoogle - Gemini", () => {
     expect(data.contents[1].parts).toBeDefined();
     expect(data.contents[1].parts.length).toBeGreaterThanOrEqual(1);
     expect(data.systemInstruction).not.toBeDefined();
+  });
+
+  test("1. Basic request format - retryable request", async () => {
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "chat-1-mock.json",
+    };
+    const model = new ChatGoogle({
+      authOptions,
+    });
+    const messages: BaseMessageLike[] = [
+      new HumanMessage("Flip a coin and tell me H for heads and T for tails"),
+      new AIMessage("H"),
+      new HumanMessage("Flip it again"),
+    ];
+
+    const retryableError = new MockClientError(429);
+    const requestSpy = jest
+      .spyOn(MockClient.prototype, "request")
+      .mockRejectedValueOnce(retryableError);
+
+    await model.invoke(messages);
+
+    expect(record.opts).toBeDefined();
+    expect(record.opts.data).toBeDefined();
+    const { data } = record.opts;
+    expect(data.contents).toBeDefined();
+    expect(data.contents.length).toEqual(3);
+
+    expect(requestSpy).toHaveBeenCalledTimes(2);
   });
 
   test("1. Invoke request format", async () => {

libs/langchain-google-common/src/tests/llms.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@jest/globals";
+import { expect, test, jest } from "@jest/globals";
 import {
   BaseMessage,
   HumanMessageChunk,
@@ -10,6 +10,7 @@ import {
   authOptions,
   MockClient,
   MockClientAuthInfo,
+  MockClientError,
   mockFile,
   mockId,
 } from "./mock.js";
@@ -192,6 +193,32 @@ describe("Mock Google LLM", () => {
     );
     // expect(record.opts.url).toEqual(`https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/publishers/google/models/gemini-pro:generateContent`)
     // console.log("record", JSON.stringify(record, null, 2));
+  });
+
+  test("1: generateContent - retryable request", async () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const record: Record<string, any> = {};
+    const projectId = mockId();
+    const authOptions: MockClientAuthInfo = {
+      record,
+      projectId,
+      resultFile: "llm-1-mock.json",
+    };
+    const model = new GoogleLLM({
+      authOptions,
+    });
+
+    const retryableError = new MockClientError(429);
+    const requestSpy = jest
+      .spyOn(MockClient.prototype, "request")
+      .mockRejectedValueOnce(retryableError);
+
+    const response = await model.invoke("Hello world");
+
+    expect(response).toEqual(
+      "1. Sock it to Me!\n2. Heel Yeah Socks\n3. Sole Mates\n4. Happy Soles\n5. Toe-tally Awesome Socks\n6. Sock Appeal\n7. Footsie Wootsies\n8. Thread Heads\n9. Sock Squad\n10. Sock-a-licious\n11. Darn Good Socks\n12. Sockcessories\n13. Sole Searching\n14. Sockstar\n15. Socktopia\n16. Sockology\n17. Elevated Toes\n18. The Urban Sole\n19. The Hippie Sole\n20. Sole Fuel"
+    );
+    expect(requestSpy).toHaveBeenCalledTimes(2);
   });
 
   test("1: invoke", async () => {

libs/langchain-google-common/src/tests/mock.ts
@@ -117,3 +117,12 @@ export class MockClient implements GoogleAbstractedClient {
     }
   }
 }
+
+export class MockClientError extends Error {
+  public response: { status: number };
+
+  constructor(status: number) {
+    super();
+    this.response = { status };
+  }
+}