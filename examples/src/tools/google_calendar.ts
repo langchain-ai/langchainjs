790b5bc4752408e7affdd7183e5e873332b0dca7
ucev <zhangshuaiyf@icloud.com>
2025-01-14 08:58:29 +0800

    feat: use createReactAgent from @langchain/langgraph

--- examples/src/tools/google_calendar.ts
+++ examples/src/tools/google_calendar.ts
@@ -1,12 +1,10 @@
-import { AgentExecutor, createReactAgent } from "langchain/agents";
-import { pull } from "langchain/hub";
+import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { ChatOpenAI } from "@langchain/openai";
 import { Calculator } from "@langchain/community/tools/calculator";
 import {
   GoogleCalendarCreateTool,
   GoogleCalendarViewTool,
 } from "@langchain/community/tools/google_calendar";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
 
 export async function run() {
   const model = new ChatOpenAI({
@@ -34,20 +32,14 @@ export async function run() {
     new GoogleCalendarViewTool(googleCalendarParams),
   ];
 
-  const prompt: ChatPromptTemplate = await pull("hwchase17/react");
   const calendarAgent = await createReactAgent({
     llm: model,
-    tools,
-    prompt,
-  });
-  const agentExecutor = new AgentExecutor({
-    agent: calendarAgent,
-    tools,
+    tools
   });
 
   const createInput = `Create a meeting with John Doe next Friday at 4pm - adding to the agenda of it the result of 99 + 99`;
 
-  const createResult = await agentExecutor.invoke({ input: createInput });
+  const createResult = calendarAgent.invoke({ input: createInput });
   //   Create Result {
   //     output: 'A meeting with John Doe on 29th September at 4pm has been created and the result of 99 + 99 has been added to the agenda.'
   //   }
@@ -55,7 +47,7 @@ export async function run() {
 
   const viewInput = `What meetings do I have this week?`;
 
-  const viewResult = await agentExecutor.invoke({ input: viewInput });
+  const viewResult = await calendarAgent.invoke({ input: viewInput });
   //   View Result {
   //     output: "You have no meetings this week between 8am and 8pm."
   //   }
