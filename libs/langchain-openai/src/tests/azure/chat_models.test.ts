d15a45dfbcb70b56c6f450763da0e1160d6d1f8f
jacoblee93 <jacoblee93@gmail.com>
2025-01-24 15:16:05 -0800

    Fix tests

--- libs/langchain-openai/src/tests/azure/chat_models.test.ts
+++ libs/langchain-openai/src/tests/azure/chat_models.test.ts
@@ -1,5 +1,9 @@
 import { AzureChatOpenAI } from "../../azure/chat_models.js";
 
+beforeEach(() => {
+  delete process.env.OPENAI_API_KEY;
+});
+
 test("Test Azure OpenAI serialization from azure endpoint", async () => {
   const chat = new AzureChatOpenAI({
     azureOpenAIEndpoint: "https://foobar.openai.azure.com/",
@@ -8,7 +12,7 @@ test("Test Azure OpenAI serialization from azure endpoint", async () => {
     azureOpenAIApiKey: "foo",
   });
   expect(JSON.stringify(chat)).toEqual(
-    `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"azure_endpoint":"https://foobar.openai.azure.com/","openai_api_key":{"lc":1,"type":"secret","id":["OPENAI_API_KEY"]},"deployment_name":"gpt-4o"}}`
+    `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"azure_endpoint":"https://foobar.openai.azure.com/","deployment_name":"gpt-4o","openai_api_version":"2024-08-01-preview","azure_open_ai_api_key":{"lc":1,"type":"secret","id":["AZURE_OPENAI_API_KEY"]}}}`
   );
 });
 
@@ -20,7 +24,7 @@ test("Test Azure OpenAI serialization from base path", async () => {
     azureOpenAIApiKey: "foo",
   });
   expect(JSON.stringify(chat)).toEqual(
-    `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"openai_api_key":{"lc":1,"type":"secret","id":["OPENAI_API_KEY"]},"azure_endpoint":"https://foobar.openai.azure.com","deployment_name":"gpt-4o"}}`
+    `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"openai_api_version":"2024-08-01-preview","azure_open_ai_api_key":{"lc":1,"type":"secret","id":["AZURE_OPENAI_API_KEY"]},"azure_endpoint":"https://foobar.openai.azure.com","deployment_name":"gpt-4o-mini"}}`
   );
 });
 
@@ -32,6 +36,6 @@ test("Test Azure OpenAI serialization from instance name", async () => {
     azureOpenAIApiKey: "foo",
   });
   expect(JSON.stringify(chat)).toEqual(
-    `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"openai_api_key":{"lc":1,"type":"secret","id":["OPENAI_API_KEY"]},"azure_endpoint":"https://foobar.openai.azure.com/","deployment_name":"gpt-4o"}}`
+    `{"lc":1,"type":"constructor","id":["langchain","chat_models","azure_openai","AzureChatOpenAI"],"kwargs":{"azure_open_ai_api_instance_name":"foobar","deployment_name":"gpt-4o","openai_api_version":"2024-08-01-preview","azure_open_ai_api_key":{"lc":1,"type":"secret","id":["AZURE_OPENAI_API_KEY"]},"azure_endpoint":"https://foobar.openai.azure.com/"}}`
   );
 });
