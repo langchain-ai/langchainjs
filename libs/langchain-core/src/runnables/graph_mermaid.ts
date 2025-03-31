2522089a832d2bcda321d79d8e1533fc696aab5d
Jason Phillips <jason.surname.phillips@gmail.com>
2025-03-31 14:30:51 -0500

    fix: throw on duplicate subgraphs in diagram, only descend 1 level at a time

--- libs/langchain-core/src/runnables/graph_mermaid.ts
+++ libs/langchain-core/src/runnables/graph_mermaid.ts
@@ -111,7 +111,11 @@ export function drawMermaid(
       const subgraph = prefix.split(":").pop()!;
 
       if (seenSubgraphs.has(prefix)) {
-        return;
+        throw new Error(
+          `Found duplicate subgraph '${subgraph}' at '${prefix} -- this likely means that ` +
+            "you're reusing a subgraph node with the same name. " +
+            "Please adjust your graph to have subgraph nodes with unique names."
+        );
       }
 
       seenSubgraphs.add(prefix);
@@ -122,7 +126,9 @@ export function drawMermaid(
     const nestedPrefixes = sortPrefixesByDepth(
       Object.keys(edgeGroups).filter(
         (nestedPrefix) =>
-          nestedPrefix.startsWith(`${prefix}:`) && nestedPrefix !== prefix
+          nestedPrefix.startsWith(`${prefix}:`) &&
+          nestedPrefix !== prefix &&
+          nestedPrefix.split(":").length === prefix.split(":").length + 1
       )
     );
 
