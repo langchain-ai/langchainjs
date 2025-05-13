2f5470100c5cfc77c37366d128f48544773f7fe3
bailey <bailey.pearson@mongodb.com>
2025-05-13 10:54:45 -0600

    remove unnecessary environment from test setup

--- libs/langchain-mongodb/src/tests/setup.ts
+++ libs/langchain-mongodb/src/tests/setup.ts
@@ -71,11 +71,6 @@ export default async function setup() {
 
   const container = await new GenericContainer("mongodb/mongodb-atlas-local")
     .withExposedPorts({ host: 27017, container: 27017 })
-    .withEnvironment({
-      // `mongot` logs otherwise don't get written to the container log, which we
-      // need for the wait strategy
-      MONGOT_LOG_FILE: "/dev/stdout",
-    })
     .withWaitStrategy(new ReadyWhenMongotEstablished())
     .withStartupTimeout(30_000)
     .start();
