0c31bc32920e4c1dd1f1a7e79c1c0c1f42cf7cb9
Elliot Scribner <elliot.scribner@couchbase.com>
2025-05-23 14:02:17 -0700

    Deprecate CouchbaseVectorStore and create CouchbaseSearchVectorStore

--- libs/langchain-community/src/load/import_constants.ts
+++ libs/langchain-community/src/load/import_constants.ts
@@ -53,6 +53,7 @@ export const optionalImportEntrypoints: string[] = [
   "langchain_community/vectorstores/cloudflare_vectorize",
   "langchain_community/vectorstores/convex",
   "langchain_community/vectorstores/couchbase",
+  "langchain_community/vectorstores/couchbase_search",
   "langchain_community/vectorstores/elasticsearch",
   "langchain_community/vectorstores/faiss",
   "langchain_community/vectorstores/googlevertexai",
