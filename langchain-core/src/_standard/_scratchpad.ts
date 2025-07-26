2b6770dc774326538e38a6ab485e9ddbc5e41d4e
Hunter Lovell <hunter@hntrl.io>
2025-07-26 16:23:20 -0700

    x

--- langchain-core/src/_standard/_scratchpad.ts
+++ langchain-core/src/_standard/_scratchpad.ts
@@ -1,92 +1,104 @@
-// import { ContentBlock } from "./content/index.js";
-// import { Message, $MessageComplex } from "./message.js";
+import { ContentBlock } from "./content/index.js";
+import { BaseMessage, $MessageComplex } from "./message.js";
 
 // Example usage with OpenAI special content blocks
 
-// interface OpenAITextContentBlock {
-//   readonly type: "text";
-//   openaiOnly: number;
-// }
+interface OpenAITextContentBlock {
+  readonly type: "text";
+  openaiOnly: number;
+}
 
-// interface OpenAIMcpContentBlock {
-//   readonly type: "openai_mcp";
-//   openaiOnly: number;
-// }
+interface OpenAIMcpContentBlock {
+  readonly type: "openai_mcp";
+  openaiOnly: number;
+}
 
-// interface $OpenAIMessageComplex extends $MessageComplex {
-//   content: {
-//     user: OpenAITextContentBlock | OpenAIMcpContentBlock;
-//     assistant: OpenAIMcpContentBlock;
-//     tool: OpenAIMcpContentBlock;
-//     system: OpenAIMcpContentBlock;
-//   };
-// }
+interface $OpenAIMessageComplex extends $MessageComplex {
+  content: {
+    user: OpenAITextContentBlock | OpenAIMcpContentBlock;
+    assistant: OpenAIMcpContentBlock;
+    tool: OpenAIMcpContentBlock;
+    system: OpenAIMcpContentBlock;
+  };
+}
 
-// function openaiReturn(): Message<$OpenAIMessageComplex> {
-//   return {
-//     type: "user",
-//     content: [],
-//   };
-// }
+function openaiReturn(): BaseMessage<$OpenAIMessageComplex> {
+  return {
+    type: "user",
+    content: [],
+    responseMetadata: {},
+    usageMetadata: {},
+  };
+}
 
-// const openaiMessage = openaiReturn();
-// if (openaiMessage.type === "user") {
-//   for (const content of openaiMessage.content) {
-//     if (content.type === "text") {
-//       // openaiOnly is a property provided by OpenAIMessageComplex
-//       console.log(content.openaiOnly);
-//       // console.log(content);
-//     }
-//   }
-// }
+const openaiMessage = openaiReturn();
+if (openaiMessage.type === "user") {
+  for (const content of openaiMessage.content) {
+    if (content.type === "text") {
+      // openaiOnly is a property provided by OpenAIMessageComplex
+      console.log(content.openaiOnly);
+    }
+  }
+}
 
-// function normalReturn(): Message {
-//   return {
-//     type: "user",
-//     content: [],
-//   };
-// }
+function normalReturn(): BaseMessage {
+  return {
+    type: "user",
+    content: [],
+    responseMetadata: {},
+    usageMetadata: {},
+  };
+}
 
-// const normalMessage = normalReturn();
-// if (normalMessage.type === "user") {
-//   for (const content of normalMessage.content) {
-//     if (content.type === "text") {
-//       // @ts-expect-error - openaiOnly doesn't exist on the standard MessageComplex
-//       console.log(content.openaiOnly);
-//     }
-//   }
-// }
+const normalMessage = normalReturn();
+if (normalMessage.type === "user") {
+  for (const content of normalMessage.content) {
+    if (content.type === "text") {
+      // @ts-expect-error - openaiOnly doesn't exist on the standard MessageComplex
+      console.log(content.openaiOnly);
+    }
+  }
+}
 
-// // Test with multiple tool call interfaces
+// Test with multiple tool call interfaces
 
-// interface ToolCallA extends ContentBlock.Tools.ToolCallContentBlock {
-//   name: "tool_a";
-//   args: {
-//     bar: string;
-//   };
-// }
+interface ToolCallA extends ContentBlock.Tools.ToolCallContentBlock {
+  name: "tool_a";
+  args: {
+    bar: string;
+  };
+}
 
-// interface ToolCallB extends ContentBlock.Tools.ToolCallContentBlock {
-//   name: "tool_b";
-//   args: {
-//     foo: string;
-//   };
-// }
+interface ToolCallB extends ContentBlock.Tools.ToolCallContentBlock {
+  name: "tool_b";
+  args: {
+    foo: string;
+  };
+}
 
-// interface TestMessageComplex extends $MessageComplex {
-//   content: {
-//     user: ToolCallA | ToolCallB;
-//   };
-// }
+interface TestMessageComplex extends $MessageComplex {
+  content: {
+    user: ToolCallA | ToolCallB;
+  };
+}
 
-// const testMessage = {} as Message<TestMessageComplex>;
+const testMessage = {} as BaseMessage<TestMessageComplex>;
 
-// if (testMessage.type === "user") {
-//   for (const content of testMessage.content) {
-//     if (content.type === "tool_call") {
-//       if (content.name === "tool_a") {
-//         content.args.bar;
-//       }
-//     }
-//   }
-// }
+if (testMessage.type === "user") {
+  for (const content of testMessage.content) {
+    if (content.type === "tool_call") {
+      if (content.name === "tool_a") {
+        // Bar is a property provided by ToolCallA
+        console.log(content.args.bar);
+      }
+      if (content.name === "tool_b") {
+        // Foo is a property provided by ToolCallB
+        console.log(content.args.foo);
+      }
+      // @ts-expect-error - tool_c doesn't exist on the provided message complex
+      if (content.name === "tool_c") {
+        console.log(":(");
+      }
+    }
+  }
+}
