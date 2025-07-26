8def7b13a7b2dd2f3b4ba98ac0ad83797f565652
Hunter Lovell <hunter@hntrl.io>
2025-07-26 15:22:45 -0700

    r

--- langchain-core/src/_standard/message.ts
+++ langchain-core/src/_standard/message.ts
@@ -1,6 +1,7 @@
 import { ContentBlock } from "./content/index.js";
 
 export type $MessageType =
+  | "ai"
   | "user"
   | "assistant"
   | "tool"
@@ -9,11 +10,9 @@ export type $MessageType =
 
 export interface $MessageComplex {
   readonly content: {
-    [key: string]: (ContentBlock & { type: string }) | undefined;
+    [key: string]: ContentBlock | undefined;
   };
 }
-
-export type $Merge<T, U> = Omit<T, keyof U> & U;
 
 export type $MergeDiscriminatedUnion<
   A extends Record<Key, PropertyKey>,
@@ -22,9 +21,7 @@ export type $MergeDiscriminatedUnion<
 > = {
   [T in A[Key] | B[Key]]: [Extract<B, Record<Key, T>>] extends [never]
     ? Extract<A, Record<Key, T>>
-    : [Extract<A, Record<Key, T>>] extends [never]
-    ? Extract<B, Record<Key, T>>
-    : $Merge<Extract<A, Record<Key, T>>, Extract<B, Record<Key, T>>>;
+    : Extract<B, Record<Key, T>>;
 }[A[Key] | B[Key]];
 
 export type $MergeMessageComplex<
@@ -32,19 +29,17 @@ export type $MergeMessageComplex<
   B extends $MessageComplex
 > = {
   content: {
-    [K in
-      | keyof A["content"]
-      | keyof B["content"]]: K extends keyof A["content"] & keyof B["content"]
-      ? $MergeDiscriminatedUnion<
-          NonNullable<A["content"][K]>,
-          NonNullable<B["content"][K]>,
-          "type"
-        >
-      : K extends keyof A["content"]
-      ? A["content"][K]
-      : K extends keyof B["content"]
-      ? B["content"][K]
-      : never;
+    [K in keyof (A["content"] & B["content"])]: NonNullable<
+      K extends keyof A["content"] & keyof B["content"]
+        ? $MergeDiscriminatedUnion<
+            NonNullable<A["content"][K]>,
+            NonNullable<B["content"][K]>,
+            "type"
+          >
+        : K extends keyof A["content"]
+        ? A["content"][K]
+        : B["content"][K]
+    >;
   };
 };
 
@@ -55,21 +50,53 @@ export interface $StandardMessageComplex extends $MessageComplex {
     tool: ContentBlock.Standard;
     system: ContentBlock.Standard;
   };
+  responseMetadata: {
+    provider?: string;
+    [key: string]: unknown;
+  };
+  usageMetadata: {
+    [key: string]: unknown;
+  };
 }
 
-export type Message<
-  TComplex extends $MessageComplex = $StandardMessageComplex
+type $NormalizedMessageComplex<TComplex extends $MessageComplex> =
+  TComplex extends $StandardMessageComplex
+    ? // hot path for perf reasons
+      TComplex
+    : $MergeMessageComplex<$StandardMessageComplex, TComplex>;
+
+type $BaseMessageShape<
+  TComplex extends $MessageComplex,
+  TRole extends keyof TComplex["content"] = keyof TComplex["content"]
 > = {
-  [TMessageType in keyof $MergeMessageComplex<
-    $StandardMessageComplex,
-    TComplex
-  >["content"]]: {
+  [TMessageType in keyof TComplex["content"]]: {
     type: TMessageType;
-    content: Array<
-      $MergeMessageComplex<
-        $StandardMessageComplex,
-        TComplex
-      >["content"][TMessageType]
-    >;
+    content: Array<TComplex["content"][TMessageType]>;
+    responseMetadata: TComplex["responseMetadata"];
+    usageMetadata: TComplex["usageMetadata"];
+    // TBD: how do we source this from content blocks without having a getter? (since these are just normal interface types? maybe proxies?)
+    // Also TBD: these need to be using the type from the message complex content (where the content block is defined with type="tool_call")
+    // toolCalls?: ContentBlock.Tools.ToolCallContentBlock[];
   };
-}[keyof $MergeMessageComplex<$StandardMessageComplex, TComplex>["content"]];
+}[TRole];
+
+type BaseMessage<
+  TRole extends keyof $NormalizedMessageComplex<TComplex>["content"],
+  TComplex extends $MessageComplex = $StandardMessageComplex
+> = $BaseMessageShape<$NormalizedMessageComplex<TComplex>, TRole>;
+
+export type AIMessage<
+  TComplex extends $MessageComplex = $StandardMessageComplex
+> = BaseMessage<"ai", TComplex>;
+
+export type HumanMessage<
+  TComplex extends $MessageComplex = $StandardMessageComplex
+> = BaseMessage<"user", TComplex>;
+
+export type SystemMessage<
+  TComplex extends $MessageComplex = $StandardMessageComplex
+> = BaseMessage<"system", TComplex>;
+
+export type ToolMessage<
+  TComplex extends $MessageComplex = $StandardMessageComplex
+> = BaseMessage<"tool", TComplex>;
