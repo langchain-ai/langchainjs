98f595e6429bfd58f1d4c447b750fa996915335a
Christian Bromann <git@bromann.dev>
2025-08-01 15:45:42 -0700

    chore(v1): cleanup indexes and prompts

--- langchain/src/prompts/index.ts
+++
@@ -1,7 +0,0 @@
-/* #__PURE__ */ console.warn(
-  [
-    `[ERROR]: The "langchain/prompts" entrypoint was removed in LangChain version 0.2.0.`,
-    ``,
-    `Instead, please import from "@langchain/core/prompts".`,
-  ].join("\n")
-);
