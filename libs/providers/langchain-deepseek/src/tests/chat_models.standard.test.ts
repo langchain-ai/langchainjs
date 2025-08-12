b4151f913f02e70e8781669e4b3998366bb2e3bb
Christian Bromann <git@bromann.dev>
2025-08-11 22:05:08 -0700

    test(deepseek): migrate to Vitest

--- libs/providers/langchain-deepseek/src/tests/chat_models.standard.test.ts
+++ libs/providers/langchain-deepseek/src/tests/chat_models.standard.test.ts
@@ -1,6 +1,5 @@
 /* eslint-disable no-process-env */
-import { test, expect } from "@jest/globals";
-import { ChatModelUnitTests } from "@langchain/standard-tests";
+import { ChatModelUnitTests } from "@langchain/standard-tests/vitest";
 import { AIMessageChunk } from "@langchain/core/messages";
 import { ChatDeepSeek, ChatDeepSeekCallOptions } from "../chat_models.js";
 
@@ -32,8 +31,4 @@ class ChatDeepSeekStandardUnitTests extends ChatModelUnitTests<
 }
 
 const testClass = new ChatDeepSeekStandardUnitTests();
-
-test("ChatDeepSeekStandardUnitTests", () => {
-  const testResults = testClass.runTests();
-  expect(testResults).toBe(true);
-});
+testClass.runTests("ChatDeepSeekStandardUnitTests");
