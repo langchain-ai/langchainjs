96a76338b4506d871b8852f7f6d2bee04e26e97a
jacoblee93 <jacoblee93@gmail.com>
2025-08-14 20:44:22 -0700

    Fix interop case where callbacks are manually passed to a runnable, then a traceable is called, then a runnable with no manually passed callbacks

--- langchain-core/src/tracers/tests/langsmith_interop.int.test.ts
+++ langchain-core/src/tracers/tests/langsmith_interop.int.test.ts
@@ -4,6 +4,7 @@ import { traceable } from "langsmith/traceable";
 import { RunnableLambda } from "../../runnables/base.js";
 import { BaseMessage, HumanMessage } from "../../messages/index.js";
 import { awaitAllCallbacks } from "../../singletons/callbacks.js";
+import { LangChainTracer } from "../tracer_langchain.js";
 
 test("traceables double nested within runnables with batching", async () => {
   const aiGreet = traceable(
@@ -24,6 +25,31 @@ test("traceables double nested within runnables with batching", async () => {
   });
 
   await root.invoke([new HumanMessage({ content: "Hello!" })]);
+
+  await awaitAllCallbacks();
+});
+
+test("runnable nested within a traceable with manual tracer passed", async () => {
+  const aiGreet = traceable(
+    async (msg: BaseMessage) => {
+      const child = RunnableLambda.from(async () => {
+        return [new HumanMessage({ content: "From child!" })];
+      }).withConfig({ runName: "child" });
+      return child.invoke([msg]);
+    },
+    { name: "aiGreet", tracingEnabled: true }
+  );
+
+  const parent = RunnableLambda.from(async () => {
+    return aiGreet(new HumanMessage({ content: "Hello!" }));
+  }).withConfig({ runName: "parent" });
+
+  await parent.invoke(
+    {},
+    {
+      callbacks: [new LangChainTracer()],
+    }
+  );
 
   await awaitAllCallbacks();
 });
