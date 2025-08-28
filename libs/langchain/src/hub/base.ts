aa9f76965ec44722ff27f526db09ebc6dee66d6a
Christian Bromann <git@bromann.dev>
2025-08-28 09:17:24 -0700

    have explicit type return

--- libs/langchain/src/hub/base.ts
+++ libs/langchain/src/hub/base.ts
@@ -1,6 +1,7 @@
 import type { BaseLanguageModel } from "@langchain/core/language_models/base";
 import type { Runnable } from "@langchain/core/runnables";
 import type { Client, ClientConfig } from "langsmith";
+import type { PromptCommit } from "langsmith/schemas";
 
 /**
  * Push a prompt to the hub.
@@ -43,7 +44,7 @@ export async function basePush(
 export async function basePull(
   ownerRepoCommit: string,
   options?: { apiKey?: string; apiUrl?: string; includeModel?: boolean }
-) {
+): Promise<PromptCommit> {
   const Client = await loadLangSmith();
   const client = new Client(options);
 
