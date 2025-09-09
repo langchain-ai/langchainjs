e9d113668ab0d81150f243846aa7d99fc1896847
Christian Bromann <git@bromann.dev>
2025-09-09 11:43:19 -0700

    fix(@langchain/community) create index aurora dsql (#8892)
    Co-authored-by: Jorge Lanzarotti <abarjorg@amazon.com>

--- libs/langchain-community/src/stores/tests/aurora_dsql.int.test.ts
+++ libs/langchain-community/src/stores/tests/aurora_dsql.int.test.ts
@@ -15,7 +15,7 @@ async function getAdminAuthToken() {
     credentials: {
       accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      // sessionToken: process.env.AWS_SESSION_TOKEN!
+      // sessionToken: process.env.AWS_SESSION_TOKEN!,
     },
   });
   return await signer.getDbConnectAdminAuthToken();
