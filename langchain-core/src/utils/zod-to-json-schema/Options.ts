9e47212e4a33ce588c91170a881433e2a440c251
Hunter Lovell <hunter@hntrl.io>
2025-09-15 11:04:53 -0700

    cr

--- langchain-core/src/utils/zod-to-json-schema/Options.ts
+++ langchain-core/src/utils/zod-to-json-schema/Options.ts
@@ -1,6 +1,6 @@
 import { ZodSchema, ZodTypeDef } from "zod/v3";
-import { Refs, Seen } from "./Refs";
-import { JsonSchema7Type } from "./parseTypes";
+import { Refs, Seen } from "./Refs.js";
+import { JsonSchema7Type } from "./parseTypes.js";
 
 export type Targets =
   | "jsonSchema7"
