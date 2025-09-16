7cd9282959b3b9e534019f467270f607577e92b8
Hunter Lovell <hunter@hntrl.io>
2025-09-16 13:52:18 -0700

    chore(core): breakout mock utils

---
+++ libs/langchain-core/src/utils/testing/message_history.ts
@@ -0,0 +1,69 @@
+import {
+  BaseChatMessageHistory,
+  BaseListChatMessageHistory,
+} from "../../chat_history.js";
+import { BaseMessage, AIMessage, HumanMessage } from "../../messages/index.js";
+import { BaseTracer, Run } from "../../tracers/base.js";
+
+export class FakeChatMessageHistory extends BaseChatMessageHistory {
+  lc_namespace = ["langchain_core", "message", "fake"];
+
+  messages: Array<BaseMessage> = [];
+
+  constructor() {
+    super();
+  }
+
+  async getMessages(): Promise<BaseMessage[]> {
+    return this.messages;
+  }
+
+  async addMessage(message: BaseMessage): Promise<void> {
+    this.messages.push(message);
+  }
+
+  async addUserMessage(message: string): Promise<void> {
+    this.messages.push(new HumanMessage(message));
+  }
+
+  async addAIChatMessage(message: string): Promise<void> {
+    this.messages.push(new AIMessage(message));
+  }
+
+  async clear(): Promise<void> {
+    this.messages = [];
+  }
+}
+
+export class FakeListChatMessageHistory extends BaseListChatMessageHistory {
+  lc_namespace = ["langchain_core", "message", "fake"];
+
+  messages: Array<BaseMessage> = [];
+
+  constructor() {
+    super();
+  }
+
+  async addMessage(message: BaseMessage): Promise<void> {
+    this.messages.push(message);
+  }
+
+  async getMessages(): Promise<BaseMessage[]> {
+    return this.messages;
+  }
+}
+
+export class FakeTracer extends BaseTracer {
+  name = "fake_tracer";
+
+  runs: Run[] = [];
+
+  constructor() {
+    super();
+  }
+
+  protected persistRun(run: Run): Promise<void> {
+    this.runs.push(run);
+    return Promise.resolve();
+  }
+}
