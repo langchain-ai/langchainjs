b2dc8a6a93a37661420efafadfd66f099a57efe3
Christian Bromann <git@bromann.dev>
2025-10-14 10:57:45 +0200

    cr

--- libs/providers/langchain-google-common/src/tests/llms.test.ts
+++ libs/providers/langchain-google-common/src/tests/llms.test.ts
@@ -72,7 +72,7 @@ describe("Mock Google LLM", () => {
     const model = new GoogleLLM({
       authOptions,
     });
-    await model.call("Hello world");
+    await model.invoke("Hello world");
 
     expect(record?.opts?.headers).toHaveProperty("User-Agent");
     expect(record.opts.headers["User-Agent"]).toMatch(
@@ -307,7 +307,7 @@ describe("Mock Google LLM", () => {
     });
     let caught = false;
     try {
-      await model.call("Hello world");
+      await model.invoke("Hello world");
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (xx: any) {
       caught = true;
