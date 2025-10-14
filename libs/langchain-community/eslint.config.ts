43f34506be19bca1b320c69691db56d18e5815a6
Hunter Lovell <hunter@hntrl.io>
2025-10-14 13:17:05 -0700

    cr

--- libs/langchain-community/eslint.config.ts
+++ libs/langchain-community/eslint.config.ts
@@ -1,9 +1,3 @@
-// @ts-nocheck
 import { langchainConfig } from "@langchain/eslint";
-import { defineConfig } from "eslint/config";
 
-export default defineConfig(...langchainConfig, {
-  rules: {
-    "import/no-extraneous-dependencies": "off",
-  },
-});
+export default langchainConfig;
