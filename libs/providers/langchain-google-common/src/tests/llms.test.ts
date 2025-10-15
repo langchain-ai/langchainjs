8013da590d265fca3871a86ae711f33948924b4f
Hunter Lovell <40191806+hntrl@users.noreply.github.com>
2025-10-14 17:37:22 -0700

    chore: update project files (#9195)

--- libs/providers/langchain-google-common/src/tests/llms.test.ts
+++ libs/providers/langchain-google-common/src/tests/llms.test.ts
@@ -72,11 +72,11 @@ describe("Mock Google LLM", () => {
     const model = new GoogleLLM({
       authOptions,
     });
-    await model.call("Hello world");
+    await model.invoke("Hello world");
 
     expect(record?.opts?.headers).toHaveProperty("User-Agent");
     expect(record.opts.headers["User-Agent"]).toMatch(
-      /langchain-js\/[0-9.]+-GoogleLLMConnection/
+      /langchain-js\/[0-9.]+-ChatConnection/
     );
   });
 
@@ -307,7 +307,7 @@ describe("Mock Google LLM", () => {
     });
     let caught = false;
     try {
-      await model.call("Hello world");
+      await model.generate(["Hello world"]);
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (xx: any) {
       caught = true;
