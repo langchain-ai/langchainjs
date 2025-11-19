b598f7772c365780483ad16d10bed03cad390892
Hunter Lovell <hunter@hntrl.io>
2025-11-19 14:10:58 -0800

    cr

--- internal/build/src/index.ts
+++ internal/build/src/index.ts
@@ -1,5 +1,5 @@
-import { Options as BuildOptions } from "tsdown";
-import { PackageJson } from "type-fest";
+import { type Options as BuildOptions } from "tsdown";
+import { type PackageJson } from "type-fest";
 import path from "node:path";
 
 export {
