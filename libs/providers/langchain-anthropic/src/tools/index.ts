737a37cc7673072e9a1885ef9c5c1456519df7a5
Christian Bromann <git@bromann.dev>
2025-12-02 15:08:23 -0800

    feat(anthropic): support tool search tool

--- libs/providers/langchain-anthropic/src/tools/index.ts
+++ libs/providers/langchain-anthropic/src/tools/index.ts
@@ -1,11 +1,17 @@
 import { memory_20250818 } from "./memory.js";
 import { webSearch_20250305 } from "./webSearch.js";
 import { webFetch_20250910 } from "./webFetch.js";
+import {
+  toolSearchRegex_20251119,
+  toolSearchBM25_20251119,
+} from "./toolSearch.js";
 
 export const tools = {
   memory_20250818,
   webSearch_20250305,
   webFetch_20250910,
+  toolSearchRegex_20251119,
+  toolSearchBM25_20251119,
 };
 
 export type * from "./types.js";
