71c783af909d0cf9d38350e3839e6075055b1357
Christian Bromann <git@bromann.dev>
2025-12-02 14:25:40 -0800

    feat(anthropic): introduce typed webSearch tool

--- libs/providers/langchain-anthropic/src/tools/index.ts
+++ libs/providers/langchain-anthropic/src/tools/index.ts
@@ -1,7 +1,9 @@
 import { memory_20250818 } from "./memory.js";
+import { webSearch_20250305 } from "./webSearch.js";
 
 export const tools = {
   memory_20250818,
+  webSearch_20250305,
 };
 
 export type * from "./types.js";
