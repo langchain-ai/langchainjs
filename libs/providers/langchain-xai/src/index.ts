ce5d98cb9d501aaea09c7054b3130b4a49ba5fb0
Paweł Twardziak <pawel.twardziak.dev@gmail.com>
2025-12-03 16:05:26 +0100

    Remove redundant exports

--- libs/providers/langchain-xai/src/index.ts
+++ libs/providers/langchain-xai/src/index.ts
@@ -1,10 +1,1 @@
 export * from "./chat_models.js";
-export type {
-  XAILiveSearchTool,
-  XAIBuiltInTool,
-  XAISearchParameters,
-  ChatXAICompletionsInvocationParams,
-  XAIAdditionalKwargs,
-  XAIResponseMetadata,
-} from "./chat_models.js";
-export { isXAIBuiltInTool } from "./chat_models.js";
