8bff31ee07e3c7f112a74442e63e07a5fb887b43
Hunter Lovell <hunter@hntrl.io>
2025-12-05 09:01:59 -0800

    chore: bin p-retry

---
+++ .changeset/cool-cobras-bathe.md
@@ -0,0 +1,5 @@
+---
+"@langchain/core": patch
+---
+
+bin p-retry
--- libs/langchain-core/src/utils/async_caller.ts
+++ libs/langchain-core/src/utils/async_caller.ts
@@ -1,19 +1,7 @@
 import PQueueMod from "p-queue";
 
 import { getAbortSignalError } from "./signal.js";
-
-// p-retry is ESM-only, so we use dynamic import for CJS compatibility.
-// The module is cached after first import, so subsequent calls are essentially free.
-// This approach is recommended by the p-retry author for async contexts:
-// https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c
-let pRetryModule: typeof import("p-retry") | null = null;
-
-async function getPRetry() {
-  if (!pRetryModule) {
-    pRetryModule = await import("p-retry");
-  }
-  return pRetryModule.default;
-}
+import pRetry from "./p-retry/index.js";
 
 const STATUS_NO_RETRY = [
   400, // Bad Request
@@ -119,7 +107,6 @@ export class AsyncCaller {
     callable: T,
     ...args: Parameters<T>
   ): Promise<Awaited<ReturnType<T>>> {
-    const pRetry = await getPRetry();
     return this.queue.add(
       () =>
         pRetry(
