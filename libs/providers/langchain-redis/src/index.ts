825b9f47f954f5a72f472b7780f73118b8a8e8a7
Tihomir Mateev <tihomir.mateev@gmail.com>
2026-01-08 13:59:12 +0200

    Provide the new advanced filtering as an alternative (1 of 3) - instead of trying to put the legacy simple string mode, the legacy custom filter mode and the new advanced filtering mode we provide a new solution - the new solution would NOT be compatible with the old implementation - step 2 - fix integration tests and provide migration guide - step 3 - finalize solution and polish

--- libs/providers/langchain-redis/src/index.ts
+++ libs/providers/langchain-redis/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./chat_histories.js";
 export * from "./vectorstores.js";
+export * from "./vectorstores_advanced.js";
 export * from "./caches.js";
 export * from "./filters.js";
 export * from "./schema.js";
