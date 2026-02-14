39a1df0c7837bf55855ed431e4219ca81becf9f0
Christian Bromann <git@bromann.dev>
2026-02-13 20:14:10 -0800

    fix

--- libs/providers/langchain-aws/tsdown.config.ts
+++ libs/providers/langchain-aws/tsdown.config.ts
@@ -6,6 +6,7 @@ export default getBuildConfig({
   // of @aws-sdk/client-bedrock-runtime. They are listed as devDependencies
   // since they're only needed for type checking, not at runtime.
   external: ["@aws-sdk/types", "@smithy/types"],
+  inlineOnly: false,
   plugins: [
     cjsCompatPlugin({
       files: ["dist/", "CHANGELOG.md", "README.md", "LICENSE"],
