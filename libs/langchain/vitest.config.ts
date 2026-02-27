6032cd9579c8af2adc0d129fdb0f12856e600daa
Maahir Sachdev <maahir.sachdev@langchain.dev>
2026-02-27 14:57:34 -0800

    fix

--- libs/langchain/vitest.config.ts
+++ libs/langchain/vitest.config.ts
@@ -20,7 +20,7 @@ export default defineConfig((env) => {
         "**/*.bench.test.ts",
         ...configDefaults.exclude,
       ],
-      setupFiles: ["dotenv/config", "@langchain/standard-tests/vitest-setup"],
+      setupFiles: ["dotenv/config", "../../internal/standard-tests/src/vitest-setup.ts"],
     },
   };
 
