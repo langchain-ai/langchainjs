0a7a7298b719f579854e41adddb04f17dc82640b
Christian Bromann <git@bromann.dev>
2026-04-02 08:27:51 -0700

    fix(examples): update dependencies (#10651)

--- examples/src/langchain-classic/embeddings/cohere.ts
+++
@@ -1,9 +0,0 @@
-import { CohereEmbeddings } from "@langchain/cohere";
-
-export const run = async () => {
-  const model = new CohereEmbeddings({ model: "embed-english-v3.0" });
-  const res = await model.embedQuery(
-    "What would be a good company name a company that makes colorful socks?"
-  );
-  console.log({ res });
-};
