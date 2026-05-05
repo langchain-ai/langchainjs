03658f2a49b736a6a9dabb2ccbb8e72c181443fe
Colin Francis <colin.francis@langchain.dev>
2026-05-05 15:18:27 -0700

    clean up naming

--- libs/langchain-core/src/utils/env.ts
+++ libs/langchain-core/src/utils/env.ts
@@ -1,6 +1,6 @@
 // Inlined from https://github.com/flexdinesh/browser-or-node
 
-type DenoGlobal = {
+type Deno = {
   version?: { deno: string };
   env?: { get: (name: string) => string | undefined };
 };
@@ -20,7 +20,7 @@ export const isJsDom = () =>
 // Supabase Edge Function provides a `Deno` global object
 // without `version` property
 export const isDeno = () =>
-  typeof (globalThis as typeof globalThis & { Deno?: DenoGlobal }).Deno !==
+  typeof (globalThis as typeof globalThis & { Deno?: Deno }).Deno !==
   "undefined";
 
 // Mark not-as-node if in Supabase Edge Function
@@ -78,9 +78,9 @@ export function getEnvironmentVariable(name: string): string | undefined {
       // oxlint-disable-next-line no-process-env
       return process.env?.[name];
     } else if (isDeno()) {
-      return (
-        globalThis as typeof globalThis & { Deno?: DenoGlobal }
-      ).Deno?.env?.get(name);
+      return (globalThis as typeof globalThis & { Deno?: Deno }).Deno?.env?.get(
+        name
+      );
     } else {
       return undefined;
     }
