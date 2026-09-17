5965627cb6a091a44cbaa4f13a012ecb850bc163
Ike Hentrel <ihentrel@hotmail.com>
2026-09-17 11:17:56 -0400

    feat(community): add x402-scraper-langchain tool for Base mainnet HTTP 402 micro-settlements

---
+++ libs/langchain-community/src/tools/x402/index.ts
@@ -0,0 +1,6 @@
+import { x402ScraperTool } from "@ihentrel/x402-scraper-langchain";
+
+/**
+ * Export standard x402 micro-payment scraper for community integrations.
+ */
+export { x402ScraperTool };
