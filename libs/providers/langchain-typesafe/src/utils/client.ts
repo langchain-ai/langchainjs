d02698c826afc29a95ebcd574f8371603c42b0bb
Hunter Lovell <hunter@hntrl.io>
2026-09-19 09:53:36 -0700

    cr

--- libs/providers/langchain-typesafe/src/utils/client.ts
+++ libs/providers/langchain-typesafe/src/utils/client.ts
@@ -10,7 +10,7 @@ import {
 
 export const SYSTEMONE_PATH = "/v1/systemone";
 
-const CLIENT_ID = `langchainjs-typesafe/dev`;
+const CLIENT_ID = `langchainjs-typesafe/${__PKG_VERSION__}`;
 
 /**
  * Builds the request headers.
